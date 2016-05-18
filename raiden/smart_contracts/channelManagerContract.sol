17f9809daefdb4eda549f213d9430c0f890e3129
Konrad Feldmeier <konrad.feldmeier@brainbot.com>
2016-05-18 18:38:38 +0200

    Move channelManager contract and library code
    The code from the pytest is now in two files:
      - `channelManagerContract.slb` # the library
      - `channelManagerContract.sol` # the main code

--- raiden/smart_contracts/channelManagerContract.sol
+++ raiden/smart_contracts/channelManagerContract.sol
@@ -1,3 +1,93 @@
+library IterableMappingNcc {
+    // Might have to define the NettingContract type here for insertion
+    struct itmap {
+        mapping(bytes32 => IndexValue) data;
+        KeyFlag[] keys;
+        uint size;
+    }
+    struct IndexValue { uint keyIndex; NettingContract value; }
+    struct KeyFlag { bytes32 key; bool deleted; }
+
+
+    function insert(itmap storage self, bytes32 key, NettingContract value) returns (bool replaced) {
+        uint keyIndex = self.data[key].keyIndex;
+        self.data[key].value = value;
+        if (keyIndex > 0)
+            return true;
+        else {
+            keyIndex = self.keys.length++;
+            self.data[key].keyIndex = keyIndex + 1;
+            self.keys[keyIndex].key = key;
+            self.size++;
+            return false;
+        }
+    }
+
+
+    function remove(itmap storage self, bytes32 key) returns (bool success){
+        uint keyIndex = self.data[key].keyIndex;
+        if (keyIndex == 0)
+          return false;
+        delete self.data[key];
+        self.keys[keyIndex - 1].deleted = true;
+        self.size --;
+    }
+
+
+    function contains(itmap storage self, bytes32 key) returns (bool) {
+        return self.data[key].keyIndex > 0;
+    }
+
+
+    function atIndex(itmap storage self, bytes32 key) returns (uint index) {
+        return self.data[key].keyIndex;
+    }
+
+
+    function iterate_start(itmap storage self) returns (uint keyIndex){
+        return iterate_next(self, uint(-1));
+    }
+
+
+    function iterate_valid(itmap storage self, uint keyIndex) returns (bool){
+        return keyIndex < self.keys.length;
+    }
+
+
+    function iterate_next(itmap storage self, uint keyIndex) returns (uint r_keyIndex){
+        keyIndex++;
+        while (keyIndex < self.keys.length && self.keys[keyIndex].deleted)
+            keyIndex++;
+        return keyIndex;
+    }
+
+
+    function iterate_get(itmap storage self, uint keyIndex) returns (bytes32 key, NettingContract value){
+        key = self.keys[keyIndex].key;
+        value = self.data[key].value;
+    }
+}
+
+contract NettingContract {
+    address public assetAddress;
+    
+    struct Participant
+    {
+        address addr;
+        uint deposit;
+    }
+
+    Participant[2] public participants;
+
+    function NettingContract(address assetAdr, address participant1, address participant2) {
+        assetAddress = assetAdr;
+        participants[0].addr = participant1;
+        participants[1].addr = participant2;
+        participants[0].deposit = 10;
+        participants[1].deposit = 10;
+    }
+}
+
 contract ChannelManagerContract {
 
     IterableMappingNcc.itmap data;
@@ -14,7 +104,7 @@ contract ChannelManagerContract {
     /// @dev Initiate the contract with the constructor
     /// @param assetAdr (address) the address of an asset
     function ChannelManagerContract(address assetAdr) {
-        assetAddress = assetAdr;
+        assetAddress = 0x0bd4060688a1800ae986e4840aebc924bb40b5bf;
     }
 
 
@@ -41,11 +131,6 @@ contract ChannelManagerContract {
         }
     }
 
-    /// @notice numberOfItems(address) to get the number of items channels that a given address 
-    /// partipates in.
-    /// @dev helper function to provide the needed length of the array for nettingContractsByAddress
-    /// @param adr (address) the address to look for
-    /// @return items (uint) the number of items the address participates in
     function numberOfItems(address adr) returns (uint items) {
         items = 0;
         for (var i = IterableMappingNcc.iterate_start(data); IterableMappingNcc.iterate_valid(data, i); i = IterableMappingNcc.iterate_next(data, i)) {
@@ -101,13 +186,13 @@ contract ChannelManagerContract {
     /// @dev Create a new channel between two parties
     /// @param partner (address) address of one partner
     /// @return channel (NettingContract) the NettingContract of the two parties.
-    function newChannel(address partner) returns (NettingContract c){
+    function newChannel(address partner) returns (NettingContract c, address sender){
         bytes32 k = key(msg.sender, partner);
         c = new NettingContract(assetAddress, msg.sender, partner);
         add(k, c);
+        sender = msg.sender; // Only for testing purpose, should not be added to live net
         ChannelNew(partner); //Triggers event
     }
-
 
     // empty function to handle wrong calls
     function () { throw; }
