92d4c291c6a1557fe0b32bf34a4b1f50e20a92fc
Konrad Feldmeier <konrad.feldmeier@brainbot.com>
2016-05-18 18:43:48 +0200

    Move registry+library code from test to .sol/.slb

--- raiden/smart_contracts/registry.sol
+++ raiden/smart_contracts/registry.sol
@@ -1,8 +1,81 @@
-// For iterable mapping
-import "cmcItSet.sol";
+library IterableMappingCMC {
+    // Might have to define the NettingContract type here for insertion
+    struct itmap {
+        mapping(address => IndexValue) data;
+        KeyFlag[] keys;
+        uint size;
+    }
+    struct IndexValue { uint keyIndex; ChannelManagerContract value; }
+    struct KeyFlag { address key; bool deleted; }
+
+
+    function insert(itmap storage self, address key, ChannelManagerContract value) returns (bool replaced) {
+        uint keyIndex = self.data[key].keyIndex;
+        self.data[key].value = value;
+        if (keyIndex > 0)
+            return true;
+        else {
+            keyIndex = self.keys.length++;
+            self.data[key].keyIndex = keyIndex + 1;
+            self.keys[keyIndex].key = key;
+            self.size++;
+            return false;
+        }
+    }
+
+
+    function remove(itmap storage self, address key) returns (bool success){
+        uint keyIndex = self.data[key].keyIndex;
+        if (keyIndex == 0) return false;
+        delete self.data[key];
+        self.keys[keyIndex - 1].deleted = true;
+        self.size --;
+    }
+
+
+    function contains(itmap storage self, address key) returns (bool) {
+        return self.data[key].keyIndex > 0;
+    }
+
+
+    function atIndex(itmap storage self, address key) returns (uint index) {
+        return self.data[key].keyIndex;
+    }
+
+
+    function iterate_start(itmap storage self) returns (uint keyIndex){
+        return iterate_next(self, uint(-1));
+    }
+
+
+    function iterate_valid(itmap storage self, uint keyIndex) returns (bool){
+        return keyIndex < self.keys.length;
+    }
+
+
+    function iterate_next(itmap storage self, uint keyIndex) returns (uint r_keyIndex){
+        keyIndex++;
+        while (keyIndex < self.keys.length && self.keys[keyIndex].deleted)
+            keyIndex++;
+        return keyIndex;
+    }
+
+
+    function iterate_get(itmap storage self, uint keyIndex) returns (address key, ChannelManagerContract value){
+        key = self.keys[keyIndex].key;
+        value = self.data[key].value;
+    }
+}
+contract ChannelManagerContract {
+    address public assetAddress;
+    
+    function ChannelManagerContract(address testAddress) {
+        assetAddress = testAddress;
+    }
+}
 
 contract Registry {
-    IterableMappingCMC.itmap data; // Might be the data structure to use
+    IterableMappingCMC.itmap data;
 
 
     /// @notice addAsset(address) to add a new ChannelManagerContract to channelManagerContracts
@@ -43,23 +116,6 @@ contract Registry {
             assetAddresses[i] = key;
         }
     }
-
-
-    // ONLY FOR TESTING PURPOSES
-    /*
-    address constant TEST_ADDRESS1 = 0x123345;
-    address constant ASSET_ADDRESS = 0xC0FFEE;
-    address constant TEST_ADDRESS2 = 0xDEADBEEF;
-    address constant TEST_ADDRESS3 = 0xABCDEF;
-    function testGetAllAddresses() returns (bool success, address[] a) {
-        ChannelManagerContract cmc = new ChannelManagerContract(ASSET_ADDRESS);
-        addAsset(TEST_ADDRESS1);
-        addAsset(TEST_ADDRESS2);
-        addAsset(TEST_ADDRESS3);
-        success = data.size == 3;
-        a = assetAddresses();
-    }
-    */
 
     // empty function to handle wrong calls
     function () { throw; }
