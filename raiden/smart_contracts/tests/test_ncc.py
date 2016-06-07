d63900ac115b953868ca2ef25533d56c678490cb
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2016-06-08 01:13:59 +0200

    got deposit() working with StandardToken and we can now open channels

--- raiden/smart_contracts/tests/test_ncc.py
+++ raiden/smart_contracts/tests/test_ncc.py
@@ -6,15 +6,15 @@ from ethereum import tester
 from ethereum.utils import sha3, privtoaddr
 from ethereum.tester import TransactionFailed
 
-from raiden.network.rpc.client import get_contract_path
+# from raiden.network.rpc.client import get_contract_path
 
-from ethereum.slogging import configure
+token_library_path = "raiden/smart_contracts/StandardToken.sol"
+token_path = "raiden/smart_contracts/HumanStandardToken.sol"
 
-configure("eth.vm:trace, :debug", log_json=True)
-
-library_path = get_contract_path('Decoder.sol')
-ncc_path = get_contract_path('NettingChannelContract.sol.old')
-
+# library_path = get_contract_path('Decoder.sol')
+library_path = "raiden/smart_contracts/Decoder.sol"
+# ncc_path = get_contract_path('NettingChannelContract.sol.old')
+ncc_path = "raiden/smart_contracts/NettingChannelContract.sol.old"
 
 
 def test_ncc():
@@ -22,21 +22,33 @@ def test_ncc():
     assert s.block.number < 1150000
     s.block.number = 1158001
     assert s.block.number > 1150000
+    # Token creation
+    lib_token = s.abi_contract(None, path=token_library_path, language="solidity")
+    token = s.abi_contract(None, path=token_path, language="solidity", libraries={'StandardToken': lib_token.address.encode('hex')}, constructor_parameters=[10000, "raiden", 0, "rd"])
+
+    s.mine()
+
     lib_c = s.abi_contract(None, path=library_path, language="solidity")
     s.mine()
-    c = s.abi_contract(None, path=ncc_path, language="solidity", libraries={'Decoder': lib_c.address.encode('hex')}, constructor_parameters=[sha3('assetAddress')[:20], sha3('address1')[:20], sha3('address2')[:20], 30])
+    c = s.abi_contract(None, path=ncc_path, language="solidity", libraries={'Decoder': lib_c.address.encode('hex')}, constructor_parameters=[token.address, tester.a0, tester.a1, 30])
+
+    # test tokens and distribute tokens
+    assert token.balanceOf(tester.a0) == 10000
+    assert token.balanceOf(tester.a1) == 0
+    assert token.transfer(tester.a1, 5000) == True
+    assert token.balanceOf(tester.a0) == 5000
+    assert token.balanceOf(tester.a1) == 5000
 
     # test global variables
     assert c.lockedTime() == 30
-    assert c.assetAddress() == sha3('assetAddress')[:20].encode('hex')
+    assert c.assetAddress() == token.address.encode('hex')
     assert c.opened() == 0
     assert c.closed() == 0
     assert c.settled() == 0
 
     # test participants variables changed when constructing
-    assert c.participants(0)[0] == sha3('address1')[:20].encode('hex')
-    assert c.participants(1)[0] == sha3('address2')[:20].encode('hex')
-
+    assert c.participants(0)[0] == tester.a0.encode('hex')
+    assert c.participants(1)[0] == tester.a1.encode('hex')
 
     # test atIndex()
     # private must be removed from the function in order to work
@@ -44,8 +56,16 @@ def test_ncc():
     # assert c.atIndex(sha3('address2')[:20]) == 1
 
     # test deposit(uint)
-    # c.deposit(30)
-    # TODO find a way to add HumanToken to this test
+    assert token.balanceOf(c.address) == 0
+    assert token.approve(c.address, 30) == True # allow the contract do deposit
+    assert c.participants(0)[1] == 0
+    with pytest.raises(TransactionFailed):
+        c.deposit(5001)
+    c.deposit(30)
+    assert c.participants(0)[1] == 30
+    assert token.balanceOf(c.address) == 30
+    assert token.balanceOf(tester.a0) == 4970
+    assert c.opened() == s.block.number
 
     # test open()
     # private must be removed from the function in order to work
@@ -61,7 +81,7 @@ def test_ncc():
 
     # test addrAndDep()
     a1, d1, a2, d2 = c.addrAndDep()
-    assert a1 == sha3('address1')[:20].encode('hex')
-    assert a2 == sha3('address2')[:20].encode('hex')
+    assert a1 == tester.a0.encode('hex')
+    assert a2 == tester.a1.encode('hex')
     # assert d1 == 30  # failing until we can use deposit in the tests
     # assert d2 == 20  # failing until we can use deposit in the tests
