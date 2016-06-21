193644063b93299d38341de0ba54e06a6494b590
Augusto F. Hack <hack.augusto@gmail.com>
2016-06-21 12:32:34 -0300

    test_token using get_contract_path

--- raiden/tests/smart_contracts/test_token.py
+++ raiden/tests/smart_contracts/test_token.py
@@ -1,25 +1,39 @@
 # -*- coding: utf8 -*-
-import pytest
-
-from ethereum import utils
 from ethereum import tester
-from ethereum.utils import sha3, privtoaddr
-from ethereum.tester import TransactionFailed
 
-token_library_path = "raiden/smart_contracts/StandardToken.sol"
-token_path = "raiden/smart_contracts/HumanStandardToken.sol"
+from raiden.network.rpc.client import get_contract_path
 
 
 def test_ncc():
-    s = tester.state()
-    assert s.block.number < 1150000
-    s.block.number = 1158001
-    assert s.block.number > 1150000
-    lib_c = s.abi_contract(None, path=token_library_path, language="solidity")
-    c = s.abi_contract(None, path=token_path, language="solidity", libraries={'StandardToken': lib_c.address.encode('hex')}, constructor_parameters=[10000, "raiden", 0, "rd"])
+    token_library_path = get_contract_path('StandardToken.sol')
+    token_path = get_contract_path('HumanStandardToken.sol')
 
-    assert c.balanceOf(tester.a0) == 10000
-    assert c.balanceOf(tester.a1) == 0
-    assert c.transfer(tester.a1, 5000) == True
-    assert c.balanceOf(tester.a0) == 5000
-    assert c.balanceOf(tester.a1) == 5000
+    state = tester.state()
+
+    assert state.block.number < 1150000
+    state.block.number = 1158001
+    assert state.block.number > 1150000
+
+    token = state.abi_contract(
+        None,
+        path=token_library_path,
+        language='solidity',
+    )
+
+    contract_libraries = {
+        'StandardToken': token.address.encode('hex'),
+    }
+    contract = state.abi_contract(
+        None,
+        path=token_path,
+        language='solidity',
+        libraries=contract_libraries,
+        constructor_parameters=[10000, 'raiden', 0, 'rd'],
+    )
+
+    # pylint: disable=no-member
+    assert contract.balanceOf(tester.a0) == 10000
+    assert contract.balanceOf(tester.a1) == 0
+    assert contract.transfer(tester.a1, 5000) is True
+    assert contract.balanceOf(tester.a0) == 5000
+    assert contract.balanceOf(tester.a1) == 5000
