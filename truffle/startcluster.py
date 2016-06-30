82d3a5aaa8146ba5f66ae6efeff9b890c53ab776
Konrad Feldmeier <konrad@brainbot.com>
2016-06-30 13:43:28 +0200

    Fix outstanding issues with startcluster
    This fixes
    - the bootstrap enodes
    - the shutdown handler
    - the coinbase for the miner

--- truffle/startcluster.py
+++ truffle/startcluster.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python
 import os
-import sys
+import shlex
 import json
+import time
 import tempfile
 import signal
-from subprocess import Popen
-from ethereum.utils import denoms, sha3, privtopub, privtoaddr, encode_hex
+from subprocess import Popen, PIPE
+from ethereum.utils import denoms, sha3, privtoaddr, encode_hex
+from devp2p.crypto import privtopub as privtopub_enode
 
 # DEFAULTS
-num_nodes = 3
-cluster_name = 'raiden'
-raiden_port = sum(ord(c) for c in cluster_name)
+NUM_GETH_NODES = 3
+NUM_RAIDEN_ACCOUNTS = 10
+CLUSTER_NAME = 'raiden'
+RAIDEN_PORT = sum(ord(c) for c in CLUSTER_NAME)
 
-defaultaccounts = [
-    encode_hex(privtopub(sha3('localhost:{}'.format(raiden_port + i))))
-    for i in range(3)
+# a list of `num_raiden_accounts` account addresses with a predictable privkey:
+# privkey = sha3('localhost:`raiden_port + i`')
+DEFAULTACCOUNTS = [
+    encode_hex(privtoaddr(sha3('localhost:{}'.format(RAIDEN_PORT + i))))
+    for i in range(NUM_RAIDEN_ACCOUNTS)
 ]
 
-node_config = [
+# default args to pass to `geth` for all calls, e.g. verbosity, ...
+DEFAULT_ARGS = [
+    '--nodiscover',
+    '--rpc',
+    '--networkid {}'.format(sum(ord(c) for c in CLUSTER_NAME)),
+]
+
+# the node specific arguments to pass to `geth` that will be extracted from a
+# 'node configuration'
+NODE_CONFIG = [
     'nodekeyhex',
     'port',
     'rpcport',
@@ -25,41 +39,27 @@ node_config = [
     'minerthreads',
 ]
 
-flags = [
-    '--nodiscover',
-    '--rpc',
-    '--networkid {}'.format(sum(ord(c) for c in cluster_name)),
-]
-
-genesis_stub = {
+GENESIS_STUB = {
     'nonce': '0x0000000000000042',
     'mixhash': '0x0000000000000000000000000000000000000000000000000000000000000000',
     'difficulty': '0x4000',
     'coinbase': '0x0000000000000000000000000000000000000000',
     'timestamp': '0x00',
     'parentHash': '0x0000000000000000000000000000000000000000000000000000000000000000',
-    'extraData': cluster_name,
+    'extraData': CLUSTER_NAME,
     'gasLimit': '0xffffffff'
 }
 
 
-def to_cmd(node, accounts=defaultaccounts, datadir=None):
-    cmd = ['geth']
-    cmd.extend(
-        ['--{} {}'.format(k, v) for k, v in node.items() if k in node_config])
-    cmd.extend(flags)
-    if 'minerthreads' in node:
-        cmd.append('--mine')
-        cmd.append('--etherbase 0')
-    if datadir:
-        assert isinstance(datadir, str)
-        cmd.append('--datadir {}'.format(datadir))
-        cmd.append('--genesis {}'.format(os.path.join(datadir, 'genesis.json')))
-    return cmd
-
-
 def mk_genesis(accounts, initial_alloc=denoms.ether * 10000):
-    genesis = genesis_stub.copy()
+    """
+    Create a genesis-block dict with allocation for all `accounts`.
+
+    :param accounts: list of account addresses (hex)
+    :param initial_alloc: the amount to allocate for the `accounts`
+    :return: genesis dict
+    """
+    genesis = GENESIS_STUB.copy()
     genesis['alloc'] = {
         account: {
             'balance': str(initial_alloc)
@@ -69,36 +69,105 @@ def mk_genesis(accounts, initial_alloc=denoms.ether * 10000):
     return genesis
 
 
-def shutdown_handler(_signo, _stackframe):
-    sys.exit(0)
+def prepare_for_exec(nodes, parentdir):
+    """
+    Prepare the configurations from `nodes` for execution, i.e.
+    - prepare dataddirs
+    - create genesis-files
+    - create accounts (if necessary)
+    - transform node configuration to `geth ...` cmd args
 
-if __name__ == '__main__':
-    signal.signal(signal.SIGTERM, shutdown_handler)
-    signal.signal(signal.SIGINT, shutdown_handler)
+    :param nodes: list of node configurations
+    :param parentdir: the datadir parent for all nodes
+    :return: list of cmds for `Popen`
+    """
+    cmds = []
+    for node in nodes:
+        nodedir = os.path.join(parentdir, node['nodekeyhex'])
+        os.makedirs(nodedir)
+        with open(os.path.join(nodedir, 'genesis.json'), 'w') as f:
+            json.dump(mk_genesis(DEFAULTACCOUNTS), f)
+            if 'minerthreads' in node:
+                create_account(nodedir)
+            cmds.append(to_cmd(node, datadir=nodedir))
+    return cmds
+
+
+def to_cmd(node, datadir=None):
+    """
+    Transform a node configuration into a cmd-args list for `subprocess.Popen`.
+
+    :param node: a node configuration
+    :param datadir: the node's datadir
+    :return: cmd-args list
+    """
+    cmd = ['geth']
+    cmd.extend(
+        ['--{} {}'.format(k, v) for k, v in node.items() if k in NODE_CONFIG])
+    cmd.extend(DEFAULT_ARGS)
+    if 'minerthreads' in node:
+        cmd.append('--mine')
+        cmd.append('--etherbase 0')
+    if datadir:
+        assert isinstance(datadir, str)
+        cmd.append('--datadir {}'.format(datadir))
+        cmd.append('--genesis {}'.format(os.path.join(datadir, 'genesis.json')))
+    cmd.extend(DEFAULT_ARGS)
+    return shlex.split(' '.join(cmd))
+
+
+def create_account(datadir):
+    """
+    Create an account in `datadir` -- since we're not interested
+    in the rewards, we don't care about the created address.
+
+    :param datadir: the datadir in which the account is created
+    :return: None
+    """
+
+    create = Popen(
+        shlex.split('geth --datadir {} account new'.format(datadir)),
+        stdin=PIPE, universal_newlines=True
+    )
+    create.stdin.write('notsosecret' + os.linesep)
+    time.sleep(.1)
+    create.stdin.write('notsosecret' + os.linesep)
+    create.communicate()
+    assert create.returncode == 0
+
+
+def create_node_configurations(num_nodes, miner=True, start_port=30301, start_rpcport=8101):
+    """
+    Create configurations (ports, keys, etc...) for `num_nodes`.
+
+    :param num_nodes: the number of nodes to create
+    :param miner: if True, setup the first node to be a mining node
+    :param start_port: the first p2p port to assign
+    :param start_rpcport: the first rpc port to assign
+    :return: list of node configurations (dicts)
+    """
     nodes = []
     for i in range(num_nodes):
         node = dict()
-        if i == 0:
+        if miner and i == 0:
             node['minerthreads'] = 1  # conservative
         node['nodekey'] = sha3('node:{}'.format(i))
         node['nodekeyhex'] = encode_hex(node['nodekey'])
-        node['pub'] = encode_hex(privtopub(node['nodekey']))
+        node['pub'] = encode_hex(privtopub_enode(node['nodekey']))
         node['address'] = privtoaddr(node['nodekey'])
-        node['port'] = 30301 + i
-        node['rpcport'] = 8101 + i
+        node['port'] = start_port + i
+        node['rpcport'] = start_rpcport + i
         node['enode'] = 'enode://{pub}@127.0.0.1:{port}'.format(**node)
         nodes.append(node)
         for node in nodes:
             node['bootnodes'] = ','.join(node['enode'] for node in nodes)
+    return nodes
 
-    datadir = tempfile.mkdtemp()
-    cmds = []
-    for node in nodes:
-        nodedir = os.path.join(datadir, node['nodekeyhex'])
-        os.makedirs(nodedir)
-        with open(os.path.join(nodedir, 'genesis.json'), 'w') as f:
-            json.dump(mk_genesis(defaultaccounts), f)
-            cmds.append(' '.join(to_cmd(node, datadir=os.path.join(datadir, node['nodekeyhex']))).split())
+
+def boot(cmds):
+    """
+    Run all `cmds` with a shutdown handler attached.
+    """
     processes = []
     try:
         for cmd in cmds:
@@ -111,3 +180,18 @@ if __name__ == '__main__':
         print('clean shutdown')
     finally:
         print('Goodbye')
+
+
+def shutdown_handler(_signo, _stackframe):
+    raise SystemExit
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, shutdown_handler)
+    signal.signal(signal.SIGINT, shutdown_handler)
+
+    datadir = tempfile.mkdtemp()
+    nodes = create_node_configurations(NUM_GETH_NODES)
+    cmds = prepare_for_exec(nodes, datadir)
+    boot(cmds)
+    while True:
+        time.sleep(1)
