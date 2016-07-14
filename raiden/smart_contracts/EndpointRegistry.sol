aaf7201bfc5eacabf3a44e878cd568d99f1ae6d8
agatsoh <sohan.agate@gmail.com>
2016-07-14 21:53:16 +0530

    Added the EndpointRegistry Contract and test_endpointregistry.py for issue 80

---
+++ raiden/smart_contracts/EndpointRegistry.sol
@@ -0,0 +1,98 @@
+/*
+This contract is a registry which maps the Ethereum Address to their endpoint i.e sockets.
+The Ethereum address registers his address in this registry.
+*/
+
+contract EndPointRegistry{
+
+//Events
+
+	event AddressRegistered(address indexed eth_address,string indexed socket);
+    event AddressUpdated(address indexed eth_address,string indexed old_socket,string indexed newSocket);
+
+//Storage Variables
+
+	//Mapping of Ethereum Addresses => SocketEndpoints
+	mapping (address => string) addressToSocket;
+	//Mapping of SocketEndpoints => Ethereum Addresses
+	mapping (string => address) socketToAddress;
+	//list of all the Registered Addresses , still not used.
+	address[] eth_addresses;
+
+//modifiers
+
+    modifier nodoubleregister
+    {
+    	if(sha3(addressToSocket[msg.sender])!= sha3("")) throw;
+    	_	
+    }
+
+    modifier endpointExists
+    {
+    	if(sha3(addressToSocket[msg.sender]) == sha3("")) throw;
+        _
+    }
+
+    modifier noEmptyString(string str)
+    {
+        if(equals(str,"") == true) throw;
+        _
+    }
+
+//Functions 
+
+    /* 
+    @notice Registers the Ethereum Address to the Endpoint socket.
+    @dev Registers the Ethereum Address to the Endpoint socket.
+    @param string of socket in this format "127.0.0.1:40001" 
+    */
+    function registerEndpoint(string socket) nodoubleregister 
+    {
+    	addressToSocket[msg.sender] = socket;
+    	socketToAddress[socket] = msg.sender;
+        AddressRegistered(msg.sender,socket);
+    }
+
+    /* 
+    @notice Updates an existing mapping to a new socket
+    @dev Updates an existing mapping to a new socket
+    @param string of socket in this format "127.0.0.1:40001"  
+    */
+    function updateEndpoint(string socket) endpointExists noEmptyString(socket)
+    {
+    	string old_socket = addressToSocket[msg.sender];
+    	socketToAddress[old_socket]	= address(0);
+    	addressToSocket[msg.sender] = socket;
+    	socketToAddress[socket] = msg.sender;
+        AddressUpdated(msg.sender,old_socket,socket);
+    }
+
+    /* 
+    @notice Finds the socket if given an Ethereum Address
+    @dev Finds the socket if given an Ethereum Address
+    @param An eth_address which is a 20 byte Ethereum Address
+    @return A socket which the current Ethereum Address is using.  
+    */
+    function findEndpointByAddress(address eth_address) constant returns (string socket)
+    {
+        return addressToSocket[eth_address];
+    }
+
+    /* 
+    @notice Finds Ethreum Address if given an existing socket address 
+    @dev Finds Ethreum Address if given an existing socket address
+    @param string of socket in this format "127.0.0.1:40001"  
+    @return An ethereum address
+    */
+    function findAddressByEndpoint(string socket) constant returns (address eth_address)
+    {
+    	return socketToAddress[socket];
+    }
+
+    function equals(string a,string b) internal constant returns (bool result)
+    {
+    if(sha3(a) == sha3(b)) return true;
+    else return false;
+    }
+
+}
---
+++ raiden/tests/smart_contracts/test_endpointregistry.py
@@ -0,0 +1,27 @@
+# -*- coding: utf8 -*-
+import pytest
+
+from ethereum import utils
+from ethereum import tester
+from ethereum.utils import sha3, privtoaddr
+from ethereum.tester import TransactionFailed
+
+from raiden.network.rpc.client import get_contract_path
+
+def test_endpointregistry():
+	registry_contract_path = get_contract_path('EndpointRegistry.sol')
+	state = tester.state()
+	assert state.block.number < 1150000
+	state.block.number = 1158001
+	assert state.block.number > 1150000
+	sender = tester.a0
+	registry_contract = state.abi_contract(None, path=registry_contract_path, language="solidity")
+	sender = tester.a0.encode('hex')
+	registry_contract.registerEndpoint('127.0.0.1:4001')
+	assert registry_contract.findAddressByEndpoint('127.0.0.1:4001') == tester.a0.encode('hex')
+	assert registry_contract.findEndpointByAddress(tester.a0.encode('hex')) == '127.0.0.1:4001'
+	registry_contract.updateEndpoint('192.168.0.1:4002')
+	assert registry_contract.findAddressByEndpoint('192.168.0.1:4002') == tester.a0.encode('hex')
+	assert registry_contract.findEndpointByAddress(tester.a0.encode('hex')) == '192.168.0.1:4002'
+	
+		
