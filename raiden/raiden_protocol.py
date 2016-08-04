9c0d1a3aaf445a68909d921c7f913448d1d09e4d
Fabio Utzig <utzig@utzig.org>
2016-08-03 23:17:39 -0300

    Ack waiting loop now uses events instead of sleeps

--- raiden/raiden_protocol.py
+++ raiden/raiden_protocol.py
@@ -40,6 +40,22 @@ class NotifyingQueue(Event):
         return self._stop_flag
 
 
+class NotifyingReceive(Event):
+
+    def __init__(self):
+        super(NotifyingReceive, self).__init__()
+        self._stop_flag = False
+
+    def stop(self):
+        """Request a stop event."""
+        self._stop_flag = True
+        self.set()
+
+    def has_stop(self):
+        """True if a stop event was requested."""
+        return self._stop_flag
+
+
 class RaidenProtocol(object):
     """ Encode the message into a packet and send it.
 
@@ -59,23 +75,25 @@ class RaidenProtocol(object):
         self.transport = transport
         self.discovery = discovery
         self.raiden = raiden
-        self.message_queue_by_address = dict()
         self.running = False
-
-        self.number_of_tries = dict()  # msg hash: count_tries
-        self.status_by_message = dict()  # msg hash: status|None
         self.sent_acks = dict()  # msghash: Ack
 
         self.start()
 
     def start(self):
         if not self.running:
+            self.message_queue_by_address = dict()
+            self.response_or_stop = dict()  # msg hash: NotifyingReceive
+            self.status_by_message = dict()  # msg hash: status|None
             self.running = True
 
     def stop(self):
         if self.running:
+            # XXX: maybe should wait for the greenlets termination...
             for message_queue in self.message_queue_by_address.itervalues():
                 message_queue.stop()
+            for receiver in self.response_or_stop.itervalues():
+                receiver.stop()
             self.running = False
 
     def _send_queued_messages(self, receiver_address):
@@ -84,16 +102,16 @@ class RaidenProtocol(object):
         while True:
             message_queue.wait()
             if message_queue.has_stop():
-                break
+                return
 
             message = message_queue.get()
             data = message.encode()
+
+            # XXX: is this cached?
             host_port = self.discovery.get(receiver_address)
 
-            # msghash is removed from the `number_of_tries` once a Ack is
-            # received, resend until we receive it or give up
             msghash = sha3(data)
-            self.number_of_tries[msghash] = 0
+            self.response_or_stop[msghash] = NotifyingReceive()
 
             log.info('SENDING {} > {} : [{}] {}'.format(
                 pex(self.raiden.address),
@@ -102,18 +120,25 @@ class RaidenProtocol(object):
                 message,
             ))
 
-            loops_for_retry = self.try_interval / self.short_delay
-            loop_count = loops_for_retry  # force send on first iteration
+            number_of_tries = 0
+            while True:
+                self.transport.send(self.raiden, host_port, data)
+                number_of_tries += 1
 
-            # loop should iterate as fast as possible checking for acks
-            while msghash in self.number_of_tries:
-                # we were asked to give up...
-                if message_queue.has_stop():
-                    break
+                result = self.response_or_stop[msghash].wait(timeout=self.try_interval)
+                if result:
+                    has_stop = self.response_or_stop[msghash].has_stop()
+                    del self.response_or_stop[msghash]
 
-                if self.number_of_tries[msghash] >= self.max_tries:
-                    # free send_and_wait...
-                    del self.number_of_tries[msghash]
+                    if has_stop:
+                        return
+                    else:
+                        if msghash in self.status_by_message:
+                            self.status_by_message[msghash] = True
+                        break
+
+                if number_of_tries >= self.max_tries:
+                    del self.response_or_stop[msghash]
                     if msghash in self.status_by_message:
                         self.status_by_message[msghash] = False
 
@@ -124,15 +149,7 @@ class RaidenProtocol(object):
                         pex(receiver_address),
                         message,
                     ))
-
-                # loop as fast as possible and only send messages at every X number of loops
-                if loop_count == loops_for_retry:
-                    self.transport.send(self.raiden, host_port, data)
-                    self.number_of_tries[msghash] += 1
-                    loop_count = 0
-
-                gevent.sleep(self.short_delay)
-                loop_count += 1
+                    break
 
     def send(self, receiver_address, message, with_status=False):
         if not isaddress(receiver_address):
@@ -205,13 +222,14 @@ class RaidenProtocol(object):
 
         if isinstance(message, Ack):
             # we might receive the same Ack more than once
-            if message.echo in self.number_of_tries:
+            if message.echo in self.response_or_stop:
                 log.info('ACK RECEIVED {} [echo={}]'.format(
                     pex(self.raiden.address),
                     pex(message.echo)
                 ))
 
-                del self.number_of_tries[message.echo]
+                self.response_or_stop[message.echo].set()
+
                 if message.echo in self.status_by_message:
                     self.status_by_message[message.echo] = True
             else:
