709378e3cedf6f1051bbf36e7c26c58e516e68dd
Konrad Feldmeier <konrad@brainbot.com>
2016-08-26 14:26:53 +0200

    Fix format for state dump and use predeployed key #63

--- truffle/create_compilation_dump.py
+++ truffle/create_compilation_dump.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8
 import json
 from ethereum import tester
+from ethereum.utils import privtoaddr, remove_0x_head
 from raiden.blockchain.abi import get_contract_path
 
 from ethereum import slogging
@@ -15,9 +16,21 @@ TARGETS = dict(
     token='HumanStandardToken.sol',
 )
 
+DEFAULT_KEY = ('1' * 64).decode('hex')
+DEFAULT_ACCOUNT = privtoaddr(DEFAULT_KEY)
+
 
 def deploy_all():
-    state = tester.state()
+    log.DEV("default key", raw=tester.DEFAULT_KEY, enc=tester.DEFAULT_KEY.encode('hex'))
+    log.DEV("default account", raw=tester.DEFAULT_ACCOUNT, enc=tester.DEFAULT_ACCOUNT.encode('hex'))
+    tester.DEFAULT_KEY = DEFAULT_KEY
+    tester.DEFAULT_ACCOUNT = DEFAULT_ACCOUNT
+    tester.keys[0] = DEFAULT_KEY
+    tester.accounts[0] = DEFAULT_ACCOUNT
+    log.DEV("default key", raw=tester.DEFAULT_KEY, enc=tester.DEFAULT_KEY.encode('hex'))
+    log.DEV("default account", raw=tester.DEFAULT_ACCOUNT, enc=tester.DEFAULT_ACCOUNT.encode('hex'))
+    state = tester.state(num_accounts=1)
+    log.DEV('state', coinbase=state.block.coinbase.encode('hex'), balance=state.block.get_balance(DEFAULT_ACCOUNT))
     tester.gas_limit = 10 * 10 ** 6
     state.block.number = 1158001
     deployed = dict()
@@ -29,8 +42,6 @@ def deploy_all():
         )
     )
     libraries = deployed.copy()
-    # avoid boost::filesystem::status: File name too long:"HumanStandardToken:66548b...
-    [libraries.pop(c) for c in "HumanStandardToken StandardToken".split()]
     deployed.update(
         deploy_with_dependencies(
             TARGETS['registry'],
@@ -44,16 +55,23 @@ def deploy_all():
             state
         )
     )
+
     dump = dict()
     for account in deployed.values():
         dump.update({account: state.block.account_to_dict(account)})
-    print json.dumps(dump, indent=2)
+    cleanup(dump)
+
+    with open('state_dump_py.json', 'w') as f:
+        json.dump(dump, f, indent=2)
+    print json.dumps(deployed, indent=2)
     print '--registry_contract_adddress {Registry} --discovery_contract_address {EndpointRegistry}'.format(**deployed)
 
 
 def deploy_with_dependencies(contract_name, state, libraries=dict()):
     dependencies = find_dependencies(
         get_contract_path(contract_name))
+    [libraries.pop(key) for key in libraries.keys() if not key in
+     [d.split('.')[0] for d in dependencies]]
     log.DEV("in deploy_with_dependencies", contract=contract_name, dependencies=dependencies)
     for dependency in dependencies:
         # FIXME: this should not be needed!?
@@ -63,17 +81,21 @@ def deploy_with_dependencies(contract_name, state, libraries=dict()):
         log.DEV('deploying dependency', name=dependency)
         log.DEV('known libraries', libraries=libraries)
         deployed = state.abi_contract(None,
-                                    path=get_contract_path(dependency),
-                                    language='solidity',
-                                    libraries=libraries,
-                                    )
+                                      path=get_contract_path(dependency),
+                                      language='solidity',
+                                      libraries=libraries,
+                                      sender=DEFAULT_KEY,
+                                      )
         libraries[dependency.split('.')[0]] = deployed.address.encode('hex')
         state.mine()
+    log.DEV('deploying target', name=contract_name)
+    log.DEV('known libraries', libraries=libraries)
     contract = state.abi_contract(None,
-                                  path=get_contract_path(contract_name),
-                                  language='solidity',
-                                  libraries=libraries
-                                  )
+                                    path=get_contract_path(contract_name),
+                                    language='solidity',
+                                    libraries=libraries,
+                                    sender=DEFAULT_KEY,
+                                    )
     libraries[contract_name.split('.')[0]] = contract.address.encode('hex')
     state.mine()
     return libraries
@@ -95,6 +117,17 @@ def find_dependencies(contract_file):
         if dependency not in cleaned:
             cleaned.append(dependency)
     return cleaned
+
+
+def cleanup(dump):
+    def strip_hex(val):
+        if isinstance(val, basestring):
+            return remove_0x_head(val)
+        return val
+
+    for account, alloc in dump.items():
+        for key, value in alloc.items():
+            alloc[key] = strip_hex(value)
 
 
 if __name__ == '__main__':
