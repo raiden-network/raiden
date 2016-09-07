a21f2277d8bee2a9517c52b6030ee9372785d1bc
Konrad Feldmeier <konrad@brainbot.com>
2016-09-07 12:13:35 +0200

    Revert "Fix build #FIXME"
    This reverts commit 3051efc29982fe0d3dba046ba7333193eed1938b.
    
    We should be using the fix(es) from `master`.

--- raiden/transfermanager.py
+++ raiden/transfermanager.py
@@ -50,16 +50,6 @@ class TransferManager(object):
                 direct_transfer,
             )
 
-            #FIXME: hack to fix API expectations of RaidenAPI.transfer
-            class nooptask(object):
-                def __init__(self):
-                    import warnings
-                    warnings.warn("transfermanager.transfer needs a fix!")
-
-                def join(self):
-                    pass
-            return nooptask()
-
         else:
             task = StartMediatedTransferTask(
                 self,
@@ -71,7 +61,6 @@ class TransferManager(object):
 
             task.start()
             task.join()
-            return task
 
     def on_mediatedtransfer_message(self, transfer):
         if transfer.sender not in self.assetmanager.partneraddress_channel:
