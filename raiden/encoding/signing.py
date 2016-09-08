bee8d26011b1c59464d24868ecc2e0831e356ada
Augusto F. Hack <hack.augusto@gmail.com>
2016-09-08 11:28:03 -0300

    secp256k1 using global context

--- raiden/encoding/signing.py
+++ raiden/encoding/signing.py
@@ -1,31 +1,56 @@
 # -*- coding: utf8 -*-
-from c_secp256k1 import ecdsa_recover_compact as c_ecdsa_recover_compact
-from c_secp256k1 import ecdsa_sign_compact as c_ecdsa_sign_compact
+import secp256k1
+from secp256k1 import PrivateKey, PublicKey, ALL_FLAGS
 
 from raiden.utils import sha3
+
+GLOBAL_CTX = secp256k1.lib.secp256k1_context_create(secp256k1.ALL_FLAGS)
 
 
 def recover_publickey(messagedata, signature):
     if len(signature) != 65:
         raise ValueError('invalid signature')
 
+    key = PublicKey(
+        ctx=secp256k1.lib.secp256k1_context_clone(GLOBAL_CTX),
+        flags=ALL_FLAGS,  # FLAG_SIGN is required to recover publickeys
+    )
+
+    signature_data = key.ecdsa_recoverable_deserialize(
+        signature[:64],
+        ord(signature[64]),
+    )
+
     message_hash = sha3(messagedata)
-    publickey = c_ecdsa_recover_compact(message_hash, signature)
+    publickey_data = key.ecdsa_recover(message_hash, signature_data, raw=True)
 
-    return publickey
+    publickey = PublicKey(
+        publickey_data,
+        ctx=secp256k1.lib.secp256k1_context_clone(GLOBAL_CTX)
+    )
+
+    return publickey.serialize(compressed=False)
 
 
-def sign(data, private_key):
+def sign(messagedata, private_key):
     if not isinstance(private_key, bytes) or len(private_key) != 32:
         raise ValueError('invalid private_key')
 
-    message_hash = sha3(data)
-    signature = c_ecdsa_sign_compact(message_hash, private_key)
+    key = PrivateKey(
+        private_key,
+        ctx=secp256k1.lib.secp256k1_context_clone(GLOBAL_CTX),
+        raw=True,
+    )
+
+    message_hash = sha3(messagedata)
+    secp_signature = key.ecdsa_sign_recoverable(message_hash, raw=True)
+    signature_data = key.ecdsa_recoverable_serialize(secp_signature)
+    signature = signature_data[0] + chr(signature_data[1])
 
     if len(signature) != 65:
         raise ValueError('invalid signature')
 
-    publickey = c_ecdsa_recover_compact(message_hash, signature)
+    publickey = key.pubkey.serialize(compressed=False)
 
     return signature, publickey
 
