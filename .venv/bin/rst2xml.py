e954d8b6857a73dc5de004366636b1a9bf863a20
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2016-11-07 14:24:36 +0100

    remove .venv folder accidentally added

--- .venv/bin/rst2xml.py
+++
@@ -1,4 +0,0 @@
-#!/home/czepluch/Dropbox/prgrms/ethereum/raiden/.venv/bin/python
-# EASY-INSTALL-SCRIPT: 'docutils==0.12','rst2xml.py'
-__requires__ = 'docutils==0.12'
-__import__('pkg_resources').run_script('docutils==0.12', 'rst2xml.py')
