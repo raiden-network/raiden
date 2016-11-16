6fdbaeac3d5b21bada00006827866f7cb1e55a64
Augusto F. Hack <hack.augusto@gmail.com>
2016-11-16 09:15:06 -0200

    tests: added timeout of 1min for each integration test

--- raiden/tests/integration/test_e2e.py
+++ raiden/tests/integration/test_e2e.py
@@ -11,6 +11,7 @@ from raiden.tests.utils.transfer import (
 )
 
 
+@pytest.mark.timeout(60)
 @pytest.mark.parametrize('privatekey_seed', ['fullnetwork:{}'])
 @pytest.mark.parametrize('number_of_nodes', [3])
 @pytest.mark.parametrize('channels_per_node', [2])
--- raiden/tests/integration/test_endpointregistry.py
+++ raiden/tests/integration/test_endpointregistry.py
@@ -5,6 +5,7 @@ from raiden.utils import make_address, get_contract_path, privatekey_to_address
 from raiden.network.discovery import ContractDiscovery
 
 
+@pytest.mark.timeout(60)
 @pytest.mark.parametrize('number_of_nodes', [1])
 @pytest.mark.parametrize('poll_timeout', [80])
 def test_endpointregistry(private_keys, blockchain_services):
