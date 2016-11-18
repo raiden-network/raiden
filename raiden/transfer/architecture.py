993a2b03c2a917b4bf8e13e22ccf5e2a669f7ea1
Augusto F. Hack <hack.augusto@gmail.com>
2016-11-18 21:15:49 -0200

    typo fixes

--- raiden/transfer/architecture.py
+++ raiden/transfer/architecture.py
@@ -6,46 +6,44 @@ Iteration = namedtuple('Iteration', ('new_state', 'actions'))
 
 
 class State(object):
-    """ A isolated state representation, modified by application StateChange
-    messages.
+    """ An isolated state, modified by StateChange messages.
 
     Notes:
-    - Do not duplicate the same state data in two State subclasses, instead of
-    nesting use identifiers.
-    - These objects contain the application state and no logic by design.
+    - Don't duplicate the same state data in two different States, instead use
+    identifiers.
+    - These objects don't have logic by design.
+    - These objects must not be mutated in-place.
+    - This class is used as a marker for states.
     """
     pass
 
 
 class StateChange(object):
-    """ StateChange are used to inform the application when events happened
-    (eg. a blockchain event, a new packet, an error).
+    """ Declare the transition to be applied in a state object. (eg. a
+    blockchain event, a new packet, an error).
 
     Notes:
-    - Messages when applied change a single State object.
-    - These objects need to be constructed in such a way that reaplying a
-    complete log of StateChange reproduces the correct application state.
-    - These objects represent state changes but don't have imperative code by
-    design.
-    - This subclass is used as a marker for StateChange.
+    - Messages change a single State object.
+    - Reaplying StateChanges must produce the same result.
+    - These objects don't have logic by design.
+    - This class is used as a marker for state changes.
     """
     pass
 
 
 class StateManager(object):
-    """ The storage for all the application state that can apply state
-    trasintions. """
+    """ The mutable storage for the application state, this storage can do
+    state transitions by applying the StateChanges to the current State.
+    """
 
     def __init__(self, state_transition, current_state):
         """ Initialize the state manager.
 
         Args:
-            state_transition: function that can apply the StateChange messages,
-            it contains the business logic.
-            current_state: Restore the application to the state of a previous
-            run.
+            state_transition: function that can apply the a StateChange
+            message.
+            current_state: current application state.
         """
-
         self.state_transition = state_transition
         self.current_state = current_state
 
