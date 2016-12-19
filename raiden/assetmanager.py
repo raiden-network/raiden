dacda6f953b65818da482ae799fd0b2373b16fdc
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2016-12-19 23:49:54 +0100

    use opened block instead of current block

--- raiden/assetmanager.py
+++ raiden/assetmanager.py
@@ -119,12 +119,12 @@ class AssetManager(object):  # pylint: disable=too-many-instance-attributes
         our_state = ChannelEndState(
             channel_details['our_address'],
             channel_details['our_balance'],
-            self.raiden.chain.block_number,
+            netting_channel.opened,
         )
         partner_state = ChannelEndState(
             channel_details['partner_address'],
             channel_details['partner_balance'],
-            self.raiden.chain.block_number,
+            netting_channel.opened,
         )
 
         external_state = ChannelExternalState(
