6ad8ecbb2678f7604590dcc4e51708cfafb09a65
volt <volt@localhost.localdomain>
2017-02-13 13:52:17 -0700

    updated docstrings on nettingchannelcontact

--- raiden/smart_contracts/NettingChannelContract.sol
+++ raiden/smart_contracts/NettingChannelContract.sol
@@ -6,7 +6,7 @@ contract NettingChannelContract {
     using NettingChannelLibrary for NettingChannelLibrary.Data;
     NettingChannelLibrary.Data public data;
 
-    event ChannelNewBalance(address token_address, address participant, uint balance, uint block_number);
+    event ChannelNewBalance(address asset_address, address participant, uint balance, uint block_number);
     event ChannelClosed(address closing_address, uint block_number);
     event TransferUpdated(address node_address, uint block_number);
     event ChannelSettled(uint block_number);
@@ -18,7 +18,7 @@ contract NettingChannelContract {
     }
 
     function NettingChannelContract(
-        address token_address,
+        address asset_address,
         address participant1,
         address participant2,
         uint timeout)
@@ -31,10 +31,13 @@ contract NettingChannelContract {
         data.participants[0].node_address = participant1;
         data.participants[1].node_address = participant2;
 
-        data.token = Token(token_address);
+        data.token = Token(asset_address);
         data.settle_timeout = timeout;
     }
 
+    /// @notice Caller makes a deposit into their channel balance.
+    /// @param amount The amount caller wants to deposit.
+    /// @return True if deposit is successful.
     function deposit(uint256 amount) returns (bool) {
         bool success;
         uint256 balance;
@@ -48,10 +51,15 @@ contract NettingChannelContract {
         return success;
     }
 
+    /// @notice Get the address of the channel partner.
+    /// @param one_address The address of partner 1.
+    /// @return The address of partner 2.
     function partner(address one_address) constant returns (address) {
         return data.partner(one_address);
     }
 
+    /// @notice Get the address and balance of both partners in a channel.
+    /// @return The address and balance pairs.
     function addressAndBalance()
         constant
         returns (
@@ -63,53 +71,73 @@ contract NettingChannelContract {
         return data.addressAndBalance();
     }
 
+    /// @notice Get the amount one partner has sent to the other partner.
+    /// @param participant The address of one partner.
+    /// @return The amount one partner has sent to the other.
     function transferredAmount(address participant) constant returns (uint) {
         return data.transferredAmount(participant);
     }
 
+    /// @notice Close the channel. Can only be called by a participant in the channel.
+    /// @param theirs_encoded The state of the partner's transfers.
+    /// @param ours_encoded The state of the caller's transfers.
     function close(bytes theirs_encoded, bytes ours_encoded) {
         data.close(msg.sender, theirs_encoded, ours_encoded);
         ChannelClosed(msg.sender, data.closed);
     }
 
+    /// @notice Dispute the state after closing, called by a counterparty (the
+    ///         participant who did not close the channel).
+    /// @param theirs_encoded The transfer the counterparty believes is the valid
+    ///                       state of the first participant.
     function updateTransfer(bytes theirs_encoded) {
         data.updateTransfer(msg.sender, theirs_encoded);
         TransferUpdated(msg.sender, block.number);
     }
 
+    /// @notice Unlock a locked transfer.
     function unlock(bytes locked_encoded, bytes merkle_proof, bytes32 secret) {
         data.unlock(msg.sender, locked_encoded, merkle_proof, secret);
         ChannelSecretRevealed(secret);
     }
 
+    /// @notice Settle the transfers and balances of the channel and pay out to
+    ///         each participant. Must be called by a participating address.
     function settle() {
         data.settle(msg.sender);
         ChannelSettled(data.settled);
     }
 
+    /// @notice Returns the number of blocks until the settlement timeout.
     function settleTimeout() constant returns (uint) {
         return data.settle_timeout;
     }
 
-    function tokenAddress() constant returns (address) {
+    /// @notice Returns the address of the asset/token.
+    function assetAddress() constant returns (address) {
         return data.token;
     }
 
+    /// @notice Returns the block number for when the channel was opened.
     function opened() constant returns (uint) {
         return data.opened;
     }
 
+    /// @notice Returns the block number for when the channel was closed.
     function closed() constant returns (uint) {
         return data.closed;
     }
 
+    /// @notice Returns the block number for when the channel was settled.
     function settled() constant returns (uint) {
         return data.settled;
     }
 
+    /// @notice Returns the address of the closing participant.
     function closingAddress() constant returns (address) {
         return data.closing_address;
     }
 
+    /// @notice All other attempted functions are thrown. (Wasted gas.)
     function () { throw; }
 }
