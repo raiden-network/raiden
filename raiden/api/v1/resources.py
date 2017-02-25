cf6d49d2dc636b1b1b4992fdb6b985b5f7d637ec
Lefteris Karapetsas <lefteris@refu.co>
2017-02-25 03:14:37 +0100

    Variable endpoint needs special care
    Having a variable endpoint does not in any way mean it will be passed
    down to the processing functions like this comment suggested:
    
       # the channel_address kwarg is automatically included in the kwargs,
       # because there is an argument placeholder in the route!
    
    It actually required a special endpoint location parsing function to be
    made which I called 'query' since it parses the query string.
    
    Also renamed status to state in some places

--- raiden/api/v1/resources.py
+++ raiden/api/v1/resources.py
@@ -1,8 +1,8 @@
-from webargs.flaskparser import use_kwargs
+from webargs.flaskparser import use_args, use_kwargs, parser
 from flask_restful import Resource
 from flask import Blueprint
-
-from raiden.api.v1.encoding import ChannelRequestSchema
+from pyethapp.jsonrpc import address_encoder
+from raiden.api.v1.encoding import ChannelRequestSchema, AddressField
 
 
 def create_blueprint():
@@ -14,6 +14,24 @@ def create_blueprint():
 class BaseResource(Resource):
     def __init__(self, **kwargs):
         return
+
+
+@parser.location_handler('query')
+def parse_query_data(request, name, field):
+    if 'channel_address' in request.view_args:
+        # TODO: channel address gets encoded into the view_args from inside
+        # the flask/webargs core code as bytes and now we encode the bytes
+        # to hex again. Feels like a waste. Find a way to fix?
+        #
+        #   Option 1: Figure out how to properly add it as an argument when appearing
+        #             as a placeholder in the variable endpoint and use it from inside
+        #             the various put/patch functions.
+        #
+        #   Option 2: Remove the hexaddress part of <hexaddress:channel_address>.
+        #             With this the conversion to bytes should not be performed,
+        #             but also we would probably need to extract the channel address
+        #             in this function here by parsing the URL string.
+        return address_encoder(request.view_args['channel_address'])
 
 
 class ChannelsResource(BaseResource):
@@ -36,7 +54,7 @@ class ChannelsResource(BaseResource):
     # e.g. this endpoint will accept the args from all locations:
     # as JSON object, via form data. or as query string
     # change according to what is desired here
-    @use_kwargs(put_schema, locations=('json', 'form', 'query'))
+    @use_kwargs(put_schema, locations=('form',))
     def put(self, **kwargs):
         return self.rest_api.open(**kwargs)
 
@@ -49,11 +67,15 @@ class ChannelsResourceByChannelAddress(Resource):
         exclude=('token_address', 'partner_address', 'settle_timeout'),
     )
 
-    @use_kwargs(patch_schema, locations=('json', 'form', 'query'))
-    def patch(self, **kwargs):
-        # the channel_address kwarg is automatically included in the kwargs,
-        # because there is an argument placeholder in the route!
-        return self.rest_api.patch_channel(**kwargs)
+    @use_args(patch_schema, locations=('form',))
+    @use_kwargs({'channel_address': AddressField()}, locations=('query',))
+    def patch(self, *args, **kwargs):
+        # TODO: Perhaps use a different schema here for patching so that the
+        #      channel address does not appear twice and we don't need to combine
+        #      dictionaries like this.
+        args = args[0]
+        args.update(kwargs)
+        return self.rest_api.patch_channel(**args)
 
 
 class TokensResource(BaseResource):
