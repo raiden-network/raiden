2fe65ddca48bcef390180e4359515f08dffed194
Augusto F. Hack <hack.augusto@gmail.com>
2017-02-27 06:34:46 -0300

    moved channel for hashlock registry to raiden
    wip: centralizing the message handling

--- raiden/tokenmanager.py
+++ raiden/tokenmanager.py
@@ -31,12 +31,6 @@ class TokenManager(object):  # pylint: disable=too-many-instance-attributes
         self.partneraddress_channel = dict()  #: maps the partner address to the channel instance
         self.address_channel = dict()  #: maps the channel address to the channel instance
 
-        # This is a map from a hashlock to a list of channels, the same
-        # hashlock can be used in more than one TokenManager (for exchanges), a
-        # channel should be removed from this list only when the lock is
-        # released/withdrawed but not when the secret is registered.
-        self.hashlock_channel = defaultdict(list)  #: channels waiting on the conditional lock
-
         self.token_address = token_address
         self.channel_manager_address = channel_manager_address
         self.channelgraph = channel_graph
@@ -108,9 +102,16 @@ class TokenManager(object):  # pylint: disable=too-many-instance-attributes
             netting_channel.opened,
         )
 
+        def register_channel_for_hashlock(channel, hashlock):
+            self.raiden.register_channel_for_hashlock(
+                self.token_address,
+                channel,
+                hashlock,
+            )
+
         external_state = ChannelExternalState(
             self.raiden.alarm.register_callback,
-            self.register_channel_for_hashlock,
+            register_channel_for_hashlock,
             self.raiden.get_block_number,
             netting_channel,
         )
@@ -134,12 +135,6 @@ class TokenManager(object):  # pylint: disable=too-many-instance-attributes
             translator,
         )
 
-    def register_channel_for_hashlock(self, channel, hashlock):
-        channels_registered = self.hashlock_channel[hashlock]
-
-        if channel not in channels_registered:
-            channels_registered.append(channel)
-
     def handle_secret(self, identifier, secret):
         """ Unlock locks, register the secret, and send Secret messages as
         necessary.
@@ -152,7 +147,7 @@ class TokenManager(object):  # pylint: disable=too-many-instance-attributes
 
         Note:
             The channel needs to be registered with
-            `register_channel_for_hashlock`.
+            `raiden.register_channel_for_hashlock`.
         """
         # handling the secret needs to:
         # - unlock the token for all `forward_channel` (the current one
@@ -164,7 +159,13 @@ class TokenManager(object):  # pylint: disable=too-many-instance-attributes
         # - reveal the secret to the `sender` node (otherwise we
         #   cannot withdraw the token)
         hashlock = sha3(secret)
-        self._secret(identifier, secret, None, hashlock)
+        self.raiden.handle_secret(
+            identifier,
+            self.token_address,
+            secret,
+            None,
+            hashlock,
+        )
 
     def handle_secretmessage(self, partner_secret_message):
         """ Unlock locks, register the secret, and send Secret messages as
@@ -179,76 +180,18 @@ class TokenManager(object):  # pylint: disable=too-many-instance-attributes
 
         Note:
             The channel needs to be registered with
-            `register_channel_for_hashlock`.
+            `raiden.register_channel_for_hashlock`.
         """
         secret = partner_secret_message.secret
         identifier = partner_secret_message.identifier
         hashlock = sha3(secret)
-        self._secret(identifier, secret, partner_secret_message, hashlock)
-
-    def _secret(self, identifier, secret, partner_secret_message, hashlock):
-        channels_list = self.hashlock_channel[hashlock]
-        channels_to_remove = list()
-
-        # Dont use the partner_secret_message.token since it might not match with the
-        # current token manager
-        our_secret_message = Secret(identifier, secret, self.token_address)
-        self.raiden.sign(our_secret_message)
-
-        revealsecret_message = RevealSecret(secret)
-        self.raiden.sign(revealsecret_message)
-
-        for channel in channels_list:
-            # critical read/write section
-            # - the `release_lock` might raise if the `balance_proof` changes
-            #   after the check
-            # - a message created before the lock is release must be added in
-            #   the message queue before `our_secret_message`
-            # We are relying on the GIL and non-blocking apis instead of an
-            # explicit lock.
-
-            if channel.partner_state.balance_proof.is_unclaimed(hashlock):
-                # we are the sender, so we can release the lock once the secret
-                # is known and add the update message into the end of the
-                # message queue, all the messages will remain consistent
-                # (including the messages in-transit and the ones that are
-                # already in the queue)
-                channel.release_lock(secret)
-
-                # notify our partner that our state is updated and it can
-                # withdraw the token
-                self.raiden.send_async(channel.partner_state.address, our_secret_message)
-
-                channels_to_remove.append(channel)
-
-            # we are the recipient, we can only withdraw the token if a secret
-            # message is received from the correct sender and token address, so
-            # withdraw if a valid message is received otherwise register the
-            # secret and reveal the secret to channel patner.
-            if channel.our_state.balance_proof.is_unclaimed(hashlock):
-                # partner_secret_message might be None
-                if partner_secret_message:
-                    valid_sender = partner_secret_message.sender == channel.partner_state.address
-                    valid_token = partner_secret_message.token == channel.token_address
-
-                    if valid_sender and valid_token:
-                        channel.withdraw_lock(secret)
-                        channels_to_remove.append(channel)
-                    else:
-                        # assume our partner does not know the secret and reveal it
-                        channel.register_secret(secret)
-                        self.raiden.send_async(channel.partner_state.address, revealsecret_message)
-                else:
-                    channel.register_secret(secret)
-                    self.raiden.send_async(channel.partner_state.address, revealsecret_message)
-            # /critical read/write section
-
-        for channel in channels_to_remove:
-            channels_list.remove(channel)
-
-        # delete the list from defaultdict, it wont be used again
-        if len(channels_list) == 0:
-            del self.hashlock_channel[hashlock]
+        self.raiden.handle_secret(
+            identifier,
+            self.token_address,
+            secret,
+            partner_secret_message,
+            hashlock,
+        )
 
     def channel_isactive(self, partner_address):
         """ True if the channel with `partner_address` is open. """
