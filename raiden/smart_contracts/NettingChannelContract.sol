6af41a57ccb3f7df15faa40deedcd968cc8c0216
Adrian Brink <adrian@avogenie.com>
2017-03-20 14:39:50 +0100

    Merge branch 'master' into feature/adrian-setup_development_environment (reverted from commit 47969a2c94b3e128b80b7419071e8622c16c6029)

--- raiden/smart_contracts/NettingChannelContract.sol
+++ raiden/smart_contracts/NettingChannelContract.sol
@@ -96,9 +96,6 @@ contract NettingChannelContract {
     }
 
     /// @notice Unlock a locked transfer.
-    /// @param locked_encoded The locked transfer to be unlocked.
-    /// @param merkle_proof The merke_proof for the locked transfer.
-    /// @param secret The secret to unlock the locked transfer.
     function unlock(bytes locked_encoded, bytes merkle_proof, bytes32 secret) {
         data.unlock(msg.sender, locked_encoded, merkle_proof, secret);
         ChannelSecretRevealed(secret);
@@ -114,37 +111,31 @@ contract NettingChannelContract {
     }
 
     /// @notice Returns the number of blocks until the settlement timeout.
-    /// @return The number of blocks until the settlement timeout.
     function settleTimeout() constant returns (uint) {
         return data.settle_timeout;
     }
 
     /// @notice Returns the address of the token.
-    /// @return The address of the token.
     function tokenAddress() constant returns (address) {
         return data.token;
     }
 
     /// @notice Returns the block number for when the channel was opened.
-    /// @return The block number for when the channel was opened.
     function opened() constant returns (uint) {
         return data.opened;
     }
 
     /// @notice Returns the block number for when the channel was closed.
-    /// @return The block number for when the channel was closed.
     function closed() constant returns (uint) {
         return data.closed;
     }
 
     /// @notice Returns the block number for when the channel was settled.
-    /// @return The block number for when the channel was settled.
     function settled() constant returns (uint) {
         return data.settled;
     }
 
     /// @notice Returns the address of the closing participant.
-    /// @return The address of the closing participant.
     function closingAddress() constant returns (address) {
         return data.closing_address;
     }
