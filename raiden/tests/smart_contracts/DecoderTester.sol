d6d22555881a2a69a31959dca7ed23e52690d27b
Augusto F. Hack <hack.augusto@gmail.com>
2017-04-19 18:01:13 -0300

    extended netting channel decoder tests

--- raiden/tests/smart_contracts/DecoderTester.sol
+++ raiden/tests/smart_contracts/DecoderTester.sol
@@ -3,63 +3,19 @@ pragma solidity ^0.4.0;
 import "raiden/NettingChannelLibrary.sol";
 
 contract DecoderTester {
-    using NettingChannelLibrary for NettingChannelLibrary.Data;
-    NettingChannelLibrary.Data public data;
-
-
-    bool public decoding_complete;
-
-    modifier settleTimeoutNotTooLow(uint t) {
-        if (t < 6) throw;
-        _;
+    function decodeLock(bytes lock) returns (uint64 expiration, uint amount, bytes32 hashlock) {
+        return NettingChannelLibrary.decodeLock(lock);
     }
 
-    modifier after_decoding() {
-        if (!decoding_complete) {
-            throw;
-        }
-        _;
+    function getTransferRawAddress(bytes signed_transfer) returns (bytes transfer_raw, address signing_address) {
+        return NettingChannelLibrary.getTransferRawAddress(signed_transfer);
     }
 
-    function DecoderTester(
-        address token_address,
-        address participant1,
-        address participant2,
-        uint timeout)
-        settleTimeoutNotTooLow(timeout)
-    {
-        if (participant1 == participant2) {
-            throw;
-        }
-
-        data.participants[0].node_address = participant1;
-        data.participants[1].node_address = participant2;
-        data.participant_index[participant1] = 1;
-        data.participant_index[participant2] = 2;
-
-        data.token = Token(token_address);
-        data.settle_timeout = timeout;
+    function getNonce(bytes message) returns (uint64 nonce) {
+        return NettingChannelLibrary.getNonce(message);
     }
 
-    function testDecodeTransfer(bytes signed_transfer) returns (bool) {
-        data.close(msg.sender, signed_transfer);
-        decoding_complete = true;
-        return true;
-    }
-
-    function decodedNonce() after_decoding constant returns (uint64) {
-        return data.participants[0].nonce;
-    }
-
-    function decodedAmount() after_decoding constant returns (uint256) {
-        return data.participants[0].transferred_amount;
-    }
-
-    function decodedLocksroot() after_decoding constant returns (bytes32) {
-        return data.participants[0].locksroot;
-    }
-
-    function decodedToken() after_decoding constant returns (address) {
-        return data.token;
+    function decodeTransfer(bytes transfer_raw) returns (uint64 nonce, bytes32 locksroot, uint256 transferred_amount) {
+        return NettingChannelLibrary.decodeTransfer(transfer_raw);
     }
 }
