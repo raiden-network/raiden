23efebcea5fdf52e2db32e4dd303d2ee67472156
agatsoh <sohan.agate@gmail.com>
2017-04-24 18:32:24 +0530

    Adding blockies and Token Network table

--- raiden/raidenwebui/src/app/components/channel-table/channel-table.component.ts
+++ raiden/raidenwebui/src/app/components/channel-table/channel-table.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { RaidenService } from '../../services/raiden.service';
+import { SharedService } from '../../services/shared.service';
 import { Channel } from '../../models/channel';
-
+declare var blockies;
 @Component({
     selector: 'app-channel-table',
     templateUrl: './channel-table.component.html',
-    styleUrls: ['./channel-table.component.css']
+    styleUrls: ['./channel-table.component.css'],
+    providers: [ SharedService ]
 })
 export class ChannelTableComponent implements OnInit {
 
     public channels: Channel[];
-    constructor(private raidenService: RaidenService) { }
+    constructor(private raidenService: RaidenService,
+    private sharedService: SharedService) { }
 
     ngOnInit() {
       this.getChannels();
@@ -20,9 +23,15 @@ export class ChannelTableComponent implements OnInit {
         this.raidenService.getChannels().subscribe(
             (channels) => {
                 this.channels = <Channel[]> channels;
+                this.sharedService.setChannelData(this.channels);
             }
         );
     }
 
+    public generateBlockies(icon: any, address: string) {
+        console.log(address);
+        icon.style.backgroundImage = 'url(' + blockies.create({ seed: address , size: 8, scale: 16})
+        .toDataURL() + ')';
+    }
 
 }
--- raiden/raidenwebui/src/app/components/token-network/token-network.component.ts
+++ raiden/raidenwebui/src/app/components/token-network/token-network.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RaidenService } from '../../services/raiden.service';
+import { SharedService } from '../../services/shared.service';
 import { Usertoken } from '../../models/usertoken';
+import {Message} from 'primeng/primeng';
+import { Channel } from '../../models/channel';
 
 @Component({
   selector: 'app-token-network',
@@ -12,8 +15,12 @@ export class TokenNetworkComponent implements OnInit {
     @Input() raidenAddress: string;
     public tokenBalances: Usertoken[];
     public selectedToken: Usertoken;
-
-    constructor(private raidenService: RaidenService) { }
+    public msgs: Message[] = [];
+    @Input() channelsToken: Channel[];
+    public displayDialog: boolean = false;
+    public channelOpened: Channel = new Channel();
+    constructor(private raidenService: RaidenService,
+    private sharedService: SharedService) { }
 
 
     ngOnInit() {
@@ -23,6 +30,28 @@ export class TokenNetworkComponent implements OnInit {
             }
 
         );
+    }
+
+    public joinTokenNetwork() {
+        if (this.selectedToken.balance === 0) {
+            this.msgs.push({
+                severity: 'error',
+                summary: 'Insufficient Balance',
+                detail: 'Your Balance in this token network is zero.'
+            });
+            return;
+        }
+        for (const channel of this.channelsToken) {
+            if (this.selectedToken.address === channel.token_address) {
+                this.msgs.push({
+                    severity: 'warn',
+                    summary: 'Warining message',
+                    detail: 'You already participate in this token network'
+                });
+                return;
+            }
+        }
+        this.displayDialog = true;
     }
 
 }
