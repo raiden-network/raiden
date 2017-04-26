f6a4079d4e4a158ea46eafcfbbda309d22f4e9e5
Augusto F. Hack <hack.augusto@gmail.com>
2017-04-26 20:43:19 -0300

    reduce number of tampered tests

--- raiden/tests/smart_contracts/test_decoder_netting_channel.py
+++ raiden/tests/smart_contracts/test_decoder_netting_channel.py
@@ -154,29 +154,13 @@ def test_decode_refund_transfer(
     assert_decoder_results(refund_transfer, decoder)
 
 
-@pytest.mark.parametrize('identifier', [0, 2 ** 64 - 1])
-@pytest.mark.parametrize('nonce', [1, 2 ** 64 - 1])
-@pytest.mark.parametrize('transferred_amount', [0, 2 ** 256 - 1])
-@pytest.mark.parametrize('locksroot', VALID_LOCKSROOT)
-def test_decode_tampered_direct_transfer(
-        identifier,
-        nonce,
-        transferred_amount,
-        locksroot,
-        tester_state,
-        tester_nettingchannel_library_address):
-
+def test_decode_tampered_direct_transfer(tester_state, tester_nettingchannel_library_address):
     privatekey0 = tester.DEFAULT_KEY
     address0 = privatekey_to_address(privatekey0)
 
     decoder = deploy_decoder_tester(tester_state, tester_nettingchannel_library_address)
 
-    direct_transfer = make_direct_transfer(
-        identifier=identifier,
-        nonce=nonce,
-        transferred_amount=transferred_amount,
-        locksroot=locksroot,
-    )
+    direct_transfer = make_direct_transfer()
     direct_transfer.sign(PrivateKey(privatekey0, ctx=GLOBAL_CTX, raw=True), address0)
 
     message_encoded = direct_transfer.encode()
@@ -200,35 +184,13 @@ def test_decode_tampered_direct_transfer(
             decoder.decodeTransfer(tampered_transfer)
 
 
-@pytest.mark.parametrize('amount', [0, 2 ** 256 - 1])
-@pytest.mark.parametrize('identifier', [0, 2 ** 64 - 1])
-@pytest.mark.parametrize('nonce', [1, 2 ** 64 - 1])
-@pytest.mark.parametrize('transferred_amount', [0, 2 ** 256 - 1])
-@pytest.mark.parametrize('fee', [0, 2 ** 256 - 1])
-@pytest.mark.parametrize('locksroot', VALID_LOCKSROOT)
-def test_decode_tampered_mediated_transfer(
-        amount,
-        identifier,
-        nonce,
-        transferred_amount,
-        locksroot,
-        fee,
-        tester_state,
-        tester_nettingchannel_library_address):
-
+def test_decode_tampered_mediated_transfer(tester_state, tester_nettingchannel_library_address):
     privatekey0 = tester.DEFAULT_KEY
     address0 = privatekey_to_address(privatekey0)
 
     decoder = deploy_decoder_tester(tester_state, tester_nettingchannel_library_address)
 
-    mediated_transfer = make_mediated_transfer(
-        amount=amount,
-        identifier=identifier,
-        nonce=nonce,
-        fee=fee,
-        transferred_amount=transferred_amount,
-        locksroot=locksroot,
-    )
+    mediated_transfer = make_mediated_transfer()
 
     mediated_transfer.sign(PrivateKey(privatekey0, ctx=GLOBAL_CTX, raw=True), address0)
 
@@ -246,33 +208,20 @@ def test_decode_tampered_mediated_transfer(
         with pytest.raises(TransactionFailed):
             decoder.decodeTransfer(tampered_transfer)
 
+        tampered_transfer = bytearray(transfer_raw)
+        tampered_transfer.pop(slice_.stop - 1)
+        tampered_transfer = str(tampered_transfer)
+        with pytest.raises(TransactionFailed):
+            decoder.decodeTransfer(tampered_transfer)
 
-@pytest.mark.parametrize('amount', [0, 2 ** 256 - 1])
-@pytest.mark.parametrize('identifier', [0, 2 ** 64 - 1])
-@pytest.mark.parametrize('nonce', [1, 2 ** 64 - 1])
-@pytest.mark.parametrize('transferred_amount', [0, 2 ** 256 - 1])
-@pytest.mark.parametrize('locksroot', VALID_LOCKSROOT)
-def test_decode_tampered_refund_transfer(
-        amount,
-        identifier,
-        nonce,
-        transferred_amount,
-        locksroot,
-        tester_state,
-        tester_nettingchannel_library_address):
 
+def test_decode_tampered_refund_transfer(tester_state, tester_nettingchannel_library_address):
     privatekey0 = tester.DEFAULT_KEY
     address0 = privatekey_to_address(privatekey0)
 
     decoder = deploy_decoder_tester(tester_state, tester_nettingchannel_library_address)
 
-    refund_transfer = make_refund_transfer(
-        amount=amount,
-        identifier=identifier,
-        nonce=nonce,
-        transferred_amount=transferred_amount,
-        locksroot=locksroot,
-    )
+    refund_transfer = make_refund_transfer()
     refund_transfer.sign(PrivateKey(privatekey0, ctx=GLOBAL_CTX, raw=True), address0)
 
     message_encoded = refund_transfer.encode()
