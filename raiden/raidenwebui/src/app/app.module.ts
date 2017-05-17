5c2e9bc8181f9a709ef1f75a3922c87989279671
agatsoh <sohan.agate@gmail.com>
2017-05-17 16:31:25 +0530

    Adding config for the UI and CORS

--- raiden/raidenwebui/src/app/app.module.ts
+++ raiden/raidenwebui/src/app/app.module.ts
@@ -6,13 +6,18 @@ import { DataTableModule, SharedModule, DataListModule, CarouselModule,
 ButtonModule, AccordionModule, GrowlModule, DialogModule } from 'primeng/primeng';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MdTabsModule, MdInputModule, MdSelectModule, MdToolbarModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { ChannelTableComponent } from './components/channel-table/channel-table.component';
-import { RaidenService } from './services/raiden.service';
-import { SharedService } from './services/shared.service';
 import { EventListComponent } from './components/event-list/event-list.component';
 import { UserinteractionComponent } from './components/userinteraction/userinteraction.component';
 import { TokenNetworkComponent } from './components/token-network/token-network.component';
+
+import { APP_INITIALIZER } from '@angular/core';
+import { RaidenService } from './services/raiden.service';
+import { SharedService } from './services/shared.service';
+import { RaidenConfig } from './services/raiden.config';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -41,7 +46,20 @@ import { TokenNetworkComponent } from './components/token-network/token-network.
     MdToolbarModule,
   ],
   exports: [ MdTabsModule, MdInputModule, MdSelectModule, MdToolbarModule ],
-  providers: [RaidenService, SharedService],
+  providers: [ RaidenConfig,
+              {
+                  provide: APP_INITIALIZER,
+                  useFactory: ConfigLoader,
+                  deps: [RaidenConfig],
+                  multi: true
+              },
+              RaidenService,
+              SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function ConfigLoader(raidenConfig: RaidenConfig) {
+    // Note: this factory need to return a function (that return a promise)
+    return () => raidenConfig.load(environment.configFile);
+}
--- raiden/raidenwebui/src/app/services/raiden.service.ts
+++ raiden/raidenwebui/src/app/services/raiden.service.ts
@@ -4,30 +4,38 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
+import { RaidenConfig } from './raiden.config';
 
 @Injectable()
 export class RaidenService {
 
-    constructor(private http: Http) { }
+    constructor(private http: Http, private config: RaidenConfig) {
+    }
 
     public getChannels(): Observable<any> {
-        return this.http.get('http://127.0.0.1:5000/api/21323/channels/')
+        console.log(this.getRaidenAddress());
+        return this.http.get(this.config.apiCall + '/channels/')
         .map((response) => response.json()).catch(this.handleError);
     }
 
     public getEvents(): Observable<any> {
-        return this.http.get('http://127.0.0.1:5000/api/21323/events/')
+        return this.http.get(this.config.apiCall + '/events/')
         .map((response) => response.json()).catch(this.handleError);
     }
 
     public getTradedTokens(): Observable<any> {
-        return this.http.get('http://127.0.0.1:5000/api/21323/tokens/')
+        return this.http.get(this.config.apiCall + '/tokens/')
         .map((response) => response.json()).catch(this.handleError);
     }
 
     public getTokenBalancesOf(raidenAddress: string): Observable<any> {
         return this.http.get('/src/app/services/tokenbalance.json')
         .map((response) => response.json()).catch(this.handleError);
+    }
+
+    public getRaidenAddress() {
+        const web3 = this.config.getWeb3();
+        return web3.eth.coinbase;
     }
 
     private handleError (error: Response | any) {
