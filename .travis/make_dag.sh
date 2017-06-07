eed555be5d2cdaa749b035d6e1177cbfffa9de2f
Konrad Feldmeier <konrad@brainbot.com>
2017-06-07 12:52:00 +0200

    Add diagnostics

--- .travis/make_dag.sh
+++ .travis/make_dag.sh
@@ -6,4 +6,5 @@ mkdir -p $HOME/.ethash
 
 # this will generate the DAG once, travis is configured to cache it and
 # subsequent calls will not regenerate
+ls -alhR $HOME/.ethash
 geth makedag 0 $HOME/.ethash
