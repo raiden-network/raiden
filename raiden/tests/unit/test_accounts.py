55e0a2be0fedab19ba323b9e182fd4646b565ca8
Ulrich Petri <ulo@ulo.pe>
2017-06-20 19:55:57 +0200

    Test AccountManager behaviour with invalid / inaccessible files

--- raiden/tests/unit/test_accounts.py
+++ raiden/tests/unit/test_accounts.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import logging
 import os
 import pytest
 
@@ -6,10 +7,26 @@ from ethereum.utils import encode_hex
 from raiden.accounts import AccountManager
 from raiden.utils import get_project_root
 
-test_keystore = os.path.join(get_project_root(), 'tests', 'test_files')
+
+KEYFILE_INACCESSIBLE = "UTC--2017-06-20T16-33-00.000000000Z--inaccessible"
+KEYFILE_INVALID = "UTC--2017-06-20T16-06-00.000000000Z--invalid"
 
 
-def test_get_accounts():
+@pytest.yield_fixture(scope="module")
+def test_keystore():
+    keystore = os.path.join(get_project_root(), 'tests', 'test_files')
+    # Create inaccessible keyfile
+    inaccessible_file = os.path.join(keystore, KEYFILE_INACCESSIBLE)
+    if not os.path.exists(inaccessible_file):
+        open(inaccessible_file, "w").close()
+    os.chmod(inaccessible_file, 0)
+    yield keystore
+    # Cleanup to leave no undeletable files behind
+    os.chmod(inaccessible_file, 0o600)
+    os.unlink(inaccessible_file)
+
+
+def test_get_accounts(test_keystore):
     account_manager = AccountManager(test_keystore)
     expected_accounts = {
         '0d5a0e4fece4b84365b9b8dba6e6d41348c73645': os.path.join(
@@ -24,7 +41,7 @@ def test_get_accounts():
     assert expected_accounts == account_manager.accounts
 
 
-def test_get_account_in_keystore():
+def test_get_account_in_keystore(test_keystore):
     account_manager = AccountManager(test_keystore)
     assert account_manager.address_in_keystore('0d5a0e4fece4b84365b9b8dba6e6d41348c73645')
     assert account_manager.address_in_keystore('0x0d5a0e4fece4b84365b9b8dba6e6d41348c73645')
@@ -33,7 +50,7 @@ def test_get_account_in_keystore():
     assert not account_manager.address_in_keystore('a05934d3033d18b82f7b4adf18e1ed24e3d23b19')
 
 
-def test_get_privkey():
+def test_get_privkey(test_keystore):
     account_manager = AccountManager(test_keystore)
     'f696ecb5c767263c797a035db6f6008d38d852960ed33a491a58390b003fb605' == encode_hex(
         account_manager.get_privkey('0d5a0e4fece4b84365b9b8dba6e6d41348c73645', '123')
@@ -55,3 +72,19 @@ def test_get_privkey():
     with pytest.raises(ValueError) as exc:
         account_manager.get_privkey('a05934d3033d18b82f7b4adf18e1ed24e3d23b19', '123')
     assert 'Keystore file not found for a05934d3033d18b82f7b4adf18e1ed24e3d23b19' in exc.value
+
+
+def test_account_manager_invalid_files(test_keystore, caplog):
+    with caplog.at_level(logging.DEBUG):
+        AccountManager(test_keystore)
+
+    for msg, file_name, reason in [
+        ("Invalid account file", KEYFILE_INVALID, "No JSON object"),
+        ("Can not read account file", KEYFILE_INACCESSIBLE, "Permission denied")
+    ]:
+        for record in caplog.records:
+            message = record.getMessage()
+            if msg in message and file_name in message and reason in message:
+                break
+        else:
+            assert False, "'{}' not in log messages".format(msg)
