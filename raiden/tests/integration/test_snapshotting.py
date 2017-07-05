986bd68264b3af19b292b167a4074d742d048d50
Konrad Feldmeier <konrad@brainbot.com>
2017-07-05 10:52:23 +0200

    Flake8 fixes

--- raiden/tests/integration/test_snapshotting.py
+++ raiden/tests/integration/test_snapshotting.py
@@ -19,17 +19,17 @@ def test_snapshotting(
     raiden_network,
     token_addresses,
     settle_timeout,
-    blockchain_type):
+    blockchain_type
+):
     app0, app1, app2 = raiden_network
 
     api0 = RaidenAPI(app0.raiden)
     api1 = RaidenAPI(app1.raiden)
-    api2 = RaidenAPI(app2.raiden)
 
     channel_0_1 = api0.get_channel_list(token_addresses[0], app1.raiden.address)
     channel_0_2 = api0.get_channel_list(token_addresses[0], app2.raiden.address)
     with pytest.raises(KeyError):
-        channel_1_2 = api1.get_channel_list(token_addresses[0], app2.raiden.address)
+        api1.get_channel_list(token_addresses[0], app2.raiden.address)
     assert len(channel_0_1) == 1
     assert len(channel_0_2) == 1
     api1.transfer_and_wait(token_addresses[0], 5, app2.raiden.address)
@@ -54,15 +54,15 @@ def test_snapshotting(
         app.raiden.identifier_to_statemanagers = dict()
         app.raiden.channelgraphs = dict()
         # restore_from_snapshot is called during __init__
-        newservice = RaidenService(
+        service = RaidenService(
             app.raiden.chain,
             app.raiden.privkey,
             app.raiden.protocol.transport,
             app.raiden.protocol.discovery,
             app.config
         )
-        assert states[num]['identifiers_to_statemanagers'] == newservice.identifier_to_statemanagers
-        assert states[num]['channelgraphs'] == newservice.channelgraphs
-        assert len(newservice.channelgraphs)
-        assert len(newservice.identifier_to_statemanagers)
+        assert states[num]['identifiers_to_statemanagers'] == service.identifier_to_statemanagers
+        assert states[num]['channelgraphs'] == service.channelgraphs
+        assert len(service.channelgraphs)
+        assert len(service.identifier_to_statemanagers)
         # FIXME: testing the queue snapshot is missing
