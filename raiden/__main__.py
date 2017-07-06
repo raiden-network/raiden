5e4eb0ba88484e3075ac401906149038f88f7a1d
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2017-07-06 08:52:44 -0300

    Comments about auto_envvar_prefix use in click

--- raiden/__main__.py
+++ raiden/__main__.py
@@ -5,6 +5,9 @@ def main():
     import gevent.monkey
     gevent.monkey.patch_all()
     from raiden.ui.cli import run
+    # auto_envvar_prefix on a @click.command will cause all options to be
+    # available also through environment variables prefixed with given prefix
+    # http://click.pocoo.org/6/options/#values-from-environment-variables
     run(auto_envvar_prefix='RAIDEN')
 
 
