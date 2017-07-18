034342681930e793108ad25722ed5daf550eda97
Ulrich Petri <ulo@ulo.pe>
2017-07-18 17:58:58 +0200

    Scenario player

---
+++ tools/scenario-player/scenario_player/runner.py
@@ -0,0 +1,181 @@
+from pathlib import Path
+
+import gevent
+import structlog
+import yaml
+from eth_utils import to_checksum_address
+from requests import RequestException, Session
+from toolz import first
+from web3 import HTTPProvider, Web3
+
+from raiden.accounts import Account
+from raiden.network.rpc.client import JSONRPCClient
+from scenario_player.exceptions import NodesUnreachableError, ScenarioError, TokenRegistrationError
+from scenario_player.utils import (
+    TimeOutHTTPAdapter,
+    get_gas_price_strategy,
+    get_or_deploy_token,
+    wait_for_txs,
+)
+
+log = structlog.get_logger(__name__)
+
+
+TOKEN_BALANCE_MIN = 5 * 10 ** 4
+TIMEOUT = 200
+API_URL_ADDRESS = "{protocol}://{target_host}/api/1/address"
+API_URL_TOKENS = "{protocol}://{target_host}/api/1/tokens"
+API_URL_CHANNELS = "{protocol}://{target_host}/api/1/channels"
+API_URL_TRANSFERS = "{protocol}://{target_host}/api/1/payments/{token_address}/{partner_address}"
+API_URL_CONNECT = "{protocol}://{target_host}/api/1/connection/{token_address}"
+
+
+class ScenarioRunner(object):
+    def __init__(
+        self,
+        account: Account,
+        rpc_url: str,
+        auth: str,
+        scenario_file: Path,
+    ):
+        self.task_count = 0
+        self.running_task_count = 0
+        self.root_task = None
+        self.auth = auth
+
+        self.scenario = yaml.load(scenario_file)
+
+        nodes = self.scenario['nodes']
+        if 'range' in nodes:
+            range_config = nodes['range']
+            template = range_config['template']
+            self.raiden_nodes = [
+                template.format(i)
+                for i in range(range_config['first'], range_config['last'] + 1)
+            ]
+        else:
+            self.raiden_nodes = nodes['list']
+        self.node_commands = nodes.get('commands', {})
+
+        settings = self.scenario.get('settings')
+        if settings is None:
+            settings = {}
+        self.timeout = settings.get('timeout', TIMEOUT)
+        self.protocol = settings.get('protocol', 'http')
+        self.notification_email = settings.get('notify')
+
+        self.client = JSONRPCClient(
+            Web3(HTTPProvider(rpc_url)),
+            privkey=account.privkey,
+            gas_price_strategy=get_gas_price_strategy(settings.get('gas_price')),
+        )
+
+        self.session = Session()
+        # WTF? https://github.com/requests/requests/issues/2605
+        if auth:
+            self.session.auth = tuple(auth.split(":"))
+        self.session.mount('http', TimeOutHTTPAdapter(timeout=self.timeout))
+        self.session.mount('https', TimeOutHTTPAdapter(timeout=self.timeout))
+
+        self._node_to_address = None
+        self.token_address = None
+
+    def run_scenario(self):
+        from scenario_player.tasks import _get_task_class_for_type
+
+        log.info("Fetching node addresses")
+        unreachable_nodes = [node for node, addr in self.node_to_address.items() if not addr]
+        if not self.node_to_address or unreachable_nodes:
+            raise NodesUnreachableError(f"Raiden nodes unreachable: {','.join(unreachable_nodes)}")
+        token_ctr = get_or_deploy_token(self.client, self.scenario)
+        token_address = self.token_address = to_checksum_address(token_ctr.contract_address)
+        first_node = first(self.raiden_nodes)
+
+        mint_tx = []
+        for node, address in self.node_to_address.items():
+            balance = token_ctr.contract.functions.balanceOf(address).call()
+            if balance < TOKEN_BALANCE_MIN:
+                mint_amount = TOKEN_BALANCE_MIN * 2
+                log.debug("Minting tokens for", address=address, node=node, amount=mint_amount)
+                mint_tx.append(token_ctr.transact('mintFor', mint_amount, address))
+        wait_for_txs(self.client, mint_tx)
+
+        registered_tokens = set(
+            self.session.get(
+                API_URL_TOKENS.format(protocol=self.protocol, target_host=first_node),
+            ).json(),
+        )
+        if token_address not in registered_tokens:
+            code, msg = self.register_token(token_address, first_node)
+            if not 199 < code < 300:
+                log.error("Couldn't register token with network", code=code, message=msg)
+                raise TokenRegistrationError(msg)
+
+        scenario_config = self.scenario.get('scenario')
+        if not scenario_config:
+            raise ScenarioError("Invalid scenario definition. Missing 'scenario' key.")
+
+        try:
+            (root_task_type, root_task_config), = scenario_config.items()
+        except ValueError:
+            # will be thrown if it's not a 1-element dict
+            raise ScenarioError(
+                "Invalid scenario definition. "
+                "Exactly one root task is required below the 'scenario' key.",
+            ) from None
+
+        task_class = _get_task_class_for_type(root_task_type)
+        self.root_task = task_class(runner=self, config=root_task_config)
+        self.root_task()
+
+    def get_node_addresses(self, nodes):
+        def cb(node):
+            log.debug("Getting node address", node=node)
+            url = API_URL_ADDRESS.format(protocol=self.protocol, target_host=node)
+            log.debug("Requesting", url=url)
+            try:
+                resp = self.session.get(url)
+            except RequestException:
+                log.error("Error fetching node address", url=url, node=node)
+                return
+            try:
+                return resp.json().get('our_address', '')
+            except ValueError:
+                log.error(
+                    "Error decoding response",
+                    response=resp.text,
+                    code=resp.status_code,
+                    url=url,
+                )
+        ret = self._spawn_and_wait(nodes, cb)
+        return ret
+
+    def register_token(self, token_address, node):
+        try:
+            base_url = API_URL_TOKENS.format(protocol=self.protocol, target_host=node)
+            url = "{}/{}".format(base_url, token_address)
+            log.info("Registering token with network", url=url)
+            resp = self.session.put(url)
+            code = resp.status_code
+            msg = resp.text
+        except RequestException as ex:
+            code = -1
+            msg = str(ex)
+        return code, msg
+
+    @property
+    def node_to_address(self):
+        if not self.raiden_nodes:
+            return {}
+        if self._node_to_address is None:
+            self._node_to_address = {
+                node: address
+                for node, address
+                in self.get_node_addresses(self.raiden_nodes).items()
+            }
+        return self._node_to_address
+
+    def _spawn_and_wait(self, objects, callback):
+        tasks = {obj: gevent.spawn(callback, obj) for obj in objects}
+        gevent.joinall(tasks.values())
+        return {obj: task.get() for obj, task in tasks.items()}
---
+++ tools/scenario-player/scenario_player/utils.py
@@ -0,0 +1,152 @@
+import time
+import uuid
+from collections import deque
+from datetime import datetime
+from itertools import islice
+from typing import Union
+
+import requests
+import structlog
+from eth_utils import to_checksum_address
+from requests.adapters import HTTPAdapter
+from web3.gas_strategies.time_based import fast_gas_price_strategy, medium_gas_price_strategy
+
+from raiden.network.rpc.client import JSONRPCClient, check_address_has_code
+from raiden.network.rpc.smartcontract_proxy import ContractProxy
+from raiden_contracts.constants import CONTRACT_CUSTOM_TOKEN
+from raiden_contracts.contract_manager import CONTRACT_MANAGER
+from scenario_player.exceptions import ScenarioTxError
+
+log = structlog.get_logger(__name__)
+
+
+# Seriously requests? For Humans?
+class TimeOutHTTPAdapter(HTTPAdapter):
+    def __init__(self, *args, **kwargs):
+        self.timeout = kwargs.pop('timeout', None)
+        super().__init__(*args, **kwargs)
+
+    def send(self, *args, **kwargs):
+        if 'timeout' not in kwargs or not kwargs['timeout']:
+            kwargs['timeout'] = self.timeout
+        return super().send(*args, **kwargs)
+
+
+class LogBuffer:
+    def __init__(self, capacity=1000):
+        self.buffer = deque([''], maxlen=capacity)
+
+    def write(self, content):
+        lines = list(content.splitlines())
+        self.buffer[0] += lines[0]
+        if lines == ['']:
+            # Bare newline
+            self.buffer.appendleft('')
+        else:
+            self.buffer.extendleft(lines[1:])
+
+    def getlines(self, start, stop=None):
+        if stop:
+            slice_ = islice(self.buffer, start, stop)
+        else:
+            slice_ = islice(self.buffer, start)
+        return reversed(list(slice_))
+
+
+class DummyStream:
+    def write(self, content):
+        pass
+
+
+def wait_for_txs(client, txhashes, timeout=180):
+    start = time.monotonic()
+    outstanding = False
+    txhashes = txhashes[:]
+    while txhashes and time.monotonic() - start < timeout:
+        remaining_timeout = timeout - (time.monotonic() - start)
+        if outstanding != len(txhashes) or int(remaining_timeout) % 10 == 0:
+            outstanding = len(txhashes)
+            log.debug(
+                "Waiting for tx confirmations",
+                outstanding=outstanding,
+                timeout_remaining=int(remaining_timeout),
+            )
+        for txhash in txhashes[:]:
+            tx = client.web3.eth.getTransaction(txhash)
+            if tx and tx['blockNumber'] is not None:
+                txhashes.remove(txhash)
+            time.sleep(.1)
+        time.sleep(.5)
+    if len(txhashes):
+        raise ScenarioTxError(f"Timeout waiting for txhashes: {', '.join(txhashes)}")
+
+
+def get_or_deploy_token(client: JSONRPCClient, scenario: dict) -> ContractProxy:
+    """ Deploy or reuse  """
+    token_contract = CONTRACT_MANAGER.get_contract(CONTRACT_CUSTOM_TOKEN)
+
+    token_config = scenario.get('token', {})
+    if not token_config:
+        token_config = {}
+    address = token_config.get('address')
+    if address:
+        check_address_has_code(client, address, 'Token')
+        token_ctr = client.new_contract_proxy(token_contract['abi'], address)
+
+        log.debug(
+            "Reusing token",
+            address=to_checksum_address(address),
+            name=token_ctr.contract.functions.name().call(),
+            symbol=token_ctr.contract.functions.symbol().call(),
+        )
+        return token_ctr
+
+    token_id = uuid.uuid4()
+    now = datetime.now()
+    name = token_config.get('name', f"Scenario Test Token {token_id!s} {now:%Y-%m-%dT%H:%M}")
+    symbol = token_config.get('symbol', f"T{token_id!s:.3}")
+
+    log.debug("Deploying token", name=name, symbol=symbol)
+
+    token_ctr = client.deploy_solidity_contract(
+        'CustomToken',
+        CONTRACT_MANAGER._contracts,
+        constructor_parameters=(0, 0, name, symbol),
+        confirmations=1,
+
+    )
+    log.info(
+        "Deployed token",
+        address=to_checksum_address(token_ctr.contract_address),
+        name=name,
+        symbol=symbol,
+    )
+    return token_ctr
+
+
+def send_notification_mail(target_mail, subject, message, api_key):
+    log.debug('Sending notification mail', subject=subject, message=message)
+    res = requests.post(
+        "https://api.mailgun.net/v3/notification.brainbot.com/messages",
+        auth=("api", api_key),
+        data={
+            "from": "Raiden Scenario Player <scenario-player@notification.brainbot.com>",
+            "to": [target_mail],
+            "subject": subject,
+            "text": message,
+        },
+    )
+    log.debug('Notification mail result', code=res.status_code, text=res.text)
+
+
+def get_gas_price_strategy(gas_price: Union[int, str]) -> callable:
+    if isinstance(gas_price, int):
+        def fixed_gas_price(web3, tx):
+            return gas_price
+        return fixed_gas_price
+    elif gas_price == 'fast':
+        return fast_gas_price_strategy
+    elif gas_price == 'medium':
+        return medium_gas_price_strategy
+    else:
+        raise ValueError(f'Invalid gas_price value: "{gas_price}"')
