15e33e04932e6e855f498233be461d78e9a7d51b
Konrad Feldmeier <konrad@brainbot.com>
2017-07-24 20:00:31 +0200

    Add test for echo node
    [skip ci]

--- raiden/tests/utils/__init__.py
+++ raiden/tests/utils/__init__.py
@@ -1,6 +1,19 @@
 from functools import partial, update_wrapper
 import inspect
 
+from raiden.api.python import RaidenAPI
+
+
+def get_channel_events_for_token(app, token_address, start_block=0):
+    """ Collect all events from all channels for a given `token_address` and `app` """
+    result = list()
+    api = RaidenAPI(app.raiden)
+    channels = api.get_channel_list(token_address=token_address)
+    for channel in channels:
+        events = api.get_channel_events(channel.channel_address, start_block)
+        result.extend(events)
+    return result
+
 
 class OwnedNettingChannel(object):
     """User end for NettingChannel-Mock implementations, that will fill in 'our_address'
