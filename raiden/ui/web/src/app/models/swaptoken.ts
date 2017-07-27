fa0787058dc98b1f751b14a2148d8bb36a6938f9
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2017-07-27 19:34:05 -0300

    Swap Tokens functionality!
    Add Balances > Swap Tokens dialog, which allows to atomically and
    directly exchange tokens with a partner.
    It also provides an easy way for exchanging symetric informations (to
    Taker), through an weakly check-summed string, that can be copied and
    sent to Taker, who can then fill all informations only by pasting this
    string in the proper field.

---
+++ raiden/ui/web/src/app/models/swaptoken.ts
@@ -0,0 +1,9 @@
+export interface SwapToken {
+    partner_address: string;
+    identifier: string;
+    role: string;
+    sending_token: string;
+    sending_amount: number;
+    receiving_token: string;
+    receiving_amount: number;
+};
--- raiden/ui/web/src/polyfills.ts
+++ raiden/ui/web/src/polyfills.ts
@@ -71,9 +71,14 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/observable/timer';
+import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/scan';
 import 'rxjs/add/operator/finally';
+import 'rxjs/add/operator/share';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/operator/combineLatest';
