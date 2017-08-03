56ee569ea6b5cb488b156a1ac5fbbe35a8d7aaef
Augusto F. Hack <hack.augusto@gmail.com>
2017-08-03 06:35:41 -0300

    removed secp256k1 comment
    - The current requirement is Coincurve, which does use a global context
    object

--- raiden/utils/__init__.py
+++ raiden/utils/__init__.py
@@ -32,25 +32,6 @@ __all__ = (
 
 LETTERS = string.printable
 
-# From the secp256k1 header file:
-#
-#     The purpose of context structures is to cache large precomputed data tables
-#     that are expensive to construct, and also to maintain the randomization data
-#     for blinding.
-#
-#     Do not create a new context object for each operation, as construction is
-#     far slower than all other API calls (~100 times slower than an ECDSA
-#     verification).
-#
-#     A constructed context can safely be used from multiple threads
-#     simultaneously, but API call that take a non-const pointer to a context
-#     need exclusive access to it. In particular this is the case for
-#     secp256k1_context_destroy and secp256k1_context_randomize.
-#
-#     Regarding randomization, either do it once at creation time (in which case
-#     you do not need any locking for the other calls), or use a read-write lock.
-#
-
 
 def safe_address_decode(address):
     try:
