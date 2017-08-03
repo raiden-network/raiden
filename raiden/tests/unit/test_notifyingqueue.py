00ee23e6bb9b81eea2d1f40f0b05cb65d33e854c
Augusto F. Hack <hack.augusto@gmail.com>
2017-08-03 08:19:44 -0300

    added regression test for queue copy

---
+++ raiden/tests/unit/test_notifyingqueue.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from raiden.utils.notifying_queue import NotifyingQueue
+
+
+def test_copy():
+    queue = NotifyingQueue()
+    assert queue.copy() == []
+
+    queue.put(1)
+    assert queue.copy() == [1]
+    assert queue.peek() == 1, 'copy must preserve the queue'
+
+    queue.put(2)
+    assert queue.copy() == [1, 2], 'copy must preserve the items order'
