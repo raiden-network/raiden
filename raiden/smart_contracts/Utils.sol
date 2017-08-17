47ef2e07db9c9a0626b01db0b2c79f70faf6ac98
Lefteris Karapetsas <lefteris@refu.co>
2017-08-17 16:08:16 +0200

    Fix issue 892
    Basically skip all contracts that don't have code.

---
+++ raiden/smart_contracts/Utils.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.11;
+
+contract Utils {
+    /// @notice Check if a contract exists
+    /// @param channel The address to check whether a contract is deployed or not
+    /// @return True if a contract exists, false otherwise
+    function contractExists(address channel) constant returns (bool) {
+        uint size;
+
+        assembly {
+            size := extcodesize(channel)
+        }
+
+        return size > 0;
+    }
+}
