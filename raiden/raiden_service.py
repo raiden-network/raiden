b4ac5c1bf26c5b2bfd487f8ad96db98f0d96a6f7
Konrad Feldmeier <konrad@brainbot.com>
2017-08-29 12:10:48 +0200

    Register the 'filelock' logger with 'slogging'
    Otherwise its logger stays unconfigured and spams
    
        No handlers could be found for logger "filelock"

--- raiden/raiden_service.py
+++ raiden/raiden_service.py
@@ -92,6 +92,8 @@ from raiden.utils import (
 )
 
 log = slogging.get_logger(__name__)  # pylint: disable=invalid-name
+# register filelock logger
+filelock.logger = slogging.get_logger('filelock')
 
 
 def create_default_identifier():
