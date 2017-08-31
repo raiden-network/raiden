bebd7257a24e7c18b29879e3e0542935f4e71991
Ulrich Petri <ulo@ulo.pe>
2017-08-31 18:14:04 +0200

    Validate README syntax via Travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,5 +2,6 @@
 pip install -U pip wheel coveralls "coverage<4.4"
 pip install pytest-travis-fold
 pip install flake8
+pip install readme_renderer
 pip install -r requirements-dev.txt
 pip install .
