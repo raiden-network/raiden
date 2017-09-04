1f98be0a075dc188c66385158f9a4742389f05da
Augusto F. Hack <hack.augusto@gmail.com>
2017-09-04 12:23:27 -0300

    throttle don't sleep with 0 seconds

--- raiden/network/transport.py
+++ raiden/network/transport.py
@@ -95,7 +95,13 @@ class UDPTransport(object):
             host_port (Tuple[(str, int)]): Tuple with the host name and port number.
             bytes_ (bytes): The bytes that are going to be sent through the wire.
         """
-        gevent.sleep(self.throttle_policy.consume(1))
+        sleep_timeout = self.throttle_policy.consume(1)
+
+        # Don't sleep if timeout is zero, otherwise a context-switch is done
+        # and the message is delayed, increasing it's latency
+        if sleep_timeout:
+            gevent.sleep(sleep_timeout)
+
         self.server.sendto(bytes_, host_port)
 
         # enable debugging using the DummyNetwork callbacks
