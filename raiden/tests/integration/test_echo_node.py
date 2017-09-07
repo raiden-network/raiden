43fa6b68557a8e85fa6f8bcd1f5fe5f4f5d7a11c
Konrad Feldmeier <konrad@brainbot.com>
2017-09-07 16:21:55 +0200

    Add Echo Node lottery

--- raiden/tests/integration/test_echo_node.py
+++ raiden/tests/integration/test_echo_node.py
@@ -96,8 +96,6 @@ def test_echo_node_response(
 
         for event in events:
             if event['_event_type'] == 'EventTransferReceivedSuccess':
-                # FIXME: This is a bit iffy. The *exact same* event seems to appear in multiple
-                #        channels. Needs more investigation.
                 received[repr(event)] = event
 
         assert len(received) == 1
@@ -106,5 +104,94 @@ def test_echo_node_response(
         assert transfer['identifier'] == (
             handled_transfer['identifier'] + transfer['amount']
         )
+
+    echo_node.stop()
+
+
+# `RaidenAPI.get_channel_events` is not supported in tester
+@pytest.mark.parametrize('blockchain_type', ['geth'])
+@pytest.mark.parametrize('number_of_nodes', [8])
+@pytest.mark.parametrize('number_of_tokens', [1])
+@pytest.mark.parametrize('channels_per_node', [CHAIN])
+@pytest.mark.parametrize('reveal_timeout', [20])
+@pytest.mark.parametrize('settle_timeout', [120])
+def test_echo_node_lottery(
+    token_addresses,
+    raiden_chain,
+):
+    app0, app1, app2, app3, echo_app, app4, app5, app6 = raiden_chain
+    address_to_app = {app.raiden.address: app for app in raiden_chain}
+    token_address = token_addresses[0]
+    echo_api = RaidenAPI(echo_app.raiden)
+
+    echo_node = EchoNode(echo_api, token_address)
+    echo_node.ready.wait(timeout=30)
+    assert echo_node.ready.is_set()
+
+    expected = list()
+
+    # Let 6 participants enter the pool
+    amount = 7
+    for num, app in enumerate([app0, app1, app2, app3, app4, app5]):
+        transfer_event = RaidenAPI(app.raiden).transfer_async(
+            token_address,
+            amount,
+            echo_app.raiden.address,
+            10 ** (num + 1)
+        )
+        transfer_event.wait(timeout=20)
+        expected.append(amount)
+
+    # test duplicated identifier + amount is ignored
+    transfer_event = RaidenAPI(app5.raiden).transfer_async(
+        token_address,
+        amount,
+        echo_app.raiden.address,
+        10 ** 6
+    ).wait(timeout=20)
+    # test pool size querying
+    pool_query_identifier = 77
+    transfer_event = RaidenAPI(app5.raiden).transfer_async(
+        token_address,
+        amount,
+        echo_app.raiden.address,
+        pool_query_identifier
+    ).wait(timeout=20)
+    expected.append(amount)
+
+    # fill the pool
+    transfer_event = RaidenAPI(app6.raiden).transfer_async(
+        token_address,
+        amount,
+        echo_app.raiden.address,
+        10 ** 7
+    ).wait(timeout=20)
+    expected.append(amount)
+
+    while len(echo_node.handled_transfers) < len(expected):
+        gevent.sleep(.5)
+
+    received = {}
+    # Check that payout was generated and pool_size_query answered
+    for handled_transfer in echo_node.handled_transfers:
+        app = address_to_app[handled_transfer['initiator']]
+        events = get_channel_events_for_token(app, token_address, 0)
+
+        for event in events:
+            if event['_event_type'] == 'EventTransferReceivedSuccess':
+                received[repr(event)] = event
+
+    assert len(received) == 2
+
+    received = sorted(received.values(), key=lambda transfer: transfer['amount'])
+
+    pool_query = received[0]
+    assert pool_query['amount'] == 6
+    assert pool_query['identifier'] == pool_query_identifier + 6
+
+    winning_transfer = received[1]
+    assert winning_transfer['initiator'] == echo_app.raiden.address
+    assert winning_transfer['amount'] == 49
+    assert (winning_transfer['identifier'] - 49) % 10 == 0
 
     echo_node.stop()
