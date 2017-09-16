09ee8bc6f8f89a32547b795f3e2eece1a4be85b4
Ulrich Petri <ulo@ulo.pe>
2017-09-16 17:54:02 +0200

    Add user selectable nat traversal
    Fixes: #870

--- raiden/network/sockfactory.py
+++ raiden/network/sockfactory.py
@@ -1,7 +1,9 @@
-from contextlib import contextmanager
+import socket
 
 from ethereum import slogging
+import netifaces
 
+from raiden.exceptions import STUNUnavailableException
 from raiden.network import upnpsock, stunsock
 
 log = slogging.getLogger(__name__)
@@ -26,43 +28,151 @@ class PortMappedSocket(object):
         )
 
 
-@contextmanager
-def socket_factory(source_ip, source_port, *args, **kwargs):
-    """
-    Create a port mapped socket via uPnP or STUN.
-    Args:
-        source_ip (ip string): the network interface/ip to bind
-        source_port (int): the local port to bind
-        *args: generic args that are passed to the below implementations
-        **kargs: generic kwargs that are passed to the below implementations
-    Return:
-        PortMappedSocket
-    """
-    # prefer uPnP over STUN
-    log.info('trying to find uPnP port mapper...')
-    upnp = upnpsock.connect()
-    if upnp is not None:
-        router, location = upnp
-        result = upnpsock.open_port(
-            router,
-            source_port,
-        )
-        if result is not None:
-            with stunsock.open_bare_socket(source_ip, source_port, *args, **kwargs) as sock:
-                try:
-                    yield PortMappedSocket(sock, 'uPnP', result[0], result[1], **dict(
-                        router_location=location
-                    ))
-                finally:
-                    upnpsock.release_port(router, source_port)
+class SocketFactory(object):
+    STRATEGY_TO_METHODS = {
+        'auto': ['upnp', 'stun', 'none'],
+        'upnp': ['upnp', 'none'],
+        'stun': ['stun', 'none'],
+        'none': ['none'],
+        'ext': ['ext'],
+    }
 
-    else:
-        log.info('uPnP not available, trying STUN for port mapping...')
-        with stunsock.stun_socket(source_ip, source_port, *args, **kwargs) as (
-            sock,
-            external_ip,
-            external_port,
-            nat
-        ):
+    def __init__(self, source_ip, source_port, strategy='auto', **kwargs):
+        """
+        Create a port mapped socket via selectable strategy.
+        Args:
+            source_ip (ip string): the network interface/ip to bind
+            source_port (int): the local port to bind
+            strategy (str, tuple): Strategy to use to traverse NAT (auto, upnp, stun, none,
+                                   (ip, port))
+            **kwargs: generic kwargs that are passed to the underlying implementations
+
+        The traversal methods are implemented in the `map_<method>` and `unmap_<method>` methods.
+        """
+        self.source_ip = source_ip
+        self.source_port = source_port
+        self.method_args = None
+        if isinstance(strategy, tuple):
+            if strategy[1] is None:
+                # If no external port was provided use local one
+                strategy = strategy[0], source_port
+            self.strategy_args = strategy
+            strategy = 'ext'
+        self.strategy = strategy
+        self.kwargs = kwargs
+        self.method = None
+        self.socket = None
+        self.storage = {}
+
+    def __enter__(self):
+        log.debug("Acquiring socket", strategy=self.strategy_description)
+
+        self._open_socket()
+
+        for method in self.STRATEGY_TO_METHODS[self.strategy]:
+            log.debug("Trying", method=method)
+
+            mapped_socket = getattr(self, 'map_{}'.format(method))()
+            if mapped_socket:
+                assert isinstance(mapped_socket, PortMappedSocket)
+                self.method = method
+                log.debug("Success", method=method)
+                break
+            log.debug("Unavailable", method=method)
+        else:
+            # This should not happen unless the method mapping has been broken (e.g. by
+            # removing the 'none' fallback)
+            raise RuntimeError("Couldn't create PortMappedSocket!")
+
+        if method == 'ext':
+            method = "ext:{}".format(
+                ":".join(str(s) for s in self.strategy_args)
+            )
+        log.info(
+            'Network port opened',
+            method=method,
+            internal='{}:{}'.format(self.source_ip, self.source_port),
+            external='{}:{}'.format(mapped_socket.external_ip, mapped_socket.external_port),
+        )
+        return mapped_socket
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.method:
+            try:
+                getattr(self, 'unmap_{}'.format(self.method))()
+            finally:
+                if self.socket:
+                    self.socket.close()
+
+    def map_upnp(self):
+        upnp = upnpsock.connect()
+        if upnp is not None:
+            router, location = upnp
+            result = upnpsock.open_port(
+                router,
+                self.source_port,
+            )
+            if result is not None:
+                self.storage['router'] = router
+                return PortMappedSocket(self.socket, 'UPnP', result[0], result[1],
+                                        router_location=location)
+
+    def unmap_upnp(self):
+        upnpsock.release_port(self.storage['router'], self.source_port)
+
+    def map_stun(self):
+        try:
+            external_ip, external_port, nat = stunsock.stun_socket(
+                self.socket,
+                self.source_ip,
+                self.source_port,
+                **self.kwargs
+            )
             if external_port is not None:
-                yield PortMappedSocket(sock, 'STUN', external_ip, external_port, **nat)
+                return PortMappedSocket(self.socket, 'STUN', external_ip, external_port, **nat)
+        except STUNUnavailableException:
+            pass
+
+    def unmap_stun(self):
+        pass
+
+    def map_none(self):
+        if self.source_ip == '0.0.0.0':
+            try:
+                default_gw_if = netifaces.gateways()['default'][netifaces.AF_INET][1]
+                self.source_ip = netifaces.ifaddresses(default_gw_if)[netifaces.AF_INET][0]['addr']
+            except (OSError, IndexError, KeyError):
+                log.critical("Couldn't get interface address. "
+                             "Try specifying with '--nat ext:<ip>'.")
+                raise
+        log.warning("Using internal interface address. Connectivity issues are likely.")
+        return PortMappedSocket(self.socket, 'NONE', self.source_ip, self.source_port)
+
+    def unmap_none(self):
+        pass
+
+    def map_ext(self):
+        return PortMappedSocket(self.socket, 'EXT', self.strategy_args[0], self.strategy_args[1])
+
+    def unmap_ext(self):
+        pass
+
+    def _open_socket(self):
+        sock = socket.socket(
+            socket.AF_INET,  # Internet
+            socket.SOCK_DGRAM  # UDP
+        )
+
+        sock.bind((self.source_ip, self.source_port))
+        log.debug(
+            'Socket opened',
+            ip=sock.getsockname()[0],
+            port=sock.getsockname()[1],
+        )
+        self.socket = sock
+
+    @property
+    def strategy_description(self):
+        if self.strategy != 'ext':
+            return self.strategy
+        return '{}:{}'.format(self.strategy, ":".join(str(s) for s in self.strategy_args))
