0b9362b474246b1e3a8bb1db25aaea91a160bca8
Ulrich Petri <ulo@ulo.pe>
2017-09-21 16:12:45 +0200

    Enable macOS packaging on travis
    This doesn't include running tests yet as travis macOS is too slow.

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
-"./.travis/download_geth.sh"
-"./.travis/download_solc.sh"
+
+set -e
+set -x
+
+.travis/prepare_os_${TRAVIS_OS_NAME}.sh
+
+.travis/download_solc.sh
+.travis/download_geth.sh
--- .travis/install.sh
+++ .travis/install.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env sh
-pip install -U pip wheel coveralls "coverage<4.4"
-pip install pytest-travis-fold
-pip install flake8
-pip install readme_renderer
-pip install -r requirements-dev.txt
-pip install .
+
+set -e
+set -x
+
+INSTALL_OPT=""
+if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+    # install into user dir on macos to avoid sudo
+    INSTALL_OPT="--user"
+fi
+
+pip install ${INSTALL_OPT} -U pip wheel coveralls "coverage<4.4"
+pip install ${INSTALL_OPT} pytest-travis-fold
+pip install ${INSTALL_OPT} flake8
+pip install ${INSTALL_OPT} readme_renderer
+pip install ${INSTALL_OPT} pyinstaller
+pip install ${INSTALL_OPT} -r requirements-dev.txt
+pip install ${INSTALL_OPT} -e .
