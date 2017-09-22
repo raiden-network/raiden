d66d10ed9502044c61256c83c85c2b79219bf0b9
Ulrich Petri <ulo@ulo.pe>
2017-09-22 19:06:32 +0200

    tractor

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env sh
 
+.travis/prepare_os_${TRAVIS_OS_NAME}.sh
 .travis/download_geth.sh
 .travis/download_solc_${TRAVIS_OS_NAME}.sh
-
-if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-    for tool in automake libtool pkg-config libffi gmp openssl node ; do
-        travis_wait brew install ${tool} || travis_wait brew upgrade ${tool}
-    done
-
-    curl -O https://bootstrap.pypa.io/get-pip.py
-    python get-pip.py --user
-fi
--- .travis/download_solc_osx.sh
+++ .travis/download_solc_osx.sh
@@ -39,6 +39,45 @@ warn() {
     fi
 }
 
+travis_wait() {
+  local cmd="$@"
+  local log_file=travis_wait_$$.log
+
+  $cmd 2>&1 >$log_file &
+  local cmd_pid=$!
+
+  travis_jigger $! $cmd &
+  local jigger_pid=$!
+  local result
+
+  {
+    wait $cmd_pid 2>/dev/null
+    result=$?
+    ps -p$jigger_pid 2>&1>/dev/null && kill $jigger_pid
+  } || exit 1
+
+  exit $result
+}
+
+travis_jigger() {
+  # helper method for travis_wait()
+  local timeout=20 # in minutes
+  local count=0
+
+  local cmd_pid=$1
+  shift
+
+  while [ $count -lt $timeout ]; do
+    count=$(($count + 1))
+    echo -ne "Still running ($count of $timeout): $@\r"
+    sleep 60
+  done
+
+  echo -e "\n\033[31;1mTimeout reached. Terminating $@\033[0m\n"
+  kill -9 $cmd_pid
+}
+
+
 [ -z "${SOLC_URL_MACOS}" ] && fail 'missing SOLC_URL_MACOS'
 [ -z "${SOLC_VERSION}" ] && fail 'missing SOLC_VERSION'
 
