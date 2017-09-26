316a5041658fbedd6cd63bb13cbca48907dcf014
Ulrich Petri <ulo@ulo.pe>
2017-09-26 05:21:25 +0200

    table

--- .travis/prepare_os_osx.sh
+++ .travis/prepare_os_osx.sh
@@ -3,9 +3,22 @@
 set -e
 set -x
 
-for tool in automake libtool pkg-config libffi gmp openssl node ; do
+for tool in automake libtool pkg-config libffi gmp openssl ; do
     brew install ${tool} || brew upgrade ${tool}
 done
+
+if [ ! -x $HOME/.bin/geth-${GETH_VERSION}-${TRAVIS_OS_NAME} ]; then
+    mkdir -p $HOME/.bin
+
+    TEMP=$(mktemp -d)
+    cd $TEMP
+    curl -L https://nodejs.org/dist/v6.11.3/node-v6.11.3-darwin-x64.tar.gz > node.tgz
+    tar xzf node.tgz
+
+    cd node*
+    install -m 755 bin/npm $HOME/.bin/npm
+    install -m 755 bin/node $HOME/.bin/node
+fi
 
 curl -O https://bootstrap.pypa.io/get-pip.py
 python get-pip.py --user
