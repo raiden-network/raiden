c6bb70916254e4564a98971c03e9b77f7e01c6be
Augusto F. Hack <hack.augusto@gmail.com>
2017-10-02 16:27:14 -0300

    moved utils from pyethapp to raiden

--- raiden/ui/console.py
+++ raiden/ui/console.py
@@ -2,7 +2,11 @@
 from __future__ import print_function
 
 import cStringIO
+import errno
 import json
+import os
+import select
+import signal
 import sys
 import time
 from logging import StreamHandler, Formatter
@@ -14,34 +18,157 @@ import gevent
 from gevent.event import Event
 from gevent import Greenlet
 import IPython
-from IPython.lib.inputhook import inputhook_manager
-from pyethapp.console_service import GeventInputHook, SigINTHandler
-from pyethapp.utils import bcolors as bc
+from IPython.lib.inputhook import inputhook_manager, stdin_ready
 
 from raiden.api.python import RaidenAPI
 from raiden.utils import events, get_contract_path, safe_address_decode
 from raiden.settings import GAS_PRICE
 
+ENTER_CONSOLE_TIMEOUT = 3
+GUI_GEVENT = 'gevent'
+
+# ansi escape code for typesetting
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[91m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
 # ipython needs to accept "--gui gevent" option
 IPython.core.shellapp.InteractiveShellApp.gui.values += ('gevent',)
-inputhook_manager.register('gevent')(GeventInputHook)
 
 
 def print_usage():
     print("\t{}use `{}raiden{}` to interact with the raiden service.".format(
-        bc.OKBLUE, bc.HEADER, bc.OKBLUE))
-    print("\tuse `{}chain{}` to interact with the blockchain.".format(bc.HEADER, bc.OKBLUE))
-    print("\tuse `{}discovery{}` to find raiden nodes.".format(bc.HEADER, bc.OKBLUE))
+        OKBLUE, HEADER, OKBLUE))
+    print("\tuse `{}chain{}` to interact with the blockchain.".format(HEADER, OKBLUE))
+    print("\tuse `{}discovery{}` to find raiden nodes.".format(HEADER, OKBLUE))
     print("\tuse `{}tools{}` for convenience with tokens, channels, funding, ...".format(
-        bc.HEADER, bc.OKBLUE))
-    print("\tuse `{}denoms{}` for ether calculations".format(bc.HEADER, bc.OKBLUE))
+        HEADER, OKBLUE))
+    print("\tuse `{}denoms{}` for ether calculations".format(HEADER, OKBLUE))
     print("\tuse `{}lastlog(n){}` to see n lines of log-output. [default 10] ".format(
-        bc.HEADER, bc.OKBLUE))
+        HEADER, OKBLUE))
     print("\tuse `{}lasterr(n){}` to see n lines of stderr. [default 1]".format(
-        bc.HEADER, bc.OKBLUE))
-    print("\tuse `{}help(<topic>){}` for help on a specific topic.".format(bc.HEADER, bc.OKBLUE))
-    print("\ttype `{}usage(){}` to see this help again.".format(bc.HEADER, bc.OKBLUE))
-    print("\n" + bc.ENDC)
+        HEADER, OKBLUE))
+    print("\tuse `{}help(<topic>){}` for help on a specific topic.".format(HEADER, OKBLUE))
+    print("\ttype `{}usage(){}` to see this help again.".format(HEADER, OKBLUE))
+    print("\n" + ENDC)
+
+
+def inputhook_gevent():
+    while not stdin_ready():
+        gevent.sleep(0.05)
+    return 0
+
+
+@inputhook_manager.register('gevent')
+class GeventInputHook(object):
+
+    def __init__(self, manager):
+        self.manager = manager
+        self._current_gui = GUI_GEVENT
+
+    def enable(self, app=None):
+        """Enable event loop integration with gevent.
+        Parameters
+        ----------
+        app : ignored
+            Ignored, it's only a placeholder to keep the call signature of all
+            gui activation methods consistent, which simplifies the logic of
+            supporting magics.
+        Notes
+        -----
+        This methods sets the PyOS_InputHook for gevent, which allows
+        gevent greenlets to run in the background while interactively using
+        IPython.
+        """
+        self.manager.set_inputhook(inputhook_gevent)
+        self._current_gui = GUI_GEVENT
+        return app
+
+    def disable(self):
+        """Disable event loop integration with gevent.
+        This merely sets PyOS_InputHook to NULL.
+        """
+        self.manager.clear_inputhook()
+
+
+class SigINTHandler(object):
+
+    def __init__(self, event):
+        self.event = event
+        self.installed = None
+        self.installed_force = None
+        self.install_handler()
+
+    def install_handler(self):
+        if self.installed_force:
+            self.installed_force.cancel()
+            self.installed_force = None
+        self.installed = gevent.signal(signal.SIGINT, self.handle_int)
+
+    def install_handler_force(self):
+        if self.installed:
+            self.installed.cancel()
+            self.installed = None
+        self.installed_force = gevent.signal(signal.SIGINT, self.handle_force)
+
+    def handle_int(self):
+        self.install_handler_force()
+
+        gevent.spawn(self._confirm_enter_console)
+
+    def handle_force(self):  # pylint: disable=no-self-use
+        """
+        User pressed ^C a second time. Send SIGTERM to ourself.
+        """
+        os.kill(os.getpid(), signal.SIGTERM)
+
+    def _confirm_enter_console(self):
+        start = time.time()
+        sys.stdout.write("\n")
+        enter_console = False
+        while time.time() - start < ENTER_CONSOLE_TIMEOUT:
+            prompt = (
+                "\r{}{}Hit [ENTER], to launch console; [Ctrl+C] again to quit! [{:1.0f}s]{}"
+            ).format(
+                OKGREEN,
+                BOLD,
+                ENTER_CONSOLE_TIMEOUT - (time.time() - start),
+                ENDC
+            )
+
+            sys.stdout.write(prompt)
+            sys.stdout.flush()
+
+            try:
+                r, _, _ = select.select([sys.stdin], [], [], .5)
+            except select.error as ex:
+                sys.stdout.write("\n")
+                # "Interrupted sytem call" means the user pressed ^C again
+                if ex.args[0] == errno.EINTR:
+                    self.handle_force()
+                    return
+                else:
+                    raise
+            if r:
+                sys.stdin.readline()
+                enter_console = True
+                break
+        if enter_console:
+            sys.stdout.write("\n")
+            self.installed_force.cancel()
+            self.event.set()
+        else:
+            sys.stdout.write(
+                "\n{}{}No answer after {}s. Resuming.{}\n".format(
+                    WARNING, BOLD, ENTER_CONSOLE_TIMEOUT, ENDC))
+            sys.stdout.flush()
+            # Restore regular handler
+            self.install_handler()
 
 
 class BaseService(Greenlet):
@@ -103,8 +230,8 @@ class Console(BaseService):
     def _run(self):  # pylint: disable=method-hidden
         self.interrupt.wait()
         print('\n' * 2)
-        print("Entering Console" + bc.OKGREEN)
-        print("Tip:" + bc.OKBLUE)
+        print("Entering Console" + OKGREEN)
+        print("Tip:" + OKBLUE)
         print_usage()
 
         # Remove handlers that log to stderr
---
+++ raiden/utils/debug.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import os
+import signal
+
+import gevent
+from IPython.core import ultratb
+
+
+def enable_greenlet_debugger():
+    def _print_exception(self, context, type_, value, traceback):
+        ultratb.VerboseTB(call_pdb=True)(type_, value, traceback)
+        resp = raw_input('Debugger exited. Do you want to quit raiden? [Y/n] ').strip().lower()
+
+        if not resp or resp.startswith('y'):
+            os.kill(os.getpid(), signal.SIGTERM)
+
+    gevent.get_hub().__class__.print_exception = _print_exception
