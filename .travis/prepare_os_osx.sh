81352eabef8f896407636add143f5fcc4056f13e
Paul Lange <palango@gmx.de>
2017-10-26 18:11:41 +0200

    link docker binary

--- .travis/prepare_os_osx.sh
+++ .travis/prepare_os_osx.sh
@@ -7,9 +7,15 @@ for tool in automake libtool pkg-config libffi gmp openssl ; do
     brew install ${tool} || brew upgrade ${tool}
 done
 
+mkdir -p $HOME/.bin
+
 brew tap caskroom/cask
 brew cask install docker
-ln -s /Applications/Docker.app/Contents/Resources/bin /usr/local/bin/docker
+ln -s /Applications/Docker.app/Contents/Resources/bin/docker /usr/local/bin/docker
+ln -s /Applications/Docker.app/Contents/Resources/bin/docker $HOME/.bin/docker
+echo $PATH
+
+which docker
 docker run hellow-world
 
 if [ ! -x $HOME/.bin/geth-${GETH_VERSION}-${TRAVIS_OS_NAME} ]; then
