5e7bdf6e45d421ee9f25564c7b68becb208f0ec8
Paul Lange <palango@gmx.de>
2017-11-21 17:50:52 +0100

    Fix some solc warnings

--- raiden/tests/integration/rpc/RpcTest.sol
+++ raiden/tests/integration/rpc/RpcTest.sol
@@ -1,15 +1,15 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.16;
 
 contract RpcTest {
-    function fail() {
+    function fail() pure public {
         require(false);
     }
 
-    function ret() returns (uint) {
+    function ret() pure public returns (uint) {
         return 1;
     }
 
-    function loop(uint reps) returns (uint) {
+    function loop(uint reps) pure public returns (uint) {
         uint result = 0;
         for (uint i=0; i<reps; i++) {
             result *= i;
