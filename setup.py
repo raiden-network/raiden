25eeb802efe36ccae3801576d65864f81907827c
Paul Lange <palango@gmx.de>
2017-12-18 14:16:06 +0100

    Update 'setup.py' classifiers, set py3 in travis

--- setup.py
+++ setup.py
@@ -176,8 +176,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
     cmdclass={
         'test': PyTest,
