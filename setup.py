8c49717634e8e4303b10275c906f5fd446c374d4
Paul Lange <palango@gmx.de>
2018-01-15 18:18:09 +0100

    Support and test python 3.5

--- setup.py
+++ setup.py
@@ -170,6 +170,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     cmdclass={
