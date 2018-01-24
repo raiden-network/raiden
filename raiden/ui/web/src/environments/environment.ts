6fd574cbaf8c755ef89e7d6014d4cfeae322843a
André Vitor de Lima Matos <andre@brainbot.com>
2018-01-24 18:49:20 -0200

    Lint fixes according to updated angular-cli project

--- raiden/ui/web/src/environments/environment.ts
+++ raiden/ui/web/src/environments/environment.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  configFile: 'assets/config/config.development.json'
+    production: false,
+    configFile: 'assets/config/config.development.json'
 };
