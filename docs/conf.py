58d99333f16d140ae38b8fa4cad9b0001e9b710b
Lefteris Karapetsas <lefteris@refu.co>
2018-01-25 14:16:54 +0100

    Some doc fixes

--- docs/conf.py
+++ docs/conf.py
@@ -194,7 +194,7 @@ html_theme_options = {'logo_only': True, 'display_version': False}
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = 'raiden.svg'
+html_logo = 'raiden.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -319,7 +319,7 @@ latex_documents = [
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
 #
-latex_logo = 'raiden.svg'
+latex_logo = 'raiden.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
