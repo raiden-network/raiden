20075a18e67a5b47feffb7f4006393d458b6a4db
Lefteris Karapetsas <lefteris@refu.co>
2018-01-25 12:40:50 +0100

    Add a logo to the docs
    [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -173,7 +173,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {'logo_only': True, 'display_version': False}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -190,7 +190,7 @@ html_theme = 'sphinx_rtd_theme'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-# html_logo = None
+html_logo = 'raiden.svg'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -315,7 +315,7 @@ latex_documents = [
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
 #
-# latex_logo = None
+latex_logo = 'raiden.svg'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
