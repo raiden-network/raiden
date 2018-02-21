e254dad00dc52772b4913ddf209b24a7f2fa314b
Augusto F. Hack <hack.augusto@gmail.com>
2018-02-21 10:26:41 -0300

    Moved SQLLite storage to storage module
    - Removed the ABC dependency
    - Using python's 3.1 multiple context expressions for the write lock and
    the transaction management.
    - Removed `get_state_change_by_id` and added
    `get_statechanges_by_identifier`, which works on ranges.
    - Added `get_events_by_identifier`
    - Renamed `get_events_in_range` to `get_events_by_block`

---
+++ raiden/storage/sqlite.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+import threading
+
+
+def sqlite_sanity_check(conn):
+    """ Ensures that NUL character can be safely inserted and recovered
+    from the database.
+
+    http://bugs.python.org/issue13676
+    """
+
+    data = '\x00a'
+    conn.execute(
+        'INSERT INTO state_changes (identifier, data) VALUES (null, ?)',
+        (data, ),
+    )
+
+    result = next(conn.execute('SELECT data FROM state_changes ORDER BY identifier DESC'))
+
+    if result[0] != data:
+        raise RuntimeError(
+            'Database cannot save NUL character, ensure python is at least 2.7.3'
+        )
+
+    conn.rollback()
+
+
+class SQLiteStorage:
+    def __init__(self, database_path, serializer):
+        conn = sqlite3.connect(database_path)
+        conn.text_factory = str
+        conn.execute('PRAGMA foreign_keys=ON')
+
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                'CREATE TABLE IF NOT EXISTS state_changes ('
+                '    identifier INTEGER PRIMARY KEY AUTOINCREMENT, '
+                '    data BINARY'
+                ')'
+            )
+            cursor.execute(
+                'CREATE TABLE IF NOT EXISTS state_snapshot ('
+                '    identifier INTEGER PRIMARY KEY, '
+                '    statechange_id INTEGER, '
+                '    data BINARY, '
+                '    FOREIGN KEY(statechange_id) REFERENCES state_changes(identifier)'
+                ')'
+            )
+            cursor.execute(
+                'CREATE TABLE IF NOT EXISTS state_events ('
+                '    identifier INTEGER PRIMARY KEY, '
+                '    source_statechange_id INTEGER NOT NULL, '
+                '    block_number INTEGER NOT NULL, '
+                '    data BINARY, '
+                '    FOREIGN KEY(source_statechange_id) REFERENCES state_changes(identifier)'
+                ')'
+            )
+
+        sqlite_sanity_check(conn)
+
+        # When writting to a table where the primary key is the identifier and we want
+        # to return said identifier we use cursor.lastrowid, which uses sqlite's last_insert_rowid
+        # https://github.com/python/cpython/blob/2.7/Modules/_sqlite/cursor.c#L727-L732
+        #
+        # According to the documentation (http://www.sqlite.org/c3ref/last_insert_rowid.html)
+        # if a different thread tries to use the same connection to write into the table
+        # while we query the last_insert_rowid, the result is unpredictable. For that reason
+        # we have this write lock here.
+        #
+        # TODO (If possible):
+        # Improve on this and find a better way to protect against this potential race
+        # condition.
+        self.write_lock = threading.Lock()
+        self.conn = conn
+        self.serializer = serializer
+
+    def write_state_change(self, state_change):
+        serialized_data = self.serializer.serialize(state_change)
+
+        with self.write_lock, self.conn:
+            cursor = self.conn.execute(
+                'INSERT INTO state_changes(identifier, data) VALUES(null, ?)',
+                (serialized_data,)
+            )
+            last_id = cursor.lastrowid
+
+        return last_id
+
+    def write_state_snapshot(self, statechange_id, snapshot):
+        # TODO: Snapshotting is not yet implemented. This is just skeleton code
+        # Issue: https://github.com/raiden-network/raiden/issues/593
+        # This skeleton code assumes we only keep a single snapshot and overwrite it each time.
+        serialized_data = self.serializer.serialize(snapshot)
+
+        with self.write_lock, self.conn:
+            cursor = self.conn.execute(
+                'INSERT OR REPLACE INTO state_snapshot('
+                '    identifier, statechange_id, data'
+                ') VALUES(?, ?, ?)',
+                (1, statechange_id, serialized_data)
+            )
+            last_id = cursor.lastrowid
+
+        return last_id
+
+    def write_events(self, state_change_id, block_number, events):
+        """ Save events.
+
+        Args:
+            events_data (List[Tuple]): Must be a list of tuples of the form:
+
+                (None, statechange_id, block_number, serialized_event_data)
+        """
+        events_data = [
+            (None, state_change_id, block_number, self.serializer.serialize(event))
+            for event in events
+        ]
+
+        with self.write_lock, self.conn:
+            self.conn.executemany(
+                'INSERT INTO state_events('
+                '   identifier, source_statechange_id, block_number, data'
+                ') VALUES(?, ?, ?, ?)',
+                events_data,
+            )
+
+    def get_state_snapshot(self):
+        """ Return the last state snapshot as a tuple of (state_change_id, data) """
+        cursor = self.conn.execute('SELECT * from state_snapshot')
+        serialized = cursor.fetchall()
+
+        result = None
+        if serialized:
+            assert len(serialized) == 1
+            result = self.serializer.deserialize(serialized[0][2])
+
+        return result
+
+    def get_statechanges_by_identifier(self, from_identifier, to_identifier):
+        if not (from_identifier == 'latest' or isinstance(from_identifier, int)):
+            raise ValueError("from_identifier must be an integer or 'latest'")
+
+        if not (to_identifier == 'latest' or isinstance(to_identifier, int)):
+            raise ValueError("to_identifier must be an integer or 'latest'")
+
+        cursor = self.conn.cursor()
+
+        if from_identifier == 'latest':
+            assert to_identifier is None
+
+            cursor.execute(
+                'SELECT identifier FROM state_changes ORDER BY identifier DESC LIMIT 1',
+            )
+            from_identifier = cursor.fetchone()
+
+        if to_identifier == 'latest':
+            cursor.execute(
+                'SELECT data FROM state_changes WHERE identifier >= ?',
+                (from_identifier,)
+            )
+        else:
+            cursor.execute(
+                'SELECT data FROM state_changes WHERE identifier '
+                'BETWEEN ? AND ?', (from_identifier, to_identifier)
+            )
+
+        result = [
+            self.serializer.deserialize(entry[0])
+            for entry in cursor.fetchall()
+        ]
+        return result
+
+    def get_events_by_identifier(self, from_identifier, to_identifier):
+        if not (from_identifier == 'latest' or isinstance(from_identifier, int)):
+            raise ValueError("from_identifier must be an integer or 'latest'")
+
+        if not (to_identifier == 'latest' or isinstance(to_identifier, int)):
+            raise ValueError("to_identifier must be an integer or 'latest'")
+
+        cursor = self.conn.cursor()
+
+        if from_identifier == 'latest':
+            assert to_identifier is None
+
+            cursor.execute(
+                'SELECT identifier FROM state_events ORDER BY identifier DESC LIMIT 1',
+            )
+            from_identifier = cursor.fetchone()
+
+        if to_identifier == 'latest':
+            cursor.execute(
+                'SELECT block_number, data FROM state_events WHERE identifier >= ?',
+                (from_identifier,)
+            )
+        else:
+            cursor.execute(
+                'SELECT block_number, data FROM state_events WHERE identifier '
+                'BETWEEN ? AND ?', (from_identifier, to_identifier)
+            )
+
+        result = [
+            (entry[0], self.serializer.deserialize(entry[1]))
+            for entry in cursor.fetchall()
+        ]
+        return result
+
+    def get_events_by_block(self, from_block, to_block):
+        if not (from_block == 'latest' or isinstance(from_block, int)):
+            raise ValueError("from_block must be an integer or 'latest'")
+
+        if not (to_block == 'latest' or isinstance(to_block, int)):
+            raise ValueError("to_block must be an integer or 'latest'")
+
+        cursor = self.conn.cursor()
+
+        if from_block is None:
+            from_block = 0
+
+        if from_block == 'latest':
+            assert to_block is None
+
+            cursor.execute(
+                'SELECT block_number FROM state_events ORDER BY block_number DESC LIMIT 1'
+            )
+            from_block = cursor.fetchone()
+
+        if to_block == 'latest':
+            cursor.execute(
+                'SELECT block_number, data FROM state_events WHERE block_number >= ?',
+                (from_block, )
+            )
+        else:
+            cursor.execute(
+                'SELECT block_number, data FROM state_events WHERE block_number '
+                'BETWEEN ? AND ?', (from_block, to_block)
+            )
+
+        result = [
+            (entry[0], self.serializer.deserialize(entry[1]))
+            for entry in cursor.fetchall()
+        ]
+        return result
+
+    def __del__(self):
+        self.conn.close()
