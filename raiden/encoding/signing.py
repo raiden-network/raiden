8e6b16a44ea8d05188a14e70060ad4489005838f
Augusto F. Hack <hack.augusto@gmail.com>
2018-02-21 11:36:06 -0300

    factored balance proof packing into a function

--- raiden/encoding/signing.py
+++ raiden/encoding/signing.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from coincurve import PublicKey
+from ethereum.slogging import getLogger
+
 from raiden.utils import sha3
+
+log = getLogger(__name__)  # pylint: disable=invalid-name
 
 
 def recover_publickey(messagedata, signature):
@@ -14,6 +18,24 @@ def recover_publickey(messagedata, signature):
         hasher=sha3,
     )
     return publickey.format(compressed=False)
+
+
+def recover_publickey_safe(messagedata, signature):
+    publickey = None
+
+    try:
+        publickey = recover_publickey(messagedata, signature)
+    except ValueError:
+        # raised if the signature has the wrong length
+        log.error('invalid signature')
+    except TypeError as e:
+        # raised if the PublicKey instantiation failed
+        log.error('invalid key data: {}'.format(e.message))
+    except Exception as e:  # pylint: disable=broad-except
+        # secp256k1 is using bare Exception classes: raised if the recovery failed
+        log.error('error while recovering pubkey: {}'.format(e.message))
+
+    return publickey
 
 
 def sign(messagedata, private_key):
