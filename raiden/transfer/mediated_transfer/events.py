32c7ea5f5272002493282e122923cc078d3e36e0
Augusto F. Hack <hack.augusto@gmail.com>
2018-02-22 16:21:18 -0300

    Using BalanceProofState for events

--- raiden/transfer/mediated_transfer/events.py
+++ raiden/transfer/mediated_transfer/events.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from raiden.transfer.architecture import Event
+from raiden.transfer.mediated_transfer.state import LockedTransferUnsignedState
+from raiden.utils import pex
 # pylint: disable=too-many-arguments,too-few-public-methods
+
+
+def refund_from_sendmediated(send_mediatedtransfer_event):
+    transfer = send_mediatedtransfer_event.transfer
+    return SendRefundTransfer2(
+        transfer.identifier,
+        transfer.token,
+        transfer.balance_proof,
+        transfer.lock,
+        transfer.initiator,
+        transfer.target,
+        send_mediatedtransfer_event.recipient,
+    )
 
 
 def mediatedtransfer(transfer, receiver):
@@ -38,6 +53,33 @@ class SendMediatedTransfer(Event):
         self.target = target
         self.expiration = expiration
         self.receiver = receiver
+
+
+class SendMediatedTransfer2(Event):
+    """ A locked transfer that must be sent to `recipient`. """
+
+    def __init__(self, transfer, recipient):
+        if not isinstance(transfer, LockedTransferUnsignedState):
+            raise ValueError('transfer must be a LockedTransferUnsignedState instance')
+
+        self.transfer = transfer
+        self.recipient = recipient
+
+    def __str__(self):
+        return '<SendMediatedTransfer transfer:{} recipient:{}>'.format(
+            self.transfer,
+            pex(self.recipient),
+        )
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, SendMediatedTransfer) and
+            self.transfer == other.transfer and
+            self.recipient == other.recipient
+        )
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 
 class SendRevealSecret(Event):
@@ -104,6 +146,28 @@ class SendBalanceProof(Event):
         self.secret = secret
 
 
+class SendBalanceProof2(Event):
+    """ Event to send a balance-proof to the counter-party, used after a lock
+    is unlocked locally allowing the counter-party to withdraw.
+    Used by payers: The initiator and mediator nodes.
+    Note:
+        This event has a dual role, it serves as a synchronization and as
+        balance-proof for the netting channel smart contract.
+        Nodes need to keep the last known merkle root synchronized. This is
+        required by the receiving end of a transfer in order to properly
+        validate. The rule is "only the party that owns the current payment
+        channel may change it" (remember that a netting channel is composed of
+        two uni-directional channels), as a consequence the merkle root is only
+        updated by the receiver once a balance proof message is received.
+    """
+    def __init__(self, identifier, token, receiver, secret, balance_proof):
+        self.identifier = identifier
+        self.token = token
+        self.receiver = receiver
+        self.secret = secret
+        self.balance_proof = balance_proof
+
+
 class SendSecretRequest(Event):
     """ Event used by a target node to request the secret from the initiator
     (`receiver`).
@@ -164,6 +228,62 @@ class ContractSendWithdraw(Event):
 
         self.transfer = transfer
         self.channel_address = channel_address
+
+
+class SendRefundTransfer2(Event):
+    """ Event used to cleanly backtrack the current node in the route.
+    This message will pay back the same amount of token from the receiver to
+    the sender, allowing the sender to try a different route without the risk
+    of losing token.
+    """
+    def __init__(
+            self,
+            identifier,
+            token,
+            balance_proof,
+            lock,
+            initiator,
+            target,
+            recipient):
+
+        self.identifier = identifier
+        self.token = token
+        self.balance_proof = balance_proof
+        self.lock = lock
+        self.initiator = initiator
+        self.target = target
+        self.recipient = recipient
+
+    def __str__(self):
+        return (
+            '<'
+            'SendRefundTransfer id:{} token:{} balance_proof:{} lock:{} '
+            'initiator:{} target:{} recipient:{}'
+            '>'
+        ).format(
+            self.identifier,
+            pex(self.token),
+            self.balance_proof,
+            self.lock,
+            pex(self.initiator),
+            pex(self.target),
+            pex(self.recipient),
+        )
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, SendRefundTransfer) and
+            self.identifier == other.identifier and
+            self.token == other.token and
+            self.balance_proof == other.balance_proof and
+            self.lock == other.lock and
+            self.initiator == other.initiator and
+            self.target == other.target and
+            self.recipient == other.recipient
+        )
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 
 class EventUnlockSuccess(Event):
