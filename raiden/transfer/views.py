e04bdac617cf1bd6fd25e30bce1e51abeb6cfd5c
Augusto F. Hack <hack.augusto@gmail.com>
2018-02-28 18:02:19 -0300

    added views for node state

---
+++ raiden/transfer/views.py
@@ -0,0 +1,273 @@
+# -*- coding: utf-8 -*-
+from raiden.transfer import channel
+from raiden.transfer.state import (
+    CHANNEL_STATE_OPENED,
+    CHANNEL_STATE_SETTLED,
+    NODE_NETWORK_UNKNOWN,
+    PaymentMappingState,
+)
+
+# TODO: Either enforce immutability or make a copy of the values returned by
+# the view functions
+
+
+def all_neighbour_nodes(node_state):
+    """ Return the identifiers for all nodes accross all payment networks which
+    have a channel open with this one.
+    """
+    addresses = set()
+
+    for payment_network in node_state.identifiers_to_paymentnetworks.values():
+        for token_network in payment_network.tokenidentifiers_to_tokennetworks.values():
+            for channel_state in token_network.partneraddresses_to_channels.values():
+                addresses.add(channel_state.partner_state.address)
+
+    return addresses
+
+
+def block_number(node_state):
+    return node_state.block_number
+
+
+def state_from_raiden(raiden):
+    return raiden.wal.state_manager.current_state
+
+
+def state_from_app(app):
+    return app.raiden.wal.state_manager.current_state
+
+
+def get_networkstatuses(node_state):
+    return node_state.nodeaddresses_to_networkstates
+
+
+def get_node_network_status(node_state, node_address):
+    return node_state.nodeaddresses_to_networkstates.get(
+        node_address,
+        NODE_NETWORK_UNKNOWN,
+    )
+
+
+def get_token_network_addresses_for(node_state, payment_network_id):
+    """ Return the list of tokens registered with the given payment network. """
+    payment_network = node_state.identifiers_to_paymentnetworks.get(payment_network_id)
+
+    if payment_network is not None:
+        return [
+            token_network.token_address
+            for token_network in payment_network.tokenidentifiers_to_tokennetworks.values()
+        ]
+
+    return list()
+
+
+def total_token_network_channels(node_state, payment_network_id, token_network_id):
+    token_network = get_token_network(
+        node_state,
+        payment_network_id,
+        token_network_id,
+    )
+
+    result = 0
+    if token_network:
+        result = len(token_network.channelidentifiers_to_channels)
+
+    return result
+
+
+def get_token_network(
+        node_state: 'NodeState',
+        payment_network_id,
+        token_network_id,
+) -> 'TokenNetworkState':
+
+    payment_network = node_state.identifiers_to_paymentnetworks.get(payment_network_id)
+    if payment_network is not None:
+        return payment_network.tokenaddresses_to_tokennetworks.get(token_network_id)
+
+    return None
+
+
+def get_channelstate_for(node_state, payment_network_id, token_network_id, partner_address):
+    """ Return the NettingChannelState if it exists, None otherwise. """
+    token_network = get_token_network(
+        node_state,
+        payment_network_id,
+        token_network_id,
+    )
+
+    channel_state = None
+    if token_network:
+        channel_state = token_network.partneraddresses_to_channels.get(partner_address)
+
+    return channel_state
+
+
+def get_channelstate_by_id(node_state, payment_network_id, token_network_id, channel_id):
+    token_network = get_token_network(
+        node_state,
+        payment_network_id,
+        token_network_id,
+    )
+
+    channel_state = None
+    if token_network:
+        channel_state = token_network.channelidentifiers_to_channels.get(channel_id)
+
+    return channel_state
+
+
+def get_channestate_for_receiving(node_state, payment_network_id, token_network_id):
+    """Return the state of channels that had received any transfers in this
+    token network.
+    """
+    token_network = get_token_network(
+        node_state,
+        payment_network_id,
+        token_network_id,
+    )
+
+    result = []
+    for channel_state in token_network.channelidentifiers_to_channels.values():
+        if channel_state.partner_state.balance_proof:
+            result.append(channel_state)
+
+    return result
+
+
+def get_channelstate_open(node_state, payment_network_id, token_network_id):
+    """Return the state of open channels in a token network."""
+    token_network = get_token_network(
+        node_state,
+        payment_network_id,
+        token_network_id,
+    )
+
+    result = []
+    for channel_state in token_network.channelidentifiers_to_channels.values():
+        if channel.get_status(channel_state) == CHANNEL_STATE_OPENED:
+            result.append(channel_state)
+
+    return result
+
+
+def get_channelstate_not_settled(node_state, payment_network_id, token_network_id):
+    """Return the state of open channels in a token network."""
+    token_network = get_token_network(
+        node_state,
+        payment_network_id,
+        token_network_id,
+    )
+
+    result = []
+    for channel_state in token_network.channelidentifiers_to_channels.values():
+        if channel.get_status(channel_state) == CHANNEL_STATE_SETTLED:
+            result.append(channel_state)
+
+    return result
+
+
+def get_channelstate_by_tokenaddress(node_state, payment_network_id, token_network_id, channel_id):
+    token_network = get_token_network(
+        node_state,
+        payment_network_id,
+        token_network_id,
+    )
+
+    channel_state = None
+    if token_network:
+        channel_state = token_network.channelidentifiers_to_channels.get(channel_id)
+
+    return channel_state
+
+
+def get_transfer_role(node_state, hashlock):
+    transfer_task = node_state.payment_mapping.hashlocks_to_task.get(hashlock)
+
+    result = None
+    if isinstance(transfer_task, PaymentMappingState.InitiatorTask):
+        result = 'initiator'
+    elif isinstance(transfer_task, PaymentMappingState.MediatorTask):
+        result = 'mediator'
+    elif isinstance(transfer_task, PaymentMappingState.TargetTask):
+        result = 'target'
+
+    return result
+
+
+def list_channelstate_for_tokennetwork(node_state, payment_network_id, token_network_id):
+    token_network = get_token_network(
+        node_state,
+        payment_network_id,
+        token_network_id,
+    )
+
+    if token_network:
+        result = token_network.partneraddresses_to_channels.values()
+    else:
+        result = []
+
+    return result
+
+
+def list_channelstate_for_partner(node_state, payment_network_id, partner_address):
+    payment_network = node_state.identifiers_to_paymentnetworks.get(payment_network_id)
+
+    result = []
+    if payment_network is not None:
+
+        for token_network in payment_network.tokenaddresses_to_tokennetworks.values():
+            channel_state = token_network.partneraddresses_to_channels.get(partner_address)
+            if channel_state:
+                # TODO: Either enforce immutability or make a copy
+                result.append(channel_state)
+
+    return result
+
+
+def list_all_channelstate(node_state):
+    result = []
+    for payment_network in node_state.identifiers_to_paymentnetworks.values():
+        for token_network in payment_network.tokenaddresses_to_tokennetworks.values():
+            # TODO: Either enforce immutability or make a copy
+            result.extend(
+                token_network.partneraddresses_to_channels.values()
+            )
+
+    return result
+
+
+def search_for_channel(node_state, payment_network_id, channel_address):
+    payment_network = node_state.identifiers_to_paymentnetworks.get(payment_network_id)
+
+    result = None
+    if payment_network is not None:
+        for token_network in payment_network.tokenaddresses_to_tokennetworks.values():
+            channel_state = token_network.channelidentifiers_to_channels.get(channel_address)
+
+            if channel_state:
+                result = channel_state
+                break
+
+    return result
+
+
+def filter_channels_by_partneraddress(
+        node_state,
+        payment_network_id,
+        token_network_id,
+        partner_addresses):
+
+    token_network = get_token_network(
+        node_state,
+        payment_network_id,
+        token_network_id,
+    )
+
+    result = []
+    for partner in partner_addresses:
+        channel_state = token_network.partneraddresses_to_channels.get(partner)
+        if channel_state:
+            result.append(channel_state)
+
+    return result
