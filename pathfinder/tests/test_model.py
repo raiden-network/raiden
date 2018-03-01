aa6278d246c8b6ed806b7ff7ca8374a446c4c5aa
Paul Lange <palango@gmx.de>
2018-03-01 14:06:48 +0100

    Fix lint problems in tests

--- pathfinder/tests/test_model.py
+++ pathfinder/tests/test_model.py
@@ -3,17 +3,18 @@ from pathfinder.model.lock import Lock
 
 
 def test_lock_packing():
-    l = Lock(100, 1000, b'a' * 32)
+    lock = Lock(100, 1000, b'a' * 32)
 
-    assert l.pack() == (b'\x00\x00\x00\x00\x00\x00\x03\xe8\x00\x00\x00\x00\x00'
+    assert lock.pack() == (
+        b'\x00\x00\x00\x00\x00\x00\x03\xe8\x00\x00\x00\x00\x00\x00\x00\x00\x00'
         b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00daaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        b'\x00\x00\x00\x00\x00daaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
     )
 
 
-def test_lock_packing():
-    l = Lock(100, 1000, b'a' * 32)
+def test_lock_hashing():
+    lock = Lock(100, 1000, b'a' * 32)
 
-    assert l.compute_hash() == (b'\x02S\xd3\x028 \t0B\x853\\\xe7f\xcc:\x068>'
-        b'\x84J\x0e\xe2\x0c\xdc\xc3Q\xca\x0f\x94,"'
+    assert lock.compute_hash() == (
+        b'\x02S\xd3\x028 \t0B\x853\\\xe7f\xcc:\x068>\x84J\x0e\xe2\x0c\xdc\xc3Q\xca\x0f\x94,"'
     )
