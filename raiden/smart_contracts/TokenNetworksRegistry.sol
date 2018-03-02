cb8e5531b98c80add6c80632e056334cee47b377
Loredana <loredana.cirstea@gmail.com>
2018-03-02 16:52:46 +0100

    SC - add SecretRegistry address to TokenNetworkRegistry
    1 SecretRegistry contract for all token networks.

--- raiden/smart_contracts/TokenNetworksRegistry.sol
+++ raiden/smart_contracts/TokenNetworksRegistry.sol
@@ -1,15 +1,17 @@
 pragma solidity ^0.4.17;
 
+import "./Utils.sol";
 import "./Token.sol";
 import "./TokenNetwork.sol";
 
-contract TokenNetworkRegistry {
+contract TokenNetworkRegistry is Utils {
 
     /*
      *  Data structures
      */
 
     string constant public contract_version = "0.3._";
+    address public secret_registry_address;
 
     // Token address => TokenNetwork address
     mapping(address => address) public token_to_token_networks;
@@ -19,6 +21,16 @@ contract TokenNetworkRegistry {
      */
 
     event TokenNetworkCreated(address token_address, address token_network_address);
+
+    /*
+     *  Constructor
+     */
+
+    function TokenNetworkRegistry(address _secret_registry_address) public {
+        require(_secret_registry_address != 0x0);
+        require(contractExists(_secret_registry_address));
+        secret_registry_address = _secret_registry_address;
+    }
 
     /*
      *  External Functions
@@ -33,7 +45,7 @@ contract TokenNetworkRegistry {
 
         // Token contract checks are in the corresponding TokenNetwork contract
 
-        token_network_address = new TokenNetwork(_token_address);
+        token_network_address = new TokenNetwork(_token_address, secret_registry_address);
         TokenNetworkCreated(_token_address, token_network_address);
 
         return token_network_address;
