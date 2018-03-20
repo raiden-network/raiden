33f72cb29a61f2f85c2d5a0b99dc1f2111ce43b5
Lefteris Karapetsas <lefteris@refu.co>
2018-03-20 18:34:42 +0100

    Wording on comment
    [skip ci]

--- raiden/tests/unit/transfer/mediated_transfer/test_targetstate.py
+++ raiden/tests/unit/transfer/mediated_transfer/test_targetstate.py
@@ -432,7 +432,7 @@ def test_transfer_succesful_after_secret_learned():
     # there is enough time to unlock the lock on chain.
     #
     # A mediated transfer might be received during the settlement period of the
-    # current channel, the secret request is sent to the initiator and at time
+    # current channel. The secret request is sent to the initiator and at the time
     # the secret is revealed there might not be enough time to safely unlock
     # the token on-chain.
     raise NotImplementedError()
