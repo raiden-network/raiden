cb1f2aaa36233e81844d46dbe6f189d7595315b5
Augusto F. Hack <hack.augusto@gmail.com>
2018-03-26 12:53:12 -0300

    removed StateChangeLog in favor of raiden.storage.wal

--- raiden/tests/utils/log.py
+++ raiden/tests/utils/log.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """ Utilities regarding the WAL and its DB used only in testing. """
-from raiden.transfer.log import InternalEvent
 
 
 def get_db_state_changes(storage, table):
@@ -16,12 +15,4 @@ def get_all_state_changes(log):
     return [
         (res[0], log.serializer.deserialize(res[1]))
         for res in get_db_state_changes(log.storage, 'state_changes')
-    ]
-
-
-def get_all_state_events(log):
-    """ Returns a list of tuples of event id, state_change_id, block_number and events"""
-    return [
-        (InternalEvent(res[0], res[1], res[2], log.serializer.deserialize(res[3])))
-        for res in get_db_state_changes(log.storage, 'state_events')
     ]
