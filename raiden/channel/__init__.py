7a417d7c17a6d5285227da019ddd828f589331ad
Augusto F. Hack <hack.augusto@gmail.com>
2018-03-29 16:18:42 -0300

    removed module raiden.channel
    - Delete the module raiden.channel, and as consequence the classes Channel, ChannelEndState, ChannelExternalState
    - Removed unit tests for raiden.channel, module raiden.tests.unit.test_channel
    - Removed test util get_sent_transfer that only worked with Channel
      classes

--- raiden/channel/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from raiden.channel.participant_state import ChannelEndState
-from raiden.channel.netting_channel import Channel, ChannelExternalState
-
-__all__ = (
-    'Channel',
-    'ChannelEndState',
-    'ChannelExternalState',
-)
