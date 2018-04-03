52fca9481171eb3720aeb79b8424f19ed6a7999f
Augusto F. Hack <hack.augusto@gmail.com>
2018-04-03 17:08:55 -0300

    moved version compatiblity check to utility

--- raiden/tests/unit/test_cli.py
+++ raiden/tests/unit/test_cli.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-from raiden.ui.cli import check_json_rpc
-from raiden.utils import is_minified_address
+from raiden.utils import (
+    is_minified_address,
+    is_supported_client,
+)
 
 
 class MockClient:
@@ -14,54 +16,54 @@ class MockClient:
 
 
 def test_check_json_rpc_geth():
-    assert check_json_rpc(MockClient('Geth/v1.7.3-unstable-e9295163/linux-amd64/go1.9.1'))
-    assert check_json_rpc(MockClient('Geth/v1.7.2-unstable-e9295163/linux-amd64/go1.9.1'))
-    assert check_json_rpc(MockClient('Geth/v1.8.2-unstable-e9295163/linux-amd64/go1.9.1'))
-    assert check_json_rpc(MockClient('Geth/v2.0.3-unstable-e9295163/linux-amd64/go1.9.1'))
-    assert check_json_rpc(MockClient('Geth/v11.55.86-unstable-e9295163/linux-amd64/go1.9.1'))
-    assert check_json_rpc(MockClient('Geth/v999.999.999-unstable-e9295163/linux-amd64/go1.9.1'))
+    assert is_supported_client('Geth/v1.7.3-unstable-e9295163/linux-amd64/go1.9.1')
+    assert is_supported_client('Geth/v1.7.2-unstable-e9295163/linux-amd64/go1.9.1')
+    assert is_supported_client('Geth/v1.8.2-unstable-e9295163/linux-amd64/go1.9.1')
+    assert is_supported_client('Geth/v2.0.3-unstable-e9295163/linux-amd64/go1.9.1')
+    assert is_supported_client('Geth/v11.55.86-unstable-e9295163/linux-amd64/go1.9.1')
+    assert is_supported_client('Geth/v999.999.999-unstable-e9295163/linux-amd64/go1.9.1')
 
-    assert not check_json_rpc(MockClient('Geth/v1.7.1-unstable-e9295163/linux-amd64/go1.9.1'))
-    assert not check_json_rpc(MockClient('Geth/v0.7.1-unstable-e9295163/linux-amd64/go1.9.1'))
-    assert not check_json_rpc(MockClient('Geth/v0.0.0-unstable-e9295163/linux-amd64/go1.9.1'))
-    assert not check_json_rpc(MockClient('Geth/v0.0.0-unstable-e9295163/linux-amd64/go1.9.1'))
+    assert not is_supported_client('Geth/v1.7.1-unstable-e9295163/linux-amd64/go1.9.1')
+    assert not is_supported_client('Geth/v0.7.1-unstable-e9295163/linux-amd64/go1.9.1')
+    assert not is_supported_client('Geth/v0.0.0-unstable-e9295163/linux-amd64/go1.9.1')
+    assert not is_supported_client('Geth/v0.0.0-unstable-e9295163/linux-amd64/go1.9.1')
 
 
 def test_check_json_rpc_parity():
-    assert check_json_rpc(MockClient(
+    assert is_supported_client(
         'Parity//v1.7.6-stable-19535333c-20171013/x86_64-linux-gnu/rustc1.20.0'
-    ))
-    assert check_json_rpc(MockClient(
+    )
+    assert is_supported_client(
         'Parity//v1.7.7-stable-19535333c-20171013/x86_64-linux-gnu/rustc1.20.0'
-    ))
-    assert check_json_rpc(MockClient(
+    )
+    assert is_supported_client(
         'Parity//v1.8.7-stable-19535333c-20171013/x86_64-linux-gnu/rustc1.20.0'
-    ))
-    assert check_json_rpc(MockClient(
+    )
+    assert is_supported_client(
         'Parity//v2.9.7-stable-19535333c-20171013/x86_64-linux-gnu/rustc1.20.0'
-    ))
-    assert check_json_rpc(MockClient(
+    )
+    assert is_supported_client(
         'Parity//v23.94.75-stable-19535333c-20171013/x86_64-linux-gnu/rustc1.20.0'
-    ))
-    assert check_json_rpc(MockClient(
+    )
+    assert is_supported_client(
         'Parity//v99.994.975-stable-19535333c-20171013/x86_64-linux-gnu/rustc1.20.0'
-    ))
+    )
 
-    assert not check_json_rpc(MockClient(
+    assert not is_supported_client(
         'Parity//v1.7.5-stable-19535333c-20171013/x86_64-linux-gnu/rustc1.20.0'
-    ))
-    assert not check_json_rpc(MockClient(
+    )
+    assert not is_supported_client(
         'Parity//v1.5.1-stable-19535333c-20171013/x86_64-linux-gnu/rustc1.20.0'
-    ))
-    assert not check_json_rpc(MockClient(
+    )
+    assert not is_supported_client(
         'Parity//v0.7.1-stable-19535333c-20171013/x86_64-linux-gnu/rustc1.20.0'
-    ))
-    assert not check_json_rpc(MockClient(
+    )
+    assert not is_supported_client(
         'Parity//v0.8.7-stable-19535333c-20171013/x86_64-linux-gnu/rustc1.20.0'
-    ))
-    assert not check_json_rpc(MockClient(
+    )
+    assert not is_supported_client(
         'Parity//v0.0.0-stable-19535333c-20171013/x86_64-linux-gnu/rustc1.20.0'
-    ))
+    )
 
 
 def test_minified_address_checker():
