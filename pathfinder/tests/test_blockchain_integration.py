e25bd698eaa1c1f4a64198863946c3051da748f8
err508 <tobias@brainbot.com>
2018-04-25 13:47:58 +0200

    Updating integration test (#49)
    Implements channel_close und update_fee events to be tested.

--- pathfinder/tests/test_blockchain_integration.py
+++ pathfinder/tests/test_blockchain_integration.py
@@ -6,12 +6,14 @@ processes them. Additionally, it mocks the transport layer directly. It tests th
 interaction of many moving parts - yet, it is currently really slow.
 Therefore, usually mocked_integration should be used.
 """
+import math
 from typing import List
 
 import gevent
 from raiden_contracts.contract_manager import ContractManager
+from raiden_libs.test.mocks.dummy_transport import DummyTransport
+
 from pathfinder.pathfinding_service import PathfindingService
-from pathfinder.tests.fixtures import Mock
 from raiden_libs.blockchain import BlockchainListener
 
 
@@ -28,15 +30,17 @@ def test_pfs_with_mocked_client(
     network_address = clients[0].contract.address
     pathfinding_service = PathfindingService(
         contracts_manager,
-        transport=Mock(),
+        transport=DummyTransport(),
         token_network_listener=blockchain_listener,
         follow_networks=[network_address]
     )
 
+    pathfinding_service.start()
+    token_network = pathfinding_service.token_networks[network_address]
+    graph = token_network.G
     # there should be one token network registered
     assert len(pathfinding_service.token_networks) == 1
 
-    token_network = pathfinding_service.token_networks[network_address]
     for (
         p1_index,
         p1_deposit,
@@ -51,8 +55,8 @@ def test_pfs_with_mocked_client(
         clients[p2_index].open_channel(clients[p1_index].address)
         clients[p1_index].deposit_to_channel(clients[p2_index].address, p1_deposit)
         clients[p2_index].deposit_to_channel(clients[p1_index].address, p2_deposit)
-        ethereum_tester.mine_blocks(1)
-        gevent.sleep(0)
+    ethereum_tester.mine_blocks(1)
+    gevent.sleep(0)
 
     # there should be as many open channels as described
     assert len(token_network.channel_id_to_addresses.keys()) == len(channel_descriptions_case_1)
@@ -69,8 +73,8 @@ def test_pfs_with_mocked_client(
         p2_fee
     ) in enumerate(channel_descriptions_case_1):
         p1_address, p2_address = token_network.channel_id_to_addresses[channel_id + 1]
-        view1 = token_network.G[p1_address][p2_address]['view']
-        view2 = token_network.G[p2_address][p1_address]['view']
+        view1 = graph[p1_address][p2_address]['view']
+        view2 = graph[p2_address][p1_address]['view']
 
         assert view1.deposit == p1_deposit
         assert view2.deposit == p2_deposit
@@ -83,8 +87,29 @@ def test_pfs_with_mocked_client(
                                 clients[3].address]
     assert paths[2]['path'] == [clients[0].address, clients[1].address, clients[2].address,
                                 clients[3].address]
+    # send some fee messages and check if they get processed correctly
+    for (
+        p1_index,
+        p1_deposit,
+        p1_transferred_amount,
+        p1_fee,
+        p2_index,
+        p2_deposit,
+        p2_transferred_amount,
+        p2_fee
+    ) in channel_descriptions_case_1:
+        fee_info = clients[p2_index].get_fee_info(
+            clients[p1_index].address,
+            nonce=2,
+            percentage_fee=0.11235813,
+        )
+        pathfinding_service.transport.transmit_data(fee_info.serialize_full())
+        gevent.sleep(0)
+        assert math.isclose(
+                graph[clients[p2_index].address][clients[p1_index].address]['view'].percentage_fee,
+                0.11235813
+        )
 
-    # Fixme: implement channel close events with balance proofs
     # now close all channels
     for channel_id, (
         p1_index,
@@ -96,6 +121,12 @@ def test_pfs_with_mocked_client(
         p2_transferred_amount,
         p2_fee
     ) in enumerate(channel_descriptions_case_1):
-        pass
+        balance_proof = clients[p2_index].get_balance_proof(
+            clients[p1_index].address, chain_id=2
+        )
+        clients[p2_index].close_channel(clients[p1_index].address, balance_proof)
+    ethereum_tester.mine_blocks(1)
+    gevent.sleep(0)
 
-    # there should be no open channels
+    # there should be no channels
+    assert len(token_network.channel_id_to_addresses.keys()) == 0
