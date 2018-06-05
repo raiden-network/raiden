91e8db59d7d800b9e1e737adde41af599201c0d5
Lefteris Karapetsas <lefteris@refu.co>
2018-06-05 15:24:18 +0200

    Fix flake8 errors in rpc proxies

--- raiden/network/rpc/smartcontract_proxy.py
+++ raiden/network/rpc/smartcontract_proxy.py
@@ -32,7 +32,7 @@ def decode_event(abi: Dict, log: Dict):
     return get_event_data(event_abi, log)
 
 
-def encode_function_call(abi: Dict, function: str, args: List=list()):
+def encode_function_call(abi: Dict, function: str, args: List = list()):
     fn_abi = find_matching_fn_abi(abi, function, args)
     fn_types = get_abi_input_types(fn_abi)
     return encode_abi(fn_types, args)
@@ -132,7 +132,7 @@ class ContractProxy:
     def decode_event(self, log):
         return decode_event(self.abi, log)
 
-    def encode_function_call(self, function: str, args: List=list()):
+    def encode_function_call(self, function: str, args: List = list()):
         return self.get_transaction_data(self.abi, function, args)
 
     @staticmethod
@@ -151,7 +151,7 @@ class ContractProxy:
         return output
 
     @staticmethod
-    def get_transaction_data(abi: Dict, function_name: str, args: List=list()):
+    def get_transaction_data(abi: Dict, function_name: str, args: List = list()):
         """Get encoded transaction data"""
         fn_abi = find_matching_fn_abi(
             abi,
