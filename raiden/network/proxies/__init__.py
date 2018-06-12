62f313867e5db9246b58e5838f1e381f0fc725ee
Paul Lange <palango@gmx.de>
2018-06-12 12:34:46 +0200

    Add PaymentChannel as a replacement for the NettingChannel

--- raiden/network/proxies/__init__.py
+++ raiden/network/proxies/__init__.py
@@ -7,3 +7,4 @@ from raiden.network.proxies.token import Token  # NOQA
 from raiden.network.proxies.token_network_registry import TokenNetworkRegistry  # NOQA
 from raiden.network.proxies.token_network import TokenNetwork  # NOQA
 from raiden.network.proxies.secret_registry import SecretRegistry  # NOQA
+from raiden.network.proxies.payment_channel import PaymentChannel  # NOQA
