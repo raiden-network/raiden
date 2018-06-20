20820549919798bb17809dd725dd6a2cb9cad946
Ulrich Petri <ulo@ulo.pe>
2018-06-20 16:46:50 +0200

    Really upgrade packages on travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -9,10 +9,10 @@ if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
     INSTALL_OPT="--user"
 fi
 
-pip install ${INSTALL_OPT} -U pip wheel coveralls "coverage<4.4"
+pip install ${INSTALL_OPT} --upgrade pip wheel coveralls "coverage<4.4"
 pip install ${INSTALL_OPT} pytest-travis-fold
 pip install ${INSTALL_OPT} pyinstaller
-pip install ${INSTALL_OPT} -U -r requirements-dev.txt
+pip install ${INSTALL_OPT} --upgrade --upgrade-strategy eager -r requirements-dev.txt
 pip install ${INSTALL_OPT} -e .
 
 pip list --outdated
