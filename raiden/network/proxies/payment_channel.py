61e847ccdfd66d231725473a6b2bdd0999797ac4
Paul Lange <palango@gmx.de>
2018-06-26 14:31:42 +0200

    Update TokenNetwork and PaymentChannel proxies
    - PaymentChannel now takes the channel id in the constructor
    - Untangled some functions in the TokenNetwork

--- raiden/network/proxies/payment_channel.py
+++ raiden/network/proxies/payment_channel.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+from eth_utils import decode_hex
 from web3.utils.filters import Filter
 from raiden.utils import typing
 from raiden.utils.filters import get_filter_args_for_channel_from_token_network
@@ -11,10 +12,28 @@ class PaymentChannel:
     def __init__(
         self,
         token_network: TokenNetwork,
-        partner_address: typing.Address,
+        channel_id: typing.ChannelID,
     ):
         self.token_network = token_network
-        self.partner_address = partner_address
+        self.channel_id = channel_id
+
+        # query the blockchain to get the partner addresses for the channel id
+        filter = token_network.proxy.contract.events.ChannelOpened.createFilter(
+            fromBlock=0,
+            argument_filters={
+                'channel_identifier': channel_id,
+            },
+        )
+
+        events = filter.get_all_entries()
+        token_network.proxy.contract.web3.eth.uninstallFilter(filter.filter_id)
+
+        if not len(events) > 0:
+            raise ValueError('Channel is non-existing.')
+
+        event = events[-1]
+        self.participant = decode_hex(event['args']['participant1'])
+        self.partner = decode_hex(event['args']['participant2'])
 
     def token_address(self) -> typing.Address:
         """ Returns the address of the token for the channel. """
@@ -22,11 +41,11 @@ class PaymentChannel:
 
     def channel_identifier(self) -> typing.ChannelID:
         """ Returns the channel identifier. """
-        return self.token_network.detail_channel(self.partner_address)['channel_identifier']
+        return self.channel_id
 
     def detail(self) -> Dict:
         """ Returns the channel details. """
-        return self.token_network.detail(self.partner_address)
+        return self.token_network.detail(self.participant, self.partner)
 
     def settle_timeout(self) -> int:
         """ Returns the channels settle_timeout. """
@@ -51,26 +70,26 @@ class PaymentChannel:
 
     def opened(self) -> bool:
         """ Returns if the channel is opened. """
-        return self.token_network.channel_is_opened(self.partner_address)
+        return self.token_network.channel_is_opened(self.participant, self.partner)
 
     def closed(self) -> bool:
         """ Returns if the channel is closed. """
-        return self.token_network.channel_is_closed(self.partner_address)
+        return self.token_network.channel_is_closed(self.participant, self.partner)
 
     def settled(self) -> bool:
         """ Returns if the channel is settled. """
-        return self.token_network.channel_is_settled(self.partner_address)
+        return self.token_network.channel_is_settled(self.participant, self.partner)
 
     def closing_address(self) -> typing.Address:
         """ Returns the address of the closer of the channel. """
-        return self.token_network.closing_address(self.partner_address)
+        return self.token_network.closing_address(self.participant, self.partner)
 
     def can_transfer(self) -> bool:
         """ Returns True if the channel is opened and the node has deposit in it. """
-        return self.token_network.can_transfer(self.partner_address)
+        return self.token_network.can_transfer(self.participant, self.partner)
 
     def deposit(self, total_deposit: typing.TokenAmount):
-        self.token_network.deposit(total_deposit, self.partner_address)
+        self.token_network.deposit(total_deposit, self.partner)
 
     def close(
             self,
@@ -81,7 +100,7 @@ class PaymentChannel:
     ):
         """ Closes the channel using the provided balance proof. """
         self.token_network.close(
-            partner=self.partner_address,
+            partner=self.partner,
             nonce=nonce,
             balance_hash=balance_hash,
             additional_hash=additional_hash,
@@ -98,7 +117,7 @@ class PaymentChannel:
     ):
         """ Updates the channel using the provided balance proof. """
         self.token_network.update_transfer(
-            partner=self.partner_address,
+            partner=self.partner,
             nonce=nonce,
             balance_hash=balance_hash,
             additional_hash=additional_hash,
@@ -108,7 +127,7 @@ class PaymentChannel:
 
     def unlock(self, merkle_tree_leaves: bytes):
         self.token_network.unlock(
-            self.partner_address,
+            self.partner,
             merkle_tree_leaves,
         )
 
@@ -126,7 +145,7 @@ class PaymentChannel:
             transferred_amount=transferred_amount,
             locked_amount=locked_amount,
             locksroot=locksroot,
-            partner=self.partner_address,
+            partner=self.partner,
             partner_transferred_amount=partner_transferred_amount,
             partner_locked_amount=partner_locked_amount,
             partner_locksroot=partner_locksroot,
