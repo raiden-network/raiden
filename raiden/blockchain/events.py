raiden-network/raiden

commit 9cb9af21c2906e5bd0515f48fbe9229195dc690a

simplified filter handling
- EthNodeCommunicationError must be allowed to propagated, the node must
exit if the ethereum node is unreachable
- add_event_listener must not call `poll_event_listener`, otherwise the
events will be lost
- mutable defaults are not to be used as default arguments
- raiden.utils.events was not used anywhere

raiden/blockchain/events.py
@@ -22,10 +22,7 @@ from raiden.blockchain.abi import (
     EVENT_CHANNEL_SETTLED,
     EVENT_CHANNEL_SECRET_REVEALED,
 )
-from raiden.exceptions import (
-    AddressWithoutCode,
-    EthNodeCommunicationError,
-)
+from raiden.exceptions import AddressWithoutCode
 from raiden.network.rpc.filters import get_filter_events
 from raiden.utils import address_decoder, pex
 from raiden.network.rpc.smartcontract_proxy import decode_event
@@ -46,26 +43,6 @@ Proxies2 = namedtuple(
 # `new_filter` uses None to signal the absence of topics filters
 ALL_EVENTS = None
 log = structlog.get_logger(__name__)  # pylint: disable=invalid-name
-
-
-def poll_event_listener(eth_filter, abi):
-    result = list()
-
-    for log_event in eth_filter.changes():
-        decoded_event = dict(decode_event(
-            abi,
-            log_event['event_data']
-        ))
-
-        if decoded_event is not None:
-            decoded_event['block_number'] = log_event.get('block_number')
-            event = Event(
-                log_event['address'],
-                decoded_event,
-            )
-            result.append(event)
-
-    return result
 
 
 def get_contract_events(
@@ -284,48 +261,21 @@ class BlockchainEvents:
     def __init__(self):
         self.event_listeners = list()
 
-    def poll_all_event_listeners(self, from_block=None):
-        result = list()
-        reinstalled_filters = False
+    def poll_blockchain_events(self):
+        for event_listener in self.event_listeners:
+            for log_event in event_listener.filter.changes():
+                decoded_event = dict(decode_event(
+                    event_listener.abi,
+                    log_event['event_data'],
+                ))
 
-        while True:
-            try:
-                for event_listener in self.event_listeners:
-                    decoded_events = poll_event_listener(
-                        event_listener.filter,
-                        event_listener.abi,
+                if decoded_event is not None:
+                    decoded_event['block_number'] = log_event.get('block_number')
+                    event = Event(
+                        log_event['address'],
+                        decoded_event,
                     )
-                    result.extend(decoded_events)
-                break
-            except EthNodeCommunicationError as e:
-                # If the eth client has restarted and we reconnected to it then
-                # filters will no longer exist there. In that case we will need
-                # to recreate all the filters.
-                if not reinstalled_filters and str(e) == 'filter not found':
-                    log.debug('reinstalling eth filters')
-
-                    result = list()
-                    reinstalled_filters = True
-                    updated_event_listerners = list()
-
-                    for event_listener in self.event_listeners:
-                        new_listener = EventListener(
-                            event_listener.event_name,
-                            event_listener.filter_creation_function(from_block=from_block),
-                            event_listener.abi,
-                            event_listener.filter_creation_function,
-                        )
-                        updated_event_listerners.append(new_listener)
-
-                    self.event_listeners = updated_event_listerners
-                else:
-                    raise e
-
-        return result
-
-    def poll_blockchain_events(self, from_block=None):
-        for event in self.poll_all_event_listeners(from_block):
-            yield decode_event_to_internal(event)
+                    yield decode_event_to_internal(event)
 
     def uninstall_all_event_listeners(self):
         for listener in self.event_listeners:
@@ -341,8 +291,6 @@ class BlockchainEvents:
             filter_creation_function,
         )
         self.event_listeners.append(event)
-
-        return poll_event_listener(eth_filter, abi)
 
     def add_registry_listener(self, registry_proxy, from_block=None):
         tokenadded = registry_proxy.tokenadded_filter(from_block)

raiden/utils/events.py
@@ -1,87 +1,0 @@
-"""Helpers for events and event-debugging.
-"""
-from typing import List, Dict, Union
-
-from eth_utils import to_checksum_address
-
-from raiden.blockchain.abi import (
-    CONTRACT_MANAGER,
-    CONTRACT_NETTING_CHANNEL,
-)
-from raiden.network.rpc.client import JSONRPCClient
-from raiden.network.proxies.netting_channel import NettingChannel
-from raiden.network.rpc.smartcontract_proxy import ContractProxy
-
-
-def all_contract_events_raw(
-        rpc: JSONRPCClient,
-        contract_address: str,
-        start_block: Union[str, int] = 0,
-        end_block: Union[str, int] = 'latest') -> List[Dict]:
-    """Find all events for a deployed contract given its `contract_address`.
-
-    Args:
-        rpc: client instance.
-        contract_address: hex encoded contract address.
-        start_block: read event-logs starting from this block number (default: 0).
-        end_block: read event-logs up to this block number (default: 'latest').
-    Returns:
-        events
-    """
-    return rpc.web3.eth.getLogs({
-        'fromBlock': start_block,
-        'toBlock': end_block,
-        'address': to_checksum_address(contract_address),
-        'topics': [],
-    })
-
-
-def all_contract_events(
-        rpc: JSONRPCClient,
-        contract_address: str,
-        abi,
-        start_block: Union[str, int] = 0,
-        end_block: Union[str, int] = 'latest') -> List[Dict]:
-    """Find and decode all events for a deployed contract given its `contract_address` and `abi`.
-
-    Args:
-        rpc: client instance.
-        contract_address: hex encoded contract address.
-        abi: the contract's ABI.
-        start_block: read event-logs starting from this block number (default: 0).
-        end_block: read event-logs up to this block number (default: 'latest').
-    Returns:
-        A list of all events from the given contract.
-    """
-
-    events_raw = all_contract_events_raw(
-        rpc,
-        contract_address,
-        start_block=start_block,
-        end_block=end_block
-    )
-
-    events = list()
-    for event_encoded in events_raw:
-        event = ContractProxy.decode_event(abi, event_encoded)
-        events.append(event)
-    return events
-
-
-def netting_channel_events(
-        rpc: JSONRPCClient,
-        netting_channel: NettingChannel,
-        end_block: Union[str, int] = 'latest') -> List[Dict]:
-    """Get all events for a netting_channel starting from its `opened()` block.
-    Args:
-        rpc: client instance.
-        netting_channel: the NettingChannel instance.
-        end_block: read event-logs up to this block number (default: 'latest').
-    """
-    return all_contract_events(
-        rpc,
-        netting_channel.address,
-        CONTRACT_MANAGER.get_translator(CONTRACT_NETTING_CHANNEL),
-        start_block=netting_channel.opened(),
-        end_block=end_block
-    )

raiden/network/rpc/smartcontract_proxy.py
@@ -32,7 +32,8 @@ def decode_event(abi: Dict, log: Dict):
     return get_event_data(event_abi, log)
 
 
-def encode_function_call(abi: Dict, function: str, args: List = list()):
+def encode_function_call(abi: Dict, function: str, args: List = None):
+    args = args or list()
     fn_abi = find_matching_fn_abi(abi, function, args)
     fn_types = get_abi_input_types(fn_abi)
     return encode_abi(fn_types, args)
@@ -129,10 +130,8 @@ class ContractProxy:
 
         return res
 
-    def decode_event(self, log):
-        return decode_event(self.abi, log)
-
-    def encode_function_call(self, function: str, args: List = list()):
+    def encode_function_call(self, function: str, args: List = None):
+        args = args or None
         return self.get_transaction_data(self.abi, function, args)
 
     @staticmethod
@@ -151,8 +150,9 @@ class ContractProxy:
         return output
 
     @staticmethod
-    def get_transaction_data(abi: Dict, function_name: str, args: List = list()):
+    def get_transaction_data(abi: Dict, function_name: str, args: List = None):
         """Get encoded transaction data"""
+        args = args or list()
         fn_abi = find_matching_fn_abi(
             abi,
             function_name,

raiden/raiden_service.py
@@ -358,7 +358,7 @@ class RaidenService:
     def start_health_check_for(self, node_address):
         self.protocol.start_health_check(node_address)
 
-    def poll_blockchain_events(self, current_block=None):  # pylint: disable=unused-argument
+    def poll_blockchain_events(self, block_number=None):  # pylint: disable=unused-argument
         with self.event_poll_lock:
             for event in self.blockchain_events.poll_blockchain_events():
                 on_blockchain_event(self, event)

raiden/tests/integration/test_blockchainservice.py
@@ -8,9 +8,14 @@ from eth_utils import to_canonical_address
 
 from raiden import waiting
 from raiden.api.python import RaidenAPI
-from raiden.blockchain.abi import CONTRACT_MANAGER, CONTRACT_CHANNEL_MANAGER
+from raiden.blockchain.abi import (
+    CONTRACT_CHANNEL_MANAGER,
+    CONTRACT_MANAGER,
+    CONTRACT_REGISTRY,
+)
 from raiden.exceptions import AddressWithoutCode, SamePeerAddress
 from raiden.network.rpc.client import JSONRPCClient
+from raiden.network.rpc.smartcontract_proxy import decode_event
 from raiden.network.rpc.transactions import check_transaction_threw
 from raiden.tests.utils.blockchain import wait_until_block
 from raiden.transfer import views
@@ -276,7 +281,7 @@ def test_blockchain(
     channel_manager_address = to_canonical_address(channel_manager_address_encoded)
 
     log = log_list[0]
-    event = registry_proxy.decode_event(log)
+    event = decode_event(CONTRACT_MANAGER.get_contract_abi(CONTRACT_REGISTRY), log)
     event_args = event['args']
 
     assert channel_manager_address == to_canonical_address(event_args['channel_manager_address'])

commit 15194e40fdf1054c8cefc370feb06a001d93b90b

remove filter creation function
This function was used to re-install the filter if the ethereum node
went offline. It was decided a few iterations back to move out from this
model, were the Raiden node stays running and recover from external
system unavailability, to a crash-only system (introduced by
9cb9af21c2906e5bd0515f48fbe9229195dc690a)

raiden/blockchain/events.py
@@ -35,7 +35,7 @@ from raiden.utils.typing import Address, BlockSpecification
 
 EventListener = namedtuple(
     'EventListener',
-    ('event_name', 'filter', 'abi', 'filter_creation_function'),
+    ('event_name', 'filter', 'abi'),
 )
 Proxies = namedtuple(
     'Proxies',
@@ -321,12 +321,11 @@ class BlockchainEvents:
 
         self.event_listeners = list()
 
-    def add_event_listener(self, event_name, eth_filter, abi, filter_creation_function):
+    def add_event_listener(self, event_name, eth_filter, abi):
         event = EventListener(
             event_name,
             eth_filter,
             abi,
-            filter_creation_function,
         )
         self.event_listeners.append(event)
 
@@ -342,7 +341,6 @@ class BlockchainEvents:
             'Registry {}'.format(pex(registry_address)),
             tokenadded,
             CONTRACT_MANAGER.get_contract_abi(CONTRACT_REGISTRY),
-            registry_proxy.tokenadded_filter,
         )
 
     def add_channel_manager_listener(
@@ -357,7 +355,6 @@ class BlockchainEvents:
             'ChannelManager {}'.format(pex(manager_address)),
             channelnew,
             CONTRACT_MANAGER.get_contract_abi(CONTRACT_CHANNEL_MANAGER),
-            channel_manager_proxy.channelnew_filter,
         )
 
     def add_token_network_listener(
@@ -372,7 +369,6 @@ class BlockchainEvents:
             'TokenNetwork {}'.format(pex(token_network_address)),
             channel_new_filter,
             CONTRACT_MANAGER.get_contract_abi(CONTRACT_TOKEN_NETWORK),
-            token_network_proxy.channelnew_filter,
         )
 
     def add_netting_channel_listener(
@@ -387,7 +383,6 @@ class BlockchainEvents:
             'NettingChannel Event {}'.format(pex(channel_address)),
             netting_channel_events,
             CONTRACT_MANAGER.get_contract_abi(CONTRACT_NETTING_CHANNEL),
-            netting_channel_proxy.all_events_filter,
         )
 
     def add_payment_channel_listener(
@@ -404,7 +399,6 @@ class BlockchainEvents:
             raiden_contracts.contract_manager.CONTRACT_MANAGER.get_contract_abi(
                 CONTRACT_TOKEN_NETWORK,
             ),
-            payment_channel_proxy.all_events_filter,
         )
 
     def add_proxies_listeners(self, proxies, from_block: typing.BlockSpecification = 'latest'):