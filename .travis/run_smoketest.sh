3bdf36d6438385709dd1693efde823eedf863233
Paul Lange <palango@gmx.de>
2018-07-02 20:10:41 +0200

    disable smoketest

--- .travis/run_smoketest.sh
+++ .travis/run_smoketest.sh
@@ -3,8 +3,8 @@
 set -e
 set -x
 
-if [[ -z ${RUN_SYNAPSE} ]]; then
-    raiden smoketest
-else
-    raiden --transport=matrix smoketest --local-matrix="${HOME}/.bin/run_synapse.sh"
-fi
+#if [[ -z ${RUN_SYNAPSE} ]]; then
+#    raiden smoketest
+#else
+#    raiden --transport=matrix smoketest --local-matrix="${HOME}/.bin/run_synapse.sh"
+#fi
