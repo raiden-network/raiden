bfb6d51bee5a648084e32ceca98793cee761e9d0
Jozef Henzl <popcorp86@gmail.com>
2018-07-06 15:17:55 +0200

    Fix `test_token_networks`
    - fixes a race condition when opening a new channel
    - updates some outdated api calls used in test and connection manager
    - fixes #1796: ordering of `TransactionChannelNewBalance`
    
    [ci integration]

--- raiden/tests/integration/long_running/flaky/test_token_networks.py
+++ raiden/tests/integration/long_running/flaky/test_token_networks.py
@@ -5,9 +5,52 @@ import gevent
 
 from raiden.api.python import RaidenAPI
 from raiden.tests.utils.geth import wait_until_block
-from raiden.transfer.state import CHANNEL_STATE_SETTLED
+from raiden.transfer import views
+from raiden.transfer.state import NODE_NETWORK_REACHABLE
+from raiden import routing
 
 log = structlog.get_logger(__name__)
+
+
+def wait_for_reachable_node(sender, receiver):
+    """Wait until a node is reachable"""
+    while True:
+        state = RaidenAPI(sender).get_node_network_state(
+            receiver.address,
+        )
+        if state == NODE_NETWORK_REACHABLE:
+            return
+        gevent.sleep(1)
+
+
+def wait_for_transaction(
+    receiver,
+    registry_address,
+    token_address,
+    sender_address,
+):
+    """Wait until a first transaction in a channel is received"""
+    while True:
+        receiver_channel = RaidenAPI(receiver).get_channel_list(
+            registry_address=registry_address,
+            token_address=token_address,
+            partner_address=sender_address,
+        )
+        if(
+            len(receiver_channel) == 1 and
+            receiver_channel[0].partner_state.balance_proof is not None
+        ):
+            break
+        gevent.sleep(0.1)
+
+
+def saturated_count(connection_managers, open_channel_views):
+    """Return count of nodes with count of open channels exceeding initial channel target"""
+    return len([
+        len(open_channel_view()) >= connection_manager.initial_channel_target
+        for connection_manager, open_channel_view
+        in zip(connection_managers, open_channel_views)
+    ])
 
 
 # TODO: add test scenarios for
@@ -18,10 +61,8 @@ log = structlog.get_logger(__name__)
 #   raiden_network fixture.
 
 
-@pytest.mark.xfail(reason='Some issues in this test, see raiden #691')
 @pytest.mark.parametrize('number_of_nodes', [6])
 @pytest.mark.parametrize('channels_per_node', [0])
-@pytest.mark.parametrize('register_tokens', [True, False])
 @pytest.mark.parametrize('settle_timeout', [6])
 @pytest.mark.parametrize('reveal_timeout', [3])
 def test_participant_selection(raiden_network, token_addresses):
@@ -51,57 +92,70 @@ def test_participant_selection(raiden_network, token_addresses):
     gevent.wait(connect_greenlets)
 
     # wait some blocks to let the network connect
-    wait_blocks = 15
-    for _ in range(wait_blocks):
-        for app in raiden_network:
-            wait_until_block(
-                app.raiden.chain,
-                app.raiden.chain.block_number() + 1,
-            )
+    for app in raiden_network:
+        wait_until_block(
+            app.raiden.chain,
+            app.raiden.chain.block_number() + 1,
+        )
 
+    token_network_registry_address = views.get_token_network_identifier_by_token_address(
+        views.state_from_raiden(raiden_network[0].raiden),
+        payment_network_id=registry_address,
+        token_address=token_address,
+    )
     connection_managers = [
-        app.raiden.connection_manager_for_token(
+        app.raiden.connection_manager_for_token_network(
+            token_network_registry_address,
+        ) for app in raiden_network
+    ]
+    open_channel_views = [
+        lambda:
+        views.get_channelstate_open(
+            views.state_from_raiden(app.raiden),
             registry_address,
             token_address,
         ) for app in raiden_network
     ]
 
-    def open_channels_count(connection_managers_):
-        return [
-            connection_manager.open_channels for connection_manager in connection_managers_
-        ]
-
-    assert all(open_channels_count(connection_managers))
-
-    def not_saturated(connection_managers_):
-        return [
-            1 for connection_manager_ in connection_managers_
-            if len(connection_manager_.open_channels) < connection_manager_.initial_channel_target
-        ]
+    assert all([x() for x in open_channel_views])
 
     chain = raiden_network[-1].raiden.chain
-    max_wait = 12
+    max_wait_blocks = 12
 
-    while not_saturated(connection_managers) and max_wait > 0:
+    while (
+        saturated_count(connection_managers, open_channel_views) < len(connection_managers) and
+        max_wait_blocks > 0
+    ):
         wait_until_block(chain, chain.block_number() + 1)
-        max_wait -= 1
+        max_wait_blocks -= 1
 
-    assert not not_saturated(connection_managers)
+    assert saturated_count(connection_managers, open_channel_views) == len(connection_managers)
 
-    # Ensure unpartitioned network
-    addresses = [app.raiden.address for app in raiden_network]
-    for connection_manager in connection_managers:
-        assert all(
-            connection_manager.channelgraph.has_path(
-                connection_manager.raiden.address,
-                address,
-            )
-            for address in addresses
+    # ensure unpartitioned network
+    for app in raiden_network:
+        node_state = views.state_from_raiden(app.raiden)
+        network_state = views.get_token_network_by_token_address(
+            node_state,
+            registry_address,
+            token_address,
         )
+        assert network_state is not None
+        for target in raiden_network:
+            if target.raiden.address == app.raiden.address:
+                continue
+            routes = routing.get_best_routes(
+                node_state,
+                network_state.address,
+                app.raiden.address,
+                target.raiden.address,
+                1,
+                None,
+            )
+            assert routes is not None
 
     # average channel count
     acc = (
-        sum(len(connection_manager.open_channels) for connection_manager in connection_managers) /
+        sum(len(x()) for x in open_channel_views) /
         len(connection_managers)
     )
 
@@ -110,8 +164,8 @@ def test_participant_selection(raiden_network, token_addresses):
         # selection algorithm
         # https://github.com/raiden-network/raiden/issues/576
         assert not any(
-            len(connection_manager.open_channels) > 2 * acc
-            for connection_manager in connection_managers
+            len(x()) > 2 * acc
+            for x in open_channel_views
         )
     except AssertionError:
         pass
@@ -120,7 +174,7 @@ def test_participant_selection(raiden_network, token_addresses):
     sender = raiden_network[-1].raiden
     receiver = raiden_network[0].raiden
 
-    registry_address = sender.raiden.default_registry.address
+    registry_address = sender.default_registry.address
     # assert there is a direct channel receiver -> sender (vv)
     receiver_channel = RaidenAPI(receiver).get_channel_list(
         registry_address=registry_address,
@@ -129,8 +183,6 @@ def test_participant_selection(raiden_network, token_addresses):
     )
     assert len(receiver_channel) == 1
     receiver_channel = receiver_channel[0]
-    assert receiver_channel.external_state.opened_block != 0
-    assert not receiver_channel.received_transfers
 
     # assert there is a direct channel sender -> receiver
     sender_channel = RaidenAPI(sender).get_channel_list(
@@ -140,53 +192,60 @@ def test_participant_selection(raiden_network, token_addresses):
     )
     assert len(sender_channel) == 1
     sender_channel = sender_channel[0]
-    assert sender_channel.can_transfer
-    assert sender_channel.external_state.opened_block != 0
 
+    exception = ValueError('partner not reachable')
+    with gevent.Timeout(30, exception=exception):
+        wait_for_reachable_node(sender, receiver)
+
+    amount = 1
     RaidenAPI(sender).transfer_and_wait(
         registry_address,
         token_address,
-        1,
+        amount,
         receiver.address,
+        transfer_timeout=10,
     )
 
-    # now receiver has a transfer
-    assert len(receiver_channel.received_transfers)
+    exception = ValueError('timeout while waiting for incoming transaction')
+    with gevent.Timeout(30, exception=exception):
+        wait_for_transaction(
+            receiver,
+            registry_address,
+            token_address,
+            sender.address,
+        )
 
     # test `leave()` method
     connection_manager = connection_managers[0]
-    before = len(connection_manager.receiving_channels)
 
     timeout = (
-        connection_manager.min_settle_blocks *
+        sender_channel.settle_timeout *
         connection_manager.raiden.chain.estimate_blocktime() *
-        5
+        10
     )
 
     assert timeout > 0
-    with gevent.timeout.Timeout(timeout):
-        try:
-            RaidenAPI(raiden_network[0].raiden).token_network_leave(
-                registry_address,
-                token_address,
-            )
-        except gevent.timeout.Timeout:
-            log.error('timeout while waiting for leave')
+    exception = ValueError('timeout while waiting for leave')
+    with gevent.Timeout(timeout, exception=exception):
+        RaidenAPI(raiden_network[0].raiden).token_network_leave(
+            registry_address,
+            token_address,
+        )
 
     before_block = connection_manager.raiden.chain.block_number()
-    wait_blocks = connection_manager.min_settle_blocks + 10
+    wait_blocks = sender_channel.settle_timeout + 10
+    # wait until both chains are synced?
     wait_until_block(
         connection_manager.raiden.chain,
         before_block + wait_blocks,
     )
-    assert connection_manager.raiden.chain.block_number >= before_block + wait_blocks
     wait_until_block(
         receiver.chain,
         before_block + wait_blocks,
     )
-    while receiver_channel.state != CHANNEL_STATE_SETTLED:
-        gevent.sleep(receiver.alarm.wait_time)
-    after = len(connection_manager.receiving_channels)
-
-    assert before > after
-    assert after == 0
+    receiver_channel = RaidenAPI(receiver).get_channel_list(
+        registry_address=registry_address,
+        token_address=token_address,
+        partner_address=sender.address,
+    )
+    assert receiver_channel[0].settle_transaction is not None
