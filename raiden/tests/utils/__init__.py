25b36ef28984745969f5270ba29dbe0b070bdba2
André Vitor de Lima Matos <andre@brainbot.com>
2018-07-10 18:03:13 +0200

    Remove channel_identifier usage to search for channels
    This identifier has a nonce on the blockchain, which makes it very hard
    to be calculated only from the parameters given in the request.
    As there should never exist more than 1 channel open for each tuple
    (registry_address, token_address, own_address, partner_address), we can
    just use it to search for the channels we want to interact with.
    Channel_identifier is still returned in the request, for information,
    but its value is ignored in the tests.
    [ci integration]

--- raiden/tests/utils/__init__.py
+++ raiden/tests/utils/__init__.py
@@ -15,3 +15,23 @@ def wait_blocks(web3: Web3, blocks: int):
     target_block = web3.eth.blockNumber + blocks
     while web3.eth.blockNumber < target_block:
         gevent.sleep(0.5)
+
+
+def dicts_are_equal(a: dict, b: dict) -> bool:
+    """Compare dicts, but allows ignoring specific values through the
+    dicts_are_equal.IGNORE_VALUE identifier"""
+    if set(a.keys()) != set(b.keys()):
+        return False
+    for k in a.keys():
+        va, vb = a[k], b[k]
+        if dicts_are_equal.IGNORE_VALUE in (va, vb):
+            continue
+        elif isinstance(va, dict) and isinstance(vb, dict):
+            if not dicts_are_equal(va, vb):
+                return False
+        elif va != vb:
+            return False
+    return True
+
+
+dicts_are_equal.IGNORE_VALUE = object()
