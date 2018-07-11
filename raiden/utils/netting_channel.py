61b1975017ac38e00653204ff9aa89b8b07031cd
André Vitor de Lima Matos <andre@brainbot.com>
2018-07-11 10:22:49 +0200

    Fix channel_identifier in test_events.py::test_query_events

--- raiden/utils/netting_channel.py
+++
@@ -1,7 +0,0 @@
-from raiden.utils import sha3
-
-
-def channel_identifier(participant1, participant2):
-    if participant1 < participant2:
-        return sha3(participant1 + participant2)
-    return sha3(participant2 + participant1)
