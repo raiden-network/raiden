7f8f998f31508961cc2484a78057d854a71d28a3
André Vitor de Lima Matos <andre@brainbot.com>
2018-07-13 17:07:08 +0200

    Fix channel identifier not showing up in channel events tab

---
+++ raiden/ui/web/src/app/pipes/ellipsis.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'ellipsis'
+})
+export class EllipsisPipe implements PipeTransform {
+
+  transform(val, args) {
+    if (args === undefined) {
+      return val;
+    }
+
+    if (val.length > args) {
+      return val.substring(0, args) + '...';
+    } else {
+      return val;
+    }
+  }
+
+}
