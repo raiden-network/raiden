c21341ce4faac624cc950987d5faacfe1ebd3cdc
Konstantinos Paparas <kelsos86@gmail.com>
2018-07-25 16:05:34 +0200

    Fix errors that prevented Karma from running tests.

--- raiden/ui/web/src/app/pipes/token.pipe.spec.ts
+++ raiden/ui/web/src/app/pipes/token.pipe.spec.ts
@@ -1,8 +1,27 @@
+import { async, inject, TestBed } from '@angular/core/testing';
+import { RaidenService } from '../services/raiden.service';
 import { TokenPipe } from './token.pipe';
 
 describe('TokenPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TokenPipe();
-    expect(pipe).toBeTruthy();
-  });
+    let service: jasmine.SpyObj<RaidenService>;
+
+    beforeEach(async(() => {
+        service = jasmine.createSpyObj('RaidenService', [
+            'getChannels'
+        ]);
+
+        TestBed.configureTestingModule({
+            providers: [
+                {
+                    provide: RaidenService, useClass: service
+                }
+            ]
+        }).compileComponents();
+    }));
+
+
+    it('create an instance', inject([RaidenService], (raidenService: RaidenService) => {
+        const pipe = new TokenPipe(raidenService);
+        expect(pipe).toBeTruthy();
+    }));
 });
