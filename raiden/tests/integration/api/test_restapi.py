ea29dff47d84ceb80154694755dbe556b31eaba8
Augusto F. Hack <hack.augusto@gmail.com>
2018-07-26 18:37:25 -0300

    fix test_api_deposit_limit

--- raiden/tests/integration/api/test_restapi.py
+++ raiden/tests/integration/api/test_restapi.py
@@ -235,51 +235,51 @@ def test_api_get_channel_list(
         token_addresses,
         reveal_timeout,
 ):
-        partner_address = '0x61C808D82A3Ac53231750daDc13c777b59310bD9'
+    partner_address = '0x61C808D82A3Ac53231750daDc13c777b59310bD9'
 
-        request = grequests.get(
-            api_url_for(
-                api_backend,
-                'channelsresource',
-            ),
-        )
-        response = request.send().response
-        assert_proper_response(response, HTTPStatus.OK)
-        assert response.json() == []
+    request = grequests.get(
+        api_url_for(
+            api_backend,
+            'channelsresource',
+        ),
+    )
+    response = request.send().response
+    assert_proper_response(response, HTTPStatus.OK)
+    assert response.json() == []
 
-        # let's create a new channel
-        token_address = token_addresses[0]
-        settle_timeout = 1650
-        channel_data_obj = {
-            'partner_address': partner_address,
-            'token_address': to_checksum_address(token_address),
-            'settle_timeout': settle_timeout,
-            'reveal_timeout': reveal_timeout,
-        }
+    # let's create a new channel
+    token_address = token_addresses[0]
+    settle_timeout = 1650
+    channel_data_obj = {
+        'partner_address': partner_address,
+        'token_address': to_checksum_address(token_address),
+        'settle_timeout': settle_timeout,
+        'reveal_timeout': reveal_timeout,
+    }
 
-        request = grequests.put(
-            api_url_for(
-                api_backend,
-                'channelsresource',
-            ),
-            json=channel_data_obj,
-        )
-        response = request.send().response
+    request = grequests.put(
+        api_url_for(
+            api_backend,
+            'channelsresource',
+        ),
+        json=channel_data_obj,
+    )
+    response = request.send().response
 
-        assert_proper_response(response, HTTPStatus.CREATED)
+    assert_proper_response(response, HTTPStatus.CREATED)
 
-        request = grequests.get(
-            api_url_for(
-                api_backend,
-                'channelsresource',
-            ),
-        )
-        response = request.send().response
-        assert_proper_response(response, HTTPStatus.OK)
-        channel_info = response.json()[0]
-        assert channel_info['partner_address'] == partner_address
-        assert channel_info['token_address'] == to_checksum_address(token_address)
-        assert 'token_network_identifier' in channel_info
+    request = grequests.get(
+        api_url_for(
+            api_backend,
+            'channelsresource',
+        ),
+    )
+    response = request.send().response
+    assert_proper_response(response, HTTPStatus.OK)
+    channel_info = response.json()[0]
+    assert channel_info['partner_address'] == partner_address
+    assert channel_info['token_address'] == to_checksum_address(token_address)
+    assert 'token_network_identifier' in channel_info
 
 
 @pytest.mark.parametrize('number_of_nodes', [1])
@@ -288,25 +288,25 @@ def test_api_channel_status_channel_nonexistant(
         api_backend,
         token_addresses,
 ):
-        partner_address = '0x61C808D82A3Ac53231750daDc13c777b59310bD9'
-        token_address = token_addresses[0]
+    partner_address = '0x61C808D82A3Ac53231750daDc13c777b59310bD9'
+    token_address = token_addresses[0]
 
-        request = grequests.get(
-            api_url_for(
-                api_backend,
-                'channelsresourcebytokenandpartneraddress',
-                token_address=token_address,
-                partner_address=partner_address,
-            ),
+    request = grequests.get(
+        api_url_for(
+            api_backend,
+            'channelsresourcebytokenandpartneraddress',
+            token_address=token_address,
+            partner_address=partner_address,
+        ),
+    )
+    response = request.send().response
+    assert_proper_response(response, HTTPStatus.NOT_FOUND)
+    assert response.json()['errors'] == (
+        "Channel with partner '{}' for token '{}' could not be found.".format(
+            to_checksum_address(partner_address),
+            to_checksum_address(token_address),
         )
-        response = request.send().response
-        assert_proper_response(response, HTTPStatus.NOT_FOUND)
-        assert response.json()['errors'] == (
-            "Channel with partner '{}' for token '{}' could not be found.".format(
-                to_checksum_address(partner_address),
-                to_checksum_address(token_address),
-            )
-        )
+    )
 
 
 @pytest.mark.parametrize('number_of_nodes', [1])
@@ -1194,6 +1194,6 @@ def test_api_deposit_limit(
     )
     response = request.send().response
 
-    assert_proper_response(response, HTTPStatus.EXPECTATION_FAILED)
+    assert_proper_response(response, HTTPStatus.CONFLICT)
     response = response.json()
     assert response['errors'] == 'The deposit of 10001 is bigger than the current limit of 10000'
