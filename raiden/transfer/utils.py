9d6e0a0cca9987f807ff3bc7369a9d647c297ff4
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2018-07-27 12:38:42 +0200

    Mimic hash_balance_data according to contract logic and add tests

--- raiden/transfer/utils.py
+++ raiden/transfer/utils.py
@@ -1,5 +1,7 @@
+from eth_utils import decode_hex
 from web3 import Web3
 
+from raiden.constants import EMPTY_HEX_HASH
 from raiden.utils import typing
 
 
@@ -7,7 +9,11 @@ def hash_balance_data(
         transferred_amount: typing.TokenAmount,
         locked_amount: typing.TokenAmount,
         locksroot: typing.Locksroot,
-) -> str:
+) -> bytes(32):
+    if transferred_amount == 0 and locked_amount == 0 and locksroot == b'':
+        return decode_hex(
+            EMPTY_HEX_HASH,
+        )
     return Web3.soliditySha3(
         ['uint256', 'uint256', 'bytes32'],
         [transferred_amount, locked_amount, locksroot],
