15827196b9b87695ac532f315e0c016e6f620576
Lefteris Karapetsas <lefteris@refu.co>
2018-07-29 16:47:03 +0200

    Fix the happy path of cli tests. cli_test_init

--- raiden/tests/integration/cli/conftest.py
+++ raiden/tests/integration/cli/conftest.py
@@ -1,32 +1,60 @@
-import os
 import pytest
-
 from raiden.tests.utils.smoketest import (
     load_smoketest_config,
-    start_ethereum,
+    setup_testchain_and_raiden,
 )
+
+
+def append_arg_if_existing(argname, initial_args, new_args):
+    cliname = '--' + argname.replace('_', '-')
+    if argname in initial_args:
+        new_args.extend([cliname, initial_args[argname]])
 
 
 @pytest.fixture(scope='session')
 def blockchain_provider():
-    print("fixture init")
     smoketest_config = load_smoketest_config()
-    ethereum, ethereum_config = start_ethereum(smoketest_config['genesis'])
-    discovery_contract_address = smoketest_config['contracts']['discovery_address']
-    registry_contract_address = smoketest_config['contracts']['registry_address']
-    eth_rpc_endpoint = 'http://127.0.0.1:{}'.format(ethereum_config['rpc'])
-    keystore_path = ethereum_config['keystore']
-    datadir_path = os.path.split(keystore_path)[0]
-    network_id = '627'
-    password_file_path = os.path.join(keystore_path, 'password')
-    with open(password_file_path, 'w') as handler:
-        handler.write('password')
-    return {'ethereum': ethereum,
-            'ethereum_config': ethereum_config,
-            'discovery_contract_address': discovery_contract_address,
-            'registry_contract_address': registry_contract_address,
-            'eth_rpc_endpoint': eth_rpc_endpoint,
-            'keystore_path': keystore_path,
-            'datadir_path': datadir_path,
-            'password_file_path': password_file_path,
-            'network_id': network_id}
+    result = setup_testchain_and_raiden(
+        smoketest_config,
+        'matrix',
+        'auto',
+        lambda x: None,
+    )
+    args = result['args']
+    # The setup of the testchain returns a TextIOWrapper but
+    # for the tests we need a filename
+    args['password_file'] = args['password_file'].name
+    return args
+
+
+@pytest.fixture()
+def removed_args():
+    return None
+
+
+@pytest.fixture()
+def cli_args(blockchain_provider, removed_args):
+    initial_args = blockchain_provider.copy()
+
+    if removed_args is not None:
+        for arg in removed_args:
+            if arg in initial_args:
+                del initial_args[arg]
+
+    args = [
+        '--no-sync-check',
+        '--registry-contract-address',
+        initial_args['registry_contract_address'],
+        '--secret-registry-contract-address',
+        initial_args['secret_registry_contract_address'],
+        '--discovery-contract-address',
+        initial_args['discovery_contract_address'],
+    ]
+
+    append_arg_if_existing('keystore_path', initial_args, args)
+    append_arg_if_existing('password_file', initial_args, args)
+    append_arg_if_existing('datadir', initial_args, args)
+    append_arg_if_existing('network_id', initial_args, args)
+    append_arg_if_existing('eth_rpc_endpoint', initial_args, args)
+
+    return args
