4f3b28a831f5f7549509a69ad51e13b299fd8288
Lefteris Karapetsas <lefteris@refu.co>
2018-07-30 16:05:49 +0200

    PR review fixes
    - Spawn raiden in a new way in cli tests
    - Add new key to cli_version_test
    - Fix style

--- raiden/tests/integration/cli/test_cli.py
+++ raiden/tests/integration/cli/test_cli.py
@@ -7,19 +7,29 @@ from raiden.ui.cli import run
 from eth_utils import to_checksum_address
 
 
+def spawn_raiden(args):
+    return pexpect.spawn(sys.executable, ['-m', 'raiden'] + args, logfile=sys.stdout)
+
+
 def test_cli_version():
     runner = CliRunner()
-    result = runner.invoke(run, ["version"])
+    result = runner.invoke(run, ['version'])
     assert result.exit_code == 0
     result_json = json.loads(result.output)
-    result_expected_keys = ["raiden", "python_implementation", "python_version", "system"]
+    result_expected_keys = [
+        'raiden',
+        'python_implementation',
+        'python_version',
+        'system',
+        'distribution',
+    ]
     for expected_key in result_expected_keys:
         assert expected_key in result_json
 
 
 @pytest.mark.timeout(25)
 def test_cli_full_init(cli_args):
-    child = pexpect.spawn('raiden', cli_args, logfile=sys.stdout)
+    child = spawn_raiden(cli_args)
     try:
         child.expect('Welcome to Raiden')
         child.expect('The following accounts were found in your machine:')
@@ -28,7 +38,7 @@ def test_cli_full_init(cli_args):
         child.expect('You are connected')
         child.expect('The Raiden API RPC server is now running')
     except pexpect.TIMEOUT as e:
-        print("Timed out at", e)
+        print('Timed out at', e)
     finally:
         child.close()
 
@@ -36,11 +46,11 @@ def test_cli_full_init(cli_args):
 @pytest.mark.timeout(25)
 @pytest.mark.parametrize('changed_args', [{'keystore_path': '.'}])
 def test_cli_wrong_keystore_path(cli_args):
-    child = pexpect.spawn('raiden', cli_args, logfile=sys.stdout)
+    child = spawn_raiden(cli_args)
     try:
         child.expect('No Ethereum accounts found in the provided keystore directory')
     except pexpect.TIMEOUT as e:
-        print("PEXPECT timed out at", e)
+        print('PEXPECT timed out at', e)
     finally:
         child.close()
 
@@ -48,7 +58,7 @@ def test_cli_wrong_keystore_path(cli_args):
 @pytest.mark.timeout(25)
 @pytest.mark.parametrize('removed_args', [['password_file']])
 def test_cli_missing_password_file_enter_password(blockchain_provider, cli_args):
-    child = pexpect.spawn('raiden', cli_args, logfile=sys.stdout)
+    child = spawn_raiden(cli_args)
     try:
         child.expect('Welcome to Raiden')
         child.expect('The following accounts were found in your machine:')
@@ -61,7 +71,7 @@ def test_cli_missing_password_file_enter_password(blockchain_provider, cli_args)
         child.expect('You are connected')
         child.expect('The Raiden API RPC server is now running')
     except pexpect.TIMEOUT as e:
-        print("Timed out at", e)
+        print('Timed out at', e)
     finally:
         child.close()
 
@@ -69,7 +79,7 @@ def test_cli_missing_password_file_enter_password(blockchain_provider, cli_args)
 @pytest.mark.timeout(25)
 @pytest.mark.parametrize('removed_args', [['data_dir']])
 def test_cli_missing_data_dir(cli_args):
-    child = pexpect.spawn('raiden', cli_args, logfile=sys.stdout)
+    child = spawn_raiden(cli_args)
     try:
         child.expect('Welcome to Raiden')
         child.expect('The following accounts were found in your machine:')
@@ -78,7 +88,7 @@ def test_cli_missing_data_dir(cli_args):
         child.expect('You are connected')
         child.expect('The Raiden API RPC server is now running')
     except pexpect.TIMEOUT as e:
-        print("Timed out at", e)
+        print('Timed out at', e)
     finally:
         child.close()
 
@@ -86,7 +96,7 @@ def test_cli_missing_data_dir(cli_args):
 @pytest.mark.timeout(25)
 @pytest.mark.parametrize('changed_args', [{'eth_rpc_endpoint': 'http://8.8.8.8:2020'}])
 def test_cli_wrong_rpc_endpoint(cli_args):
-    child = pexpect.spawn('raiden', cli_args, logfile=sys.stdout)
+    child = spawn_raiden(cli_args)
     try:
         child.expect('Welcome to Raiden')
         child.expect('The following accounts were found in your machine:')
@@ -94,7 +104,7 @@ def test_cli_wrong_rpc_endpoint(cli_args):
         child.sendline('0')
         child.expect('Could not contact the ethereum node through JSON-RPC')
     except pexpect.TIMEOUT as e:
-        print("Timed out at", e)
+        print('Timed out at', e)
     finally:
         child.close()
 
@@ -102,7 +112,7 @@ def test_cli_wrong_rpc_endpoint(cli_args):
 @pytest.mark.timeout(25)
 @pytest.mark.parametrize('changed_args', [{'network_id': '1'}])
 def test_cli_wrong_network_id_try_mainnet(cli_args):
-    child = pexpect.spawn('raiden', cli_args, logfile=sys.stdout)
+    child = spawn_raiden(cli_args)
     try:
         child.expect('Welcome to Raiden')
         child.expect('The following accounts were found in your machine:')
@@ -122,14 +132,14 @@ def test_cli_wrong_network_id_try_mainnet(cli_args):
     'registry_contract_address': '0xdfD10vAe9CCl5EBf11bc6309A0645eFe9f979584',
 }])
 def test_cli_malformed_registry_address(cli_args):
-    child = pexpect.spawn('raiden', cli_args, logfile=sys.stdout)
+    child = spawn_raiden(cli_args)
     try:
         child.expect(
             'Error: Invalid value for "--registry-contract-address"'
             ': Address must be EIP55 checksummed',
         )
     except pexpect.TIMEOUT as e:
-        print("Timed out at", e)
+        print('Timed out at', e)
     finally:
         child.close()
 
@@ -141,7 +151,7 @@ def test_cli_malformed_registry_address(cli_args):
 def test_cli_registry_address_without_deployed_contract(cli_args):
     # TODO: Change the expectation for this (don't expect a trace) when
     # https://github.com/raiden-network/raiden/issues/1996 is implemented
-    child = pexpect.spawn('raiden', cli_args, logfile=sys.stdout)
+    child = spawn_raiden(cli_args)
     try:
         child.expect('Welcome to Raiden')
         child.expect('The following accounts were found in your machine:')
@@ -150,6 +160,6 @@ def test_cli_registry_address_without_deployed_contract(cli_args):
         child.expect('You are connected')
         child.expect('raiden.exceptions.AddressWithoutCode')
     except pexpect.TIMEOUT as e:
-        print("Timed out at", e)
+        print('Timed out at', e)
     finally:
         child.close()
