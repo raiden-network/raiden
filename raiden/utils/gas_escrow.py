8abb7d88cf485d20cc4414c89b5b051b9c3f4974
Paul Lange <palango@gmx.de>
2018-08-02 12:46:32 +0200

    Improve naming of gas cost constants
    [ci integration]

--- raiden/utils/gas_escrow.py
+++ raiden/utils/gas_escrow.py
@@ -1,24 +1,30 @@
 from typing import Tuple
 
 from raiden.constants import (
-    GAS_USED_OPEN_CHANNEL,
-    GAS_USED_SET_TOTAL_DEPOSIT,
-    GAS_USED_CLOSE_CHANNEL,
-    GAS_USED_SETTLE_CHANNEL,
-    GAS_USED_UNLOCK_1_LOCKS,
+    GAS_REQUIRED_FOR_OPEN_CHANNEL,
+    GAS_REQUIRED_FOR_SET_TOTAL_DEPOSIT,
+    GAS_REQUIRED_FOR_CLOSE_CHANNEL,
+    GAS_REQUIRED_FOR_SETTLE_CHANNEL,
+    GAS_REQUIRED_FOR_UNLOCK_1_LOCKS,
 )
 from raiden.transfer import views
 
-CHANNEL_LIFECYCLE_SETTLE_GAS_USED = GAS_USED_SETTLE_CHANNEL + GAS_USED_UNLOCK_1_LOCKS
-CHANNEL_LIFECYCLE_CLOSE_GAS_USED = (
-    GAS_USED_CLOSE_CHANNEL + CHANNEL_LIFECYCLE_SETTLE_GAS_USED
+GAS_REQUIRED_FOR_CHANNEL_LIFECYCLE_AFTER_SETTLE = (
+    GAS_REQUIRED_FOR_UNLOCK_1_LOCKS
 )
-CHANNEL_LIFECYCLE_NO_DEPOSIT_GAS_USED = (
-    CHANNEL_LIFECYCLE_CLOSE_GAS_USED + GAS_USED_OPEN_CHANNEL
+GAS_REQUIRED_FOR_CHANNEL_LIFECYCLE_AFTER_CLOSE = (
+    GAS_REQUIRED_FOR_SETTLE_CHANNEL + GAS_REQUIRED_FOR_CHANNEL_LIFECYCLE_AFTER_SETTLE
 )
-CHANNEL_LIFECYCLE_GAS_USED = CHANNEL_LIFECYCLE_NO_DEPOSIT_GAS_USED + GAS_USED_SET_TOTAL_DEPOSIT
+GAS_REQUIRED_FOR_CHANNEL_LIFECYCLE_AFTER_OPEN = (
+    GAS_REQUIRED_FOR_CLOSE_CHANNEL + GAS_REQUIRED_FOR_CHANNEL_LIFECYCLE_AFTER_CLOSE
+)
+GAS_REQUIRED_FOR_CHANNEL_LIFECYCLE_COMPLETE = (
+    GAS_REQUIRED_FOR_OPEN_CHANNEL +
+    GAS_REQUIRED_FOR_SET_TOTAL_DEPOSIT +
+    GAS_REQUIRED_FOR_SET_TOTAL_DEPOSIT
+)
 
-GAS_PRICE_SECURITY_FACTOR = 1.1
+ESCROW_ESTIMATE_SECURITY_FACTOR = 1.1
 
 
 def _get_gas_estimate(
@@ -29,10 +35,10 @@ def _get_gas_estimate(
 ) -> int:
     estimate = 0
 
-    estimate += new_channels * CHANNEL_LIFECYCLE_GAS_USED
-    estimate += opened_channels * CHANNEL_LIFECYCLE_CLOSE_GAS_USED
-    estimate += closed_channels * CHANNEL_LIFECYCLE_SETTLE_GAS_USED
-    estimate += settled_channels * GAS_USED_UNLOCK_1_LOCKS
+    estimate += new_channels * GAS_REQUIRED_FOR_CHANNEL_LIFECYCLE_COMPLETE
+    estimate += opened_channels * GAS_REQUIRED_FOR_CHANNEL_LIFECYCLE_AFTER_OPEN
+    estimate += closed_channels * GAS_REQUIRED_FOR_CHANNEL_LIFECYCLE_AFTER_CLOSE
+    estimate += settled_channels * GAS_REQUIRED_FOR_CHANNEL_LIFECYCLE_AFTER_SETTLE
 
     return estimate
 
@@ -93,7 +99,7 @@ def has_enough_gas_escrow(
     gas_price = raiden.chain.client.gasprice()
     escrow_amount = gas_estimate * gas_price
 
-    secure_escrow_estimate = round(escrow_amount * GAS_PRICE_SECURITY_FACTOR)
+    secure_escrow_estimate = round(escrow_amount * ESCROW_ESTIMATE_SECURITY_FACTOR)
     current_account_balance = raiden.chain.client.balance(raiden.chain.client.sender)
 
     return secure_escrow_estimate <= current_account_balance, secure_escrow_estimate
