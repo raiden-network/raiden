930667e39ae93ed1c7a123199e33e3a5f6d57c3d
Jochen Müller <mail@jnmr.de>
2018-08-06 20:39:54 +0200

    Reimplement the patch for gevent exception handling

--- raiden/utils/gevent_utils.py
+++ raiden/utils/gevent_utils.py
@@ -1,30 +1,68 @@
 import gevent
 
+from raiden.exceptions import UnhandledExceptionInGreenlet
 
-def _make_greenlet_error_handler(original_handler):
-    def handle_error(self, context, type, value, tb):
-        if not issubclass(type, self.NOT_ERROR):
+
+class RaidenGreenlet(gevent.Greenlet):
+    def link_safe(self, callable):
+        self.is_safe_linked = True
+        super(RaidenGreenlet, self).link(self._wrap_link(callable))
+
+    def link_exception_safe(self, callable):
+        self.is_safe_linked = True
+        super(RaidenGreenlet, self).link_exception(self._wrap_link(callable))
+
+    def rawlink_safe(self, callable):
+        self.is_safe_linked = True
+        super(RaidenGreenlet, self).rawlink(self._wrap_link(callable))
+
+    @staticmethod
+    def _wrap_link(callable):
+        def wrapped_callable(greenlet):
+            try:
+                callable(greenlet)
+            except gevent.get_hub().SYSTEM_ERROR:
+                raise
+            except Exception as exception:
+                raise UnhandledExceptionInGreenlet(exception) from exception
+        return wrapped_callable
+
+
+def _patch_handle_error(original_handler):
+    def patched_handle_error(self, context, type, value, tb):
+        safe_linked = getattr(gevent.getcurrent(), 'is_safe_linked', False)
+        if safe_linked or issubclass(type, self.NOT_ERROR):
+            original_handler(self, context, type, value, tb)
+        else:
+            self.print_exception(context, type, value, tb)
             self.handle_system_error(type, value)
-    return handle_error
+    return patched_handle_error
 
 
 def configure_gevent():
     """
     Configure the gevent `Hub` as follows:
 
-      - Set `SYSTEM_ERROR` to `BaseException` to exit the process on any unhandled exception in
-        greenlets.
+      - Patch the `handle_error` method to bubble up any uncaught exception to the main greenlet,
+        with two restrictions: We stick to the default error handling if
+        - any of `link_exception_safe`, `link_safe` or `rawlink_safe` have been called on the
+          greenlet, we expect all Exceptions to be taken care of and stick to the default
+          behavior. Uncaught exceptions in the linked functions themselves will raise a
+          `RaidenFatalError`
+        - or the exception is listed in NOT_ERROR. This includes DNS resolve errors as well as
+          GreenletExit.
 
-      - Replace the default gevent Hub's error handler with one that only forwards exception types
-        not listed in `NOT_ERROR` to the default implementation.
-        This is necessary to avoid DNS resolver errors bubbling up into the event loop.
+      Notice that this breaks the usual `gevent.Greenlet` linking methods. Functions linked via
+      `rawlink`, `link` and `link_exception` will not be called if an exception is raised inside
+      a Greenlet, the exception will be bubbled up instead.
     """
     hub = gevent.get_hub()
 
     if getattr(hub, '_patched', False):
         return
 
-    hub.NOT_ERROR = (gevent.GreenletExit,)
-    hub.SYSTEM_ERROR = (BaseException,)
-    hub.__class__.handle_error = _make_greenlet_error_handler(hub.handle_error)
+    gevent.spawn = RaidenGreenlet.spawn
+    gevent.spawn_later = RaidenGreenlet.spawn_later
+    hub.SYSTEM_ERROR = hub.SYSTEM_ERROR + (UnhandledExceptionInGreenlet,)
+    hub.__class__.handle_error = _patch_handle_error(hub.__class__.handle_error)
     hub._patched = True
