69df8c1dd8e328b2eefa054dd3bdd0d12d6c5817
André Vitor de Lima Matos <andre@brainbot.com>
2018-08-07 15:12:52 +0200

    Allow travis to run with tester
    But with tester, some integration tests still fail,
    so this is just an improvement to allow it, not a requirement
    [ci integration]

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,12 +2,16 @@
 
 set -ex
 
-if [ -z ${BLOCKCHAIN_TYPE} ] && [[ ${TRAVIS_EVENT_TYPE} == "cron" ]]; then
+TRAVIS_COMMIT_MSG=`git log --format=%B --no-merges --topo-order -n 1`
+if [[ -z ${BLOCKCHAIN_TYPE} && ${TRAVIS_EVENT_TYPE} == "cron" ]]; then
     BLOCKCHAIN_TYPE="geth"
-elif [ -z ${BLOCKCHAIN_TYPE} ]; then
-    # FIXME: change to "tester" once the test failures are fixed
+elif [[ -z ${BLOCKCHAIN_TYPE} && "${TRAVIS_COMMIT_MSG}" =~ '[tester]' ]]; then
+    BLOCKCHAIN_TYPE="tester"
+elif [[ -z ${BLOCKCHAIN_TYPE} ]]; then
     BLOCKCHAIN_TYPE="geth"
 fi
+
+echo MSG=${TRAVIS_COMMIT_MSG}, BC=${BLOCKCHAIN_TYPE}
 
 coverage run \
     -m py.test \
