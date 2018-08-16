40bee531ba58c5db7f26248d45f5c50c3674fc4b
André Vitor de Lima Matos <andre@brainbot.com>
2018-08-16 15:05:54 +0200

    Re-enable synapse logs, but output it to stdout instead of stderr
    logging/structlog/raiden logs goes to stderr. This commit redirect
    synapse logs to stdout, so pytest folding will keep them separate, avoid
    poluting useful logs with matrix ones, but leaving them available if
    required.

--- raiden/utils/http.py
+++ raiden/utils/http.py
@@ -1,13 +1,17 @@
 import socket
+import os
+import subprocess
 from mirakuru.http import HTTPExecutor as MiHTTPExecutor, HTTPConnection, HTTPException
+from mirakuru.base import ENV_UUID
 
 
 class HTTPExecutor(MiHTTPExecutor):
     """ Subclass off mirakuru.HTTPExecutor, which allows other methods than HEAD """
 
-    def __init__(self, *args, method='HEAD', **kwargs):
+    def __init__(self, *args, method='HEAD', io=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.method = method
+        self.stdio = io
 
     def after_start_check(self):
         """ Check if defined URL returns expected status to a <method> request. """
@@ -23,3 +27,33 @@ class HTTPExecutor(MiHTTPExecutor):
 
         except (HTTPException, socket.timeout, socket.error):
             return False
+
+    def start(self):
+        """ Reimplements SimpleExecutor.start by setting stdin/out to None instead of PIPE
+
+        It may break input/output/communicate, but will ensure child output redirects won't
+        break parent process
+        """
+        if self.process is None:
+            command = self.command
+            if not self._shell:
+                command = self.command_parts
+            if isinstance(self.stdio, (list, tuple)):
+                stdin, stdout, stderr = self.stdio
+            else:
+                stdin = stdout = stderr = self.stdio
+            env = os.environ.copy()
+            env[ENV_UUID] = self._uuid
+            self.process = subprocess.Popen(
+                command,
+                shell=self._shell,
+                stdin=stdin,
+                stdout=stdout,
+                stderr=stderr,
+                universal_newlines=True,
+                preexec_fn=os.setsid,
+                env=env,
+            )
+
+        self._set_timeout()
+        return self
