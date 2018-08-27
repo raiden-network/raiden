9494f27a0d0cd972fb01bdfa9dbfdd093968f48f
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2018-08-27 18:47:43 +0200

    Implement JSON Encoder/Decoder to support from_dict and to_dict

--- raiden/utils/serialization.py
+++ raiden/utils/serialization.py
@@ -1,4 +1,5 @@
 import json
+import importlib
 
 import networkx
 from eth_utils import (
@@ -13,6 +14,59 @@ from raiden.transfer.merkle_tree import (
     LEAVES,
     compute_layers,
 )
+
+
+class RaidenJSONEncoder(json.JSONEncoder):
+    """ A custom JSON encoder to provide convenience
+    of recursive instance encoding. """
+    def default(self, obj):
+        """
+        If an object has `to_dict` method,
+        call that method.
+        """
+        if hasattr(obj, 'to_dict'):
+            result = obj.to_dict()
+            result['_type'] = f'{obj.__module__}.{obj.__class__.__name__}'
+            return result
+        return super().default(obj)
+
+
+class RaidenJSONDecoder(json.JSONDecoder):
+    """ A custom JSON decoder which facilitates
+    specific object type invocation to restore
+    it's state.
+    """
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, data):
+        """
+        detects the type of a JSON object, imports the class
+        of that type and calls `to_dict`
+        """
+        if '_type' in data:
+            _type = data['_type']
+            klass = self._import_type(_type)
+            if hasattr(klass, 'from_dict'):
+                return klass.from_dict(data)
+        return json.JSONDecoder.object_hook(data)
+
+    def _import_type(self, type_name):
+        *module_name, klass_name = type_name.split('.')
+        module_name = '.'.join(module_name)
+        module = importlib.import_module(module_name, None)
+        if not hasattr(module, klass_name):
+            raise TypeError(f'Could not find {module_name}.{klass_name}')
+        klass = getattr(module, klass_name)
+        return klass
+
+
+def json_encode(obj):
+    return json.dumps(obj, cls=RaidenJSONEncoder)
+
+
+def json_decode(obj):
+    return json.loads(obj, cls=RaidenJSONDecoder)
 
 
 def map_dict(
