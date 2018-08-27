78a9d92bab7fc8ca7045e54baeedb89158688416
Konstantinos Paparas <kelsos86@gmail.com>
2018-08-27 16:43:30 +0200

    Fixes balance update notifications showing for wrong channels
    This would happen because the the polling mechanism only checked a channels identifier to determine unique channels.

--- raiden/ui/web/src/app/services/channel-polling.service.spec.ts
+++ raiden/ui/web/src/app/services/channel-polling.service.spec.ts
@@ -44,6 +44,15 @@ describe('ChannelPollingService', () => {
         decimals: 8
     };
 
+    const token2: UserToken = {
+        address: '0xeB7f4BBAa1714F3E5a12fF8B681908D7b98BD195',
+        symbol: 'TST2',
+        name: 'Test Suite Token 2',
+        balance: 20,
+        decimals: 8
+    };
+
+
     const channel1: Channel = {
         state: 'opened',
         channel_identifier: 1,
@@ -54,6 +63,18 @@ describe('ChannelPollingService', () => {
         total_deposit: 10,
         settle_timeout: 500,
         userToken: token
+    };
+
+    const channel1Network2: Channel = {
+        state: 'opened',
+        channel_identifier: 1,
+        token_address: '0xeB7f4BBAa1714F3E5a12fF8B681908D7b98BD195',
+        partner_address: '0x774aFb0652ca2c711fD13e6E9d51620568f6Ca82',
+        reveal_timeout: 600,
+        balance: 20,
+        total_deposit: 10,
+        settle_timeout: 500,
+        userToken: token2
     };
 
     const channel1Updated: Channel = {
@@ -116,55 +137,64 @@ describe('ChannelPollingService', () => {
 
     it('should show a notification on balance increases', fakeAsync(() => {
         raidenServiceSpy.and.returnValues(from([[channel1], [channel1Updated]]));
-        const subcription = pollingService.channels().subscribe();
+        const subscription = pollingService.channels().subscribe();
         expect(sharedService.info).toHaveBeenCalledTimes(1);
         // @ts-ignore
         const payload = sharedService.info.calls.first().args[0];
         expect(payload.title).toBe('Balance Update');
-        subcription.unsubscribe();
+        subscription.unsubscribe();
         flush();
     }));
 
     it('should not show a notification on when balance is reduced', fakeAsync(() => {
         raidenServiceSpy.and.returnValues(from([[channel1], [channel1UpdatedNegative]]));
-        const subcription = pollingService.channels().subscribe();
+        const subscription = pollingService.channels().subscribe();
         expect(sharedService.info).toHaveBeenCalledTimes(0);
-        subcription.unsubscribe();
+        subscription.unsubscribe();
         flush();
     }));
 
     it('should not send notification about channel the first time loading the channels', fakeAsync(() => {
         raidenServiceSpy.and.returnValues(from([[], [channel1]]));
-        const subcription = pollingService.channels().subscribe();
+        const subscription = pollingService.channels().subscribe();
         expect(sharedService.info).toHaveBeenCalledTimes(0);
-        subcription.unsubscribe();
+        subscription.unsubscribe();
         flush();
     }));
 
     it('should show notification if new channels are detected', fakeAsync(() => {
         raidenServiceSpy.and.returnValues(from([[channel1], [channel1, channel2]]));
-        const subcription = pollingService.channels().subscribe();
+        const subscription = pollingService.channels().subscribe();
         expect(sharedService.info).toHaveBeenCalledTimes(1);
         // @ts-ignore
         const payload = sharedService.info.calls.first().args[0];
         expect(payload.title).toBe('New channel');
-        subcription.unsubscribe();
+        subscription.unsubscribe();
         flush();
     }));
 
     it('should not show a notification if no new channels are detected', fakeAsync(() => {
         raidenServiceSpy.and.returnValues(from([[channel1], [channel1]]));
-        const subcription = pollingService.channels().subscribe();
+        const subscription = pollingService.channels().subscribe();
         expect(sharedService.info).toHaveBeenCalledTimes(0);
-        subcription.unsubscribe();
+        subscription.unsubscribe();
         flush();
     }));
 
     it('should not throw if a channel is removed from the list', fakeAsync(() => {
         raidenServiceSpy.and.returnValues(from([[channel1, channel2], [channel1]]));
-        const subcription = pollingService.channels().subscribe();
+        const subscription = pollingService.channels().subscribe();
         expect(sharedService.info).toHaveBeenCalledTimes(0);
-        subcription.unsubscribe();
+        subscription.unsubscribe();
+        flush();
+    }));
+
+
+    it('should not show a notification for the same identifier on different network', fakeAsync(() => {
+        raidenServiceSpy.and.returnValues(from([[channel1, channel1Network2], [channel1Network2]]));
+        const subscription = pollingService.channels().subscribe();
+        expect(sharedService.info).toHaveBeenCalledTimes(0);
+        subscription.unsubscribe();
         flush();
     }));
 });
