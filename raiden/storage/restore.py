dbfa22c5c07f6586678c6060c9dece04c8441470
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2018-09-13 17:27:45 +0200

    Use current channel state if no snapshots were found

--- raiden/storage/restore.py
+++ raiden/storage/restore.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from raiden.storage import wal
 from raiden.transfer import node, views
 from raiden.transfer.state import NettingChannelState
@@ -15,11 +17,23 @@ def channel_state_until_balance_hash(
 
     # Restore state from the latest snapshot
     snapshot = raiden.wal.storage.get_latest_state_snapshot()
+    if not snapshot:
+        # No snapshots were taken before this taking place.
+        # Therefore, we return a copy of the current channel state
+        channel_state = deepcopy(views.get_channelstate_by_id(
+            chain_state=views.state_from_raiden(raiden),
+            payment_network_id=raiden.default_registry.address,
+            token_address=token_address,
+            channel_id=channel_identifier,
+        ))
+        return channel_state
+
     last_applied_state_change_id, chain_state = snapshot
     unapplied_state_changes = raiden.wal.storage.get_statechanges_by_identifier(
         from_identifier=last_applied_state_change_id,
         to_identifier='latest',
     )
+
     # Create a copy WAL
     log = wal.wal_from_snapshot(node.state_transition, raiden.wal.storage, chain_state)
     for state_change in unapplied_state_changes:
@@ -41,4 +55,5 @@ def channel_state_until_balance_hash(
         )
         if target_balance_hash == balance_hash:
             return channel_state
+
     return None
