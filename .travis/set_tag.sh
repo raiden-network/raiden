9051d5093caa90e82b82d739fffc6f4f336e4fc6
Lefteris Karapetsas <lefteris@refu.co>
2018-09-14 18:37:46 +0200

    Remove colon from date, improve pip install command
    [ci nightly]

--- .travis/set_tag.sh
+++ .travis/set_tag.sh
@@ -3,5 +3,5 @@
 if [[ ! -z ${TRAVIS_TAG} ]]; then
     export ARCHIVE_TAG=${TRAVIS_TAG}
 else
-    DATE=$(date +%Y-%m-%dT%T) && export ARCHIVE_TAG="nightly-$DATE"
+    DATE=$(date date +%Y-%m-%dT%H-%M-%S) && export ARCHIVE_TAG="nightly-$DATE"
 fi
