52e1298feeba1622ae6e85a1dde65d5a6299f2f6
Lefteris Karapetsas <lefteris@refu.co>
2018-09-14 19:10:10 +0200

    add set -e -x to set_tag.sh

--- .travis/set_tag.sh
+++ .travis/set_tag.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+set -ex
 
+#!/bin/bash
 if [[ ! -z ${TRAVIS_TAG} ]]; then
     export ARCHIVE_TAG=${TRAVIS_TAG}
 else
-    DATE=$(date date +%Y-%m-%dT%H-%M-%S) && export ARCHIVE_TAG="nightly-$DATE"
+    DATE=$(date +%Y-%m-%dT%H-%M-%S) && export ARCHIVE_TAG="nightly-$DATE"
 fi
+
+set +ex
