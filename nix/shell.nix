b926c9c7ec743a9dba80611d2daaa46b58fb595d
sveitser <sveitser@gmail.com>
2018-09-14 08:17:46 +0800

    Remove unnecessary comment

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,6 +1,5 @@
 with import (
-    # to ge the hash.
-builtins.fetchTarball {
+  builtins.fetchTarball {
     # To update
     #     curl -sI https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz | awk '/Location:/ {print $2}'
     url = "https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-18.09pre146409.2391d1dbf31/nixexprs.tar.xz";
