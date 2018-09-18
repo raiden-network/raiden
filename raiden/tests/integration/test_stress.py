4085938973e998edab827e27179f0defc0f0b752
Augusto F. Hack <hack.augusto@gmail.com>
2018-09-18 10:44:35 -0300

    stress test: unskipped it
    [ci integration]

--- raiden/tests/integration/test_stress.py
+++ raiden/tests/integration/test_stress.py
@@ -296,7 +296,6 @@ def assert_channels(raiden_network, token_network_identifier, deposit):
 @pytest.mark.parametrize('deposit', [5])
 @pytest.mark.parametrize('reveal_timeout', [15])
 @pytest.mark.parametrize('settle_timeout', [120])
-@pytest.mark.skip(reason='Is flaky. Issue: #2492')
 def test_stress(
         raiden_network,
         deposit,
