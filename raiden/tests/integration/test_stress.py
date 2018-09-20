4aa005b931d4a0c326b1f9a3788a19e544ec253f
Lefteris Karapetsas <lefteris@refu.co>
2018-09-20 13:47:41 +0200

    Skip stress test again
    Reopened issue #2492

--- raiden/tests/integration/test_stress.py
+++ raiden/tests/integration/test_stress.py
@@ -296,6 +296,7 @@ def assert_channels(raiden_network, token_network_identifier, deposit):
 @pytest.mark.parametrize('deposit', [5])
 @pytest.mark.parametrize('reveal_timeout', [15])
 @pytest.mark.parametrize('settle_timeout', [120])
+@pytest.mark.skip(reason='Issue 2492')
 def test_stress(
         raiden_network,
         deposit,
