1702a3f760d094759a0fc577ab36c51e0200614b
Ulrich Petri <ulo@ulo.pe>
2018-09-25 17:02:04 +0200

    Implement http status code matching
    All Raiden API tasks now take an `expected_http_status` argument
    which defaults to `2..` and is parsed as a regex (integers are also accepted).
    If the API call's http status doesn't match the regex a
    `RESTAPIStatusMismatchError` exception is raised.
    
    This can be used to test for various failure cases.

--- tools/scenario-player/scenario_player/tasks/raiden_api.py
+++ tools/scenario-player/scenario_player/tasks/raiden_api.py
@@ -1,6 +1,10 @@
+import re
+from typing import Any, Union
+
 from requests import RequestException
 
-from scenario_player.exceptions import RESTAPIError
+from scenario_player.exceptions import RESTAPIError, RESTAPIStatusMismatchError
+from scenario_player.runner import ScenarioRunner
 
 from .base import Task
 
@@ -9,6 +13,19 @@ class RaidenAPIActionTask(Task):
     _name = ''
     _url_template = ""
     _method = 'get'
+    _expected_http_status: Union[int, str] = '2..'
+
+    def __init__(
+        self,
+        runner: ScenarioRunner,
+        config: Any,
+        parent: 'Task' = None,
+        abort_on_fail=True,
+    ) -> None:
+        super().__init__(runner, config, parent, abort_on_fail)
+
+        self._expected_http_status = config.get('expected_http_status', self._expected_http_status)
+        self._http_status_re = re.compile(f'^{self._expected_http_status}$')
 
     @property
     def _request_params(self):
@@ -22,8 +39,8 @@ class RaidenAPIActionTask(Task):
     def _target_host(self):
         return self._runner.raiden_nodes[self._config['from']]
 
-    def _process_response(self, response: dict):
-        return response
+    def _process_response(self, response_dict: dict):
+        return response_dict
 
     def _run(self, *args, **kwargs):
         url = self._url_template.format(
@@ -35,8 +52,11 @@ class RaidenAPIActionTask(Task):
             resp = self._runner.session.request(self._method, url, json=self._request_params)
         except RequestException as ex:
             raise RESTAPIError(f'Error performing REST-API call: {self._name}') from ex
-        if not 199 < resp.status_code < 300:
-            raise RESTAPIError(f'Status {resp.status_code} while fetching {url}: {resp.text}')
+        if not self._http_status_re.match(str(resp.status_code)):
+            raise RESTAPIStatusMismatchError(
+                f'HTTP status code "{resp.status_code}" while fetching {url}. '
+                f'Expected {self._expected_http_status}: {resp.text}',
+            )
         try:
             return self._process_response(resp.json())
         except (ValueError, UnicodeDecodeError) as ex:
