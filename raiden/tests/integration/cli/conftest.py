581b3bf36b2683022aec5da4d88e17520043a5f7
Augusto F. Hack <hack.augusto@gmail.com>
2018-09-26 16:02:39 -0300

    smoketest: simplified smoketest configuration file
    The only piece of data used from the smoketest configuration file was
    the genesis file. The smart contract addresses were out-dated since the
    test deploy the contracts in each run, the alloc was duplicated, and
    the versions didn't correspond to the deployed version of the smart
    contract.

--- raiden/tests/integration/cli/conftest.py
+++ raiden/tests/integration/cli/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from raiden.tests.utils.smoketest import load_smoketest_config, setup_testchain_and_raiden
+from raiden.tests.utils.smoketest import setup_testchain_and_raiden
 
 
 def append_arg_if_existing(argname, initial_args, new_args):
@@ -11,9 +11,7 @@ def append_arg_if_existing(argname, initial_args, new_args):
 
 @pytest.fixture(scope='session')
 def blockchain_provider():
-    smoketest_config = load_smoketest_config()
     result = setup_testchain_and_raiden(
-        smoketest_config,
         'matrix',
         'auto',
         lambda x: None,
