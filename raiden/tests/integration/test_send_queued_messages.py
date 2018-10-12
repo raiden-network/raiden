0c86b8783b0da55d63b0ec13b01fe0be4f32698d
Augusto F. Hack <hack.augusto@gmail.com>
2018-10-12 10:44:16 -0300

    simplified test_send_queued_messages

--- raiden/tests/integration/test_send_queued_messages.py
+++ raiden/tests/integration/test_send_queued_messages.py
@@ -2,10 +2,6 @@ import gevent
 import pytest
 
 from raiden import waiting
-from raiden.app import App
-from raiden.message_handler import MessageHandler
-from raiden.network.transport import MatrixTransport
-from raiden.raiden_event_handler import RaidenEventHandler
 from raiden.tests.utils.network import CHAIN
 from raiden.tests.utils.protocol import dont_handle_node_change_network_state
 from raiden.tests.utils.transfer import assert_synced_channel_state
@@ -21,7 +17,7 @@ def test_send_queued_messages(
         deposit,
         token_addresses,
         network_wait,
-        skip_if_not_matrix,
+        skip_if_not_matrix,  # pylint: disable=unused-argument
 ):
     """Test re-sending of undelivered messages on node restart"""
     app0, app1 = raiden_network
@@ -51,49 +47,28 @@ def test_send_queued_messages(
             )
             identifier += 1
 
-    # restart app0
-    app0.raiden.stop()
-
-    new_transport = MatrixTransport(
-        app0.raiden.config['transport']['matrix'],
-    )
-
-    raiden_event_handler = RaidenEventHandler()
-    message_handler = MessageHandler()
-
-    app0_restart = App(
-        config=app0.config,
-        chain=app0.raiden.chain,
-        query_start_block=0,
-        default_registry=app0.raiden.default_registry,
-        default_secret_registry=app0.raiden.default_secret_registry,
-        transport=new_transport,
-        raiden_event_handler=raiden_event_handler,
-        message_handler=message_handler,
-        discovery=app0.raiden.discovery,
-    )
-
     app0.stop()
-    del app0  # from here on the app0_restart should be used
+    app0.get()
+    app0.start()
+
+    app1.get()
     app1.start()
 
-    app0_restart.start()
-
     waiting.wait_for_healthy(
-        app0_restart.raiden,
+        app0.raiden,
         app1.raiden.address,
         network_wait,
     )
     waiting.wait_for_healthy(
         app1.raiden,
-        app0_restart.raiden.address,
+        app0.raiden.address,
         network_wait,
     )
 
     exception = RuntimeError('Timeout while waiting for new channel')
     with gevent.Timeout(5, exception=exception):
         waiting.wait_for_newchannel(
-            raiden=app0_restart.raiden,
+            raiden=app0.raiden,
             payment_network_id=payment_network_id,
             token_address=token_address,
             partner_address=app1.raiden.address,
@@ -102,7 +77,7 @@ def test_send_queued_messages(
     exception = RuntimeError('Timeout while waiting for balance update for app0')
     with gevent.Timeout(30, exception=exception):
         waiting.wait_for_payment_balance(
-            raiden=app0_restart.raiden,
+            raiden=app0.raiden,
             payment_network_id=payment_network_id,
             token_address=token_address,
             partner_address=app1.raiden.address,
@@ -115,7 +90,7 @@ def test_send_queued_messages(
         raiden=app1.raiden,
         payment_network_id=payment_network_id,
         token_address=token_address,
-        partner_address=app0_restart.raiden.address,
+        partner_address=app0.raiden.address,
         target_address=app1.raiden.address,
         target_balance=spent_amount,
         retry_timeout=network_wait,
@@ -123,6 +98,6 @@ def test_send_queued_messages(
 
     assert_synced_channel_state(
         token_network_identifier,
-        app0_restart, deposit - spent_amount, [],
+        app0, deposit - spent_amount, [],
         app1, deposit + spent_amount, [],
     )
