8f8912716e4e1f51e58135b6a1b8bfe4012afe03
Ulrich Petri <ulo@ulo.pe>
2018-10-18 15:59:00 +0200

    Fix homebrew formula updating
    - Use sed extended regexes
    - Allow multiple digit version numbers
    - Fix working directory bug
    
    [no ci integration]
    
    Fixes #2426

--- .travis/update_formula.sh
+++ .travis/update_formula.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
-set -x
-
-clone_repo() {
-    git clone git@github.com:raiden-network/homebrew-raiden
-    cd homebrew-raiden
-}
+set -ex
 
 setup_git() {
     openssl aes-256-cbc -K ${encrypted_d89a2734327d_key} -iv ${encrypted_d89a2734327d_iv} -in .travis/homebrew-raiden_github_deploy.enc -out ${HOME}/homebrew-raiden_github_deploy -d
@@ -18,24 +12,32 @@ setup_git() {
     git config --global user.name "Raiden Network"
 }
 
+clone_repo() {
+    git clone git@github.com:raiden-network/homebrew-raiden
+}
+
 update_formula() {
-    UPDATED_SHA256=$(openssl sha -sha256 dist/archive/raiden-${TRAVIS_TAG}-macOS.zip)
+    UPDATED_SHA256=$(openssl dgst -sha256 dist/archive/raiden-${TRAVIS_TAG}-macOS.zip)
     FORMULA_FILE="homebrew-raiden/raiden.rb"
 
-    sed -i .bak "s/[0-9]\.[0-9]\.[0-9]/${TRAVIS_TAG/v/}/g" $FORMULA_FILE
-    sed -i .bak "s/sha256 \"[a-f0-9]\{64\}\"/sha256 \"${UPDATED_SHA256: -64}\"/g" $FORMULA_FILE
+    sed -i .bak -E "s/[0-9]+\.[0-9]+\.[0-9]+/${TRAVIS_TAG/v/}/g" $FORMULA_FILE
+    sed -i .bak -E "s/sha256 \"[a-f0-9]\{64\}\"/sha256 \"${UPDATED_SHA256: -64}\"/g" $FORMULA_FILE
 
     rm $FORMULA_FILE.bak
 }
 
 commit_formula_file() {
+    pushd homebrew-raiden
     git add raiden.rb
     git commit -m "Update formula to ${TRAVIS_TAG}"
-    git tag -a "${TRAVIS_TAG}"
+    git tag -a "${TRAVIS_TAG}" -m "${TRAVIS_TAG}"
+    popd
 }
 
 upload_file() {
+    pushd homebrew-raiden
     git push --follow-tags
+    popd
 }
 
 setup_git
