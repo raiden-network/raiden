01239097f49ef9d1fcf8b55dc73235b604d0b13e
Lefteris Karapetsas <lefteris@refu.co>
2018-10-19 12:12:50 +0200

    Take expected_version from deployed contract data
    Also reinstate version check. Fix #2836

--- raiden/network/proxies/secret_registry.py
+++ raiden/network/proxies/secret_registry.py
@@ -14,7 +14,6 @@ from raiden.exceptions import (
 )
 from raiden.network.rpc.client import StatelessFilter, check_address_has_code
 from raiden.network.rpc.transactions import check_transaction_threw
-from raiden.settings import EXPECTED_CONTRACTS_VERSION
 from raiden.utils import compare_versions, pex, privatekey_to_address, sha3, typing
 from raiden_contracts.constants import CONTRACT_SECRET_REGISTRY, EVENT_SECRET_REVEALED
 from raiden_contracts.contract_manager import ContractManager
@@ -43,7 +42,7 @@ class SecretRegistry:
         try:
             if not compare_versions(
                     proxy.contract.functions.contract_version().call(),
-                    EXPECTED_CONTRACTS_VERSION,
+                    contract_manager.contracts_version,
             ):
                 raise ContractVersionMismatch('Incompatible ABI for SecretRegistry')
         except BadFunctionCallOutput:
