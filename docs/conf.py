bfd8bac0daabfca5f77deaef067f9140bc7d2a03
Lefteris Karapetsas <lefteris@refu.co>
2018-10-25 11:47:15 +0200

    Bump version: 0.15.1 → 0.100.0

--- docs/conf.py
+++ docs/conf.py
@@ -94,7 +94,7 @@ master_doc = 'index'
 project = 'Raiden Network'
 author = 'Raiden Project'
 
-version_string = '0.16.0'
+version_string = '0.100.0'
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
