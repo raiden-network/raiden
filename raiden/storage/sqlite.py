bb5f5aee37f48d95209d0d0aeb63cdead50ce24b
Paul Lange <palango@gmx.de>
2018-11-05 15:43:08 +0100

    Increase DB version for PR #2929
    [no ci integration]

--- raiden/storage/sqlite.py
+++ raiden/storage/sqlite.py
@@ -8,7 +8,7 @@ from raiden.storage.utils import DB_SCRIPT_CREATE_TABLES, TimestampedEvent
 from raiden.utils import typing
 
 # The latest DB version
-RAIDEN_DB_VERSION = 9
+RAIDEN_DB_VERSION = 10
 
 
 class EventRecord(typing.NamedTuple):
