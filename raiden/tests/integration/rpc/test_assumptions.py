3da0baded2863b07462a0c79d1ce7d147d7efb45
Paul Lange <palango@gmx.de>
2018-11-07 12:22:26 +0100

    PR review 3

--- raiden/tests/integration/rpc/test_assumptions.py
+++ raiden/tests/integration/rpc/test_assumptions.py
@@ -187,7 +187,8 @@ def test_transact_throws_opcode(deploy_client):
     address = contract_proxy.contract_address
     assert len(deploy_client.web3.eth.getCode(to_checksum_address(address))) > 0
 
-    startgas = deploy_client.gaslimit()
+    # the gas estimation returns 0 here, so hardcode a value
+    startgas = safe_gas_limit(22_000)
 
     transaction = contract_proxy.transact('fail', startgas)
     deploy_client.poll(transaction)
@@ -202,7 +203,8 @@ def test_transact_opcode_oog(deploy_client):
     address = contract_proxy.contract_address
     assert len(deploy_client.web3.eth.getCode(to_checksum_address(address))) > 0
 
-    startgas = min(contract_proxy.estimate_gas('loop', 1000) // 2, deploy_client.gaslimit())
+    # divide the estimate by 2 to run into out-of-gas
+    startgas = safe_gas_limit(contract_proxy.estimate_gas('loop', 1000)) // 2
 
     transaction = contract_proxy.transact('loop', startgas, 1000)
     deploy_client.poll(transaction)
