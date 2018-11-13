b21fe2cfe595c4a4177ccf07e506aa399a41f5a9
Augusto F. Hack <hack.augusto@gmail.com>
2018-11-13 06:22:01 -0200

    PR review

--- raiden/storage/versions.py
+++ raiden/storage/versions.py
@@ -9,9 +9,10 @@ from .sqlite import RAIDEN_DB_VERSION
 VERSION_RE = re.compile("^v(\d+).*")
 
 
-def older_db_files_exist(database_base_path: str) -> typing.Optional[str]:
-    """ Check if the directory contains database files that
-    belong to the previous version of the schema. """
+def older_db_file(database_base_path: str) -> typing.Optional[str]:
+    """ Returns the path to a database file that belong to the previous version
+    of the schema.
+    """
     database_base_path = os.path.expanduser(database_base_path)
     db_files = glob(f'{database_base_path}/**/*.db', recursive=True)
     for db_file in db_files:
