4c25917e0d31e86cb95247878bd966949640209c
Paul Lange <palango@gmx.de>
2018-11-13 15:34:57 +0100

    Increment DB version

--- raiden/storage/sqlite.py
+++ raiden/storage/sqlite.py
@@ -8,7 +8,7 @@ from raiden.storage.utils import DB_SCRIPT_CREATE_TABLES, TimestampedEvent
 from raiden.utils import typing
 
 # The latest DB version
-RAIDEN_DB_VERSION = 11
+RAIDEN_DB_VERSION = 12
 
 
 class EventRecord(typing.NamedTuple):
