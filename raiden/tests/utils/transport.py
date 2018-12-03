6e7adb39b6b0287c5fb3efa2072aac2d0de69bca
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2018-12-03 14:51:13 +0100

    Give the RaidenServiceMock additional attributes to work with matrix

--- raiden/tests/utils/transport.py
+++ raiden/tests/utils/transport.py
@@ -1,4 +1,12 @@
+import random
+
+from raiden.storage.serialize import JSONSerializer
+from raiden.storage.sqlite import SQLiteStorage
+from raiden.storage.wal import WriteAheadLog
 from raiden.tests.utils import factories
+from raiden.transfer import node
+from raiden.transfer.architecture import StateManager
+from raiden.transfer.state_change import ActionInitChain
 
 
 class MockChain:
@@ -7,10 +15,29 @@ class MockChain:
 
 
 class MockRaidenService:
-    def __init__(self, message_handler=None):
+    def __init__(self, message_handler=None, state_transition=None):
         self.chain = MockChain()
         self.private_key, self.address = factories.make_privatekey_address()
+
+        self.chain.node_address = self.address
         self.message_handler = message_handler
+
+        if state_transition is None:
+            state_transition = node.state_transition
+
+        serializer = JSONSerializer
+        state_manager = StateManager(state_transition, None)
+        storage = SQLiteStorage(':memory:', serializer)
+        self.wal = WriteAheadLog(state_manager, storage)
+
+        state_change = ActionInitChain(
+            random.Random(),
+            0,
+            self.chain.node_address,
+            self.chain.network_id,
+        )
+
+        self.wal.log_and_dispatch(state_change)
 
     def on_message(self, message):
         if self.message_handler:
