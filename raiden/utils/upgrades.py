3979e4c80e5e482977e7b0721ebbd920e3e86d2e
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-01-21 14:40:48 +0100

    Introduce a hardcoded upgrades list for the upgrade manager

--- raiden/utils/upgrades.py
+++ raiden/utils/upgrades.py
@@ -1,11 +1,15 @@
-import importlib
 import os
 import shutil
 from pathlib import Path
 
-from raiden.exceptions import RaidenDBUpgradeBackupError, RaidenDBUpgradeExecutionError
+from raiden.exceptions import RaidenDBUpgradeBackupError
 from raiden.storage.serialize import JSONSerializer
 from raiden.storage.sqlite import SQLiteStorage
+from raiden.utils.migrations.v16_to_v17 import upgrade_initiator_manager
+
+UPGRADES_LIST = [
+    upgrade_initiator_manager,
+]
 
 
 class UpgradeManager:
@@ -13,17 +17,14 @@ class UpgradeManager:
     need to be executed in order to bring the database up to date
     with the current implementation.
     """
-
-    MIGRATIONS_MODULE = 'raiden.utils.migrations'
-
     def __init__(
             self,
             db_filename: str,
+            old_version: int,
             current_version: int,
-            new_version: int,
     ):
+        self._old_version = old_version
         self._current_version = current_version
-        self._new_version = new_version
         self._db_filename = Path(db_filename)
         self._backup_filename = self._db_filename.parent / Path(
             f'version{self._current_version}_db.backup',
@@ -34,10 +35,8 @@ class UpgradeManager:
 
         self._backup()
 
-        for version in range(self._current_version, self._new_version):
-            upgrade_module_name = f'v{version}_to_v{version+1}'
-            upgrade_module = self._load_module(upgrade_module_name)
-            upgrade_module.upgrade(storage)
+        for upgrade_func in UPGRADES_LIST:
+            upgrade_func(storage, self._old_version, self._current_version)
 
     def restore_backup(self):
         os.remove(str(self._db_filename))
@@ -48,13 +47,3 @@ class UpgradeManager:
 
         if not self._backup_filename.exists():
             raise RaidenDBUpgradeBackupError()
-
-    def _load_module(self, module_name):
-        try:
-            return importlib.import_module(
-                f'{self.MIGRATIONS_MODULE}.{module_name}',
-            )
-        except ImportError:
-            raise RaidenDBUpgradeExecutionError(
-                f'Could not import module {module_name}',
-            )
