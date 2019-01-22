80e5b93601b7e2d0b7be4cad866952f5c5b5c982
Augusto F. Hack <hack.augusto@gmail.com>
2019-01-21 23:46:30 -0200

    fix unlock calls
    The token network smart contract has a few optimizations, one of these
    optimizations is to remove storage data as soon as possible, to reduce
    gas usage.
    
    Because of that, once the channel is settled the channel structs and
    their corresponding participant structs are removed. This removes the
    following data from the smart contract:
    
    participant.deposit
    participant.withdraw
    participant.is_the_closer
    participant.balance_hash
    participant.nonce
    
    and
    
    channel.settle_block_number
    channel.state
    
    Meaning the processing of unlock requests cannot rely on the
    availability of the balance_hash and nonce data to find the
    corresponding merkle tree. For a proper unlock the locksroot must be
    used.
    
    In the other hand, because of privacy concerns, the data provided to the
    functions closed and updateTransfer is blinded, this is done to avoid
    sharing the all the details with the monitoring service. Therefore the
    event which request the call to settle, which has to find the complete
    balance proof can not rely on the locksroot for the search, it must use
    the balance_hash.
    
    Because of the above, now we have four functions to search for the
    channel data, two for our data, two for the partner data, one of each
    for the call to settle and unlock.

--- raiden/transfer/utils.py
+++ raiden/transfer/utils.py
@@ -17,7 +17,7 @@ from raiden.utils.typing import (
 )
 
 
-def get_state_change_with_balance_proof(
+def get_state_change_with_balance_proof_by_balance_hash(
         storage: sqlite.SQLiteStorage,
         chain_id: ChainID,
         token_network_identifier: TokenNetworkID,
@@ -27,6 +27,9 @@ def get_state_change_with_balance_proof(
 ) -> sqlite.StateChangeRecord:
     """ Returns the state change which contains the corresponding balance
     proof.
+
+    Use this function to find a balance proof for a call to settle, which only
+    have the blinded balance proof data.
     """
     return storage.get_latest_state_change_by_data_field({
         'balance_proof.chain_id': chain_id,
@@ -37,7 +40,31 @@ def get_state_change_with_balance_proof(
     })
 
 
-def get_event_with_balance_proof(
+def get_state_change_with_balance_proof_by_locksroot(
+        storage: sqlite.SQLiteStorage,
+        chain_id: ChainID,
+        token_network_identifier: TokenNetworkID,
+        channel_identifier: ChannelID,
+        locksroot: Locksroot,
+        sender: Address,
+) -> sqlite.StateChangeRecord:
+    """ Returns the state change which contains the corresponding balance
+    proof.
+
+    Use this function to find a balance proof for a call to unlock, which only
+    happens after settle, so the channel has the unblinded of the balance
+    proof.
+    """
+    return storage.get_latest_state_change_by_data_field({
+        'balance_proof.chain_id': chain_id,
+        'balance_proof.token_network_identifier': to_checksum_address(token_network_identifier),
+        'balance_proof.channel_identifier': str(channel_identifier),
+        'balance_proof.locksroot': serialize_bytes(locksroot),
+        'balance_proof.sender': to_checksum_address(sender),
+    })
+
+
+def get_event_with_balance_proof_by_balance_hash(
         storage: sqlite.SQLiteStorage,
         chain_id: ChainID,
         token_network_identifier: TokenNetworkID,
@@ -46,12 +73,36 @@ def get_event_with_balance_proof(
 ) -> sqlite.EventRecord:
     """ Returns the event which contains the corresponding balance
     proof.
+
+    Use this function to find a balance proof for a call to settle, which only
+    have the blinded balance proof data.
     """
     return storage.get_latest_event_by_data_field({
         'balance_proof.chain_id': chain_id,
         'balance_proof.token_network_identifier': to_checksum_address(token_network_identifier),
         'balance_proof.channel_identifier': str(channel_identifier),
         'balance_proof.balance_hash': serialize_bytes(balance_hash),
+    })
+
+
+def get_event_with_balance_proof_by_locksroot(
+        storage: sqlite.SQLiteStorage,
+        chain_id: ChainID,
+        token_network_identifier: TokenNetworkID,
+        channel_identifier: ChannelID,
+        locksroot: Locksroot,
+) -> sqlite.EventRecord:
+    """ Returns the event which contains the corresponding balance proof.
+
+    Use this function to find a balance proof for a call to unlock, which only
+    happens after settle, so the channel has the unblinded of the balance
+    proof.
+    """
+    return storage.get_latest_event_by_data_field({
+        'balance_proof.chain_id': chain_id,
+        'balance_proof.token_network_identifier': to_checksum_address(token_network_identifier),
+        'balance_proof.channel_identifier': str(channel_identifier),
+        'balance_proof.locksroot': serialize_bytes(locksroot),
     })
 
 
