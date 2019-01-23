raiden-network/raiden

commit f3f9facbbc6550e9aadd291a8eb565f3c225fbce

PR review 3

raiden/network/proxies/secret_registry.py
@@ -99,10 +99,7 @@ class SecretRegistry:
 
     def _register_secret_batch(self, secrets):
         gas_limit = self.proxy.estimate_gas('registerSecretBatch', secrets)
-        gas_limit = max(
-            gas_limit,
-            safe_gas_limit(len(secrets) * GAS_REQUIRED_PER_SECRET_IN_BATCH),
-        )
+        gas_limit = safe_gas_limit(gas_limit, len(secrets) * GAS_REQUIRED_PER_SECRET_IN_BATCH)
         transaction_hash = self.proxy.transact('registerSecretBatch', gas_limit, secrets)
         self.client.poll(transaction_hash)
         receipt_or_none = check_transaction_threw(self.client, transaction_hash)

raiden/network/proxies/token.py
@@ -6,7 +6,7 @@ from raiden.exceptions import TransactionThrew
 from raiden.network.rpc.client import check_address_has_code
 from raiden.network.rpc.smartcontract_proxy import ContractProxy
 from raiden.network.rpc.transactions import check_transaction_threw
-from raiden.utils import pex, privatekey_to_address
+from raiden.utils import pex, privatekey_to_address, safe_gas_limit
 from raiden_contracts.constants import CONTRACT_HUMAN_STANDARD_TOKEN
 from raiden_contracts.contract_manager import ContractManager
 
@@ -67,7 +67,7 @@ class Token:
 
         transaction_hash = self.proxy.transact(
             'approve',
-            startgas,
+            safe_gas_limit(startgas),
             to_checksum_address(allowed_address),
             allowance,
         )
@@ -133,7 +133,7 @@ class Token:
         startgas = GAS_LIMIT_FOR_TOKEN_CONTRACT_CALL
         transaction_hash = self.proxy.transact(
             'transfer',
-            startgas,
+            safe_gas_limit(startgas),
             to_checksum_address(to_address),
             amount,
         )

raiden/network/rpc/client.py
@@ -257,21 +257,12 @@ class JSONRPCClient:
         """ Return the balance of the account of given address. """
         return self.web3.eth.getBalance(to_checksum_address(account), 'pending')
 
-    def gaslimit(self, location='latest') -> int:
-        gas_limit = self.web3.eth.getBlock(location)['gasLimit']
-        return gas_limit * 8 // 10
-
     def gas_price(self) -> int:
         # generateGasPrice takes the transaction to be send as an optional argument
         # but both strategies that we are using (time-based and rpc-based) don't make
         # use of this argument. It is therefore safe to not provide it at the moment.
         # This needs to be reevaluated if we use different gas price strategies
         return int(self.web3.eth.generateGasPrice())
-
-    def check_startgas(self, startgas):
-        if not startgas:
-            return self.gaslimit()
-        return startgas
 
     def new_contract_proxy(self, contract_interface, contract_address: typing.Address):
         """ Return a proxy for interacting with a smart contract.
@@ -368,9 +359,10 @@ class JSONRPCClient:
 
                 dependency_contract['bin'] = bytecode
 
+                gas_limit = self.web3.eth.getBlock('latest')['gasLimit'] * 8 // 10
                 transaction_hash = self.send_transaction(
                     to=typing.Address(b''),
-                    startgas=self.gaslimit(),
+                    startgas=gas_limit,
                     data=bytecode,
                 )
 

raiden/tests/integration/rpc/test_assumptions.py
@@ -187,7 +187,8 @@ def test_transact_throws_opcode(deploy_client):
     address = contract_proxy.contract_address
     assert len(deploy_client.web3.eth.getCode(to_checksum_address(address))) > 0
 
-    startgas = deploy_client.gaslimit()
+    # the gas estimation returns 0 here, so hardcode a value
+    startgas = safe_gas_limit(22_000)
 
     transaction = contract_proxy.transact('fail', startgas)
     deploy_client.poll(transaction)
@@ -202,7 +203,8 @@ def test_transact_opcode_oog(deploy_client):
     address = contract_proxy.contract_address
     assert len(deploy_client.web3.eth.getCode(to_checksum_address(address))) > 0
 
-    startgas = min(contract_proxy.estimate_gas('loop', 1000) // 2, deploy_client.gaslimit())
+    # divide the estimate by 2 to run into out-of-gas
+    startgas = safe_gas_limit(contract_proxy.estimate_gas('loop', 1000)) // 2
 
     transaction = contract_proxy.transact('loop', startgas, 1000)
     deploy_client.poll(transaction)

raiden/tests/integration/test_endpointregistry.py
@@ -4,7 +4,7 @@ from raiden.exceptions import UnknownAddress
 from raiden.network.discovery import ContractDiscovery
 from raiden.tests.utils.factories import make_address
 from raiden.tests.utils.smartcontracts import deploy_contract_web3
-from raiden.utils import host_port_to_endpoint, privatekey_to_address
+from raiden.utils import host_port_to_endpoint, privatekey_to_address, safe_gas_limit
 from raiden_contracts.constants import (
     CONTRACT_ENDPOINT_REGISTRY,
     GAS_REQUIRED_FOR_ENDPOINT_REGISTER,
@@ -55,7 +55,7 @@ def test_endpointregistry_gas(endpoint_discovery_services):
 
     transaction_hash = discovery_proxy.proxy.transact(
         'registerEndpoint',
-        GAS_REQUIRED_FOR_ENDPOINT_REGISTER,
+        safe_gas_limit(GAS_REQUIRED_FOR_ENDPOINT_REGISTER),
         endpoint,
     )
     discovery_proxy.client.poll(transaction_hash)

commit 7e4cd43953f62b8eec48d4026e93bae9ce4a706a

Try to handle estimateGas predicting that a call will fail
After the changes introduced by
https://github.com/raiden-network/raiden/pull/2623/ we again rely on gas
estimation for the gas limit to provide to an onchain call which is great since
it reduces our overall ETH balance requirements.

But we don't take into account the fact that if estimateGas returns None it's
because the transaction will fail, either due to our account not having enough
funds or because the call will just revert somewhere down the line due to the
contract logic (e.g. calling settle before closing).

With that in mind we can now detect those bad cases a bit earlier and handle
them without sending an onchain transaction. This is what this PR is trying to achieve.

raiden/network/proxies/secret_registry.py
@@ -5,7 +5,7 @@ from eth_utils import encode_hex, event_abi_to_log_topic, is_binary_address, to_
 from gevent.event import AsyncResult
 
 from raiden.constants import GAS_REQUIRED_PER_SECRET_IN_BATCH, GENESIS_BLOCK_NUMBER
-from raiden.exceptions import InvalidAddress, TransactionThrew
+from raiden.exceptions import InvalidAddress, RaidenUnrecoverableError, TransactionThrew
 from raiden.network.proxies.utils import compare_contract_versions
 from raiden.network.rpc.client import StatelessFilter, check_address_has_code
 from raiden.network.rpc.transactions import check_transaction_threw
@@ -82,10 +82,17 @@ class SecretRegistry:
             log.debug('registerSecretBatch skipped', **log_details)
             return
 
-        log.debug('registerSecretBatch called', **log_details)
+        gas_limit = self.proxy.estimate_gas('registerSecretBatch', secrets)
+        if not gas_limit:
+            log.critical(
+                'registerSecretBatch will always fail',
+                **log_details,
+            )
+            raise RaidenUnrecoverableError('RegisterSecretBatch transaction will always fail')
 
         try:
-            transaction_hash = self._register_secret_batch(secrets_to_register)
+            log.debug('registerSecretBatch called', **log_details)
+            transaction_hash = self._register_secret_batch(secrets_to_register, gas_limit)
         except Exception as e:
             log.critical('registerSecretBatch failed', **log_details)
             secret_registry_transaction.set_exception(e)
@@ -97,8 +104,12 @@ class SecretRegistry:
             for secret in secrets_to_register:
                 self.open_secret_transactions.pop(secret, None)
 
-    def _register_secret_batch(self, secrets):
-        gas_limit = self.proxy.estimate_gas('registerSecretBatch', secrets)
+    def _register_secret_batch(
+            self,
+            secrets: List[typing.Secret],
+            gas_limit: int,
+    ) -> typing.TransactionHash:
+
         gas_limit = safe_gas_limit(gas_limit, len(secrets) * GAS_REQUIRED_PER_SECRET_IN_BATCH)
         transaction_hash = self.proxy.transact('registerSecretBatch', gas_limit, secrets)
         self.client.poll(transaction_hash)

raiden/network/proxies/token.py
@@ -2,11 +2,11 @@ import structlog
 from eth_utils import is_binary_address, to_checksum_address, to_normalized_address
 
 from raiden.constants import GAS_LIMIT_FOR_TOKEN_CONTRACT_CALL
-from raiden.exceptions import TransactionThrew
+from raiden.exceptions import RaidenUnrecoverableError, TransactionThrew
 from raiden.network.rpc.client import check_address_has_code
 from raiden.network.rpc.smartcontract_proxy import ContractProxy
 from raiden.network.rpc.transactions import check_transaction_threw
-from raiden.utils import pex, privatekey_to_address, safe_gas_limit
+from raiden.utils import pex, privatekey_to_address, safe_gas_limit, typing
 from raiden_contracts.constants import CONTRACT_HUMAN_STANDARD_TOKEN
 from raiden_contracts.contract_manager import ContractManager
 
@@ -42,7 +42,7 @@ class Token:
             to_checksum_address(spender),
         ).call()
 
-    def approve(self, allowed_address, allowance):
+    def approve(self, allowed_address: typing.AddressHex, allowance: typing.TokenAmount):
         """ Aprove `allowed_address` to transfer up to `deposit` amount of token.
 
         Note:
@@ -57,14 +57,21 @@ class Token:
             'allowed_address': pex(allowed_address),
             'allowance': allowance,
         }
-        log.debug('approve called', **log_details)
 
         startgas = self.proxy.estimate_gas(
             'approve',
             to_checksum_address(allowed_address),
             allowance,
         )
+        if not startgas:
+            msg = self._check_why_approved_failed(allowance)
+            log.critical(
+                f'approve transaction will always fail due to,  {msg}',
+                **log_details,
+            )
+            raise RaidenUnrecoverableError('Approve transaction will always fail')
 
+        log.debug('approve called', **log_details)
         transaction_hash = self.proxy.transact(
             'approve',
             safe_gas_limit(startgas),
@@ -76,44 +83,47 @@ class Token:
         receipt_or_none = check_transaction_threw(self.client, transaction_hash)
 
         if receipt_or_none:
-            user_balance = self.balance_of(self.client.address)
-
-            # If the balance is zero, either the smart contract doesnt have a
-            # balanceOf function or the actual balance is zero
-            if user_balance == 0:
-                msg = (
-                    "Approve failed. \n"
-                    "Your account balance is 0 (zero), either the smart "
-                    "contract is not a valid ERC20 token or you don't have funds "
-                    "to use for openning a channel. "
-                )
-
-            # The approve call failed, check the user has enough balance
-            # (assuming the token smart contract may check for the maximum
-            # allowance, which is not necessarily the case)
-            elif user_balance < allowance:
-                msg = (
-                    'Approve failed. \n'
-                    'Your account balance is {}, nevertheless the call to '
-                    'approve failed. Please make sure the corresponding smart '
-                    'contract is a valid ERC20 token.'
-                ).format(user_balance)
-
-            # If the user has enough balance, warn the user the smart contract
-            # may not have the approve function.
-            else:
-                msg = (
-                    f'Approve failed. \n'
-                    f'Your account balance is {user_balance}, '
-                    f'the request allowance is {allowance}. '
-                    f'The smart contract may be rejecting your request for the '
-                    f'lack of balance.'
-                )
-
+            msg = self._check_why_approved_failed(allowance)
             log.critical(f'approve failed, {msg}', **log_details)
             raise TransactionThrew(msg, receipt_or_none)
 
         log.info('approve successful', **log_details)
+
+    def _check_why_approved_failed(self, allowance: typing.TokenAmount) -> str:
+        user_balance = self.balance_of(self.client.address)
+
+        # If the balance is zero, either the smart contract doesnt have a
+        # balanceOf function or the actual balance is zero
+        if user_balance == 0:
+            msg = (
+                "Approve failed. \n"
+                "Your account balance is 0 (zero), either the smart "
+                "contract is not a valid ERC20 token or you don't have funds "
+                "to use for openning a channel. "
+            )
+
+        # The approve call failed, check the user has enough balance
+        # (assuming the token smart contract may check for the maximum
+        # allowance, which is not necessarily the case)
+        elif user_balance < allowance:
+            msg = (
+                'Approve failed. \n'
+                'Your account balance is {}, nevertheless the call to '
+                'approve failed. Please make sure the corresponding smart '
+                'contract is a valid ERC20 token.'
+            ).format(user_balance)
+
+        # If the user has enough balance, warn the user the smart contract
+        # may not have the approve function.
+        else:
+            msg = (
+                f'Approve failed. \n'
+                f'Your account balance is {user_balance}, '
+                f'the request allowance is {allowance}. '
+                f'The smart contract may be rejecting your request for the '
+                f'lack of balance.'
+            )
+        return msg
 
     def balance_of(self, address):
         """ Return the balance of `address`. """

raiden/network/proxies/token_network.py
@@ -166,6 +166,16 @@ class TokenNetwork:
             'peer1': pex(self.node_address),
             'peer2': pex(partner),
         }
+        gas_limit = self.proxy.estimate_gas(
+            'openChannel',
+            self.node_address,
+            partner,
+            settle_timeout,
+        )
+        if not gas_limit:
+            log.critical('openChannel will always fail', **log_details)
+            raise RaidenUnrecoverableError('creating new channel failed')
+
         log.debug('new_netting_channel called', **log_details)
 
         # Prevent concurrent attempts to open a channel with the same token and
@@ -175,7 +185,11 @@ class TokenNetwork:
             self.open_channel_transactions[partner] = new_open_channel_transaction
 
             try:
-                transaction_hash = self._new_netting_channel(partner, settle_timeout)
+                transaction_hash = self._new_netting_channel(
+                    partner=partner,
+                    settle_timeout=settle_timeout,
+                    gas_limit=gas_limit,
+                )
             except Exception as e:
                 log.critical('new_netting_channel failed', **log_details)
                 new_open_channel_transaction.set_exception(e)
@@ -202,16 +216,15 @@ class TokenNetwork:
 
         return channel_identifier
 
-    def _new_netting_channel(self, partner: typing.Address, settle_timeout: int):
+    def _new_netting_channel(
+            self,
+            partner: typing.Address,
+            settle_timeout: int,
+            gas_limit: int,
+    ) -> typing.TransactionHash:
         if self.channel_exists_and_not_settled(self.node_address, partner):
             raise DuplicatedChannelError('Channel with given partner address already exists')
 
-        gas_limit = self.proxy.estimate_gas(
-            'openChannel',
-            self.node_address,
-            partner,
-            settle_timeout,
-        )
         gas_limit = safe_gas_limit(gas_limit, GAS_REQUIRED_FOR_OPEN_CHANNEL)
 
         transaction_hash = self.proxy.transact(
@@ -545,7 +558,6 @@ class TokenNetwork:
                 'new_total_deposit': total_deposit,
                 'previous_total_deposit': previous_total_deposit,
             }
-            log.debug('setTotalDeposit called', **log_details)
 
             # These two scenarios can happen if two calls to deposit happen
             # and then we get here on the second call
@@ -615,8 +627,20 @@ class TokenNetwork:
                 total_deposit,
                 partner,
             )
+            if not gas_limit:
+                self._check_why_deposit_failed(
+                    channel_identifier=channel_identifier,
+                    partner=partner,
+                    token=token,
+                    amount_to_deposit=amount_to_deposit,
+                    total_deposit=total_deposit,
+                    log_details=log_details,
+                )
+                raise RaidenUnrecoverableError('Deposit transaction will always fail')
+
             gas_limit = safe_gas_limit(gas_limit, GAS_REQUIRED_FOR_SET_TOTAL_DEPOSIT)
 
+            log.debug('setTotalDeposit called', **log_details)
             transaction_hash = self.proxy.transact(
                 'setTotalDeposit',
                 gas_limit,
@@ -630,37 +654,54 @@ class TokenNetwork:
             receipt_or_none = check_transaction_threw(self.client, transaction_hash)
 
             if receipt_or_none:
-                latest_deposit = self.detail_participant(
-                    channel_identifier,
-                    self.node_address,
-                    partner,
-                ).deposit
-
-                if token.allowance(self.node_address, self.address) < amount_to_deposit:
-                    log_msg = (
-                        'The allowance is insufficient, '
-                        'check concurrent deposits for the same token network '
-                        'but different proxies.'
-                    )
-                elif token.balance_of(self.node_address) < amount_to_deposit:
-                    log_msg = "The address doesn't have enough funds"
-                elif latest_deposit < total_deposit:
-                    log_msg = 'The tokens were not transferred'
-                else:
-                    log_msg = 'unknown'
-
-                log.critical('setTotalDeposit failed', reason=log_msg, **log_details)
-
-                self._check_channel_state_for_deposit(
-                    self.node_address,
-                    partner,
-                    channel_identifier,
-                    total_deposit,
+                self._check_why_deposit_failed(
+                    channel_identifier=channel_identifier,
+                    partner=partner,
+                    token=token,
+                    amount_to_deposit=amount_to_deposit,
+                    total_deposit=total_deposit,
+                    log_details=log_details,
                 )
-
                 raise TransactionThrew('Deposit', receipt_or_none)
 
             log.info('setTotalDeposit successful', **log_details)
+
+    def _check_why_deposit_failed(
+            self,
+            channel_identifier: typing.ChannelID,
+            partner: typing.Address,
+            token: Token,
+            amount_to_deposit: typing.TokenAmount,
+            total_deposit: typing.TokenAmount,
+            log_details: typing.Dict,
+    ):
+        latest_deposit = self.detail_participant(
+            channel_identifier,
+            self.node_address,
+            partner,
+        ).deposit
+
+        if token.allowance(self.node_address, self.address) < amount_to_deposit:
+            log_msg = (
+                'setTotalDeposit failed. The allowance is insufficient, '
+                'check concurrent deposits for the same token network '
+                'but different proxies.'
+            )
+        elif token.balance_of(self.node_address) < amount_to_deposit:
+            log_msg = 'setTotalDeposit failed. The address doesnt have funds'
+        elif latest_deposit < total_deposit:
+            log_msg = 'setTotalDeposit failed. The tokens were not transferred'
+        else:
+            log_msg = 'setTotalDeposit failed'
+
+        log.critical(log_msg, **log_details)
+
+        self._check_channel_state_for_deposit(
+            self.node_address,
+            partner,
+            channel_identifier,
+            total_deposit,
+        )
 
     def close(
             self,
@@ -880,8 +921,6 @@ class TokenNetwork:
             log.info('skipping unlock, tree is empty', **log_details)
             return
 
-        log.info('unlock called', **log_details)
-
         leaves_packed = b''.join(lock.encoded for lock in merkle_tree_leaves)
 
         gas_limit = self.proxy.estimate_gas(
@@ -891,8 +930,13 @@ class TokenNetwork:
             partner,
             leaves_packed,
         )
+        if not gas_limit:
+            log.critical('unlock transaction will always fail', **log_details)
+            raise RaidenUnrecoverableError('Unlock transaction will always fail')
+
         gas_limit = safe_gas_limit(gas_limit, UNLOCK_TX_GAS_LIMIT)
 
+        log.info('unlock called', **log_details)
         transaction_hash = self.proxy.transact(
             'unlock',
             gas_limit,
@@ -975,6 +1019,15 @@ class TokenNetwork:
                     locked_amount,
                     locksroot,
                 )
+                if not gas_limit:
+                    log.critical('settle transaction will always fail', **log_details)
+                    self._check_channel_state_for_settle(
+                        participant1=self.node_address,
+                        participant2=partner,
+                        channel_identifier=channel_identifier,
+                    )
+                    raise RaidenUnrecoverableError('Settle transaction will always fail')
+
                 gas_limit = safe_gas_limit(gas_limit, GAS_REQUIRED_FOR_SETTLE_CHANNEL)
 
                 transaction_hash = self.proxy.transact(
@@ -1003,6 +1056,15 @@ class TokenNetwork:
                     partner_locked_amount,
                     partner_locksroot,
                 )
+                if not gas_limit:
+                    log.critical('settle transaction will always fail', **log_details)
+                    self._check_channel_state_for_settle(
+                        participant1=self.node_address,
+                        participant2=partner,
+                        channel_identifier=channel_identifier,
+                    )
+                    raise RaidenUnrecoverableError('Settle transaction will always fail')
+
                 gas_limit = safe_gas_limit(gas_limit, GAS_REQUIRED_FOR_SETTLE_CHANNEL)
 
                 transaction_hash = self.proxy.transact(
@@ -1024,9 +1086,9 @@ class TokenNetwork:
             if receipt_or_none:
                 log.critical('settle failed', **log_details)
                 self._check_channel_state_for_settle(
-                    self.node_address,
-                    partner,
-                    channel_identifier,
+                    participant1=self.node_address,
+                    participant2=partner,
+                    channel_identifier=channel_identifier,
                 )
                 raise TransactionThrew('Settle', receipt_or_none)
 
@@ -1182,7 +1244,11 @@ class TokenNetwork:
             participant2: typing.Address,
             channel_identifier: typing.ChannelID,
     ) -> None:
-        channel_data = self.detail_channel(participant1, participant2, channel_identifier)
+        channel_data = self.detail_channel(
+            participant1=participant1,
+            participant2=participant2,
+            channel_identifier=channel_identifier,
+        )
         if channel_data.state == ChannelState.SETTLED:
             raise RaidenRecoverableError(
                 'Channel is already settled',

raiden/network/rpc/smartcontract_proxy.py
@@ -181,7 +181,10 @@ class ContractProxy:
     def encode_function_call(self, function: str, args: List = None):
         return self.get_transaction_data(self.contract.abi, function, args)
 
-    def estimate_gas(self, function: str, *args):
+    def estimate_gas(self, function: str, *args) -> typing.Optional[int]:
+        """Returns a gas estimate for the function with the given arguments or
+        None if the function call will always fail due to Insufficient funds or
+        the logic in the called function."""
         fn = getattr(self.contract.functions, function)
         try:
             return fn(*args).estimateGas({

raiden/utils/__init__.py
@@ -280,11 +280,10 @@ def optional_address_to_string(
     return to_checksum_address(address)
 
 
-def safe_gas_limit(*estimates) -> int:
-    """ Calculates a safe gas limit for a number of estimates.
-
-    Even though it's not documented, it does happen that estimate_gas returns `None`.
-    This function takes care of this and adds a security margin as well.
+def safe_gas_limit(*estimates: int) -> int:
+    """ Calculates a safe gas limit for a number of gas estimates
+    including a security margin
     """
-    calculated_limit = max(gas or 0 for gas in estimates)
+    assert None not in estimates, 'if estimateGas returned None it should not reach here'
+    calculated_limit = max(estimates)
     return int(calculated_limit * constants.GAS_FACTOR)