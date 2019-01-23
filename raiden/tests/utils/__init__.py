46ec6b10e2cef24e4aeba80c1cee284da6a37dad
Augusto F. Hack <hack.augusto@gmail.com>
2019-01-23 17:28:03 -0200

    removed dulicated tool to wait for block

--- raiden/tests/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-import gevent
-from web3 import Web3
-
-
-def wait_blocks(web3: Web3, blocks: int):
-    target_block = web3.eth.blockNumber + blocks
-    while web3.eth.blockNumber < target_block:
-        gevent.sleep(0.5)
