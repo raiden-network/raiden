d349d193893f27aa1d0ca032de9c902af8b6cfe9
Augusto F. Hack <hack.augusto@gmail.com>
2019-01-24 17:24:19 -0200

    tests: fixed logging path
    The tests for the configure_logging produced two dozens of files for
    each run, in the diretory where the pytest was executed. This
    unecessarily cluterred the file system and gets annoying really quick.
    
    This commit moves all the log files to a temp directory.

--- raiden/tests/conftest.py
+++ raiden/tests/conftest.py
@@ -4,6 +4,7 @@ from gevent import monkey
 monkey.patch_all()
 
 if True:
+    import datetime
     import os
     import re
     import sys
@@ -147,11 +148,17 @@ def logging_level(request):
     else:
         logging_levels = {'': level}
 
+    time = datetime.datetime.utcnow().isoformat()
+    debug_path = os.path.join(
+        tempfile.gettempdir(),
+        f'raiden-debug_{time}.log',
+    )
     configure_logging(
         logging_levels,
         colorize=not request.config.option.plain_log,
         log_file=request.config.option.log_file,
         cache_logger_on_first_use=False,
+        debug_log_file_name=debug_path,
     )
 
 
