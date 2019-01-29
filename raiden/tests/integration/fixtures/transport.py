4f032b52648c7941d26bb9e2eb7fe5c5cd173e98
err508 <theldt@posteo.de>
2019-01-29 17:24:46 +0100

    Adds round-robin scheme to matrix_transports fixture to support simple load balancing when number_of_transports > #local_matrix_servers.
    Closes #3290

--- raiden/tests/integration/fixtures/transport.py
+++ raiden/tests/integration/fixtures/transport.py
@@ -44,9 +44,16 @@ def local_matrix_servers(
 
 
 @pytest.fixture
-def matrix_transports(local_matrix_servers, retries_before_backoff, retry_interval, private_rooms):
+def matrix_transports(
+        local_matrix_servers,
+        retries_before_backoff,
+        retry_interval,
+        private_rooms,
+        number_of_transports,
+):
     transports = []
-    for server in local_matrix_servers:
+    for transport_index in range(number_of_transports):
+        server = local_matrix_servers[transport_index % len(local_matrix_servers)]
         transports.append(
             MatrixTransport({
                 'discovery_room': 'discovery',
