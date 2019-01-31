c25bb7a73e7d7adf4bec1ff5608630b366c8bb5a
Konrad Feldmeier <konrad@brainbot.com>
2019-01-31 18:25:31 +0100

    Add RequestMonitoring encoding spec

--- raiden/encoding/messages.py
+++ raiden/encoding/messages.py
@@ -19,6 +19,7 @@ REFUNDTRANSFER = 8
 REVEALSECRET = 11
 DELIVERED = 12
 LOCKEXPIRED = 13
+REQUESTMONITORING = 20
 
 
 # pylint: disable=invalid-name
@@ -47,13 +48,17 @@ channel_identifier = make_field('channel_identifier', 32, '32s', integer(0, UINT
 
 locksroot = make_field('locksroot', 32, '32s')
 secrethash = make_field('secrethash', 32, '32s')
+balance_hash = make_field('balance_hash', 32, '32s')
 secret = make_field('secret', 32, '32s')
 transferred_amount = make_field('transferred_amount', 32, '32s', integer(0, UINT256_MAX))
 locked_amount = make_field('locked_amount', 32, '32s', integer(0, UINT256_MAX))
 amount = make_field('amount', 32, '32s', integer(0, UINT256_MAX))
+reward_amount = make_field('reward_amount', 32, '32s', integer(0, UINT256_MAX))
 fee = make_field('fee', 32, '32s', integer(0, UINT256_MAX))
 
 signature = make_field('signature', 65, '65s')
+non_closing_signature = make_field('non_closing_signature', 65, '65s')
+reward_proof_signature = make_field('reward_proof_signature', 65, '65s')
 
 Processed = namedbuffer(
     'processed',
@@ -222,6 +227,23 @@ Lock = namedbuffer(
 )
 
 
+RequestMonitoring = namedbuffer(
+    'request_monitoring',
+    [
+        cmdid(REQUESTMONITORING),
+        pad(3),
+        nonce,
+        chain_id,
+        token_network_address,
+        channel_identifier,
+        balance_hash,
+        signature,
+        non_closing_signature,
+        reward_amount,
+        reward_proof_signature,
+    ],
+)
+
 CMDID_MESSAGE = {
     PROCESSED: Processed,
     PING: Ping,
@@ -233,6 +255,7 @@ CMDID_MESSAGE = {
     REFUNDTRANSFER: RefundTransfer,
     DELIVERED: Delivered,
     LOCKEXPIRED: LockExpired,
+    REQUESTMONITORING: RequestMonitoring,
 }
 
 
