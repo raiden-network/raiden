878ee418287a800cd1301d4ba80459edb06bd52f
André Vitor de Lima Matos <andre@brainbot.com>
2019-02-01 21:12:23 -0200

    Improve unit test coverage to signer.py

--- raiden/tests/unit/test_utils.py
+++ raiden/tests/unit/test_utils.py
@@ -1,4 +1,7 @@
+from eth_utils import decode_hex, to_canonical_address
+
 from raiden.utils import privatekey_to_publickey, sha3
+from raiden.utils.signer import LocalSigner, Signer, recover
 
 
 def test_privatekey_to_publickey():
@@ -7,3 +10,36 @@ def test_privatekey_to_publickey():
               '705f70c7554b26e82b90d2d1bbbaf711b10c6c8b807077f4070200a8fb4c6b771')
 
     assert pubkey == privatekey_to_publickey(privkey).hex()
+
+
+def test_signer_sign():
+    privkey = sha3(b'secret')  # 0x38e959391dD8598aE80d5d6D114a7822A09d313A
+    message = b'message'
+    # generated with Metamask's web3.personal.sign
+    signature = decode_hex('0x1eff8317c59ab169037f5063a5129bb1bab0299fef0b5621d866b07be59e2c0a'
+                           '6a404e88d3360fb58bd13daf577807c2cf9b6b26d80fc929c52e952769a460981c')
+
+    signer: Signer = LocalSigner(privkey)
+
+    assert signer.sign(message) == signature
+
+
+def test_signer_recover():
+    account = to_canonical_address('0x38e959391dD8598aE80d5d6D114a7822A09d313A')
+    message = b'message'
+    # generated with Metamask's web3.personal.sign
+    signature = decode_hex('0x1eff8317c59ab169037f5063a5129bb1bab0299fef0b5621d866b07be59e2c0a'
+                           '6a404e88d3360fb58bd13daf577807c2cf9b6b26d80fc929c52e952769a460981c')
+    signer: Signer = LocalSigner(sha3(b'deadbeef'))
+
+    assert signer.recover(data=message, signature=signature) == account
+
+
+def test_standalone_recover():
+    account = to_canonical_address('0x38e959391dD8598aE80d5d6D114a7822A09d313A')
+    message = b'message'
+    # generated with Metamask's web3.personal.sign
+    signature = decode_hex('0x1eff8317c59ab169037f5063a5129bb1bab0299fef0b5621d866b07be59e2c0a'
+                           '6a404e88d3360fb58bd13daf577807c2cf9b6b26d80fc929c52e952769a460981c')
+
+    assert recover(data=message, signature=signature) == account
