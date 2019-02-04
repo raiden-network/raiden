4f5d57754dd1e6550a641c0b448acb3c49e15cc8
André Vitor de Lima Matos <andre@brainbot.com>
2019-02-04 17:06:24 -0200

    Move matrix transport to own module, pull in raiden_libs matrix classes

---
+++ raiden/network/transport/matrix/__init__.py
@@ -0,0 +1,10 @@
+from raiden.network.transport.matrix.transport import (  # noqa
+    MatrixTransport,
+    UserPresence,
+    _RetryQueue,
+)
+from raiden.network.transport.matrix.utils import (  # noqa
+    join_global_room,
+    login_or_register,
+    validate_userid_signature,
+)
