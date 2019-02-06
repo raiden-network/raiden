0eb096d7ad593b778110a009ea2246c8ca18de1e
Paul Lange <palango@gmx.de>
2019-02-06 22:24:15 +0100

    Remove raiden-libs dependency

--- raiden/tests/integration/conftest.py
+++ raiden/tests/integration/conftest.py
@@ -2,4 +2,3 @@ from raiden.tests.integration.fixtures.blockchain import *  # noqa: F401,F403
 from raiden.tests.integration.fixtures.raiden_network import *  # noqa: F401,F403
 from raiden.tests.integration.fixtures.smartcontracts import *  # noqa: F401,F403
 from raiden.tests.integration.fixtures.transport import *  # noqa: F401,F403
-from raiden_libs.test.fixtures.web3 import patch_genesis_gas_limit  # noqa: F401, F403
