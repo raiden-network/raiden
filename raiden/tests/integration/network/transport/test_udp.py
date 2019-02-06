83ee34c115c3c4e0d39f195271b3b4ef0f86b838
Augusto F. Hack <hack.augusto@gmail.com>
2019-02-05 22:44:26 -0200

    tests: reorg for test_service

--- raiden/tests/integration/network/transport/test_udp.py
+++ raiden/tests/integration/network/transport/test_udp.py
@@ -1,14 +1,14 @@
 import pytest
 
 from raiden.messages import Ping
-from raiden.settings import DEFAULT_NUMBER_OF_BLOCK_CONFIRMATIONS
-from raiden.tests.utils.events import search_for_item
 from raiden.transfer import state, views
-from raiden.transfer.state_change import Block
 
 
 @pytest.mark.parametrize('number_of_nodes', [2])
-def test_udp_ping_pong(raiden_network, skip_if_not_udp):
+def test_udp_reachable_node(raiden_network, skip_if_not_udp):
+    """A node that answers the ping message must have it state set to
+    reachable.
+    """
     app0, app1 = raiden_network
 
     ping_message = Ping(nonce=0, current_protocol_version=0)
@@ -31,7 +31,10 @@ def test_udp_ping_pong(raiden_network, skip_if_not_udp):
 
 
 @pytest.mark.parametrize('number_of_nodes', [2])
-def test_udp_ping_pong_unreachable_node(raiden_network, skip_if_not_udp):
+def test_udp_unreachable_node(raiden_network, skip_if_not_udp):
+    """A node that does *not* answer the ping message must have it state set to
+    reachable.
+    """
     app0, app1 = raiden_network
 
     app1.raiden.transport.stop()
@@ -60,31 +63,3 @@ def test_udp_ping_pong_unreachable_node(raiden_network, skip_if_not_udp):
         app1.raiden.address,
     )
     assert network_state is state.NODE_NETWORK_UNREACHABLE
-
-
-@pytest.mark.parametrize('number_of_nodes', [1])
-@pytest.mark.parametrize('channels_per_node', [0])
-@pytest.mark.parametrize('number_of_tokens', [1])
-def test_raiden_service_callback_new_block(raiden_network):
-    """ Regression test for: https://github.com/raiden-network/raiden/issues/2894 """
-    app0 = raiden_network[0]
-
-    app0.raiden.alarm.stop()
-    target_block_num = app0.raiden.chain.block_number() + DEFAULT_NUMBER_OF_BLOCK_CONFIRMATIONS + 1
-    app0.raiden.chain.wait_until_block(target_block_num)
-
-    latest_block = app0.raiden.chain.get_block(block_identifier='latest')
-    app0.raiden._callback_new_block(latest_block=latest_block)
-    target_block_num = latest_block['number']
-
-    app0_state_changes = app0.raiden.wal.storage.get_statechanges_by_identifier(
-        from_identifier=0,
-        to_identifier='latest',
-    )
-
-    assert search_for_item(app0_state_changes, Block, {
-        'block_number': target_block_num - DEFAULT_NUMBER_OF_BLOCK_CONFIRMATIONS,
-    })
-    assert not search_for_item(app0_state_changes, Block, {
-        'block_number': target_block_num,
-    })
