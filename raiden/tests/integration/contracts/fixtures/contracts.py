5708b1e4c25909a180fe18d3162f0a0988992bc2
Augusto F. Hack <hack.augusto@gmail.com>
2019-02-07 20:39:05 -0200

    removed extranous fixtures

--- raiden/tests/integration/contracts/fixtures/contracts.py
+++ raiden/tests/integration/contracts/fixtures/contracts.py
@@ -5,29 +5,22 @@ from raiden.network.proxies import Token, TokenNetwork, TokenNetworkRegistry
 from raiden.tests.utils.smartcontracts import deploy_token
 
 
-@pytest.fixture(name='token_network_registry_proxy')
-def create_token_network_registry_proxy(
-        deploy_client,
-        token_network_registry_address,
-        contract_manager,
-):
-    return TokenNetworkRegistry(
-        jsonrpc_client=deploy_client,
-        registry_address=to_canonical_address(token_network_registry_address),
-        contract_manager=contract_manager,
-    )
-
-
 @pytest.fixture(name='token_network_proxy')
 def register_token_and_return_the_network_proxy(
         contract_manager,
         deploy_client,
-        token_contract,
-        token_network_registry_proxy,
+        token_proxy,
+        token_network_registry_address,
 ):
-    token_address = to_canonical_address(token_contract.contract.address)
+    registry_address = to_canonical_address(token_network_registry_address)
+
+    token_network_registry_proxy = TokenNetworkRegistry(
+        jsonrpc_client=deploy_client,
+        registry_address=registry_address,
+        contract_manager=contract_manager,
+    )
     token_network_address = token_network_registry_proxy.add_token(
-        token_address=token_address,
+        token_address=token_proxy.address,
         given_block_identifier='latest',
     )
 
@@ -38,9 +31,9 @@ def register_token_and_return_the_network_proxy(
     )
 
 
-@pytest.fixture(name='token_contract')
-def deploy_token_and_return_jsonrpc_proxy(deploy_client, contract_manager):
-    return deploy_token(
+@pytest.fixture(name='token_proxy')
+def deploy_token_and_return_proxy(deploy_client, contract_manager):
+    token_contract = deploy_token(
         deploy_client=deploy_client,
         contract_manager=contract_manager,
         initial_amount=10000,
@@ -49,9 +42,6 @@ def deploy_token_and_return_jsonrpc_proxy(deploy_client, contract_manager):
         token_symbol='TKN',
     )
 
-
-@pytest.fixture
-def token_proxy(deploy_client, token_contract, contract_manager):
     return Token(
         jsonrpc_client=deploy_client,
         token_address=to_canonical_address(token_contract.contract.address),
