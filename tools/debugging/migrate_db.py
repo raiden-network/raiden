1fffce3eff8e42472c401935fc658eec50580e2d
Lefteris Karapetsas <lefteris@refu.co>
2019-02-07 12:33:17 +0100

    Fix newly detected flake8 violations.
    Thank you flake8 3.7.5 <3

--- tools/debugging/migrate_db.py
+++ tools/debugging/migrate_db.py
@@ -15,19 +15,19 @@ database_path = ""
 
 
 def upgrade_db(current_version: int, new_version: int):
-        log.debug(f'Upgrading database from v{current_version} to v{new_version}')
-        # Prevent unique constraint error in DB when recording raiden "runs"
-        gevent.sleep(1)
-        manager = UpgradeManager(
-            db_filename=database_path,
-            current_version=current_version,
-            new_version=new_version,
-        )
-        try:
-            manager.run()
-        except (RaidenDBUpgradeError, InvalidDBData) as e:
-            manager.restore_backup()
-            log.error(f'Failed to upgrade database: {str(e)}')
+    log.debug(f'Upgrading database from v{current_version} to v{new_version}')
+    # Prevent unique constraint error in DB when recording raiden "runs"
+    gevent.sleep(1)
+    manager = UpgradeManager(
+        db_filename=database_path,
+        current_version=current_version,
+        new_version=new_version,
+    )
+    try:
+        manager.run()
+    except (RaidenDBUpgradeError, InvalidDBData) as e:
+        manager.restore_backup()
+        log.error(f'Failed to upgrade database: {str(e)}')
 
 
 def migrate_db(storage):
