55ec58511f3e212fb3ff463dc1f202f8561d0cd2
André Vitor de Lima Matos <andre@brainbot.com>
2019-02-06 22:16:50 -0200

    Use make_client on matrix transport

--- raiden/network/transport/matrix/__init__.py
+++ raiden/network/transport/matrix/__init__.py
@@ -6,5 +6,7 @@ from raiden.network.transport.matrix.transport import (  # noqa
 from raiden.network.transport.matrix.utils import (  # noqa
     join_global_room,
     login_or_register,
+    make_client,
+    sort_servers_closest,
     validate_userid_signature,
 )
