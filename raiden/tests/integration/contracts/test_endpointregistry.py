10308bf205781c60365fd6b6b780471a4f7f70bf
Augusto F. Hack <hack.augusto@gmail.com>
2019-02-07 21:11:42 -0200

    using existing fixtures for endpoint registry

--- raiden/tests/integration/contracts/test_endpointregistry.py
+++ raiden/tests/integration/contracts/test_endpointregistry.py
@@ -1,29 +1,19 @@
 import pytest
 
 from raiden.exceptions import UnknownAddress
-from raiden.network.discovery import ContractDiscovery
 from raiden.tests.utils.factories import make_address
-from raiden.tests.utils.smartcontracts import deploy_contract_web3
 from raiden.utils import host_port_to_endpoint, privatekey_to_address, safe_gas_limit
-from raiden_contracts.constants import (
-    CONTRACT_ENDPOINT_REGISTRY,
-    GAS_REQUIRED_FOR_ENDPOINT_REGISTER,
-)
+from raiden_contracts.constants import GAS_REQUIRED_FOR_ENDPOINT_REGISTER
 
 
 @pytest.mark.parametrize('number_of_nodes', [1])
-def test_endpointregistry(private_keys, blockchain_services, contract_manager):
-    chain = blockchain_services.blockchain_services[0]
+def test_endpointregistry(
+        private_keys,
+        endpoint_discovery_services,
+):
     my_address = privatekey_to_address(private_keys[0])
 
-    endpointregistry_address = deploy_contract_web3(
-        contract_name=CONTRACT_ENDPOINT_REGISTRY,
-        deploy_client=chain.client,
-        contract_manager=contract_manager,
-    )
-    discovery_proxy = chain.discovery(endpointregistry_address)
-
-    contract_discovery = ContractDiscovery(my_address, discovery_proxy)
+    contract_discovery = endpoint_discovery_services[0]
 
     unregistered_address = make_address()
 
