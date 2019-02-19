2d6b5268db85eecafb044776e2cd887d2d6dbeff
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-02-19 14:51:08 +0100

    Only trigger checker if monitoring or PFS are enabled

--- raiden/ui/runners.py
+++ raiden/ui/runners.py
@@ -29,7 +29,6 @@ from raiden.tasks import check_gas_reserve, check_network_id, check_rdn_deposits
 from raiden.utils import get_system_spec, merge_dict, split_endpoint, typing
 from raiden.utils.echo_node import EchoNode
 from raiden.utils.runnable import Runnable
-from raiden_contracts.contract_manager import ContractManager
 
 from .app import run_app
 from .config import dump_cmd_options, dump_config, dump_module
@@ -167,12 +166,13 @@ class NodeRunner:
             app_.raiden.chain.client.web3,
         ))
 
-        # spawn a greenlet to handle RDN deposits check
-        tasks.append(gevent.spawn(
-            check_rdn_deposits,
-            app_.raiden,
-            app_.user_deposit,
-        ))
+        if self._options['pathfinding_service_address'] or self._options['enable_monitoring']:
+            # spawn a greenlet to handle RDN deposits check
+            tasks.append(gevent.spawn(
+                check_rdn_deposits,
+                app_.raiden,
+                app_.user_deposit,
+            ))
 
         # spawn a greenlet to handle the functions
 
