eaee52ef752b6b43e4836f5fbc52cf3b2be78482
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-02-19 13:34:31 +0100

    Introduce user deposits proxy

--- raiden/network/proxies/__init__.py
+++ raiden/network/proxies/__init__.py
@@ -1,6 +1,9 @@
+# isort:skip_file
 from raiden.network.proxies.discovery import Discovery  # NOQA
 from raiden.network.proxies.token import Token  # NOQA
 from raiden.network.proxies.token_network_registry import TokenNetworkRegistry  # NOQA
 from raiden.network.proxies.token_network import TokenNetwork  # NOQA
 from raiden.network.proxies.secret_registry import SecretRegistry  # NOQA
 from raiden.network.proxies.payment_channel import PaymentChannel  # NOQA
+from raiden.network.proxies.token_network_registry import TokenNetworkRegistry  # NOQA
+from raiden.network.proxies.user_deposit import UserDeposit  # NOQA
