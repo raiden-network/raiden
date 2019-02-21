6c7d6b2aa35453757e1d4a0f6125c9a2b494e325
Lefteris Karapetsas <lefteris@refu.co>
2019-02-21 16:34:20 +0100

    Bring .circleci directory up to date with master

--- .circleci/fetch_geth_solc.sh
+++ .circleci/fetch_geth_solc.sh
@@ -3,24 +3,28 @@
 set -e
 set -x
 
-GETH_PATH="${HOME}/.local/bin/geth-${OS_NAME}-${GETH_VERSION}"
+if [[ -z ${LOCAL_BASE} ]]; then
+    LOCAL_BASE=~/.local
+fi
+
+GETH_PATH="${LOCAL_BASE}/bin/geth-${OS_NAME}-${GETH_VERSION}"
 if [[ ! -x ${GETH_PATH} ]]; then
-  mkdir -p ${HOME}/.local/bin
+  mkdir -p ${LOCAL_BASE}/bin
   TEMP=$(mktemp -d 2>/dev/null || mktemp -d -t 'gethtmp')
   pushd ${TEMP}
   GETH_URL_VAR="GETH_URL_${OS_NAME}"
-  wget -O geth.tar.gz ${!GETH_URL_VAR}
+  curl -o geth.tar.gz ${!GETH_URL_VAR}
   tar xzf geth.tar.gz
   cd geth*/
   install -m 755 geth ${GETH_PATH}
 fi
-ln -sf ${GETH_PATH} ${HOME}/.local/bin/geth
+ln -sf ${GETH_PATH} ${LOCAL_BASE}/bin/geth
 
-SOLC_PATH="${HOME}/.local/bin/solc-${OS_NAME}-${SOLC_VERSION}"
+SOLC_PATH="${LOCAL_BASE}/bin/solc-${OS_NAME}-${SOLC_VERSION}"
 if [[ ! -x ${SOLC_PATH} ]]; then
-  mkdir -p ${HOME}/.local/bin
+  mkdir -p ${LOCAL_BASE}/bin
   SOLC_URL_VAR="SOLC_URL_${OS_NAME}"
   curl -L ${!SOLC_URL_VAR} > ${SOLC_PATH}
   chmod 775 ${SOLC_PATH}
 fi
-ln -sf ${SOLC_PATH} ${HOME}/.local/bin/solc
+ln -sf ${SOLC_PATH} ${LOCAL_BASE}/bin/solc
