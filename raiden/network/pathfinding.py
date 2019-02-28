97c8a071655d7fa6863c50b2ffe59f27d82636c6
Jochen Müller <mail@jnmr.de>
2019-02-28 17:12:54 +0100

    Beautify and fix a typo

--- raiden/network/pathfinding.py
+++ raiden/network/pathfinding.py
@@ -1,12 +1,10 @@
-from typing import Dict, Union
-
 import requests
 from raiden.constants import DEFAULT_HTTP_REQUEST_TIMEOUT
+from raiden.utils import typing
 
 
-def get_pfs_info(url: str) -> Union[Dict, bool]:
+def get_pfs_info(url: str) -> typing.Optional[typing.Dict]:
     try:
-        return requests.get('{}/api/v1/info'.format(url),
-                            timeout=DEFAULT_HTTP_REQUEST_TIMEOUT).json()
+        return requests.get(f'{url}/api/v1/info', timeout=DEFAULT_HTTP_REQUEST_TIMEOUT).json()
     except requests.exceptions.RequestException:
-        return False
+        return None
