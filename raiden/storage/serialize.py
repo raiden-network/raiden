ecf2868d94490462987abb073d15a4382eec2443
Augusto F. Hack <hack.augusto@gmail.com>
2019-02-28 13:03:04 -0300

    pr review

--- raiden/storage/serialize.py
+++ raiden/storage/serialize.py
@@ -2,7 +2,7 @@
 this means that arbitrary modules are imported and potentially arbitrary code
 can be executed (altough, the code which can be executed is limited to our
 internal interfaces). Nevertheless, because of this, this must only be used
-with sanitize input, to avoid the risk of exploits.
+with sanitized input, to avoid the risk of exploits.
 """
 import importlib
 import json
@@ -28,12 +28,12 @@ def from_dict_hook(data):
     """Decode internal objects encoded using `to_dict_hook`.
 
     This automatically imports the class defined in the `_type` metadata field,
-    and calls the `from_dict` method hook to insantiate an object of that
+    and calls the `from_dict` method hook to instantiate an object of that
     class.
 
     Note:
         Because this function will do automatic module loading it's really
-        important to only use this with sanitize or trusted input, otherwise
+        important to only use this with sanitized or trusted input, otherwise
         arbitrary modules can be imported and potentially arbitrary code can be
         executed.
     """
@@ -42,7 +42,7 @@ def from_dict_hook(data):
         klass = _import_type(type_)
 
         msg = '_type must point to a class with `from_dict` static method'
-        assert isinstance(klass, 'from_dict'), msg
+        assert hasattr(klass, 'from_dict'), msg
 
         return klass.from_dict(data)
     return data
@@ -82,7 +82,9 @@ def to_dict_hook(obj):
         result['_version'] = 0
         return result
 
-    return obj
+    raise TypeError(
+        f'Object of type {obj.__class__.__name__} is not JSON serializable',
+    )
 
 
 class SerializationBase:
