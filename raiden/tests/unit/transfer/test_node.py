0112b362e467726d0ebc6fe0e9b36b4fd12acb14
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-03-02 12:28:23 +0100

    Clear channel if there's no locks to unlock

--- raiden/tests/unit/transfer/test_node.py
+++ raiden/tests/unit/transfer/test_node.py
@@ -16,7 +16,7 @@ def test_is_transaction_effect_satisfied(
     transaction = ContractSendChannelBatchUnlock(
         token_address=token_network_state.token_address,
         canonical_identifier=canonical_identifier,
-        participant=HOP2,
+        participant=netting_channel_state.partner_state.address,
         triggered_by_block_hash=make_block_hash(),
     )
     state_change = ContractReceiveChannelBatchUnlock(
@@ -34,10 +34,10 @@ def test_is_transaction_effect_satisfied(
     assert not is_transaction_effect_satisfied(chain_state, transaction, state_change)
 
     # now call normally with us being the partner and not the participant
-    state_change.partner = netting_channel_state.our_state.address
-    state_change.participant = netting_channel_state.partner_state.address
+    state_change.partner = netting_channel_state.partner_state.address
+    state_change.participant = netting_channel_state.our_state.address
     assert not is_transaction_effect_satisfied(chain_state, transaction, state_change)
     # finally call with us being the participant and not the partner which should check out
-    state_change.participant = netting_channel_state.our_state.address
-    state_change.partner = netting_channel_state.partner_state.address
+    state_change.participant = netting_channel_state.partner_state.address
+    state_change.partner = netting_channel_state.our_state.address
     assert is_transaction_effect_satisfied(chain_state, transaction, state_change)
