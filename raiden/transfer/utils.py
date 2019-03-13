3073e65285761428531128b07d5302085787c8b1
Konrad Feldmeier <konrad@brainbot.com>
2019-03-13 18:50:40 +0100

    Use `CanonicalIdentifier` in `get_event_with_balance_proof_by_balance_hash`

--- raiden/transfer/utils.py
+++ raiden/transfer/utils.py
@@ -7,15 +7,7 @@ from raiden.constants import EMPTY_HASH
 from raiden.storage import sqlite
 from raiden.utils import CanonicalIdentifier
 from raiden.utils.serialization import serialize_bytes
-from raiden.utils.typing import (
-    Address,
-    BalanceHash,
-    ChainID,
-    ChannelID,
-    Locksroot,
-    TokenAmount,
-    TokenNetworkID,
-)
+from raiden.utils.typing import Address, BalanceHash, Locksroot, TokenAmount
 
 
 def get_state_change_with_balance_proof_by_balance_hash(
@@ -67,9 +59,7 @@ def get_state_change_with_balance_proof_by_locksroot(
 
 def get_event_with_balance_proof_by_balance_hash(
         storage: sqlite.SQLiteStorage,
-        chain_id: ChainID,
-        token_network_identifier: TokenNetworkID,
-        channel_identifier: ChannelID,
+        canonical_identifier: CanonicalIdentifier,
         balance_hash: BalanceHash,
 ) -> sqlite.EventRecord:
     """ Returns the event which contains the corresponding balance
@@ -79,9 +69,11 @@ def get_event_with_balance_proof_by_balance_hash(
     has the blinded balance proof data.
     """
     return storage.get_latest_event_by_data_field({
-        'balance_proof.chain_id': chain_id,
-        'balance_proof.token_network_identifier': to_checksum_address(token_network_identifier),
-        'balance_proof.channel_identifier': str(channel_identifier),
+        'balance_proof.chain_id': canonical_identifier.chain_identifier,
+        'balance_proof.token_network_identifier': to_checksum_address(
+            canonical_identifier.token_network_address,
+        ),
+        'balance_proof.channel_identifier': str(canonical_identifier.channel_identifier),
         'balance_proof.balance_hash': serialize_bytes(balance_hash),
     })
 
