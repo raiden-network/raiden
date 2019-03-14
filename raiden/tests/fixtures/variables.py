bb179c297a1b5a0e94d5a09d05eb1a1f9e6a3fc2
err508 <tobias@brainbot.com>
2019-03-14 20:22:41 +0100

    Revert "Fix #3588"

--- raiden/tests/fixtures/variables.py
+++ raiden/tests/fixtures/variables.py
@@ -320,6 +320,11 @@ def database_paths(tmpdir, private_keys):
 
 
 @pytest.fixture
+def private_rooms():
+    return False
+
+
+@pytest.fixture
 def environment_type():
     """Specifies the environment type"""
     return Environment.DEVELOPMENT
