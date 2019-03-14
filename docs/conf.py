45eb3da223872451c0989a31a881421fe3162895
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2019-03-14 14:17:15 +0100

    improvements to docs layout and index page

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,11 @@ def check_if_nightly(version):
         return version
 
 
+# Add customized stylesheet
+def setup(app):
+    app.add_stylesheet('css/custom.css')
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
