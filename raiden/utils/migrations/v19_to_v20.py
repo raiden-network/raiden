1f1125e5b0dac423c9e3d657d0e4aaeee5480536
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-03-15 14:20:49 +0100

    Add migration for locksroots

--- raiden/utils/migrations/v19_to_v20.py
+++ raiden/utils/migrations/v19_to_v20.py
@@ -1,7 +1,6 @@
 import json
 
 from raiden.storage.sqlite import SQLiteStorage
-from raiden.utils.serialization import serialize_bytes
 
 SOURCE_VERSION = 19
 TARGET_VERSION = 20
@@ -29,35 +28,14 @@ def _add_onchain_locksroot_to_channel_settled_state_changes(
             msg = 'v18 state changes cant contain partner_onchain_locksroot'
             assert 'partner_onchain_locksroot' not in data, msg
 
-            data['our_onchain_locksroot'] = serialize_bytes(bytes(32))
-            data['partner_onchain_locksroot'] = serialize_bytes(bytes(32))
+            data['our_onchain_locksroot'] = None
+            data['partner_onchain_locksroot'] = None
+
             updated_state_changes.append((
                 json.dumps(data),
                 state_change.state_change_identifier,
             ))
         storage.update_state_changes(updated_state_changes)
-
-
-def _add_partner_to_batch_unlock_events(storage: SQLiteStorage) -> None:
-    """Adds partner to all ContractSendXXX events"""
-    batch_query = storage.batch_query_event_records(
-        batch_size=500,
-        filters=[('_type', 'raiden.transfer.events.ContractSendChannelBatchUnlock')],
-    )
-    for events_batch in batch_query:
-        updated_events = []
-        for event in events_batch:
-            data = json.loads(event.data)
-            assert 'partner' not in data, 'v19 ContractSendChannelBatchUnlock contain partner'
-
-            data['partner'] = 'something'
-
-            updated_events.append((
-                json.dumps(data),
-                event.event_identifier,
-            ))
-
-        storage.update_events(updated_events)
 
 
 def _add_onchain_locksroot_to_snapshot(raw_snapshot):
@@ -69,8 +47,8 @@ def _add_onchain_locksroot_to_snapshot(raw_snapshot):
     for payment_network in snapshot.get('identifiers_to_paymentnetworks', {}).values():
         for token_network in payment_network.get('tokennetworks', []):
             for channel in token_network.get('channelidentifiers_to_channels', []).values():
-                channel['our_state']['onchain_locksroot'] = serialize_bytes(bytes(32))
-                channel['partner_state']['onchain_locksroot'] = serialize_bytes(bytes(32))
+                channel['our_state']['onchain_locksroot'] = None
+                channel['partner_state']['onchain_locksroot'] = None
 
     return json.dumps(snapshot, indent=4)
 
@@ -93,6 +71,5 @@ def upgrade_v19_to_v20(
     if old_version == SOURCE_VERSION:
         _add_onchain_locksroot_to_channel_settled_state_changes(storage)
         _add_onchain_locksroot_to_snapshots(storage)
-        _add_partner_to_batch_unlock_events(storage)
 
     return TARGET_VERSION
