4da6fd7c696dda9ea67f60a542cbb6f58e5bb76d
Konrad Feldmeier <konrad@brainbot.com>
2019-03-19 17:01:01 +0100

    Use only `CanonicalIdentifier` in `ContractReceiveRouteClosed`

--- raiden/transfer/state_change.py
+++ raiden/transfer/state_change.py
@@ -1116,9 +1116,16 @@ class ContractReceiveRouteClosed(ContractReceiveStateChange):
             block_hash: BlockHash,
     ) -> None:
         super().__init__(transaction_hash, block_number, block_hash)
+        canonical_identifier.validate()
+        self.canonical_identifier = canonical_identifier
 
-        self.token_network_identifier = canonical_identifier.token_network_address
-        self.channel_identifier = canonical_identifier.channel_identifier
+    @property
+    def channel_identifier(self) -> ChannelID:
+        return self.canonical_identifier.channel_identifier
+
+    @property
+    def token_network_identifier(self) -> TokenNetworkAddress:
+        return TokenNetworkAddress(self.canonical_identifier.token_network_address)
 
     def __repr__(self):
         return '<ContractReceiveRouteClosed token_network:{} id:{} block:{}>'.format(
@@ -1130,8 +1137,7 @@ class ContractReceiveRouteClosed(ContractReceiveStateChange):
     def __eq__(self, other: Any) -> bool:
         return (
             isinstance(other, ContractReceiveRouteClosed) and
-            self.token_network_identifier == other.token_network_identifier and
-            self.channel_identifier == other.channel_identifier and
+            self.canonical_identifier == other.canonical_identifier and
             super().__eq__(other)
         )
 
@@ -1141,8 +1147,7 @@ class ContractReceiveRouteClosed(ContractReceiveStateChange):
     def to_dict(self) -> Dict[str, Any]:
         return {
             'transaction_hash': serialize_bytes(self.transaction_hash),
-            'token_network_identifier': to_checksum_address(self.token_network_identifier),
-            'channel_identifier': str(self.channel_identifier),
+            'canonical_identifier': self.canonical_identifier.to_dict(),
             'block_number': str(self.block_number),
             'block_hash': serialize_bytes(self.block_hash),
         }
@@ -1151,11 +1156,7 @@ class ContractReceiveRouteClosed(ContractReceiveStateChange):
     def from_dict(cls, data: Dict[str, Any]) -> 'ContractReceiveRouteClosed':
         return cls(
             transaction_hash=deserialize_transactionhash(data['transaction_hash']),
-            canonical_identifier=CanonicalIdentifier(
-                chain_identifier=CHAIN_ID_UNSPECIFIED,
-                token_network_address=to_canonical_address(data['token_network_identifier']),
-                channel_identifier=ChannelID(int(data['channel_identifier'])),
-            ),
+            canonical_identifier=CanonicalIdentifier.from_dict(data['canonical_identifier']),
             block_number=BlockNumber(int(data['block_number'])),
             block_hash=BlockHash(deserialize_bytes(data['block_hash'])),
         )
