7efa28ded50f5362999486deae9c5fae6b81e451
Ulrich Petri <ulo@ulo.pe>
2019-04-16 14:23:53 +0200

    Don't skip tests on git tag triggered builds

--- .circleci/conditional_skip.sh
+++ .circleci/conditional_skip.sh
@@ -6,6 +6,11 @@ set +e
 
 SKIP_TAG="\[skip tests\]"
 
+if [[ ! -z ${CIRCLE_TAG} ]]; then
+    # TAG build - never skip those
+    exit 0
+fi
+
 if [[ -a ~/.local/BASE_COMMIT ]]; then
     # The is a PR and we know the base commit (see fetch_pr_base_commit.sh)
     LOG_RANGE="$(cat ~/.local/BASE_COMMIT)..${CIRCLE_SHA1}"
