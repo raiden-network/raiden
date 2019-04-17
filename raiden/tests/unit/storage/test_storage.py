0eb648a58dc59a36b83537a6de9bc4635035b0a4
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-04-17 12:58:51 +0200

    Add small storage transaction tests

---
+++ raiden/tests/unit/storage/test_storage.py
@@ -0,0 +1,31 @@
+from unittest.mock import patch
+
+import pytest
+
+from raiden.storage.sqlite import RAIDEN_DB_VERSION, SQLiteStorage
+
+
+def test_transaction_commit(tmp_path):
+    filename = f'v{RAIDEN_DB_VERSION}_db.log'
+    storage = SQLiteStorage(f'{tmp_path}/{filename}')
+
+    with storage.transaction():
+        with patch('raiden.storage.sqlite.RAIDEN_DB_VERSION', new=1000):
+            storage.update_version()
+
+    assert storage.get_version() == 1000
+
+
+def test_transaction_rollback(tmp_path):
+    filename = f'v{RAIDEN_DB_VERSION}_db.log'
+    storage = SQLiteStorage(f'{tmp_path}/{filename}')
+    storage.update_version()
+
+    assert storage.get_version() == RAIDEN_DB_VERSION
+
+    with pytest.raises(KeyboardInterrupt):
+        with storage.transaction():
+            with patch('raiden.storage.sqlite.RAIDEN_DB_VERSION', new=1000):
+                storage.update_version()
+                raise KeyboardInterrupt()
+    assert storage.get_version() == RAIDEN_DB_VERSION
