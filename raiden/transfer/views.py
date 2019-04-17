4eb3bfef059aaa0ba09613c607fb59bffbd48c18
Augusto F. Hack <hack.augusto@gmail.com>
2019-04-17 06:56:43 -0300

    fix linting

--- raiden/transfer/views.py
+++ raiden/transfer/views.py
@@ -1,6 +1,5 @@
-from enum import Enum
-
 from dataclasses import dataclass
+from enum import Enum
 
 from raiden.transfer import channel
 from raiden.transfer.architecture import ContractSendEvent, State
