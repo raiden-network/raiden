f5dc97577922d202d85cc871ecb299ba8f330056
Lefteris Karapetsas <lefteris@refu.co>
2019-04-18 17:48:08 +0200

    Add some assertions, uncovered due to mypy runs

--- raiden/transfer/mediated_transfer/initiator_manager.py
+++ raiden/transfer/mediated_transfer/initiator_manager.py
@@ -371,6 +371,7 @@ def handle_lock_expired(
         state_change=state_change,
         block_number=block_number,
     )
+    assert result.new_state, 'handle_receive_lock_expired should not delete the task'
 
     if not channel.get_lock(result.new_state.partner_state, secrethash):
         transfer = initiator_state.transfer
