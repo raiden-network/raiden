5aad76a5005ab094f762d909549db3bb643b6a95
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-04-23 17:37:20 +0200

    Update is_transaction_effect_satisfied test

--- raiden/tests/unit/transfer/test_node.py
+++ raiden/tests/unit/transfer/test_node.py
@@ -1,8 +1,11 @@
 from raiden.constants import EMPTY_MERKLE_ROOT
 from raiden.tests.utils.factories import HOP1, HOP2, UNIT_SECRETHASH, make_block_hash
 from raiden.transfer.events import ContractSendChannelBatchUnlock
-from raiden.transfer.node import is_transaction_effect_satisfied
-from raiden.transfer.state_change import ContractReceiveChannelBatchUnlock
+from raiden.transfer.node import is_transaction_effect_satisfied, state_transition
+from raiden.transfer.state_change import (
+    ContractReceiveChannelBatchUnlock,
+    ContractReceiveChannelSettled,
+)
 
 
 def test_is_transaction_effect_satisfied(
@@ -40,4 +43,22 @@ def test_is_transaction_effect_satisfied(
     # finally call with us being the participant and not the partner which should check out
     state_change.participant = netting_channel_state.partner_state.address
     state_change.partner = netting_channel_state.our_state.address
-    assert is_transaction_effect_satisfied(chain_state, transaction, state_change)
+
+    # ContractSendChannelBatchUnlock would only be satisfied if both sides are unlocked
+    # and if the channel was cleared
+
+    channel_settled = ContractReceiveChannelSettled(
+        transaction_hash=bytes(32),
+        canonical_identifier=canonical_identifier,
+        our_onchain_locksroot=EMPTY_MERKLE_ROOT,
+        partner_onchain_locksroot=EMPTY_MERKLE_ROOT,
+        block_number=1,
+        block_hash=make_block_hash(),
+    )
+
+    iteration = state_transition(
+        chain_state=chain_state,
+        state_change=channel_settled,
+    )
+
+    assert is_transaction_effect_satisfied(iteration.new_state, transaction, state_change)
