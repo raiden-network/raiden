1679decb46ec86210a44a92cec15103ff298c8d3
Augusto F. Hack <hack.augusto@gmail.com>
2019-04-26 11:12:38 -0300

    using the app number in test logs

---
+++ raiden/tests/utils/app.py
@@ -0,0 +1,17 @@
+import os
+import os.path
+
+from raiden.constants import RAIDEN_DB_VERSION
+
+
+def database_from_privatekey(base_dir, app_number):
+    """ Format a database path based on the private key and app number. """
+    dbpath = os.path.join(
+        base_dir,
+        'raiden_nodes',
+        str(app_number),
+        f'v{RAIDEN_DB_VERSION}_log.db',
+    )
+    os.makedirs(os.path.dirname(dbpath))
+
+    return dbpath
