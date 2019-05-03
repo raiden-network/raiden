ae9300f2ebfe2f440fb2beb618cb6a47d6c168eb
Augusto F. Hack <hack.augusto@gmail.com>
2019-05-03 11:24:35 -0300

    Reintroduced psutils based port generator
    psutils cannot be used for mac, however it works reliably in the CI
    system. The solution based on socket bind/TIME_WAIT is flaky and leads
    to multiple build failures a day. This adds back the psutils for
    non-macos systems only, the goal is to decrease the number of flaky
    build failures.
    
    Note: The problem may persist for MacOS systems, since SO_REUSEADDR
    behaves differently across Unixes, ref:
    
    https://stackoverflow.com/questions/14388706/socket-options-so-reuseaddr-and-so-reuseport-how-do-they-differ-do-they-mean-t

--- raiden/network/utils.py
+++ raiden/network/utils.py
@@ -1,67 +1,85 @@
 import errno
 import socket
+import sys
 from contextlib import closing
 from itertools import count, repeat
 from socket import SocketKind
 from time import sleep
-from typing import Optional
 
+import psutil
 import requests
 from requests import RequestException
 
+from raiden.utils.typing import Iterable, Optional, Port
 
-def get_free_port(
-        initial_port: int = 0,
-        socket_kind: SocketKind = SocketKind.SOCK_STREAM,
-        reliable: bool = True,
-):
-    """
-    Find an unused TCP port.
+LOOPBACK = '127.0.0.1'
 
-    Unless the `reliable` parameter is set to `True` (the default) this is prone to race
-    conditions - some other process may grab the port before the caller of this function has
-    a chance to use it.
-    When using `reliable` the port is forced into TIME_WAIT mode, ensuring that it will not be
-    considered 'free' by the OS for the next 60 seconds. This does however require that the
-    process using the port sets SO_REUSEADDR on it's sockets. Most 'server' applications do.
+# The solution based on psutils does not work on MacOS because it needs
+# root access
+if sys.platform == 'darwin':
+    def _unused_ports(initial_port: Optional[int]) -> Iterable[Port]:
+        socket_kind: SocketKind = SocketKind.SOCK_STREAM
+
+        if not initial_port:
+            next_port = repeat(0)
+        else:
+            next_port = count(start=initial_port)
+
+        for port_candidate in next_port:
+            # Don't inline the variable until
+            # https://github.com/PyCQA/pylint/issues/1437 is fixed
+            sock = socket.socket(socket.AF_INET, socket_kind)
+            with closing(sock):
+                # Force the port into TIME_WAIT mode, ensuring that it will not
+                # be considered 'free' by the OS for the next 60 seconds. This
+                # does however require that the process using the port sets
+                # SO_REUSEADDR on it's sockets. Most 'server' applications do.
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                try:
+                    sock.bind((LOOPBACK, port_candidate))
+                except OSError as ex:
+                    if ex.errno == errno.EADDRINUSE:
+                        continue
+                    raise
+
+                sock_addr = sock.getsockname()
+                port = sock_addr[1]
+
+                # Connect to the socket to force it into TIME_WAIT state (see
+                # above)
+                sock.listen(1)
+                sock2 = socket.socket(socket.AF_INET, socket_kind)
+                with closing(sock2):
+                    sock2.connect(sock_addr)
+                    sock.accept()
+
+            yield Port(port)
+else:
+    def _unused_ports(initial_port: Optional[int]) -> Iterable[Port]:
+        initial_port = initial_port or 27854
+
+        for port in count(initial_port):
+            connect_using_port = (
+                conn
+                for conn in psutil.net_connections()
+                if hasattr(conn, 'laddr') and
+                conn.laddr[0] == LOOPBACK and
+                conn.laddr[1] == port
+            )
+
+            if not any(connect_using_port):
+                yield Port(port)
+
+
+def get_free_port(initial_port: Optional[int] = None) -> Iterable[Port]:
+    """Find an unused TCP port.
 
     If `initial_port` is passed the function will try to find a port as close as possible.
     Otherwise a random port is chosen by the OS.
 
     Returns an iterator that will return unused port numbers.
     """
-
-    def _port_generator():
-        if initial_port == 0:
-            next_port = repeat(0)
-        else:
-            next_port = count(start=initial_port)
-
-        for port_candidate in next_port:
-            # Don't inline the variable until https://github.com/PyCQA/pylint/issues/1437 is fixed
-            sock = socket.socket(socket.AF_INET, socket_kind)
-            with closing(sock):
-                if reliable:
-                    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                try:
-                    sock.bind(('127.0.0.1', port_candidate))
-                except OSError as ex:
-                    if ex.errno == errno.EADDRINUSE:
-                        continue
-
-                sock_addr = sock.getsockname()
-                port = sock_addr[1]
-
-                if reliable:
-                    # Connect to the socket to force it into TIME_WAIT state
-                    sock.listen(1)
-                    # see above
-                    sock2 = socket.socket(socket.AF_INET, socket_kind)
-                    with closing(sock2):
-                        sock2.connect(sock_addr)
-                        sock.accept()
-            yield port
-    return _port_generator()
+    return _unused_ports(initial_port=initial_port)
 
 
 def get_http_rtt(
