ef52d3cad2368d557369afc64321f1ead4d7629f
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-05-06 23:07:31 +0200

    Introduce DictSerializer

--- raiden/storage/serialization/__init__.py
+++ raiden/storage/serialization/__init__.py
@@ -1,3 +1,3 @@
 from raiden.storage.serialization.dataclass import dataclass, field, fields, replace  # noqa
 
-from .serializer import JSONSerializer, SerializationBase  # noqa
+from .serializer import DictSerializer, JSONSerializer, SerializationBase  # noqa
