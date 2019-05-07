0d615bc7529ec1d40d845627edb6c3eed61ffa94
Konrad Feldmeier <konrad@brainbot.com>
2019-05-07 16:11:00 +0200

    Remove dead code

--- raiden/tests/unit/transfer/test_views.py
+++
@@ -1,8 +0,0 @@
-from raiden.tests.utils.factories import UNIT_SECRETHASH
-from raiden.transfer.views import get_transfer_task
-
-
-def test_get_transfer_task(chain_state):
-    subtask = object()
-    chain_state.payment_mapping.secrethashes_to_task[UNIT_SECRETHASH] = subtask
-    assert get_transfer_task(chain_state=chain_state, secrethash=UNIT_SECRETHASH) == subtask
