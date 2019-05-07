6c6f5594ad28be00e061fb61c9188fdd0347ad14
Augusto F. Hack <hack.augusto@gmail.com>
2019-05-07 06:44:19 -0300

    print the stack trace of the failed test

--- raiden/tests/utils/detect_failure.py
+++ raiden/tests/utils/detect_failure.py
@@ -1,3 +1,5 @@
+import traceback
+
 import gevent
 from gevent.event import AsyncResult
 
@@ -10,9 +12,25 @@ def raise_on_failure(raiden_apps, test_function, **kwargs):
     """
     result = AsyncResult()
 
+    # Do not use `link` or `link_value`, an app an be stopped to test restarts.
     for app in raiden_apps:
-        assert app.raiden
-        app.raiden.link(result)
+        assert app.raiden, "The RaidenService must be started"
+        app.raiden.link_exception(result)
 
-    gevent.spawn(test_function, **kwargs).link(result)
-    result.get()
+    test_greenlet = gevent.spawn(test_function, **kwargs)
+    test_greenlet.link(result)
+
+    # Returns if either happens:
+    # - The test finished (successfully or not)
+    # - One of the apps crashed during the test
+    try:
+        result.get()
+    except:  # noqa
+        # Always print the stack trace of the test greenlet. The stack trace of
+        # the greenlet that was killed is always printed, because the test
+        # itself may be killed with anassert its stack trace may be printed
+        # twice, however this is necessary for tests which a app was killed,
+        # otherwise the state of the test is unknown.
+        print("".join(traceback.format_stack(test_greenlet.gr_frame)))
+
+        raise
