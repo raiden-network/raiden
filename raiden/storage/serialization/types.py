46fdf674faa90eb8555fa134bfcb481e6d64353e
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-05-13 12:34:25 +0200

    Cache schema generation and attach type to serialized data

--- raiden/storage/serialization/types.py
+++ raiden/storage/serialization/types.py
@@ -1,8 +1,9 @@
 from random import Random
 
 from marshmallow import Schema, fields
-from marshmallow_dataclass import _native_to_marshmallow, class_schema
+from marshmallow_dataclass import _native_to_marshmallow
 
+from raiden.storage.serialization.cache import SchemaCache
 from raiden.storage.serialization.fields import (
     AddressField,
     BytesField,
@@ -10,7 +11,11 @@ from raiden.storage.serialization.fields import (
     IntegerToStringField,
     PRNGField,
 )
-from raiden.transfer.architecture import TransferTask
+from raiden.transfer.architecture import (
+    BalanceProofSignedState,
+    BalanceProofUnsignedState,
+    TransferTask,
+)
 from raiden.utils.typing import (
     AdditionalHash,
     Address,
@@ -53,7 +58,7 @@ from raiden.utils.typing import (
 
 def transfer_task_schema_serialization(task: TransferTask, parent: Any) -> Schema:
     # pylint: disable=unused-argument
-    return class_schema(task.__class__)()
+    return SchemaCache.get_or_create_schema(task.__class__)
 
 
 def transfer_task_schema_deserialization(
@@ -63,16 +68,39 @@ def transfer_task_schema_deserialization(
     # Avoid cyclic dependencies
     from raiden.transfer.mediated_transfer.tasks import InitiatorTask, MediatorTask, TargetTask
 
-    task_type = task_dict.get("type")
+    task_type = task_dict.get("_type")
     if task_type is None:
         return None
 
     if task_type.endswith("InitiatorTask"):
-        return class_schema(InitiatorTask)()
+        return SchemaCache.get_or_create_schema(InitiatorTask)
     if task_type.endswith("MediatorTask"):
-        return class_schema(MediatorTask)()
+        return SchemaCache.get_or_create_schema(MediatorTask)
     if task_type.endswith("TargetTask"):
-        return class_schema(TargetTask)()
+        return SchemaCache.get_or_create_schema(TargetTask)
+
+    return None
+
+
+def balance_proof_schema_serialization(
+    balance_proof: Union[BalanceProofSignedState, BalanceProofUnsignedState], parent: Any
+) -> Schema:
+    # pylint: disable=unused-argument
+    return SchemaCache.get_or_create_schema(balance_proof.__class__)
+
+
+def balance_proof_schema_deserialization(
+    balance_proof_dict: Dict[str, Any], parent: Dict[str, Any]
+) -> Optional[Schema]:
+    # pylint: disable=unused-argument
+    bp_type = balance_proof_dict.get("_type")
+    if bp_type is None:
+        return None
+
+    if bp_type.endswith("UnsignedState"):
+        return SchemaCache.get_or_create_schema(BalanceProofUnsignedState)
+    elif bp_type.endswith("SignedState"):
+        return SchemaCache.get_or_create_schema(BalanceProofSignedState)
 
     return None
 
@@ -119,6 +147,15 @@ _native_to_marshmallow.update(
         ChannelID: IntegerToStringField,
         # Union
         Union[TokenNetworkAddress, TokenNetworkID]: AddressField,
+        Union[BalanceProofUnsignedState, BalanceProofSignedState]: CallablePolyField(
+            serialization_schema_selector=balance_proof_schema_serialization,
+            deserialization_schema_selector=balance_proof_schema_deserialization,
+        ),
+        Optional[Union[BalanceProofUnsignedState, BalanceProofSignedState]]: CallablePolyField(
+            serialization_schema_selector=balance_proof_schema_serialization,
+            deserialization_schema_selector=balance_proof_schema_deserialization,
+            allow_none=True,
+        ),
         # Polymorphic fields
         TransferTask: CallablePolyField(
             serialization_schema_selector=transfer_task_schema_serialization,
