2bb19ac038e504a2ac8744a4da26529e20cb6bb2
Jochen Müller <mail@jnmr.de>
2019-06-06 17:03:16 +0200

    Refactor and test unlock handler

--- raiden/tests/unit/test_blockchain_events_handler.py
+++ raiden/tests/unit/test_blockchain_events_handler.py
@@ -1,12 +1,19 @@
+from unittest.mock import patch
+
 import pytest
 
 from raiden.blockchain.events import Event
 from raiden.blockchain_events_handler import (
+    create_batch_unlock_state_change,
     create_channel_closed_state_change,
     create_update_transfer_state_change,
 )
+from raiden.storage.sqlite import EventRecord, StateChangeRecord
 from raiden.tests.utils import factories
+from raiden.transfer.mediated_transfer.events import SendBalanceProof
 from raiden.transfer.state_change import (
+    BalanceProofStateChange,
+    ContractReceiveChannelBatchUnlock,
     ContractReceiveChannelClosed,
     ContractReceiveRouteClosed,
     ContractReceiveUpdateTransfer,
@@ -87,3 +94,83 @@ def test_create_channel_closed_state_change_unknown_channel(container, channel_c
         chain_state=container.chain_state, event=channel_closed
     )
     assert isinstance(state_change, ContractReceiveRouteClosed)
+
+
+@pytest.fixture
+def unlock(container, event_data):
+    event_data["event"] = ChannelEvent.UNLOCKED
+    event_data["args"] = dict(
+        locksroot=factories.EMPTY_MERKLE_ROOT,
+        receiver=factories.make_address(),
+        sender=factories.make_address(),
+        unlocked_amount=factories.UNIT_TRANSFER_AMOUNT,
+        returned_tokens=factories.UNIT_TRANSFER_AMOUNT,
+    )
+    return Event(originating_contract=container.token_network_address, event_data=event_data)
+
+
+def test_create_channel_batch_unlock_state_change_discards_if_not_participant(container, unlock):
+    address = factories.make_address()
+    state_change = create_batch_unlock_state_change(
+        chain_state=container.chain_state, event=unlock, our_address=address, storage=None
+    )
+    assert state_change is None
+
+
+def test_create_channel_batch_unlock_state_change_as_receiver(container, unlock):
+    sender = container.channels[1].partner_state.address
+    unlock.event_data["args"]["sender"] = sender
+    receiver = container.channels[1].our_state.address
+    unlock.event_data["args"]["receiver"] = receiver
+
+    last_balance_proof = factories.create(
+        factories.BalanceProofSignedStateProperties(
+            canonical_identifier=factories.UNIT_CANONICAL_ID
+        )
+    )
+    state_change_record = StateChangeRecord(
+        state_change_identifier=1,
+        data=BalanceProofStateChange(sender=receiver, balance_proof=last_balance_proof),
+    )
+
+    with patch(
+        "raiden.blockchain_events_handler.get_state_change_with_balance_proof_by_locksroot",
+        return_value=state_change_record,
+    ):
+        state_change = create_batch_unlock_state_change(
+            chain_state=container.chain_state, event=unlock, our_address=receiver, storage=None
+        )
+    assert isinstance(state_change, ContractReceiveChannelBatchUnlock)
+
+
+def test_create_channel_batch_unlock_state_change_as_sender(container, unlock):
+    sender = container.channels[0].our_state.address
+    unlock.event_data["args"]["sender"] = sender
+    receiver = container.channels[0].partner_state.address
+    unlock.event_data["args"]["receiver"] = receiver
+
+    last_balance_proof = factories.create(
+        factories.BalanceProofProperties(canonical_identifier=factories.UNIT_CANONICAL_ID)
+    )
+    event_record = EventRecord(
+        state_change_identifier=1,
+        event_identifier=1,
+        data=SendBalanceProof(
+            message_identifier=1,
+            payment_identifier=1,
+            token_address=container.token_address,
+            balance_proof=last_balance_proof,
+            secret=factories.make_secret(),
+            canonical_identifier=factories.UNIT_CANONICAL_ID,
+            recipient=receiver,
+        ),
+    )
+
+    with patch(
+        "raiden.blockchain_events_handler.get_event_with_balance_proof_by_locksroot",
+        return_value=event_record,
+    ):
+        state_change = create_batch_unlock_state_change(
+            chain_state=container.chain_state, event=unlock, our_address=sender, storage=None
+        )
+    assert isinstance(state_change, ContractReceiveChannelBatchUnlock)
