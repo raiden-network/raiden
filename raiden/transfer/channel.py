afa9be71ae7c90b49699340adada8ddc7935bb9a
Jochen Müller <mail@jnmr.de>
2019-06-06 12:58:15 +0200

    Omit units from unittest coverage measure
    message handlers that are too small for the logic to be extracted

--- raiden/transfer/channel.py
+++ raiden/transfer/channel.py
@@ -2097,7 +2097,7 @@ def state_transition(
     block_number: BlockNumber,
     block_hash: BlockHash,
     pseudo_random_generator: random.Random,
-) -> TransitionResult[Optional[NettingChannelState]]:
+) -> TransitionResult[Optional[NettingChannelState]]:  # pragma: no unittest
     # pylint: disable=too-many-branches,unidiomatic-typecheck
 
     events: List[Event] = list()
