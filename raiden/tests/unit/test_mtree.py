d815d72128d987e91178d33813b316efe50528f2
Yoichi Hirai <yoichi@brainbot.com>
2019-06-11 17:20:51 +0200

    Remove a bit more merkles

--- raiden/tests/unit/test_mtree.py
+++ raiden/tests/unit/test_mtree.py
@@ -1,15 +1,18 @@
+from collections import OrderedDict
+
 import pytest
 
-from raiden.constants import EMPTY_MERKLE_ROOT
 from raiden.exceptions import HashLengthNot32
-from raiden.transfer.merkle_tree import MERKLEROOT, compute_layers, merkleroot
+from raiden.transfer.channel import compute_locksroot
+from raiden.transfer.merkle_tree import MERKLEROOT, compute_layers
 from raiden.transfer.state import MerkleTreeState
 from raiden.utils import sha3
+from raiden_contracts.tests.constants import LOCKSROOT_OF_NO_LOCKS
 
 
 def test_empty():
-    tree = MerkleTreeState([[EMPTY_MERKLE_ROOT]])
-    assert merkleroot(tree) == EMPTY_MERKLE_ROOT
+    locks = OrderedDict()
+    assert compute_locksroot(locks) == LOCKSROOT_OF_NO_LOCKS
 
 
 def test_compute_layers_empty():
@@ -42,4 +45,4 @@ def test_compute_layers_single_entry():
     assert layers[MERKLEROOT][0] == hash_0
 
     tree = MerkleTreeState(layers)
-    assert merkleroot(tree) == hash_0
+    assert compute_locksroot(tree) == hash_0
