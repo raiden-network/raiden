936d371da8e5aff75783adc7815c5a4852c282b3
Yoichi Hirai <yoichi@brainbot.com>
2019-06-11 18:56:27 +0200

    Remove a Merkle tree specific test

--- raiden/tests/unit/test_mtree.py
+++ raiden/tests/unit/test_mtree.py
@@ -4,8 +4,7 @@ import pytest
 
 from raiden.exceptions import HashLengthNot32
 from raiden.transfer.channel import compute_locksroot
-from raiden.transfer.merkle_tree import MERKLEROOT, compute_layers
-from raiden.transfer.state import MerkleTreeState
+from raiden.transfer.merkle_tree import compute_layers
 from raiden.utils import sha3
 from raiden_contracts.tests.utils import LOCKSROOT_OF_NO_LOCKS
 
@@ -37,12 +36,3 @@ def test_compute_layers_duplicated():
 
     with pytest.raises(ValueError):
         compute_layers([hash_0, hash_1, hash_0])
-
-
-def test_compute_layers_single_entry():
-    hash_0 = sha3(b"x")
-    layers = compute_layers([hash_0])
-    assert layers[MERKLEROOT][0] == hash_0
-
-    tree = MerkleTreeState(layers)
-    assert compute_locksroot(tree) == hash_0
