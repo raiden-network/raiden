ab138a07f1be980939a46e77845a9564b63400a4
Augusto F. Hack <hack.augusto@gmail.com>
2019-06-17 12:55:03 -0300

    Separate debug log for each test
    This also fixed a bug in the get_artifacts_storage usage, which added
    the test name twice in local runs.

--- raiden/tests/utils/ci.py
+++ raiden/tests/utils/ci.py
@@ -1,5 +1,12 @@
 import os
 
+from raiden.utils.typing import Optional
 
-def get_artifacts_storage(default):
-    return os.environ.get("RAIDEN_TESTS_ETH_LOGSDIR", default)
+
+def get_artifacts_storage(*parts) -> Optional[str]:
+    artifact_dir = os.environ.get("RAIDEN_TESTS_ETH_LOGSDIR")
+
+    if artifact_dir:
+        os.path.join(artifact_dir, *parts)
+
+    return None
