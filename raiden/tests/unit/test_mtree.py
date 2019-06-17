fc883effb41ba21e0a5b148407f8415ec409c2a3
Yoichi Hirai <yoichi@brainbot.com>
2019-06-17 11:04:46 +0200

    Rename a merkle related argument

--- raiden/tests/unit/test_mtree.py
+++ raiden/tests/unit/test_mtree.py
@@ -1,37 +1,8 @@
-import pytest
-
-from raiden.exceptions import HashLengthNot32
 from raiden.transfer.channel import compute_locksroot
-from raiden.transfer.merkle_tree import compute_layers
 from raiden.transfer.state import PendingLocksState
-from raiden.utils import sha3
 from raiden_contracts.tests.utils import LOCKSROOT_OF_NO_LOCKS
 
 
 def test_empty():
     locks = PendingLocksState(dict())
     assert compute_locksroot(locks) == LOCKSROOT_OF_NO_LOCKS
-
-
-def test_compute_layers_empty():
-    with pytest.raises(AssertionError):
-        compute_layers([])
-
-
-def test_compute_layers_invalid_length():
-    with pytest.raises(HashLengthNot32):
-        compute_layers([b"not32bytes", b"neither"])
-
-    with pytest.raises(HashLengthNot32):
-        compute_layers([b""])
-
-
-def test_compute_layers_duplicated():
-    hash_0 = sha3(b"x")
-    hash_1 = sha3(b"y")
-
-    with pytest.raises(ValueError):
-        compute_layers([hash_0, hash_0])
-
-    with pytest.raises(ValueError):
-        compute_layers([hash_0, hash_1, hash_0])
