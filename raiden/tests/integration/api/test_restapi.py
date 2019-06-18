923524d39405685b6490cfae976d1780786bc57d
Konrad Feldmeier <konrad@brainbot.com>
2019-06-18 18:02:32 +0200

    Remove unnecessary second test run

--- raiden/tests/integration/api/test_restapi.py
+++ raiden/tests/integration/api/test_restapi.py
@@ -1869,7 +1869,7 @@ def test_pending_transfers_endpoint(raiden_network, token_addresses):
 
 
 @pytest.mark.parametrize("number_of_nodes", [2])
-@pytest.mark.parametrize("deposit", [1000, 1000])
+@pytest.mark.parametrize("deposit", [1000])
 def test_api_withdraw(api_server_test_instance, raiden_network, token_addresses):
     _, app1 = raiden_network
     token_address = token_addresses[0]
