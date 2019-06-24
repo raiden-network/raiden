9d82ad95221e7faaf2b806696f99db5d4808eba2
Augusto F. Hack <hack.augusto@gmail.com>
2019-06-24 09:29:55 -0300

    fix get_artifacts_storage

--- raiden/tests/utils/ci.py
+++ raiden/tests/utils/ci.py
@@ -7,6 +7,6 @@ def get_artifacts_storage(*parts) -> Optional[str]:
     artifact_dir = os.environ.get("RAIDEN_TESTS_ETH_LOGSDIR")
 
     if artifact_dir:
-        os.path.join(artifact_dir, *parts)
+        return os.path.join(artifact_dir, *parts)
 
     return None
