cb2558342318f11aaee75db0ce5715519e28bc4a
Augusto F. Hack <hack.augusto@gmail.com>
2019-06-24 11:16:36 -0300

    Fixed tests path length

--- raiden/tests/conftest.py
+++ raiden/tests/conftest.py
@@ -33,6 +33,7 @@ from raiden.tests.fixtures.blockchain import *  # noqa: F401,F403
 from raiden.tests.fixtures.variables import *  # noqa: F401,F403
 from raiden.tests.utils.ci import get_artifacts_storage
 from raiden.tests.utils.transport import make_requests_insecure
+from raiden.utils import pex
 from raiden.utils.cli import LogLevelConfigType
 from raiden.utils.debugging import enable_gevent_monitoring_signal
 
@@ -136,7 +137,13 @@ def logging_level(request, tmpdir):
     else:
         logging_levels = {"": level}
 
-    base_dir = get_artifacts_storage(request.node.name) or str(tmpdir)
+    # The test name including the arguments cannot be used because of the path
+    # length. Some tests will have a path longer than 300 characters.
+    original_name = request.node.originalname
+    shorted_arguments = pex(request.node.name.encode("utf8"))
+    shortened_test_name = f"{original_name}-{shorted_arguments}"
+
+    base_dir = get_artifacts_storage(shortened_test_name) or str(tmpdir)
     os.makedirs(base_dir, exist_ok=True)
 
     time = datetime.datetime.utcnow().isoformat()
