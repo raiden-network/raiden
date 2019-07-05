52bab466989aba88bbd373583cacdcd37ee6e0eb
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-07-05 12:29:53 +0200

    Fix proxy PR feedback

--- raiden/network/proxies/utils.py
+++ raiden/network/proxies/utils.py
@@ -1,26 +1,65 @@
 from contextlib import contextmanager
 from typing import TYPE_CHECKING
 
-from eth_utils import to_hex
+from eth_utils import decode_hex, to_hex
 from structlog import BoundLoggerBase
 
+from raiden.constants import GENESIS_BLOCK_NUMBER
 from raiden.exceptions import RaidenUnrecoverableError
+from raiden.network.proxies.token_network import TokenNetwork
 from raiden.transfer.identifiers import CanonicalIdentifier
+from raiden.utils.filters import decode_event, get_filter_args_for_specific_event_from_channel
 from raiden.utils.typing import (
     Address,
     Any,
     BlockSpecification,
+    ChannelID,
     Dict,
     Generator,
     Locksroot,
     NoReturn,
+    Optional,
     T_BlockHash,
     Tuple,
 )
+from raiden_contracts.constants import CONTRACT_TOKEN_NETWORK, ChannelEvent
+from raiden_contracts.contract_manager import ContractManager
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import
     from raiden.network.blockchain_service import BlockChainService
+
+
+def get_channel_participants_from_open_event(
+    token_network: TokenNetwork, channel_identifier: ChannelID, contract_manager: ContractManager
+) -> Optional[Tuple[Address, Address]]:
+    # FIXME: Issue #3958
+    from_block = GENESIS_BLOCK_NUMBER
+
+    # For this check it is perfectly fine to use a `latest` block number.
+    # Because the filter is looking just for the OPENED event.
+    to_block = "latest"
+
+    filter_args = get_filter_args_for_specific_event_from_channel(
+        token_network_address=token_network.address,
+        channel_identifier=channel_identifier,
+        event_name=ChannelEvent.OPENED,
+        contract_manager=contract_manager,
+        from_block=from_block,
+        to_block=to_block,
+    )
+
+    events = token_network.proxy.contract.web3.eth.getLogs(filter_args)
+
+    # There must be only one channel open event per channel identifier
+    if len(events) != 1:
+        return None
+
+    event = decode_event(contract_manager.get_contract_abi(CONTRACT_TOKEN_NETWORK), events[0])
+    participant1 = Address(decode_hex(event["args"]["participant1"]))
+    participant2 = Address(decode_hex(event["args"]["participant2"]))
+
+    return participant1, participant2
 
 
 def get_onchain_locksroots(
