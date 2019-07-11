bec5dac7eaace5331135e502269f3018d30a0ee7
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-07-11 14:49:41 +0200

    Mark stress test as flaky

--- raiden/tests/integration/long_running/test_stress.py
+++ raiden/tests/integration/long_running/test_stress.py
@@ -322,6 +322,7 @@ def assert_channels(raiden_network, token_network_address, deposit):
         )
 
 
+@pytest.mark.flaky(max_runs=5)
 @pytest.mark.parametrize("number_of_nodes", [3])
 @pytest.mark.parametrize("number_of_tokens", [1])
 @pytest.mark.parametrize("channels_per_node", [2])
