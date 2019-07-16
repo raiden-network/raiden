61217205dd70a55c2e53f109b8604ea4763e97dc
Augusto F. Hack <hack.augusto@gmail.com>
2019-07-16 07:45:13 -0300

    mark stress test as flaky #4380

--- raiden/tests/integration/long_running/test_stress.py
+++ raiden/tests/integration/long_running/test_stress.py
@@ -329,6 +329,7 @@ def assert_channels(raiden_network, token_network_address, deposit):
 @pytest.mark.parametrize("deposit", [5])
 @pytest.mark.parametrize("reveal_timeout", [15])
 @pytest.mark.parametrize("settle_timeout", [120])
+@pytest.mark.flaky(max_runs=5)
 def test_stress(request, raiden_network, deposit, retry_timeout, token_addresses, port_generator):
 
     config_converter = LogLevelConfigType()
