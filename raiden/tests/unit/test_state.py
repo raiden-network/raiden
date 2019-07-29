f87e8c1013d6275de26acb69a90175ac347c0d1d
Karl Bartel <karl42@gmail.com>
2019-07-29 13:53:57 +0200

    Remove deposit event confirmation handling
    The code was written when the incoming event was unconfirmed. Now, all
    events are already confirmed when an Event instance is created. The
    confirmation handling that is left is not only unnecessary, but even
    creates wrong behavior by adding an additional latency of
    `confirmation_blocks` to the event processing.
    
    This commit removes the confirmation handling and the respective tests.
    No other changes intended.
    
    Closes https://github.com/raiden-network/raiden/issues/4377.

--- raiden/tests/unit/test_state.py
+++ raiden/tests/unit/test_state.py
@@ -1,4 +1,4 @@
-from raiden.transfer.state import TransactionChannelDeposit, TransactionOrder
+from raiden.transfer.state import TransactionChannelDeposit
 
 
 def test_transaction_channel_new_balance_ordering():
@@ -19,29 +19,3 @@ def test_transaction_channel_new_balance_ordering():
     assert a == b
     assert not a > b
     assert not b > a
-
-
-def test_transaction_order_ordering():
-    a = TransactionOrder(1, TransactionChannelDeposit(bytes(1), 1, 1))
-    b = TransactionOrder(2, TransactionChannelDeposit(bytes(2), 2, 2))
-    assert a != b
-    assert a < b
-    assert b > a
-
-    a = TransactionOrder(1, TransactionChannelDeposit(bytes(1), 1, 1))
-    b = TransactionOrder(2, TransactionChannelDeposit(bytes(2), 2, 1))
-    assert a != b
-    assert a < b
-    assert b > a
-
-    a = TransactionOrder(3, TransactionChannelDeposit(bytes(3), 3, 3))
-    b = TransactionOrder(3, TransactionChannelDeposit(bytes(3), 3, 3))
-    assert a == b
-    assert not a > b
-    assert not b > a
-
-    a = TransactionOrder(1, TransactionChannelDeposit(bytes(1), 1, 1))
-    b = TransactionOrder(2, TransactionChannelDeposit(bytes(1), 1, 1))
-    assert a != b
-    assert a < b
-    assert b > a
