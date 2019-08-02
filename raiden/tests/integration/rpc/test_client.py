e81dde25a28b595fe95aa2847cc8614be65e63aa
Karl Bartel <karl42@gmail.com>
2019-08-02 11:53:41 +0200

    Remove sleep for test_geth_discover_next_..._nonce
    Part of https://github.com/raiden-network/raiden/issues/4368

--- raiden/tests/integration/rpc/test_client.py
+++ raiden/tests/integration/rpc/test_client.py
@@ -13,17 +13,14 @@ def test_geth_discover_next_available_nonce(
     https://github.com/raiden-network/raiden/pull/3683#issue-264551799
     """
 
-    greenlets = set()
-    for _ in range(100):
-        greenlets.add(
-            gevent.spawn(deploy_client.send_transaction, make_address(), 50000)  # to  # startgas
-        )
-    gevent.sleep(0.5)
+    greenlets = {
+        gevent.spawn(deploy_client.send_transaction, make_address(), 50000)  # to  # startgas
+        for _ in range(100)
+    }
+    gevent.joinall(set(greenlets), raise_error=True)
 
     nonce = geth_discover_next_available_nonce(
         web3=deploy_client.web3, address=deploy_client.address
     )
     assert nonce > 0
-    assert nonce < 100
-
-    gevent.joinall(greenlets, raise_error=True)
+    assert nonce <= 100
