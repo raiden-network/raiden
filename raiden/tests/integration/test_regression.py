528df4e9c650a966ee8172345e2a3e58ae97678d
Paul Lange <palango@gmx.de>
2019-08-12 17:13:27 +0200

    Fixes flaky #4532
    The test works fine, just the timeout for the second tranbsfer is to
    slow.

--- raiden/tests/integration/test_regression.py
+++ raiden/tests/integration/test_regression.py
@@ -11,14 +11,18 @@ from raiden.messages.transfers import Lock, LockedTransfer, RevealSecret, Unlock
 from raiden.tests.fixtures.variables import TransportProtocol
 from raiden.tests.integration.fixtures.raiden_network import CHAIN, wait_for_channels
 from raiden.tests.utils.detect_failure import raise_on_failure
-from raiden.tests.utils.events import raiden_state_changes_search_for_item, search_for_item
+from raiden.tests.utils.events import (
+    raiden_events_search_for_item,
+    raiden_state_changes_search_for_item,
+    search_for_item,
+)
 from raiden.tests.utils.factories import UNIT_CHAIN_ID
 from raiden.tests.utils.network import payment_channel_open_and_deposit
 from raiden.tests.utils.transfer import get_channelstate, transfer
 from raiden.transfer import views
-from raiden.transfer.mediated_transfer.events import SendSecretReveal
+from raiden.transfer.mediated_transfer.events import EventRouteFailed, SendSecretReveal
 from raiden.transfer.mediated_transfer.state_change import ReceiveTransferCancelRoute
-from raiden.utils import sha3
+from raiden.utils import PaymentID, sha3
 
 # pylint: disable=too-many-locals
 
@@ -247,7 +251,6 @@ def test_regression_register_secret_once(secret_registry_address, deploy_service
     assert previous_nonce == deploy_service.client._available_nonce
 
 
-@pytest.mark.skip(reason="flaky, see https://github.com/raiden-network/raiden/issues/4532")
 @pytest.mark.parametrize("number_of_nodes", [5])
 @pytest.mark.parametrize("channels_per_node", [0])
 def test_regression_payment_complete_after_refund_to_the_initiator(
@@ -255,8 +258,8 @@ def test_regression_payment_complete_after_refund_to_the_initiator(
 ):
     """Regression test for issue #3915"""
     raise_on_failure(
-        raiden_network,
-        run_regression_payment_complete_after_refund_to_the_initiator,
+        raiden_apps=raiden_network,
+        test_function=run_regression_payment_complete_after_refund_to_the_initiator,
         raiden_network=raiden_network,
         token_addresses=token_addresses,
         settle_timeout=settle_timeout,
@@ -274,23 +277,35 @@ def run_regression_payment_complete_after_refund_to_the_initiator(
     # Topology:
     #
     #  0 -> 1 -> 2
-    #  v         ^
+    #  |         ^
+    #  v         |
     #  3 ------> 4
 
     app_channels = [(app0, app1), (app1, app2), (app0, app3), (app3, app4), (app4, app2)]
-
     open_and_wait_for_channels(app_channels, registry_address, token, deposit, settle_timeout)
 
     # Use all deposit from app1->app2 to force a refund
     transfer(
-        initiator_app=app1, target_app=app2, token_address=token, amount=deposit, identifier=1
+        initiator_app=app1,
+        target_app=app2,
+        token_address=token,
+        amount=deposit,
+        identifier=PaymentID(1),
     )
 
     # Send a transfer that will result in a refund app1->app0
     transfer(
-        initiator_app=app0, target_app=app2, token_address=token, amount=deposit, identifier=2
+        initiator_app=app0,
+        target_app=app2,
+        token_address=token,
+        amount=deposit,
+        identifier=PaymentID(2),
+        timeout=20,
     )
 
     assert raiden_state_changes_search_for_item(
         raiden=app0.raiden, item_type=ReceiveTransferCancelRoute, attributes={}
+    )
+    assert raiden_events_search_for_item(
+        raiden=app0.raiden, item_type=EventRouteFailed, attributes={}
     )
