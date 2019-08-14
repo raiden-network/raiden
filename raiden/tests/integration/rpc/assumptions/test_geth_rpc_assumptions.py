5a3326feb905dbefdb75d463eaebbc303fb06ed4
Lefteris Karapetsas <lefteris@refu.co>
2019-08-14 13:47:04 +0200

    Attempt to fix flakiness at geth_request_pruned_data test
    According to the geth devs if we run a PoA chain (clique, dev mode) and
    HEAD-127 doesn't contain any transactions, then the state of HEAD-127 will
    be the same as HEAD-128, so it will still be referenced and not deleted.
    To avoid this and make sure the pruned_block_number is pruned let's mine
    buffer period more blocks while at the same time sending transactions
    
    Fix #4545

--- raiden/tests/integration/rpc/assumptions/test_geth_rpc_assumptions.py
+++ raiden/tests/integration/rpc/assumptions/test_geth_rpc_assumptions.py
@@ -6,7 +6,6 @@ from raiden.utils import safe_gas_limit
 pytestmark = pytest.mark.usefixtures("skip_if_not_geth")
 
 
-@pytest.mark.skip(reason="Flaky, see https://github.com/raiden-network/raiden/issues/4545")
 def test_geth_request_pruned_data_raises_an_exception(deploy_client, web3):
     """ Interacting with an old block identifier with a pruning client throws. """
     contract_proxy, _ = deploy_rpc_test_contract(deploy_client, "RpcWithStorageTest")
@@ -26,7 +25,14 @@ def test_geth_request_pruned_data_raises_an_exception(deploy_client, web3):
     # geth keeps the latest 128 blocks before pruning. Unfortunately, this can
     # not be configured to speed this test up.
     non_pruned_blocks = 128
-    while web3.eth.blockNumber < pruned_block_number + non_pruned_blocks + 1:
+    # According to the geth devs if we run a PoA chain (clique, dev mode) and
+    # HEAD-127 doesn't contain any transactions, then the state of HEAD-127 will
+    # be the same as HEAD-128, so it will still be referenced and not deleted.
+    # To avoid this and make sure the pruned_block_number is pruned let's mine
+    # buffer period more blocks while at the same time sending transactions
+    buffer_period_blocks = 5
+    target_block = pruned_block_number + non_pruned_blocks + buffer_period_blocks + 1
+    while web3.eth.blockNumber <= target_block:
         send_transaction()
 
     with pytest.raises(ValueError):
