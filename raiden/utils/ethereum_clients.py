c0763b023cc18c7262196ff70da466be2c3ddef9
Lefteris Karapetsas <lefteris@refu.co>
2019-08-15 12:58:44 +0200

    Change eth client version support check to ignore patch version bump
    Fix #4608

--- raiden/utils/ethereum_clients.py
+++ raiden/utils/ethereum_clients.py
@@ -1,6 +1,7 @@
 import re
 
 from pkg_resources import parse_version
+from pkg_resources.extern.packaging.version import Version
 
 from raiden.constants import (
     HIGHEST_SUPPORTED_GETH_VERSION,
@@ -24,6 +25,32 @@ def parse_geth_version(client_version: str) -> Optional[tuple]:
     return parse_version(matches.groups()[0])
 
 
+def support_check(
+    our_version: Version,
+    highest_supported_version_string: str,
+    lowest_supported_version_string: str,
+) -> bool:
+    if len(our_version._key[1]) != 3:
+        # This can happen for versions such as 0.0.0
+        return False
+
+    # our_major_num = our_version._key[1][0]
+    our_minor_num = our_version._key[1][1]
+    highest_supported_version: Version = parse_version(highest_supported_version_string)
+    # highest_supported_major_num = highest_supported_version._key[1][0]
+    highest_supported_min_num = highest_supported_version._key[1][1]
+    if our_version < parse_version(lowest_supported_version_string):
+        return False
+
+    if our_version > highest_supported_version:
+        if our_minor_num == highest_supported_min_num:
+            return True
+        # else
+        return False
+
+    return True
+
+
 def is_supported_client(client_version: str) -> Tuple[bool, Optional[EthClient], Optional[str]]:
     """Takes a client version string either from web3.version.node or from
     `geth version` or `parity --version` and sees if it is supported.
@@ -36,21 +63,22 @@ def is_supported_client(client_version: str) -> Tuple[bool, Optional[EthClient],
         matches = re.search(r"/+v(\d+\.\d+\.\d+)", client_version)
         if matches is None:
             return False, None, None
+
         our_parity_version = parse_version(matches.groups()[0])
-        supported = (
-            parse_version(LOWEST_SUPPORTED_PARITY_VERSION)
-            <= our_parity_version
-            <= parse_version(HIGHEST_SUPPORTED_PARITY_VERSION)
+        supported = support_check(
+            our_version=our_parity_version,
+            highest_supported_version_string=HIGHEST_SUPPORTED_PARITY_VERSION,
+            lowest_supported_version_string=LOWEST_SUPPORTED_PARITY_VERSION,
         )
         return supported, EthClient.PARITY, str(our_parity_version)
     elif client_version.startswith("Geth"):
         our_geth_version = parse_geth_version(client_version)
         if our_geth_version is None:
             return False, None, None
-        supported = (
-            parse_version(LOWEST_SUPPORTED_GETH_VERSION)
-            <= our_geth_version
-            <= parse_version(HIGHEST_SUPPORTED_GETH_VERSION)
+        supported = support_check(
+            our_version=our_geth_version,
+            highest_supported_version_string=HIGHEST_SUPPORTED_GETH_VERSION,
+            lowest_supported_version_string=LOWEST_SUPPORTED_GETH_VERSION,
         )
         return supported, EthClient.GETH, str(our_geth_version)
 
