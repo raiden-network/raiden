9a261593d11c2eb823a40eac5bde40165656a971
Lefteris Karapetsas <lefteris@refu.co>
2019-08-23 11:45:24 +0200

    Simplify the geth pruning test

--- raiden/tests/integration/rpc/assumptions/test_geth_rpc_assumptions.py
+++ raiden/tests/integration/rpc/assumptions/test_geth_rpc_assumptions.py
@@ -1,4 +1,6 @@
+import gevent
 import pytest
+
 
 from raiden.tests.utils.smartcontracts import deploy_rpc_test_contract
 from raiden.utils import safe_gas_limit
@@ -20,21 +22,20 @@ def test_geth_request_pruned_data_raises_an_exception(deploy_client, web3):
         return deploy_client.get_transaction_receipt(transaction)
 
     first_receipt = send_transaction()
-    pruned_block_number = first_receipt["blockNumber"]
+    mined_block_number = first_receipt["blockNumber"]
+
+    while mined_block_number + 127 > web3.eth.blockNumber:
+        gevent.sleep(0.5)
 
     # geth keeps the latest 128 blocks before pruning. Unfortunately, this can
     # not be configured to speed this test up.
-    non_pruned_blocks = 128
     # According to the geth devs if we run a PoA chain (clique, dev mode) and
     # HEAD-127 doesn't contain any transactions, then the state of HEAD-127 will
     # be the same as HEAD-128, so it will still be referenced and not deleted.
-    # To avoid this and make sure the pruned_block_number is pruned let's mine
-    # buffer period more blocks while at the same time sending transactions
-    buffer_period_blocks = 5
-    target_block = pruned_block_number + non_pruned_blocks + buffer_period_blocks + 1
-    while web3.eth.blockNumber <= target_block:
-        send_transaction()
+    # So for this test we mine a transaction, wait for mined + 127 block and then
+    # query the previous block which should be pruned
 
+    pruned_block_number = mined_block_number - 1
     with pytest.raises(ValueError):
         contract_proxy.contract.functions.const().call(block_identifier=pruned_block_number)
 
