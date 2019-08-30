8460d65fc28a2fd93482cb909397ca312c6c20e8
Paul Lange <palango@gmx.de>
2019-08-30 14:48:43 +0200

    Remove type aliases from test_mediation_fee

--- raiden/tests/unit/transfer/mediated_transfer/test_mediation_fee.py
+++ raiden/tests/unit/transfer/mediated_transfer/test_mediation_fee.py
@@ -7,13 +7,7 @@ from raiden.transfer.mediated_transfer.mediation_fee import (
     calculate_imbalance_fees,
     linspace,
 )
-from raiden.utils.typing import (
-    Balance,
-    FeeAmount as FA,
-    PaymentAmount,
-    RelativeFeeAmount as RFA,
-    TokenAmount as TA,
-)
+from raiden.utils.typing import Balance, FeeAmount, PaymentAmount, RelativeFeeAmount, TokenAmount
 
 
 def test_interpolation():
@@ -38,68 +32,86 @@ def test_interpolation():
 
 
 def test_basic_fee():
-    flat_schedule = FeeScheduleState(flat=FA(2))
-    assert flat_schedule.fee(PaymentAmount(10), channel_balance=Balance(0)) == FA(2)
+    flat_schedule = FeeScheduleState(flat=FeeAmount(2))
+    assert flat_schedule.fee(PaymentAmount(10), channel_balance=Balance(0)) == FeeAmount(2)
 
-    prop_schedule = FeeScheduleState(proportional=RFA(int(0.01e6)))
-    assert prop_schedule.fee(PaymentAmount(40), channel_balance=Balance(0)) == FA(0)
-    assert prop_schedule.fee(PaymentAmount(60), channel_balance=Balance(0)) == FA(1)
-    assert prop_schedule.fee(PaymentAmount(1000), channel_balance=Balance(0)) == FA(10)
+    prop_schedule = FeeScheduleState(proportional=RelativeFeeAmount(int(0.01e6)))
+    assert prop_schedule.fee(PaymentAmount(40), channel_balance=Balance(0)) == FeeAmount(0)
+    assert prop_schedule.fee(PaymentAmount(60), channel_balance=Balance(0)) == FeeAmount(1)
+    assert prop_schedule.fee(PaymentAmount(1000), channel_balance=Balance(0)) == FeeAmount(10)
 
-    combined_schedule = FeeScheduleState(flat=FA(2), proportional=RFA(int(0.01e6)))
-    assert combined_schedule.fee(PaymentAmount(60), channel_balance=Balance(0)) == FA(3)
+    combined_schedule = FeeScheduleState(
+        flat=FeeAmount(2), proportional=RelativeFeeAmount(int(0.01e6))
+    )
+    assert combined_schedule.fee(PaymentAmount(60), channel_balance=Balance(0)) == FeeAmount(3)
 
 
 def test_imbalance_penalty():
     v_schedule = FeeScheduleState(
-        imbalance_penalty=[(TA(0), FA(10)), (TA(50), FA(0)), (TA(100), FA(10))]
+        imbalance_penalty=[
+            (TokenAmount(0), FeeAmount(10)),
+            (TokenAmount(50), FeeAmount(0)),
+            (TokenAmount(100), FeeAmount(10)),
+        ]
     )
-    assert v_schedule.fee(channel_balance=Balance(100 - 0), amount=PaymentAmount(50)) == FA(-10)
-    assert v_schedule.fee(channel_balance=Balance(100 - 50), amount=PaymentAmount(50)) == FA(10)
-    assert v_schedule.fee(channel_balance=Balance(100 - 0), amount=PaymentAmount(10)) == FA(-2)
-    assert v_schedule.fee(channel_balance=Balance(100 - 10), amount=PaymentAmount(10)) == FA(-2)
-    assert v_schedule.fee(channel_balance=Balance(100 - 0), amount=PaymentAmount(20)) == FA(-4)
-    assert v_schedule.fee(channel_balance=Balance(100 - 40), amount=PaymentAmount(20)) == FA(0)
+    assert v_schedule.fee(channel_balance=Balance(100 - 0), amount=PaymentAmount(50)) == FeeAmount(
+        -10
+    )
+    assert v_schedule.fee(
+        channel_balance=Balance(100 - 50), amount=PaymentAmount(50)
+    ) == FeeAmount(10)
+    assert v_schedule.fee(channel_balance=Balance(100 - 0), amount=PaymentAmount(10)) == FeeAmount(
+        -2
+    )
+    assert v_schedule.fee(
+        channel_balance=Balance(100 - 10), amount=PaymentAmount(10)
+    ) == FeeAmount(-2)
+    assert v_schedule.fee(channel_balance=Balance(100 - 0), amount=PaymentAmount(20)) == FeeAmount(
+        -4
+    )
+    assert v_schedule.fee(
+        channel_balance=Balance(100 - 40), amount=PaymentAmount(20)
+    ) == FeeAmount(0)
 
 
 def test_linspace():
-    assert linspace(TA(0), TA(4), 5) == [0, 1, 2, 3, 4]
-    assert linspace(TA(0), TA(4), 4) == [0, 1, 3, 4]
-    assert linspace(TA(0), TA(4), 3) == [0, 2, 4]
-    assert linspace(TA(0), TA(4), 2) == [0, 4]
-    assert linspace(TA(0), TA(0), 3) == [0, 0, 0]
+    assert linspace(TokenAmount(0), TokenAmount(4), 5) == [0, 1, 2, 3, 4]
+    assert linspace(TokenAmount(0), TokenAmount(4), 4) == [0, 1, 3, 4]
+    assert linspace(TokenAmount(0), TokenAmount(4), 3) == [0, 2, 4]
+    assert linspace(TokenAmount(0), TokenAmount(4), 2) == [0, 4]
+    assert linspace(TokenAmount(0), TokenAmount(0), 3) == [0, 0, 0]
 
     with pytest.raises(AssertionError):
-        assert linspace(TA(0), TA(4), 1)
+        assert linspace(TokenAmount(0), TokenAmount(4), 1)
 
     with pytest.raises(AssertionError):
-        assert linspace(TA(4), TA(0), 2)
+        assert linspace(TokenAmount(4), TokenAmount(0), 2)
 
 
 def test_rebalancing_fee_calculation():
-    sample = calculate_imbalance_fees(TA(200), RFA(500_000))  # 50%
+    sample = calculate_imbalance_fees(TokenAmount(200), RelativeFeeAmount(500_000))  # 50%
     assert sample is not None
     assert len(sample) == NUM_DISCRETISATION_POINTS
     assert all(0 <= x <= 200 for x, _ in sample)
     assert max(x for x, _ in sample) == 200
     assert all(0 <= y <= 100 for _, y in sample)
-    assert max(y for _, y in sample) == 100  # 50% of the 200 TA capacity
+    assert max(y for _, y in sample) == 100  # 50% of the 200 TokenAmount capacity
 
-    sample = calculate_imbalance_fees(TA(10), RFA(200_000))  # 20%
+    sample = calculate_imbalance_fees(TokenAmount(10), RelativeFeeAmount(200_000))  # 20%
     assert sample is not None
     assert len(sample) == 11
     assert all(0 <= x <= 10 for x, _ in sample)
     assert max(x for x, _ in sample) == 10
     assert all(0 <= y <= 2 for _, y in sample)
-    assert max(y for _, y in sample) == 2  # 20% of the 10 TA capacity
+    assert max(y for _, y in sample) == 2  # 20% of the 10 TokenAmount capacity
 
-    sample = calculate_imbalance_fees(TA(1), RFA(1_000_000))  # 100%
+    sample = calculate_imbalance_fees(TokenAmount(1), RelativeFeeAmount(1_000_000))  # 100%
     assert sample is not None
     assert len(sample) == 2
     assert all(0 <= x <= 1 for x, _ in sample)
     assert max(x for x, _ in sample) == 1
     assert all(0 <= y <= 1 for _, y in sample)
-    assert max(y for _, y in sample) == 1  # 100% of the 1 TA capacity
+    assert max(y for _, y in sample) == 1  # 100% of the 1 TokenAmount capacity
 
-    assert calculate_imbalance_fees(TA(0), RFA(1)) is None
-    assert calculate_imbalance_fees(TA(10), RFA(0)) is None
+    assert calculate_imbalance_fees(TokenAmount(0), RelativeFeeAmount(1)) is None
+    assert calculate_imbalance_fees(TokenAmount(10), RelativeFeeAmount(0)) is None
