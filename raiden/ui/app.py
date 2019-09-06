7e8321e13e57f6b34ceffac8eb6baafee46f92a9
Paul Lange <palango@gmx.de>
2019-09-06 11:44:06 +0200

    Properly calculate per-channel proportional fee

--- raiden/ui/app.py
+++ raiden/ui/app.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from math import sqrt
 from typing import Any, Callable, Dict, TextIO
 from urllib.parse import urlparse
 
@@ -125,6 +126,37 @@ def rpc_normalized_endpoint(eth_rpc_endpoint: str) -> str:
     return f"http://{eth_rpc_endpoint}"
 
 
+def prepare_mediation_fee_config(
+    flat_fees: Tuple[Tuple[TokenNetworkAddress, FeeAmount], ...],
+    proportional_fee: ProportionalFeeAmount,
+    proportional_imbalance_fee: ProportionalFeeAmount,
+) -> MediationFeeConfig:
+    """ Converts the mediation fee CLI args to proper per-channel
+    mediation fees. """
+    # Store the flat fee settings for the given token networks
+    # The given flat fee is for the whole mediation, but that includes two channels.
+    # Therefore divide by 2 here.
+    token_network_to_flat_fee: Dict[TokenNetworkAddress, FeeAmount] = {
+        address: FeeAmount(fee // 2) for address, fee in flat_fees
+    }
+    # Given prop. fee also counts per mediation, therefore it needs to be adjusted on
+    # a per-channel basis:
+    # x * (1 - p) * (1 - p) = x * (1 - q)
+    # where
+    #    x = payment amount
+    #    q = cli proportional fee
+    #    p = per channel proportional fee
+    # Leads to: p = 1 - sqrt(1 - q)
+    proportional_fee_ratio = proportional_fee / 1e6
+    channel_prop_fee_ratio = 1 - sqrt(1 - proportional_fee_ratio)
+    channel_prop_fee = round(channel_prop_fee_ratio * 1e6)
+    return MediationFeeConfig(
+        token_network_to_flat_fee=token_network_to_flat_fee,
+        proportional_fee=ProportionalFeeAmount(channel_prop_fee),
+        proportional_imbalance_fee=proportional_imbalance_fee,
+    )
+
+
 def run_app(
     address: Address,
     keystore_path: str,
@@ -180,17 +212,9 @@ def run_app(
     if not api_port:
         api_port = Port(DEFAULT_HTTP_SERVER_PORT)
 
-    # Store the flat fee settings for the given token networks
-    # The given flat fee is for the whole mediation, but that includes two channels.
-    # Therefore divide by 2 here.
-    token_network_to_flat_fee: Dict[TokenNetworkAddress, FeeAmount] = {
-        address: FeeAmount(fee // 2) for address, fee in flat_fee
-    }
-    # Given prop. fee also counts per mediation, therefore divide by 2 as well
-    # Division is fine here, as the prop. fees are applied independently, not subsequently
-    fee_config = MediationFeeConfig(
-        token_network_to_flat_fee=token_network_to_flat_fee,
-        proportional_fee=ProportionalFeeAmount(proportional_fee // 2),
+    fee_config = prepare_mediation_fee_config(
+        flat_fees=flat_fee,
+        proportional_fee=proportional_fee,
         proportional_imbalance_fee=proportional_imbalance_fee,
     )
 
