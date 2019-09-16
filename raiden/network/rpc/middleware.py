adf8074d926a8269bf58e8e2f7fb3459b268dafe
Paul Lange <palango@gmx.de>
2019-09-16 12:28:25 +0200

    Fix block hash cache
    The cache was not properly initialized before, which resulted in a lot
    of unnecessary RPC calls.

--- raiden/network/rpc/middleware.py
+++ raiden/network/rpc/middleware.py
@@ -1,7 +1,7 @@
 import functools
 from json.decoder import JSONDecodeError
 
-from cachetools import LRUCache
+import lru
 from web3.middleware.cache import construct_simple_cache_middleware
 
 from raiden.exceptions import EthNodeCommunicationError
@@ -36,6 +36,6 @@ BLOCK_HASH_CACHE_RPC_WHITELIST = {"eth_getBlockByHash"}
 
 block_hash_cache_middleware = construct_simple_cache_middleware(
     # default sample size of gas price strategies is 120
-    cache_class=functools.partial(LRUCache, 150),
+    cache_class=functools.partial(lru.LRU, 256),
     rpc_whitelist=BLOCK_HASH_CACHE_RPC_WHITELIST,
 )
