d0452ed1bb5aea142d443966e5e15357e7cf34ac
Augusto F. Hack <hack.augusto@gmail.com>
2019-09-20 13:19:33 -0300

    Added typing information to smartcontract fixtures

--- raiden/tests/integration/fixtures/smartcontracts.py
+++ raiden/tests/integration/fixtures/smartcontracts.py
@@ -11,14 +11,26 @@ from raiden.constants import (
 from raiden.network.proxies.proxy_manager import ProxyManager, ProxyManagerMetadata
 from raiden.network.proxies.secret_registry import SecretRegistry
 from raiden.network.proxies.token import Token
+from raiden.network.proxies.token_network import TokenNetwork
+from raiden.network.rpc.client import JSONRPCClient
 from raiden.settings import MONITORING_REWARD
 from raiden.tests.utils.smartcontracts import (
     deploy_contract_web3,
     deploy_token,
     deploy_tokens_and_fund_accounts,
 )
-from raiden.utils import privatekey_to_address, typing
-from raiden.utils.typing import Optional
+from raiden.utils import privatekey_to_address
+from raiden.utils.typing import (
+    Address,
+    ChainID,
+    List,
+    Optional,
+    PrivateKey,
+    SecretRegistryAddress,
+    TokenAddress,
+    TokenAmount,
+    TokenNetworkRegistryAddress,
+)
 from raiden_contracts.constants import (
     CONTRACT_CUSTOM_TOKEN,
     CONTRACT_ONE_TO_N,
@@ -27,9 +39,10 @@ from raiden_contracts.constants import (
     CONTRACT_TOKEN_NETWORK_REGISTRY,
     CONTRACT_USER_DEPOSIT,
 )
+from raiden_contracts.contract_manager import ContractManager
 
-RED_EYES_PER_CHANNEL_PARTICIPANT_LIMIT = int(0.075 * 10 ** 18)
-RED_EYES_PER_TOKEN_NETWORK_LIMIT = int(250 * 10 ** 18)
+RED_EYES_PER_CHANNEL_PARTICIPANT_LIMIT = TokenAmount(int(0.075 * 10 ** 18))
+RED_EYES_PER_TOKEN_NETWORK_LIMIT = TokenAmount(int(250 * 10 ** 18))
 
 
 @pytest.fixture
@@ -39,24 +52,24 @@ def token_contract_name() -> str:
 
 @pytest.fixture(name="token_addresses")
 def deploy_all_tokens_register_and_return_their_addresses(
-    token_amount,
-    number_of_tokens,
-    private_keys,
-    proxy_manager,
-    token_network_registry_address,
-    register_tokens,
-    contract_manager,
-    token_contract_name,
-) -> typing.List[typing.TokenAddress]:
+    token_amount: TokenAmount,
+    number_of_tokens: int,
+    private_keys: List[PrivateKey],
+    proxy_manager: ProxyManager,
+    token_network_registry_address: TokenNetworkRegistryAddress,
+    register_tokens: bool,
+    contract_manager: ContractManager,
+    token_contract_name: str,
+) -> List[TokenAddress]:
     """ Fixture that yields `number_of_tokens` ERC20 token addresses, where the
     `token_amount` (per token) is distributed among the addresses behind `deploy_client` and
     potentially pre-registered with the Raiden Registry.
     The following arguments can control the behavior:
 
     Args:
-        token_amount (int): the overall number of units minted per token
-        number_of_tokens (int): the number of token instances
-        register_tokens (bool): controls if tokens will be registered with raiden Registry
+        token_amount: the overall number of units minted per token
+        number_of_tokens: the number of token instances
+        register_tokens: controls if tokens will be registered with raiden Registry
     """
 
     participants = [privatekey_to_address(key) for key in private_keys]
@@ -83,7 +96,9 @@ def deploy_all_tokens_register_and_return_their_addresses(
 
 
 @pytest.fixture(name="secret_registry_address")
-def deploy_secret_registry_and_return_address(deploy_client, contract_manager) -> typing.Address:
+def deploy_secret_registry_and_return_address(
+    deploy_client: JSONRPCClient, contract_manager: ContractManager
+) -> Address:
     address = deploy_contract_web3(
         contract_name=CONTRACT_SECRET_REGISTRY,
         deploy_client=deploy_client,
@@ -94,8 +109,11 @@ def deploy_secret_registry_and_return_address(deploy_client, contract_manager) -
 
 @pytest.fixture(name="service_registry_address")
 def maybe_deploy_service_registry_and_return_address(
-    deploy_client, contract_manager, token_proxy, environment_type
-) -> Optional[typing.Address]:
+    deploy_client: JSONRPCClient,
+    contract_manager: ContractManager,
+    token_proxy: Token,
+    environment_type: Environment,
+) -> Optional[Address]:
     if environment_type == Environment.PRODUCTION:
         return None
     # Not sure what to put in the registration fee token for testing, so using
@@ -121,8 +139,13 @@ def maybe_deploy_service_registry_and_return_address(
 
 @pytest.fixture(name="user_deposit_address")
 def deploy_user_deposit_and_return_address(
-    proxy_manager, deploy_client, contract_manager, token_proxy, private_keys, environment_type
-) -> typing.Optional[typing.Address]:
+    proxy_manager: ProxyManager,
+    deploy_client: JSONRPCClient,
+    contract_manager: ContractManager,
+    token_proxy: Token,
+    private_keys: List[PrivateKey],
+    environment_type: Environment,
+) -> Optional[Address]:
     """ Deploy UserDeposit and fund accounts with some balances """
     if environment_type != Environment.DEVELOPMENT:
         return None
@@ -151,12 +174,12 @@ def deploy_user_deposit_and_return_address(
 @pytest.fixture(name="one_to_n_address")
 def deploy_one_to_n_and_return_address(
     user_deposit_address,
-    deploy_client,
-    contract_manager,
-    environment_type,
-    chain_id,
-    service_registry_address,
-) -> typing.Optional[typing.Address]:
+    deploy_client: JSONRPCClient,
+    contract_manager: ContractManager,
+    environment_type: Environment,
+    chain_id: ChainID,
+    service_registry_address: SecretRegistryAddress,
+) -> Optional[Address]:
     """ Deploy OneToN contract and return the address """
     if environment_type != Environment.DEVELOPMENT:
         return None
@@ -173,7 +196,11 @@ def deploy_one_to_n_and_return_address(
 
 
 @pytest.fixture
-def secret_registry_proxy(deploy_client, secret_registry_address, contract_manager):
+def secret_registry_proxy(
+    deploy_client: JSONRPCClient,
+    secret_registry_address: SecretRegistryAddress,
+    contract_manager: ContractManager,
+) -> SecretRegistry:
     """This uses the available SecretRegistry JSONRPCClient proxy to
     instantiate a Raiden proxy.
 
@@ -191,13 +218,13 @@ def secret_registry_proxy(deploy_client, secret_registry_address, contract_manag
 
 @pytest.fixture(name="token_network_registry_address")
 def deploy_token_network_registry_and_return_address(
-    deploy_client,
-    secret_registry_address,
-    chain_id,
-    settle_timeout_min,
-    settle_timeout_max,
-    contract_manager,
-) -> typing.Address:
+    deploy_client: JSONRPCClient,
+    secret_registry_address: SecretRegistryAddress,
+    chain_id: ChainID,
+    settle_timeout_min: int,
+    settle_timeout_max: int,
+    contract_manager: ContractManager,
+) -> TokenNetworkRegistryAddress:
     constructor_arguments = [
         to_checksum_address(secret_registry_address),
         chain_id,
@@ -212,15 +239,17 @@ def deploy_token_network_registry_and_return_address(
         contract_manager=contract_manager,
         constructor_arguments=constructor_arguments,
     )
-    return address
+
+    return TokenNetworkRegistryAddress(to_canonical_address(address))
 
 
 @pytest.fixture(name="token_network_proxy")
 def register_token_and_return_the_network_proxy(
-    contract_manager, deploy_client, token_proxy, token_network_registry_address
-):
-    registry_address = to_canonical_address(token_network_registry_address)
-
+    contract_manager: ContractManager,
+    deploy_client: JSONRPCClient,
+    token_proxy: Token,
+    token_network_registry_address: TokenNetworkRegistryAddress,
+) -> TokenNetwork:
     blockchain_service = ProxyManager(
         rpc_client=deploy_client,
         contract_manager=contract_manager,
@@ -230,7 +259,9 @@ def register_token_and_return_the_network_proxy(
         ),
     )
 
-    token_network_registry_proxy = blockchain_service.token_network_registry(registry_address)
+    token_network_registry_proxy = blockchain_service.token_network_registry(
+        token_network_registry_address
+    )
     token_network_address = token_network_registry_proxy.add_token(
         token_address=token_proxy.address,
         channel_participant_deposit_limit=RED_EYES_PER_CHANNEL_PARTICIPANT_LIMIT,
@@ -250,11 +281,13 @@ def register_token_and_return_the_network_proxy(
 
 
 @pytest.fixture(name="token_proxy")
-def deploy_token_and_return_proxy(deploy_client, contract_manager, token_contract_name):
+def deploy_token_and_return_proxy(
+    deploy_client: JSONRPCClient, contract_manager: ContractManager, token_contract_name: str
+) -> Token:
     token_contract = deploy_token(
         deploy_client=deploy_client,
         contract_manager=contract_manager,
-        initial_amount=1000 * 10 ** 18,
+        initial_amount=TokenAmount(1000 * 10 ** 18),
         decimals=0,
         token_name="TKN",
         token_symbol="TKN",
@@ -263,6 +296,6 @@ def deploy_token_and_return_proxy(deploy_client, contract_manager, token_contrac
 
     return Token(
         jsonrpc_client=deploy_client,
-        token_address=to_canonical_address(token_contract.contract.address),
+        token_address=TokenAddress(token_contract.contract_address),
         contract_manager=contract_manager,
     )
