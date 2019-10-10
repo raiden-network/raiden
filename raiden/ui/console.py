f32684299c21d1b03db0d01deed4f7d7aef988aa
Paul Lange <palango@gmx.de>
2019-10-10 23:44:43 +0200

    Type annotate and fix console

--- raiden/ui/console.py
+++ raiden/ui/console.py
@@ -10,11 +10,22 @@ from IPython.lib.inputhook import inputhook_manager, stdin_ready
 
 from raiden import waiting
 from raiden.api.python import RaidenAPI
+from raiden.app import App
 from raiden.constants import UINT256_MAX
 from raiden.network.proxies.token_network import TokenNetwork
+from raiden.raiden_service import RaidenService
 from raiden.settings import DEFAULT_RETRY_TIMEOUT
-from raiden.utils import TokenAddress, typing
 from raiden.utils.smart_contracts import deploy_contract_web3
+from raiden.utils.typing import (
+    AddressHex,
+    Any,
+    BlockTimeout,
+    Dict,
+    NetworkTimeout,
+    TokenAddress,
+    TokenAmount,
+    TokenNetworkRegistryAddress,
+)
 from raiden_contracts.constants import CONTRACT_HUMAN_STANDARD_TOKEN
 
 GUI_GEVENT = "gevent"
@@ -29,7 +40,7 @@ ENDC = "\033[0m"
 IPython.core.shellapp.InteractiveShellApp.gui.values += ("gevent",)
 
 
-def print_usage():
+def print_usage() -> None:
     print(f"\t{OKBLUE}use `{HEADER}raiden{OKBLUE}` to interact with the raiden service.")
     print(f"\tuse `{HEADER}chain{OKBLUE}` to interact with the blockchain.")
     print(
@@ -45,7 +56,7 @@ def print_usage():
     print("\n" + ENDC)
 
 
-def inputhook_gevent():
+def inputhook_gevent() -> int:
     while not stdin_ready():
         gevent.sleep(0.05)
     return 0
@@ -53,11 +64,11 @@ def inputhook_gevent():
 
 @inputhook_manager.register("gevent")
 class GeventInputHook:
-    def __init__(self, manager):
+    def __init__(self, manager: Any) -> None:
         self.manager = manager
         self._current_gui = GUI_GEVENT
 
-    def enable(self, app=None):
+    def enable(self, app: Any = None) -> Any:
         """ Enable event loop integration with gevent.
 
         Args:
@@ -74,7 +85,7 @@ class GeventInputHook:
         self._current_gui = GUI_GEVENT
         return app
 
-    def disable(self):
+    def disable(self) -> None:
         """ Disable event loop integration with gevent.
 
         This merely sets PyOS_InputHook to NULL.
@@ -87,12 +98,12 @@ class Console(gevent.Greenlet):
     SIGSTP signal (e.g. via keyboard shortcut CTRL-Z).
     """
 
-    def __init__(self, app):
+    def __init__(self, app: App) -> None:
         super().__init__()
         self.app = app
-        self.console_locals = None
+        self.console_locals: Dict[str, Any] = {}
 
-    def _run(self):  # pylint: disable=method-hidden
+    def _run(self) -> None:  # pylint: disable=method-hidden
         # Remove handlers that log to stderr
         root = logging.getLogger()
         for handler in root.handlers[:]:
@@ -106,7 +117,7 @@ class Console(gevent.Greenlet):
         err = io.StringIO()
         sys.stderr = err
 
-        def lastlog(n=10, prefix=None, level=None):
+        def lastlog(n: int = 10, prefix: str = None, level: str = None) -> None:
             """ Print the last `n` log lines to stdout.
             Use `prefix='p2p'` to filter for a specific logger.
             Use `level=INFO` to filter for a specific level.
@@ -120,12 +131,12 @@ class Console(gevent.Greenlet):
             for line in lines[-n:]:
                 print(line)
 
-        def lasterr(n=1):
+        def lasterr(n: int = 1) -> None:
             """ Print the last `n` entries of stderr to stdout. """
             for line in (err.getvalue().strip().split("\n") or [])[-n:]:
                 print(line)
 
-        tools = ConsoleTools(self.app.raiden, self.app.config["settle_timeout"])
+        tools = ConsoleTools(self.app.raiden)
 
         self.console_locals = {
             "app": self.app,
@@ -147,33 +158,34 @@ class Console(gevent.Greenlet):
 
 
 class ConsoleTools:
-    def __init__(self, raiden_service, settle_timeout):
-        self._chain = raiden_service.chain
+    """ Some functions to make working in the console easier. """
+
+    def __init__(self, raiden_service: RaidenService) -> None:
         self._raiden = raiden_service
         self._api = RaidenAPI(raiden_service)
-        self.settle_timeout = settle_timeout
 
     def create_token(
         self,
-        registry_address,
-        initial_alloc=10 ** 6,
-        name="raidentester",
-        symbol="RDT",
-        decimals=2,
-        timeout=60,
-        auto_register=True,
-    ):
+        registry_address_hex: AddressHex,
+        initial_alloc: int = 10 ** 6,
+        name: str = "raidentester",
+        symbol: str = "RDT",
+        decimals: int = 2,
+        timeout: int = 60,
+        auto_register: bool = True,
+    ) -> AddressHex:
         """ Create a proxy for a new HumanStandardToken (ERC20), that is
         initialized with Args(below).
         Per default it will be registered with 'raiden'.
 
         Args:
-            initial_alloc (int): amount of initial tokens.
-            name (str): human readable token name.
-            symbol (str): token shorthand symbol.
-            decimals (int): decimal places.
-            timeout (int): timeout in seconds for creation.
-            auto_register (boolean): if True(default), automatically register
+            registry_address_hex: a hex encoded registry address.
+            initial_alloc: amount of initial tokens.
+            name: human readable token name.
+            symbol: token shorthand symbol.
+            decimals: decimal places.
+            timeout: timeout in seconds for creation.
+            auto_register: if True(default), automatically register
                 the token with raiden.
 
         Returns:
@@ -182,45 +194,48 @@ class ConsoleTools:
         with gevent.Timeout(timeout):
             token_address = deploy_contract_web3(
                 CONTRACT_HUMAN_STANDARD_TOKEN,
-                self._chain.client,
+                self._raiden.rpc_client,
                 contract_manager=self._raiden.contract_manager,
                 constructor_arguments=(initial_alloc, name, decimals, symbol),
             )
 
         token_address_hex = to_checksum_address(token_address)
         if auto_register:
-            self.register_token(registry_address, token_address_hex)
+            self.register_token(registry_address_hex, token_address_hex)
+
         print(
             "Successfully created {}the token '{}'.".format(
-                "and registered " if auto_register else " ", name
+                "and registered " if auto_register else "", name
             )
         )
         return token_address_hex
 
     def register_token(
         self,
-        registry_address_hex: typing.AddressHex,
-        token_address_hex: typing.AddressHex,
-        retry_timeout: typing.NetworkTimeout = DEFAULT_RETRY_TIMEOUT,
+        registry_address_hex: AddressHex,
+        token_address_hex: AddressHex,
+        retry_timeout: NetworkTimeout = DEFAULT_RETRY_TIMEOUT,
     ) -> TokenNetwork:
         """ Register a token with the raiden token manager.
 
         Args:
             registry_address_hex: a hex encoded registry address.
             token_address_hex: a hex encoded token address.
+            retry_timeout: the retry timeout
 
         Returns:
             The token network proxy.
         """
-        registry_address = to_canonical_address(registry_address_hex)
+        registry_address = TokenNetworkRegistryAddress(to_canonical_address(registry_address_hex))
         token_address = TokenAddress(to_canonical_address(token_address_hex))
 
         registry = self._raiden.proxy_manager.token_network_registry(registry_address)
 
         token_network_address = registry.add_token(
             token_address=token_address,
-            channel_participant_deposit_limit=UINT256_MAX,
-            token_network_deposit_limit=UINT256_MAX,
+            channel_participant_deposit_limit=TokenAmount(UINT256_MAX),
+            token_network_deposit_limit=TokenAmount(UINT256_MAX),
+            block_identifier="latest",
         )
         waiting.wait_for_token_network(
             self._raiden, registry.address, token_address, retry_timeout
@@ -230,43 +245,43 @@ class ConsoleTools:
 
     def open_channel_with_funding(
         self,
-        registry_address_hex,
-        token_address_hex,
-        peer_address_hex,
-        total_deposit,
-        settle_timeout=None,
-    ):
+        registry_address_hex: AddressHex,
+        token_address_hex: AddressHex,
+        peer_address_hex: AddressHex,
+        total_deposit: TokenAmount,
+        settle_timeout: BlockTimeout = None,
+    ) -> None:
         """ Convenience method to open a channel.
 
         Args:
-            registry_address_hex (str): hex encoded address of the registry for the channel.
-            token_address_hex (str): hex encoded address of the token for the channel.
-            peer_address_hex (str): hex encoded address of the channel peer.
-            total_deposit (int): amount of total funding for the channel.
-            settle_timeout (int): amount of blocks for the settle time (if None use app defaults).
+            registry_address_hex: hex encoded address of the registry for the channel.
+            token_address_hex: hex encoded address of the token for the channel.
+            peer_address_hex: hex encoded address of the channel peer.
+            total_deposit: amount of total funding for the channel.
+            settle_timeout: amount of blocks for the settle time (if None use app defaults).
 
         Return:
             netting_channel: the (newly opened) netting channel object.
         """
         # Check, if peer is discoverable
-        registry_address = decode_hex(registry_address_hex)
-        peer_address = decode_hex(peer_address_hex)
-        token_address = decode_hex(token_address_hex)
+        registry_address = TokenNetworkRegistryAddress(to_canonical_address(registry_address_hex))
+        peer_address = to_canonical_address(peer_address_hex)
+        token_address = TokenAddress(to_canonical_address(token_address_hex))
 
         self._api.channel_open(
             registry_address, token_address, peer_address, settle_timeout=settle_timeout
         )
 
-        return self._api.set_total_channel_deposit(
+        self._api.set_total_channel_deposit(
             registry_address, token_address, peer_address, total_deposit
         )
 
-    def wait_for_contract(self, contract_address_hex, timeout=None):
+    def wait_for_contract(self, contract_address_hex: AddressHex, timeout: int = None) -> bool:
         """ Wait until a contract is mined
 
         Args:
-            contract_address_hex (string): hex encoded address of the contract
-            timeout (int): time to wait for the contract to get mined
+            contract_address_hex: hex encoded address of the contract
+            timeout: time to wait for the contract to get mined
 
         Returns:
             True if the contract got mined, false otherwise
