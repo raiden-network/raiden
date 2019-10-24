1def906aceaa53df4495c91a6d13b5fa097a4a9f
Karl Bartel <karl42@gmail.com>
2019-10-24 11:00:32 +0200

    Check untyped defs in tests.unit.storage

--- raiden/tests/unit/storage/test_storage.py
+++ raiden/tests/unit/storage/test_storage.py
@@ -1,4 +1,5 @@
 import os.path
+from pathlib import Path
 from unittest.mock import patch
 
 import pytest
@@ -9,7 +10,7 @@ from raiden.utils.upgrades import UpgradeManager, UpgradeRecord
 
 def test_transaction_commit(tmp_path):
     filename = f"v{RAIDEN_DB_VERSION}_log.db"
-    storage = SQLiteStorage(f"{tmp_path}/{filename}")
+    storage = SQLiteStorage(Path(f"{tmp_path}/{filename}"))
 
     with storage.transaction():
         with patch("raiden.storage.sqlite.RAIDEN_DB_VERSION", new=1000):
@@ -20,7 +21,7 @@ def test_transaction_commit(tmp_path):
 
 def test_transaction_rollback(tmp_path):
     filename = f"v{RAIDEN_DB_VERSION}_log.db"
-    db_path = os.path.join(tmp_path, filename)
+    db_path = Path(tmp_path / filename)
     storage = SQLiteStorage(db_path)
     storage.update_version()
 
@@ -44,7 +45,7 @@ def test_upgrade_manager_transaction_rollback(tmp_path, monkeypatch):
     # Create the db to be upgraded
     with monkeypatch.context() as m:
         m.setattr("raiden.storage.sqlite.RAIDEN_DB_VERSION", 1)
-        storage = SQLiteStorage(FORMAT.format(1))
+        storage = SQLiteStorage(Path(FORMAT.format(1)))
         storage.update_version()
         del storage
 
@@ -57,7 +58,7 @@ def test_upgrade_manager_transaction_rollback(tmp_path, monkeypatch):
             manager = UpgradeManager(FORMAT.format(2))
             manager.run()
 
-    storage = SQLiteStorage(FORMAT.format(2))
+    storage = SQLiteStorage(Path(FORMAT.format(2)))
     assert storage.get_version() == 1, "The upgrade must have failed"
 
 
@@ -70,7 +71,7 @@ def test_regression_delete_should_not_commit_the_upgrade_transaction(tmp_path, m
     # Create the db to be upgraded
     with monkeypatch.context() as m:
         m.setattr("raiden.storage.sqlite.RAIDEN_DB_VERSION", 1)
-        storage = SQLiteStorage(FORMAT.format(1))
+        storage = SQLiteStorage(Path(FORMAT.format(1)))
         storage.update_version()
         del storage
 
@@ -83,5 +84,5 @@ def test_regression_delete_should_not_commit_the_upgrade_transaction(tmp_path, m
             manager = UpgradeManager(FORMAT.format(2))
             manager.run()
 
-    storage = SQLiteStorage(FORMAT.format(2))
+    storage = SQLiteStorage(Path(FORMAT.format(2)))
     assert storage.get_version() == 1, "The upgrade must have failed"
