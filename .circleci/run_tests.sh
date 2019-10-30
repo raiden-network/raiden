ba7ba20ea0f3c55ffbf6f7e410bd015cbac831a6
Augusto F. Hack <hack.augusto@gmail.com>
2019-10-30 11:02:20 -0300

    Added tools/debugging/split_debug_log_by_node.log
    It is just a tiny utility to split the log files by node, which is
    usually makes debugging a test based on the logs a bit easier.

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -43,3 +43,9 @@ dormant_signal=SIGUSR1
     --select-fail-on-missing \
     --select-from-file selected-tests.txt \
     "${@}"
+
+if [ -n ${RAIDEN_TESTS_LOGSDIR} ]; then
+    for log in ${RAIDEN_TESTS_LOGSDIR}/*/raiden-debug*.log; do
+        ./tools/debugging/split_debug_log_by_node.log ${log} ${RAIDEN_TESTS_LOGSDIR}/logs_split/
+    done
+fi
