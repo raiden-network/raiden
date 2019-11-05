8e50c47750ca5267b75e6dc17f4761fb7f2cc727
Lefteris Karapetsas <lefteris@refu.co>
2019-11-05 10:58:52 +0100

    Remove special handling of nonce and warning for Infura
    Fix #5217 -- Infura should now be fully supported

--- raiden/network/rpc/client.py
+++ raiden/network/rpc/client.py
@@ -417,7 +417,6 @@ class JSONRPCClient:
         gas_price_strategy: Callable = rpc_gas_price_strategy,
         gas_estimate_correction: Callable = lambda gas: gas,
         block_num_confirmations: int = 0,
-        uses_infura: bool = False,
     ) -> None:
         if privkey is None or len(privkey) != 32:
             raise ValueError("Invalid private key")
@@ -436,21 +435,7 @@ class JSONRPCClient:
         address = privatekey_to_address(privkey)
         address_checksumed = to_checksum_address(address)
 
-        if uses_infura:
-            warnings.warn(
-                "Infura does not provide an API to "
-                "recover the latest used nonce. This may cause the Raiden node "
-                "to error on restarts.\n"
-                "The error will manifest while there is a pending transaction "
-                "from a previous execution in the Ethereum's client pool. When "
-                "Raiden restarts the same transaction with the same nonce will "
-                "be retried and *rejected*, because the nonce is already used."
-            )
-            # The first valid nonce is 0, therefore the count is already the next
-            # available nonce
-            available_nonce = web3.eth.getTransactionCount(address_checksumed, "pending")
-
-        elif eth_node is EthClient.PARITY:
+        if eth_node is EthClient.PARITY:
             parity_assert_rpc_interfaces(web3)
             available_nonce = parity_discover_next_available_nonce(web3, address_checksumed)
 
