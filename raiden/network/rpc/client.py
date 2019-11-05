2f0dfc5db55cef9bc28691f12b0d3077d3672107
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-11-05 10:21:57 +0100

    Introduce TransactionGuard to protect against nonce race conditions

--- raiden/network/rpc/client.py
+++ raiden/network/rpc/client.py
@@ -333,6 +333,76 @@ def monkey_patch_web3(web3: Web3, gas_price_strategy: Callable) -> None:
     Eth.call = patched_web3_eth_call
 
 
+class TransactionGuard:
+    def __init__(self, client: "JSONRPCClient", available_nonce: Nonce):
+        self._client = client
+        self._available_nonce = available_nonce
+        self._nonce_lock = Semaphore()
+
+    def __enter__(self) -> "TransactionGuard":
+        self._nonce_lock.acquire()
+        return self
+
+    def __exit__(self, *args: Any, **kwargs: Any) -> None:
+        self._available_nonce = Nonce(self._available_nonce + 1)
+        self._nonce_lock.release()
+
+    def send_transaction(
+        self, to: Address, startgas: int, value: int = 0, data: bytes = b""
+    ) -> TransactionHash:
+        """ Helper to send signed messages.
+
+        This method will use the `privkey` provided in the constructor to
+        locally sign the transaction. This requires an extended server
+        implementation that accepts the variables v, r, and s.
+        """
+        if to == to_canonical_address(NULL_ADDRESS_HEX):
+            warnings.warn("For contract creation the empty string must be used.")
+
+        nonce = self._available_nonce
+        gas_price = self._client.gas_price()
+
+        transaction = {
+            "data": data,
+            "gas": startgas,
+            "nonce": nonce,
+            "value": value,
+            "gasPrice": gas_price,
+        }
+        node_gas_price = self._client.web3.eth.gasPrice
+        log.debug(
+            "Calculated gas price for transaction",
+            node=to_checksum_address(self._client.address),
+            calculated_gas_price=gas_price,
+            node_gas_price=node_gas_price,
+        )
+
+        # add the to address if not deploying a contract
+        if to != b"":
+            transaction["to"] = to_checksum_address(to)
+
+        signed_txn = self._client.web3.eth.account.signTransaction(
+            transaction, self._client.privkey
+        )
+
+        log_details = {
+            "node": to_checksum_address(self._client.address),
+            "nonce": transaction["nonce"],
+            "gasLimit": transaction["gas"],
+            "gasPrice": transaction["gasPrice"],
+        }
+        log.debug("send_raw_transaction called", **log_details)
+
+        tx_hash = self._client.web3.eth.sendRawTransaction(signed_txn.rawTransaction)
+
+        log.debug("send_raw_transaction returned", tx_hash=encode_hex(tx_hash), **log_details)
+        return TransactionHash(tx_hash)
+
+    @property
+    def available_nonce(self) -> Nonce:
+        return self._available_nonce
+
+
 class JSONRPCClient:
     """ Ethereum JSON RPC client.
 
@@ -390,6 +460,8 @@ class JSONRPCClient:
             geth_assert_rpc_interfaces(web3)
             available_nonce = geth_discover_next_available_nonce(web3, address_checksumed)
 
+        self.transaction_guard = TransactionGuard(self, available_nonce)
+
         self.eth_node = eth_node
         self.privkey = privkey
         self.address = address
@@ -398,9 +470,6 @@ class JSONRPCClient:
 
         # Ask for the chain id only once and store it here
         self.chain_id = ChainID(int(self.web3.version.network))
-
-        self._available_nonce = available_nonce
-        self._nonce_lock = Semaphore()
         self._gas_estimate_correction = gas_estimate_correction
 
         log.debug(
@@ -413,7 +482,8 @@ class JSONRPCClient:
     def __repr__(self) -> str:
         return (
             f"<JSONRPCClient "
-            f"node:{to_checksum_address(self.address)} nonce:{self._available_nonce}"
+            f"node:{to_checksum_address(self.address)} "
+            f"nonce:{self.transaction_guard.available_nonce}"
             f">"
         )
 
@@ -530,79 +600,28 @@ class JSONRPCClient:
 
         contract_object = self.web3.eth.contract(abi=contract["abi"], bytecode=contract["bin"])
         contract_transaction = contract_object.constructor(*ctor_parameters).buildTransaction()
-        transaction_hash = self.send_transaction(
-            to=Address(b""),
-            data=contract_transaction["data"],
-            startgas=self._gas_estimate_correction(contract_transaction["gas"]),
-        )
+        with self.transaction_guard as guard:
+            transaction_hash = guard.send_transaction(
+                to=Address(b""),
+                data=contract_transaction["data"],
+                startgas=self._gas_estimate_correction(contract_transaction["gas"]),
+            )
 
-        receipt = self.poll(transaction_hash)
-        contract_address = receipt["contractAddress"]
+            receipt = self.poll(transaction_hash)
+            contract_address = receipt["contractAddress"]
 
-        deployed_code = self.web3.eth.getCode(to_checksum_address(contract_address))
+            deployed_code = self.web3.eth.getCode(to_checksum_address(contract_address))
 
-        if not deployed_code:
-            raise RuntimeError(
-                "Deployment of {} failed. Contract address has no code, check gas usage.".format(
-                    contract_name
+            if not deployed_code:
+                raise RuntimeError(
+                    "Deployment of {} failed. "
+                    "Contract address has no code, check gas usage.".format(contract_name)
                 )
+
+            return (
+                self.new_contract_proxy(abi=contract["abi"], contract_address=contract_address),
+                receipt,
             )
-
-        return (
-            self.new_contract_proxy(abi=contract["abi"], contract_address=contract_address),
-            receipt,
-        )
-
-    def send_transaction(
-        self, to: Address, startgas: int, value: int = 0, data: bytes = b""
-    ) -> TransactionHash:
-        """ Helper to send signed messages.
-
-        This method will use the `privkey` provided in the constructor to
-        locally sign the transaction. This requires an extended server
-        implementation that accepts the variables v, r, and s.
-        """
-        if to == to_canonical_address(NULL_ADDRESS_HEX):
-            warnings.warn("For contract creation the empty string must be used.")
-
-        with self._nonce_lock:
-            nonce = self._available_nonce
-            gas_price = self.gas_price()
-
-            transaction = {
-                "data": data,
-                "gas": startgas,
-                "nonce": nonce,
-                "value": value,
-                "gasPrice": gas_price,
-            }
-            node_gas_price = self.web3.eth.gasPrice
-            log.debug(
-                "Calculated gas price for transaction",
-                node=to_checksum_address(self.address),
-                calculated_gas_price=gas_price,
-                node_gas_price=node_gas_price,
-            )
-
-            # add the to address if not deploying a contract
-            if to != b"":
-                transaction["to"] = to_checksum_address(to)
-
-            signed_txn = self.web3.eth.account.signTransaction(transaction, self.privkey)
-
-            log_details = {
-                "node": to_checksum_address(self.address),
-                "nonce": transaction["nonce"],
-                "gasLimit": transaction["gas"],
-                "gasPrice": transaction["gasPrice"],
-            }
-            log.debug("send_raw_transaction called", **log_details)
-
-            tx_hash = self.web3.eth.sendRawTransaction(signed_txn.rawTransaction)
-            self._available_nonce += 1
-
-            log.debug("send_raw_transaction returned", tx_hash=encode_hex(tx_hash), **log_details)
-            return TransactionHash(tx_hash)
 
     def poll(self, transaction_hash: TransactionHash) -> Dict[str, Any]:
         """ Wait until the `transaction_hash` is mined, confirmed, handling
