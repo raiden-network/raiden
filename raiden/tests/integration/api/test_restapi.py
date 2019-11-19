4c3c6adc36f977367b124c670386e7147bb943d7
Lefteris Karapetsas <lefteris@refu.co>
2019-11-19 15:12:02 +0100

    Add test for payment failing if done on a closed channel
    Part of #5304
    
    Test for https://github.com/raiden-network/raiden/pull/5256#discussion_r347770318

--- raiden/tests/integration/api/test_restapi.py
+++ raiden/tests/integration/api/test_restapi.py
@@ -679,6 +679,20 @@ def test_api_open_close_and_settle_channel(
     }
     assert check_dict_nested_attrs(get_json_response(response), expected_response)
 
+    # Try sending a payment when channel is closed
+    request = grequests.post(
+        api_url_for(
+            api_server_test_instance,
+            "token_target_paymentresource",
+            token_address=to_checksum_address(token_address),
+            target_address=to_checksum_address(partner_address),
+        ),
+        json={"amount": 1},
+    )
+    # Payment should not work since channel is closing
+    response = request.send().response
+    assert_proper_response(response, HTTPStatus.CONFLICT)
+
 
 @pytest.mark.parametrize("number_of_nodes", [2])
 @pytest.mark.parametrize("channels_per_node", [0])
