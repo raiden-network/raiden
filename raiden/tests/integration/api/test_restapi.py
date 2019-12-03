a381d3cd7bfe5e47c28df39c839957b87d454e6e
Augusto F. Hack <hack.augusto@gmail.com>
2019-12-03 18:59:36 +0100

    readded test_api_payments_with_lock_timeout

--- raiden/tests/integration/api/test_restapi.py
+++ raiden/tests/integration/api/test_restapi.py
@@ -2539,6 +2539,73 @@ def test_api_testnet_token_mint(api_server_test_instance: APIServer, token_addre
 
 
 @pytest.mark.parametrize("number_of_nodes", [2])
+def test_api_payments_with_lock_timeout(
+    api_server_test_instance: APIServer, raiden_network, token_addresses
+):
+    _, app1 = raiden_network
+    amount = 100
+    identifier = 42
+    token_address = token_addresses[0]
+    target_address = app1.raiden.address
+    number_of_nodes = 2
+    reveal_timeout = number_of_nodes * 4 + DEFAULT_NUMBER_OF_BLOCK_CONFIRMATIONS
+    settle_timeout = 39
+
+    # try lock_timeout = reveal_timeout - should not work
+    request = grequests.post(
+        api_url_for(
+            api_server_test_instance,
+            "token_target_paymentresource",
+            token_address=to_checksum_address(token_address),
+            target_address=to_checksum_address(target_address),
+        ),
+        json={"amount": amount, "identifier": identifier, "lock_timeout": reveal_timeout},
+    )
+    response = request.send().response
+    assert_response_with_error(response, status_code=HTTPStatus.CONFLICT)
+
+    # try lock_timeout = reveal_timeout * 2  - should  work.
+    request = grequests.post(
+        api_url_for(
+            api_server_test_instance,
+            "token_target_paymentresource",
+            token_address=to_checksum_address(token_address),
+            target_address=to_checksum_address(target_address),
+        ),
+        json={"amount": amount, "identifier": identifier, "lock_timeout": 2 * reveal_timeout},
+    )
+    with watch_for_unlock_failures(*raiden_network):
+        response = request.send().response
+    assert_proper_response(response, status_code=HTTPStatus.OK)
+
+    # try lock_timeout = settle_timeout - should work.
+    request = grequests.post(
+        api_url_for(
+            api_server_test_instance,
+            "token_target_paymentresource",
+            token_address=to_checksum_address(token_address),
+            target_address=to_checksum_address(target_address),
+        ),
+        json={"amount": amount, "identifier": identifier, "lock_timeout": settle_timeout},
+    )
+    response = request.send().response
+    assert_proper_response(response, status_code=HTTPStatus.OK)
+
+    # try lock_timeout = settle_timeout+1 - should not work.
+    request = grequests.post(
+        api_url_for(
+            api_server_test_instance,
+            "token_target_paymentresource",
+            token_address=to_checksum_address(token_address),
+            target_address=to_checksum_address(target_address),
+        ),
+        json={"amount": amount, "identifier": identifier, "lock_timeout": settle_timeout + 1},
+    )
+    response = request.send().response
+    assert_response_with_error(response, status_code=HTTPStatus.CONFLICT)
+
+
+@pytest.mark.parametrize("number_of_nodes", [2])
 def test_api_payments_with_invalid_input(
     api_server_test_instance: APIServer, raiden_network, token_addresses
 ):
