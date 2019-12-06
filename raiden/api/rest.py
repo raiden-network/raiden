e72fca7034cf19ee9b66f4c8bd838a8f37725332
manuelwedler <manuel.wedler@googlemail.com>
2019-12-06 15:18:15 +0100

    Fixes connections endpoint to return integers as strings

--- raiden/api/rest.py
+++ raiden/api/rest.py
@@ -785,11 +785,11 @@ class RestAPI:  # pragma: no unittest
             )
             if connection_manager is not None and open_channels:
                 connection_managers[to_checksum_address(connection_manager.token_address)] = {
-                    "funds": connection_manager.funds,
-                    "sum_deposits": views.get_our_deposits_for_token_network(
+                    "funds": str(connection_manager.funds),
+                    "sum_deposits": str(views.get_our_deposits_for_token_network(
                         views.state_from_raiden(self.raiden_api.raiden), registry_address, token
-                    ),
-                    "channels": len(open_channels),
+                    )),
+                    "channels": str(len(open_channels)),
                 }
 
         return api_response(result=connection_managers)
