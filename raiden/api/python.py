61f6e0d097dc25e790483e85ce686a597d9b59a4
Karl Bartel <karl42@gmail.com>
2019-12-16 14:14:06 +0100

    Refactor get_transfer_from_task
    * Add comment about work around
    * Return early if possible
    * Turn missed case (no waiting_transfer) into an assert, so we have to
      explicitly handle that case if such a case can exist.
    * End each if case with a return

--- raiden/api/python.py
+++ raiden/api/python.py
@@ -124,24 +124,25 @@ def flatten_transfer(transfer: LockedTransferType, role: str) -> Dict[str, Any]:
 def get_transfer_from_task(
     secrethash: SecretHash, transfer_task: TransferTask
 ) -> Optional[LockedTransferType]:
-    transfer: LockedTransferType
     if isinstance(transfer_task, InitiatorTask):
-        if secrethash in transfer_task.manager_state.initiator_transfers:
-            transfer = transfer_task.manager_state.initiator_transfers[secrethash].transfer
-        else:
+        # Work around for https://github.com/raiden-network/raiden/issues/5480,
+        # can be removed when
+        # https://github.com/raiden-network/raiden/issues/5515 is done.
+        if secrethash not in transfer_task.manager_state.initiator_transfers:
             return None
+
+        return transfer_task.manager_state.initiator_transfers[secrethash].transfer
     elif isinstance(transfer_task, MediatorTask):
         pairs = transfer_task.mediator_state.transfers_pair
         if pairs:
-            transfer = pairs[-1].payer_transfer
-        elif transfer_task.mediator_state.waiting_transfer:
-            transfer = transfer_task.mediator_state.waiting_transfer.transfer
-    elif isinstance(transfer_task, TargetTask):
-        transfer = transfer_task.target_state.transfer
-    else:  # pragma: no unittest
-        raise ValueError("get_transfer_from_task for a non TransferTask argument")
+            return pairs[-1].payer_transfer
 
-    return transfer
+        assert transfer_task.mediator_state.waiting_transfer, "Invalid mediator_state"
+        return transfer_task.mediator_state.waiting_transfer.transfer
+    elif isinstance(transfer_task, TargetTask):
+        return transfer_task.target_state.transfer
+
+    raise ValueError("get_transfer_from_task for a non TransferTask argument")
 
 
 def transfer_tasks_view(
