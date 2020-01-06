9ecfeb2613434f06c9f03b660113d9a108d24dcf
Ulrich Petri <ulo@ulo.pe>
2020-01-06 13:28:38 +0100

    Enable pylint assert checker
    Our custom pylint assert checker wasn't included in the list of enabled
    messages.
    
    Because of this there are currently 89 `assert`s without messages in the
    main Raiden code.

--- tools/pylint/assert_checker.py
+++ tools/pylint/assert_checker.py
@@ -1,6 +1,8 @@
 from pylint.checkers import BaseChecker
 from pylint.interfaces import IAstroidChecker
 
+from . import ignore_tests
+
 ASSERT_ID = "assert-message"
 ASSERT_MSG = "Every assert must have a message describing the error to aid debugging"
 
@@ -16,6 +18,7 @@ class AssertMessage(BaseChecker):
     priority = -1
     msgs = {"E6492": (ASSERT_MSG, ASSERT_ID, "Assert without message.")}
 
+    @ignore_tests
     def visit_assert(self, node):
         if len(list(node.get_children())) != 2:
             self.add_message(ASSERT_ID, node=node)
