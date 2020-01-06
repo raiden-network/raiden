8d639f615199f02371f2e922aa120d929b41913f
Paul Lange <palango@gmx.de>
2020-01-06 15:20:34 +0100

    Expand NotifyingQueue tests

--- raiden/tests/unit/test_notifyingqueue.py
+++ raiden/tests/unit/test_notifyingqueue.py
@@ -8,7 +8,7 @@ def add_element_to_queue(queue, element):
     queue.put(element)
 
 
-def test_copy():
+def test_queue():
     queue = NotifyingQueue()
     assert queue.copy() == []
 
@@ -18,6 +18,10 @@ def test_copy():
 
     queue.put(2)
     assert queue.copy() == [1, 2], "copy must preserve the items order"
+    assert queue.peek() == 1, "copy must preserve the queue"
+
+    assert queue.get() == 1, "get should return first item"
+    assert queue.peek() == 2, "get must remove first item"
 
 
 def test_event_must_be_set():
@@ -33,5 +37,8 @@ def test_event_must_be_set():
 
 
 def test_not_empty():
+    queue = NotifyingQueue()
+    assert not queue.is_set()
+
     queue = NotifyingQueue(items=[1, 2])
     assert queue.is_set()
