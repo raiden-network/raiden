a21e09bb7d04900517a6c4abb0ca14c859918ead
Ulrich Petri <ulo@ulo.pe>
2020-01-06 13:28:38 +0100

    Enable pylint assert checker
    Our custom pylint assert checker wasn't included in the list of enabled
    messages.
    
    Because of this there are currently 89 `assert`s without messages in the
    main Raiden code.

---
+++ tools/pylint/__init__.py
@@ -0,0 +1,30 @@
+from functools import lru_cache, wraps
+from typing import Any, Callable, Optional, TypeVar
+
+from astroid import Module
+from astroid.node_classes import NodeNG
+
+RAIDEN_TESTS_MODULE = "raiden.tests"
+
+T_NODE = TypeVar("T_NODE", bound=NodeNG)
+
+
+@lru_cache(maxsize=None)
+def find_parent(node: NodeNG, scope_type: T_NODE) -> Optional[T_NODE]:
+    current_node = node
+    while current_node is not None and not isinstance(current_node, scope_type):
+        current_node = current_node.parent
+    return current_node
+
+
+def ignore_tests(func: Callable) -> Callable:
+    """ Decorator that ignores nodes below the raiden.tests module. """
+
+    @wraps(func)
+    def decorator(self, node: NodeNG) -> Any:
+        module_node = find_parent(node, Module)
+        if module_node is None or module_node.name.startswith(RAIDEN_TESTS_MODULE):
+            return None
+        return func(self, node)
+
+    return decorator
