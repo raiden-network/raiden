raiden-network/raiden

commit 0e9e3f52b1412c64058e78a2b5c2b4824e2e4029

Unify handling of pfs updates
I'm not sure if this is the right direction to go in, since having the
explicit events is nice. But this is at least consistent instead of
doing some updates as events and others by checking that type of state
change. It's also all in one place and concise.
Testability would be better with the event approach.

raiden/raiden_service.py
@@ -65,7 +65,7 @@ from raiden.tasks import AlarmTask
 from raiden.transfer import node, views
 from raiden.transfer.architecture import BalanceProofSignedState, Event as RaidenEvent, StateChange
 from raiden.transfer.channel import get_capacity
-from raiden.transfer.events import EventPaymentSentFailed, SendPFSFeeUpdate
+from raiden.transfer.events import EventPaymentSentFailed
 from raiden.transfer.identifiers import CanonicalIdentifier
 from raiden.transfer.mediated_transfer.events import SendLockedTransfer
 from raiden.transfer.mediated_transfer.mediation_fee import (
@@ -94,6 +94,7 @@ from raiden.transfer.state_change import (
     ContractReceiveChannelDeposit,
     ContractReceiveNewTokenNetworkRegistry,
     ReceiveUnlock,
+    ReceiveWithdrawConfirmation,
     ReceiveWithdrawExpired,
 )
 from raiden.utils.formatting import lpex, to_checksum_address
@@ -131,6 +132,7 @@ ConnectionManagerDict = Dict[TokenNetworkAddress, ConnectionManager]
 PATH_FINDING_SERVICE_UPDATE = (
     ContractReceiveChannelDeposit,
     ReceiveUnlock,
+    ReceiveWithdrawConfirmation,
     ReceiveWithdrawExpired,
     ActionInitMediator,
     ActionInitTarget,
@@ -139,11 +141,9 @@ PATH_FINDING_SERVICE_UPDATE = (
     ReceiveLockExpired,
     ReceiveTransferRefund,
     # State change | Reason why update is not needed
-    # ActionChannelWithdraw | Update was done by the
-    #                         SendWithdrawRequest/SendWithdrawConfirmation/SendWithdrawExpired
+    # ActionChannelWithdraw | Update done by ReceiveWithdrawConfirmation/ReceiveWithdrawExpired
     # ActionInitInitiator | The update is done by the SendLockedTransfer event
-    # ReceiveWithdrawRequest | Update was done by the SendWithdrawConfirmation/SendWithdrawExpired
-    # ReceiveWithdrawConfirmation | Update done by SendWithdrawRequest
+    # ReceiveWithdrawRequest | Update done by ReceiveWithdrawConfirmation/ReceiveWithdrawExpired
 )
 
 
@@ -1162,9 +1162,10 @@ class RaidenService(Runnable):
                     proportional=proportional_fee,
                     imbalance_penalty=imbalance_penalty,
                 )
-                self.handle_event(
-                    chain_state,
-                    SendPFSFeeUpdate(canonical_identifier=channel.canonical_identifier),
+                send_pfs_update(
+                    raiden=self,
+                    canonical_identifier=channel.canonical_identifier,
+                    update_fee_schedule=True,
                 )
 
     def _get_initial_whitelist(self, chain_state: ChainState) -> List[Address]:

raiden/raiden_event_handler.py
@@ -17,7 +17,6 @@ from raiden.network.pathfinding import post_pfs_feedback
 from raiden.network.proxies.payment_channel import PaymentChannel
 from raiden.network.proxies.token_network import TokenNetwork
 from raiden.network.resolver.client import reveal_secret_with_resolver
-from raiden.services import send_pfs_update
 from raiden.storage.restore import (
     channel_state_until_state_change,
     get_event_with_balance_proof_by_balance_hash,
@@ -47,7 +46,6 @@ from raiden.transfer.events import (
     EventPaymentReceivedSuccess,
     EventPaymentSentFailed,
     EventPaymentSentSuccess,
-    SendPFSFeeUpdate,
     SendProcessed,
     SendWithdrawConfirmation,
     SendWithdrawExpired,
@@ -188,9 +186,6 @@ class RaidenEventHandler(EventHandler):
         elif type(event) == ContractSendChannelWithdraw:
             assert isinstance(event, ContractSendChannelWithdraw), MYPY_ANNOTATION
             self.handle_contract_send_channelwithdraw(raiden, event)
-        elif type(event) == SendPFSFeeUpdate:
-            assert isinstance(event, SendPFSFeeUpdate), MYPY_ANNOTATION
-            self.handle_send_pfs_fee_update(raiden, event)
         elif type(event) in UNEVENTFUL_EVENTS:
             pass
         else:
@@ -199,16 +194,6 @@ class RaidenEventHandler(EventHandler):
                 event_type=str(type(event)),
                 node=to_checksum_address(raiden.address),
             )
-
-    @staticmethod
-    def handle_send_pfs_fee_update(
-        raiden: "RaidenService", event: SendPFSFeeUpdate
-    ) -> None:  # pragma: no unittest
-        send_pfs_update(
-            raiden=raiden,
-            canonical_identifier=event.canonical_identifier,
-            update_fee_schedule=True,
-        )
 
     @staticmethod
     def handle_send_lockexpired(

raiden/tests/unit/test_channelstate.py
@@ -46,7 +46,6 @@ from raiden.transfer.events import (
     EventInvalidReceivedWithdraw,
     EventInvalidReceivedWithdrawExpired,
     EventInvalidReceivedWithdrawRequest,
-    SendPFSFeeUpdate,
     SendProcessed,
     SendWithdrawConfirmation,
     SendWithdrawExpired,
@@ -280,9 +279,6 @@ def test_channelstate_update_contract_balance():
 
     assert_partner_state(new_state.our_state, new_state.partner_state, our_model2)
     assert_partner_state(new_state.partner_state, new_state.our_state, partner_model2)
-    # Also make sure that a fee update triggering event is created
-    assert len(iteration.events) == 1
-    assert isinstance(iteration.events[0], SendPFSFeeUpdate)
 
 
 def test_channelstate_decreasing_contract_balance():
@@ -2223,9 +2219,6 @@ def test_receive_contract_withdraw():
     assert iteration.new_state.our_state.total_withdraw == total_withdraw
     assert iteration.new_state.our_total_withdraw == total_withdraw
     assert total_withdraw not in iteration.new_state.our_state.withdraws_pending
-    # Also make sure that a fee update triggering event is created
-    assert len(iteration.events) == 1
-    assert isinstance(iteration.events[0], SendPFSFeeUpdate)
 
     contract_receive_withdraw = ContractReceiveChannelWithdraw(
         canonical_identifier=channel_state.canonical_identifier,
@@ -2248,6 +2241,3 @@ def test_receive_contract_withdraw():
     assert iteration.new_state.partner_state.total_withdraw == total_withdraw
     assert iteration.new_state.partner_total_withdraw == total_withdraw
     assert total_withdraw not in iteration.new_state.partner_state.withdraws_pending
-    # Also make sure that a fee update triggering event is created
-    assert len(iteration.events) == 1
-    assert isinstance(iteration.events[0], SendPFSFeeUpdate)

raiden/tests/unit/transfer/test_channel.py
@@ -28,7 +28,7 @@ from raiden.transfer.channel import (
     set_settled,
     update_fee_schedule_after_balance_change,
 )
-from raiden.transfer.events import EventInvalidActionSetRevealTimeout, SendPFSFeeUpdate
+from raiden.transfer.events import EventInvalidActionSetRevealTimeout
 from raiden.transfer.state import (
     ChannelState,
     HashTimeLockState,
@@ -438,8 +438,7 @@ def test_update_fee_schedule_after_balance_change():
         cli_token_to_proportional_imbalance_fee=((channel_state.token_address, 50_000),),  # 5%
         cli_cap_mediation_fees=True,
     )
-    events = update_fee_schedule_after_balance_change(channel_state, fee_config)
-    assert isinstance(events[0], SendPFSFeeUpdate)
+    update_fee_schedule_after_balance_change(channel_state, fee_config)
     assert channel_state.fee_schedule.imbalance_penalty[0] == (0, 5)
 
 

raiden/transfer/channel.py
@@ -23,7 +23,6 @@ from raiden.transfer.events import (
     EventInvalidReceivedWithdraw,
     EventInvalidReceivedWithdrawExpired,
     EventInvalidReceivedWithdrawRequest,
-    SendPFSFeeUpdate,
     SendProcessed,
     SendWithdrawConfirmation,
     SendWithdrawExpired,
@@ -2378,7 +2377,7 @@ def update_fee_schedule_after_balance_change(
         proportional=channel_state.fee_schedule.proportional,
         imbalance_penalty=imbalance_penalty,
     )
-    return [SendPFSFeeUpdate(canonical_identifier=channel_state.canonical_identifier)]
+    return []
 
 
 def handle_channel_deposit(
@@ -2393,8 +2392,8 @@ def handle_channel_deposit(
         update_contract_balance(channel_state.partner_state, contract_balance)
 
     # A deposit changes the total capacity of the channel and as such the fees need to change
-    events = update_fee_schedule_after_balance_change(channel_state, state_change.fee_config)
-    return TransitionResult(channel_state, events)
+    update_fee_schedule_after_balance_change(channel_state, state_change.fee_config)
+    return TransitionResult(channel_state, [])
 
 
 def handle_channel_withdraw(
@@ -2420,8 +2419,8 @@ def handle_channel_withdraw(
     end_state.onchain_total_withdraw = state_change.total_withdraw
 
     # A withdraw changes the total capacity of the channel and as such the fees need to change
-    events = update_fee_schedule_after_balance_change(channel_state, state_change.fee_config)
-    return TransitionResult(channel_state, events)
+    update_fee_schedule_after_balance_change(channel_state, state_change.fee_config)
+    return TransitionResult(channel_state, [])
 
 
 def handle_channel_batch_unlock(

raiden/transfer/events.py
@@ -401,13 +401,3 @@ class EventInvalidSecretRequest(Event):
     payment_identifier: PaymentID
     intended_amount: PaymentAmount
     actual_amount: PaymentAmount
-
-
-@dataclass(frozen=True)
-class SendPFSFeeUpdate(Event):
-    """ Tell the PFSs about changed fee schedules
-
-    For example when a deposit or a withdrawal is made into a channel
-    """
-
-    canonical_identifier: CanonicalIdentifier

commit 695cd047dd940ce9ddcd15478f940d27f37a8167

Send PFS update after unlock
This has been broken in
https://github.com/raiden-network/raiden/commit/0e9e3f52b1412c64058e78a2b5c2b4824e2e4029.

Closes https://github.com/raiden-network/raiden/issues/5612.

raiden/raiden_service.py
@@ -67,7 +67,7 @@ from raiden.transfer.architecture import BalanceProofSignedState, Event as Raide
 from raiden.transfer.channel import get_capacity
 from raiden.transfer.events import EventPaymentSentFailed
 from raiden.transfer.identifiers import CanonicalIdentifier
-from raiden.transfer.mediated_transfer.events import SendLockedTransfer
+from raiden.transfer.mediated_transfer.events import SendBalanceProof, SendLockedTransfer
 from raiden.transfer.mediated_transfer.mediation_fee import (
     FeeScheduleState,
     calculate_imbalance_fees,
@@ -129,7 +129,7 @@ log = structlog.get_logger(__name__)
 StatusesDict = Dict[TargetAddress, Dict[PaymentID, "PaymentStatus"]]
 ConnectionManagerDict = Dict[TokenNetworkAddress, ConnectionManager]
 
-PATH_FINDING_SERVICE_UPDATE = (
+PFS_UPDATE_STATE_CHANGES = (
     ContractReceiveChannelDeposit,
     ReceiveUnlock,
     ReceiveWithdrawConfirmation,
@@ -145,6 +145,7 @@ PATH_FINDING_SERVICE_UPDATE = (
     # ActionInitInitiator | The update is done by the SendLockedTransfer event
     # ReceiveWithdrawRequest | Update done by ReceiveWithdrawConfirmation/ReceiveWithdrawExpired
 )
+PFS_UPDATE_EVENTS = (SendBalanceProof,)
 
 
 def initiator_init(
@@ -730,7 +731,7 @@ class RaidenService(Runnable):
                     non_closing_participant=self.address,
                 )
 
-            if isinstance(state_change, PATH_FINDING_SERVICE_UPDATE):
+            if isinstance(state_change, PFS_UPDATE_STATE_CHANGES):
                 update_fee_schedule = isinstance(
                     state_change,
                     (
@@ -749,6 +750,12 @@ class RaidenService(Runnable):
                     raiden=self,
                     canonical_identifier=canonical_identifier,
                     update_fee_schedule=update_fee_schedule,
+                )
+
+        for event in raiden_event_list:
+            if isinstance(event, PFS_UPDATE_EVENTS):
+                send_pfs_update(
+                    raiden=self, canonical_identifier=event.balance_proof.canonical_identifier
                 )
 
         for state_change in state_changes:

raiden/tests/integration/test_integration_pfs.py
@@ -8,7 +8,21 @@ from raiden.constants import DISCOVERY_DEFAULT_ROOM, PATH_FINDING_BROADCASTING_R
 from raiden.network.transport.matrix import make_room_alias
 from raiden.network.transport.matrix.client import Room
 from raiden.tests.utils.detect_failure import raise_on_failure
-from raiden.utils.typing import List, TokenAddress, TokenAmount, WithdrawAmount
+from raiden.tests.utils.transfer import (
+    assert_succeeding_transfer_invariants,
+    block_timeout_for_transfer_by_secrethash,
+    transfer,
+    wait_assert,
+)
+from raiden.transfer import views
+from raiden.utils.typing import (
+    List,
+    PaymentAmount,
+    PaymentID,
+    TokenAddress,
+    TokenAmount,
+    WithdrawAmount,
+)
 
 
 @raise_on_failure
@@ -70,3 +84,52 @@ def test_pfs_send_capacity_updates_on_deposit_and_withdraw(
     # after the withdraw
     assert "PFSCapacityUpdate" in str(pfs_room.send_text.call_args_list[1])
     assert "PFSFeeUpdate" in str(pfs_room.send_text.call_args_list[1])
+
+
+@raise_on_failure
+@pytest.mark.parametrize("number_of_nodes", [2])
+@pytest.mark.parametrize("broadcast_rooms", [[PATH_FINDING_BROADCASTING_ROOM]])
+@pytest.mark.parametrize("routing_mode", [RoutingMode.PFS])
+def test_mediated_transfer(
+    raiden_network, number_of_nodes, deposit, token_addresses, network_wait
+):
+    app0, app1 = raiden_network
+    token_address = token_addresses[0]
+    chain_state = views.state_from_app(app0)
+    token_network_registry_address = app0.raiden.default_registry.address
+    token_network_address = views.get_token_network_address_by_token_address(
+        chain_state, token_network_registry_address, token_address
+    )
+
+    # Mock send_text on the PFS room
+    transport0 = app0.raiden.transport
+    pfs_room_name = make_room_alias(transport0.chain_id, PATH_FINDING_BROADCASTING_ROOM)
+    pfs_room = transport0._broadcast_rooms.get(pfs_room_name)
+    # need to assert for mypy that pfs_room is not None
+    assert isinstance(pfs_room, Room)
+    pfs_room.send_text = MagicMock(spec=pfs_room.send_text)
+
+    amount = PaymentAmount(10)
+    secrethash = transfer(
+        initiator_app=app0,
+        target_app=app1,
+        token_address=token_address,
+        amount=amount,
+        identifier=PaymentID(1),
+        timeout=network_wait * number_of_nodes,
+    )
+
+    with block_timeout_for_transfer_by_secrethash(app1.raiden, secrethash):
+        wait_assert(
+            assert_succeeding_transfer_invariants,
+            token_network_address,
+            app0,
+            deposit - amount,
+            [],
+            app1,
+            deposit + amount,
+            [],
+        )
+
+    assert pfs_room.send_text.call_count == 1
+    assert "PFSCapacityUpdate" in str(pfs_room.send_text.call_args_list[0])