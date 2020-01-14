717ecc3bb1f8700256cf54a16b2bf3f45bb9269e
Paul Lange <palango@gmx.de>
2020-01-14 11:47:16 +0100

    Add mediator to `test_pfs_send_capacity_updates_during_mediated_transfer`

--- raiden/tests/integration/test_integration_pfs.py
+++ raiden/tests/integration/test_integration_pfs.py
@@ -8,6 +8,7 @@ from raiden.constants import DISCOVERY_DEFAULT_ROOM, PATH_FINDING_BROADCASTING_R
 from raiden.network.transport.matrix import make_room_alias
 from raiden.network.transport.matrix.client import Room
 from raiden.tests.utils.detect_failure import raise_on_failure
+from raiden.tests.utils.network import CHAIN
 from raiden.tests.utils.transfer import (
     assert_succeeding_transfer_invariants,
     block_timeout_for_transfer_by_secrethash,
@@ -86,14 +87,16 @@ def test_pfs_send_capacity_updates_on_deposit_and_withdraw(
     assert "PFSFeeUpdate" in str(pfs_room.send_text.call_args_list[1])
 
 
+
 @raise_on_failure
-@pytest.mark.parametrize("number_of_nodes", [2])
+@pytest.mark.parametrize("number_of_nodes", [3])
+@pytest.mark.parametrize("channels_per_node", [CHAIN])
 @pytest.mark.parametrize("broadcast_rooms", [[PATH_FINDING_BROADCASTING_ROOM]])
 @pytest.mark.parametrize("routing_mode", [RoutingMode.PFS])
 def test_pfs_send_capacity_updates_during_mediated_transfer(
     raiden_network, number_of_nodes, deposit, token_addresses, network_wait
 ):
-    app0, app1 = raiden_network
+    app0, app1, app2 = raiden_network
     token_address = token_addresses[0]
     chain_state = views.state_from_app(app0)
     token_network_registry_address = app0.raiden.default_registry.address
@@ -106,10 +109,10 @@ def test_pfs_send_capacity_updates_during_mediated_transfer(
 
     # Mock send_text on the PFS room
     pfs_rooms: List[Room] = []
-    for app in [app0, app1]:
+    for app in [app0, app1, app2]:
         transport = app.raiden.transport
         pfs_room = transport._broadcast_rooms.get(pfs_room_name)
-        # need to assert for mypy that pfs_room0 is not None
+        # need to assert for mypy that pfs_room is not None
         assert isinstance(pfs_room, Room)
         pfs_room.send_text = MagicMock(spec=pfs_room.send_text)
         pfs_rooms.append(pfs_room)
@@ -117,7 +120,7 @@ def test_pfs_send_capacity_updates_during_mediated_transfer(
     amount = PaymentAmount(10)
     secrethash = transfer(
         initiator_app=app0,
-        target_app=app1,
+        target_app=app2,
         token_address=token_address,
         amount=amount,
         identifier=PaymentID(1),
@@ -136,10 +139,31 @@ def test_pfs_send_capacity_updates_during_mediated_transfer(
             [],
         )
 
+    with block_timeout_for_transfer_by_secrethash(app1.raiden, secrethash):
+        wait_assert(
+            assert_succeeding_transfer_invariants,
+            token_network_address,
+            app1,
+            deposit - amount,
+            [],
+            app2,
+            deposit + amount,
+            [],
+        )
+
     # Initiator: we expect one PFSCapacityUpdate when locking and one when unlocking
     assert pfs_rooms[0].send_text.call_count == 2
     assert "PFSCapacityUpdate" in str(pfs_rooms[0].send_text.call_args_list[0])
+    assert "PFSFeeUpdate" not in str(pfs_rooms[0].send_text.call_args_list[0])
+
+    # Mediator:
+    #   incoming channel: we expect one PFSCapacityUpdate when locking and one when unlocking
+    #   outgoing channel: we expect one PFSCapacityUpdate when funds are unlocked
+    assert pfs_rooms[1].send_text.call_count == 3
+    assert "PFSCapacityUpdate" in str(pfs_rooms[1].send_text.call_args_list[0])
+    assert "PFSFeeUpdate" not in str(pfs_rooms[1].send_text.call_args_list[0])
 
     # Target: we expect one PFSCapacityUpdate when funds are unlocked
-    assert pfs_rooms[1].send_text.call_count == 1
-    assert "PFSCapacityUpdate" in str(pfs_rooms[1].send_text.call_args_list[0])
+    assert pfs_rooms[2].send_text.call_count == 1
+    assert "PFSCapacityUpdate" in str(pfs_rooms[2].send_text.call_args_list[0])
+    assert "PFSFeeUpdate" not in str(pfs_rooms[2].send_text.call_args_list[0])
