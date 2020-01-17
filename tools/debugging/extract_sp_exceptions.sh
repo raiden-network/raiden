f5cde173839ced2fbbaea887c533942f7df2d81d
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2020-01-17 09:24:04 +0100

    Improve the script to extract logs
    Mostly, download the logs from 2 servers and not 1.
    Also separated the logs into their own date-prefixed directory
    so that the base directory doesn't get polluted
    
    [skip tests]

--- tools/debugging/extract_sp_exceptions.sh
+++ tools/debugging/extract_sp_exceptions.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-SCENARIO_REMOTE_URL="http://68.183.70.168:8000/scenario-player/scenarios/"
+SCENARIO_REMOTE_URL_1="http://68.183.70.168:8000/scenario-player/scenarios/"
+SCENARIO_REMOTE_URL_2="http://139.59.211.171:8000/scenario-player/scenarios/"
 CURL_COMMAND="curl --silent"
 WGET_DIR="wget --no-parent --no-clobber --continue -nH -r"
 
@@ -36,6 +37,9 @@ else
     DESTINATION_DIR=$(pwd)
 fi
 
+DESTINATION_DIR="$(realpath -s ${DESTINATION_DIR})/$(date +%m-%d-%Y)"
+mkdir -p $DESTINATION_DIR
+
 print_bold "Downloading PFS logs"
 
 ssh root@services-dev.raiden.network 'cd raiden-services/deployment/; docker-compose logs pfs-goerli | gzip' > ${DESTINATION_DIR}/pfs.log.gz
@@ -52,36 +56,46 @@ function separator {
 
 function download_nodes_logs {
     scenario=$1
-    run_number=$2
-    nodes=$(${CURL_COMMAND} ${SCENARIO_REMOTE_URL}${scenario} | sed -e 's/<[^>]*>//g' | grep -v Directory | sed -e '/^$/d' | grep "^node_${run_number}")
+    current_server=$2
+    run_number=$3
+    nodes=$(${CURL_COMMAND} ${current_server}${scenario} | sed -e 's/<[^>]*>//g' | grep -v Directory | sed -e '/^$/d' | grep "^node_${run_number}")
     for node in $nodes; do
-        $(${WGET_DIR} -q -P ${DESTINATION_DIR} ${SCENARIO_REMOTE_URL}${scenario}${node})
+        $(${WGET_DIR} -q -P ${DESTINATION_DIR} ${current_server}${scenario}${node})
     done
 }
 
-scenarios=$(${CURL_COMMAND} ${SCENARIO_REMOTE_URL} | sed -e 's/<[^>]*>//g' | grep -v Directory | sed -e '/^$/d')
+function download_server_logs {
+    current_server=$1
+    scenarios=$(${CURL_COMMAND} ${current_server} | sed -e 's/<[^>]*>//g' | grep -v Directory | sed -e '/^$/d')
+    for scenario in $scenarios; do
+        run_number=$(${CURL_COMMAND} "${current_server}${scenario}run_number.txt")
+        download_nodes_logs $scenario $current_server $run_number
+        echo -e "\t - ${scenario}, run_number: ${run_number}"
+    done;
 
-for scenario in $scenarios; do
-    run_number=$(${CURL_COMMAND} "${SCENARIO_REMOTE_URL}${scenario}run_number.txt")
-    download_nodes_logs $scenario $run_number
-    echo -e "\t - ${scenario}, run_number: ${run_number}"
-done;
-
-separator
-
-echo -e "${BOLD}Looking for failures${RESET}"
-scenarios_dir="${DESTINATION_DIR}/scenario-player/scenarios"
-for scenario in $scenarios; do
-    print_bold ${scenario}
     separator
-    scenario_dir=${scenarios_dir}/${scenario}
-    for node in $(ls $scenario_dir); do
-        node_dir="${scenario_dir}/${node}/"
-        result=$(cat ${node_dir}*.log | jq --tab 'select (.error!=null)')
-        if [[ $result != "" ]]; then
-            print_bold "- Found error in ${node_dir}"
-            echo -e "${result}"
-        fi
-    done
-    separator
-done;
+
+}
+
+function search_for_failures {
+    echo -e "${BOLD}Looking for failures${RESET}"
+    scenarios_dir="${DESTINATION_DIR}/scenario-player/scenarios"
+    for scenario in $(ls $scenarios_dir); do
+        print_bold ${scenario}
+        separator
+        scenario_dir=${scenarios_dir}/${scenario}
+        for node in $(ls $scenario_dir); do
+            node_dir="${scenario_dir}/${node}/"
+            result=$(cat ${node_dir}*.log | jq --tab 'select (.error!=null)')
+            if [[ $result != "" ]]; then
+                print_bold "- Found error in ${node_dir}"
+                echo -e "${result}"
+            fi
+        done
+        separator
+    done;
+}
+
+download_server_logs $SCENARIO_REMOTE_URL_1
+download_server_logs $SCENARIO_REMOTE_URL_2
+search_for_failures
