ee30fd873a9e34376e232ce670d3dc8891d7b35f
Karl Bartel <karl42@gmail.com>
2020-01-20 17:14:23 +0100

    Cache `message.sender`
    Checking the signature is slow enough to warrant caching.

--- raiden/messages/abstract.py
+++ raiden/messages/abstract.py
@@ -3,7 +3,35 @@ from dataclasses import dataclass
 from raiden.exceptions import InvalidSignature
 from raiden.messages.cmdid import CmdId
 from raiden.utils.signer import Signer, recover
-from raiden.utils.typing import Address, Any, ClassVar, MessageID, Optional, Signature
+from raiden.utils.typing import Address, Any, Callable, ClassVar, MessageID, Optional, Signature
+
+
+class cached_property:
+    """ Same as functools.cached_property in python 3.8
+
+    See https://docs.python.org/3/library/functools.html#functools.cached_property.
+    Remove after upgrading to python3.8
+    """
+
+    def __init__(self, func: Callable) -> None:
+        self.func = func
+        self.__doc__ = func.__doc__
+
+    def __get__(self, instance: Any, cls: Any = None) -> Any:
+        if instance is None:
+            return self
+        attrname = self.func.__name__
+        try:
+            cache = instance.__dict__
+        except AttributeError:  # objects with __slots__ have no __dict__
+            msg = (
+                f"No '__dict__' attribute on {type(instance).__name__!r} "
+                f"instance to cache {attrname!r} property."
+            )
+            raise TypeError(msg) from None
+        if attrname not in cache:
+            cache[attrname] = self.func(instance)
+        return cache[attrname]
 
 
 @dataclass(repr=False, eq=False)
@@ -51,8 +79,8 @@ class SignedMessage(AuthenticatedMessage):
         message_data = self._data_to_sign()
         self.signature = signer.sign(data=message_data)
 
-    @property
-    def sender(self) -> Optional[Address]:
+    @cached_property
+    def sender(self) -> Optional[Address]:  # type: ignore
         if not self.signature:
             return None
         data_that_was_signed = self._data_to_sign()
