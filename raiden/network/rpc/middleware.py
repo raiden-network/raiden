3543bb951f7dda677d7f6c3f0820f183bd86c160
Paul Lange <palango@gmx.de>
2020-03-23 12:25:57 +0100

    Fix typing issues in RPC module

--- raiden/network/rpc/middleware.py
+++ raiden/network/rpc/middleware.py
@@ -9,6 +9,6 @@ BLOCK_HASH_CACHE_RPC_WHITELIST = {RPCEndpoint("eth_getBlockByHash")}
 
 block_hash_cache_middleware = construct_simple_cache_middleware(
     # default sample size of gas price strategies is 120
-    cache_class=functools.partial(LRUCache, 150),
+    cache_class=functools.partial(LRUCache, 150),  # type: ignore
     rpc_whitelist=BLOCK_HASH_CACHE_RPC_WHITELIST,
 )
