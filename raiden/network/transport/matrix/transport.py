99ae8be743e8f001e48f8c3d6965a20d5cf1b905
Unknown <luehrs.fred@gmail.com>
2020-03-23 17:02:03 +0100

    remove usage of canonical alias
    remove canonical alias

--- raiden/network/transport/matrix/transport.py
+++ raiden/network/transport/matrix/transport.py
@@ -1326,9 +1326,6 @@ class MatrixTransport(Runnable):
             return room
 
     def _is_broadcast_room(self, room: Room) -> bool:
-        room_aliases = set(room.aliases)
-        if room.canonical_alias:
-            room_aliases.add(room.canonical_alias)
         return any(
             suffix in room_alias
             for suffix in self._config.broadcast_rooms
