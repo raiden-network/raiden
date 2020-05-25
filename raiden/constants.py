7e1268308d27044f8da46924ab07a02e3857398f
Paul Lange <palango@gmx.de>
2020-05-25 10:02:31 +0200

    Correct DAI contract address [skip tests]
    DAI switched to a new contract, we need to adapt for that.

--- raiden/constants.py
+++ raiden/constants.py
@@ -193,7 +193,7 @@ WETH_TOKEN_ADDRESS = TokenAddress(
     to_canonical_address("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
 )
 DAI_TOKEN_ADDRESS = TokenAddress(
-    to_canonical_address("0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359")
+    to_canonical_address("0x6B175474E89094C44Da98b954EedeAC495271d0F")
 )
 
 FLAT_MED_FEE_MIN = 0
