f1850dbab10c297ba794dab7f6bc030b7d4ee6c2
Konrad Feldmeier <konrad@brainbot.com>
2020-09-01 13:54:39 +0200

    Tighten the PeerCapabilities type

--- raiden/utils/typing.py
+++ raiden/utils/typing.py
@@ -198,7 +198,7 @@ DatabasePath = Union[Path, Literal[":memory:"]]
 T_RoomID = str
 RoomID = NewType("RoomID", T_RoomID)
 
-T_PeerCapabilities = Dict[str, Any]
+T_PeerCapabilities = Dict[str, Union[str, bool]]
 PeerCapabilities = NewType("PeerCapabilities", T_PeerCapabilities)
 
 AddressTypes = Union[
