272630a824689fff26c50ad4bff0fc374285ce2c
Karl Bartel <karl42@gmail.com>
2020-09-10 15:28:58 +0200

    Add mypy typing to test_mediatorstate.py
    I was annoyed by the lack of testing when I tried to modify a test.

--- raiden/tests/utils/factories.py
+++ raiden/tests/utils/factories.py
@@ -351,9 +351,9 @@ def make_hop_to_channel(channel_state: NettingChannelState = EMPTY) -> HopState:
 # In this module constants are in the bottom because we need some of the
 # factories.
 # Prefixing with UNIT_ to differ from the default globals.
-UNIT_SETTLE_TIMEOUT = 50
-UNIT_REVEAL_TIMEOUT = 5
-UNIT_TRANSFER_AMOUNT = 50
+UNIT_SETTLE_TIMEOUT = BlockExpiration(50)
+UNIT_REVEAL_TIMEOUT = BlockExpiration(5)
+UNIT_TRANSFER_AMOUNT = TokenAmount(50)
 UNIT_TRANSFER_FEE = 2
 UNIT_SECRET = Secret(b"secretsecretsecretsecretsecretse")
 UNIT_SECRETHASH = sha256_secrethash(UNIT_SECRET)
