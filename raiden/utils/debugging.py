fe88f55f60deadc02c7067c7ed872fdcf50bc6ee
Karl Bartel <karl42@gmail.com>
2020-11-13 05:29:20 -0800

    Do not listen for SIGUSR1 on windows
    This signal does not exist on Windows, is only for debugging, and rarely
    used anyway. So we can safely skip that feature on Windows.

--- raiden/utils/debugging.py
+++ raiden/utils/debugging.py
@@ -1,3 +1,4 @@
+import os
 import signal
 import time
 from dataclasses import dataclass, field
@@ -29,6 +30,10 @@ def enable_gevent_monitoring_signal() -> None:
 
     def on_signal(signalnum: Any, stack_frame: Any) -> None:  # pylint: disable=unused-argument
         gevent.util.print_run_info()
+
+    if os.name == "nt":
+        # SIGUSR1 not supported on Windows
+        return
 
     signal.signal(signal.SIGUSR1, on_signal)
 
