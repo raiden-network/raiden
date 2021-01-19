9459c46de6b68e9a0cabe150592f91c3490338f4
Karl Bartel <karl42@gmail.com>
2021-01-19 11:36:52 +0100

    Update restored channel states
    The new state might not contain the locks required by the on-chain state
    and the old state does not know about unlocks that happened in the mean
    time. So the only chance to correctly do this is to update the old state
    with the new unlocking information.
    
    Closes https://github.com/raiden-network/raiden/issues/6706.

--- raiden/raiden_event_handler.py
+++ raiden/raiden_event_handler.py
@@ -67,7 +67,7 @@ from raiden.transfer.mediated_transfer.events import (
     SendSecretReveal,
     SendUnlock,
 )
-from raiden.transfer.state import ChainState, NettingChannelEndState
+from raiden.transfer.state import ChainState, NettingChannelEndState, NettingChannelState
 from raiden.transfer.views import (
     get_channelstate_by_canonical_identifier,
     get_channelstate_by_token_network_and_partner,
@@ -125,6 +125,58 @@ def unlock(
         pending_locks=pending_locks,
         given_block_identifier=given_block_identifier,
     )
+
+
+def _update_lock_info(
+    state_to_update: NettingChannelState,
+    update_from_state: NettingChannelState,
+    chain_state: ChainState,
+) -> None:
+    """Update old channel state with lock information from current state
+
+    Call this after settlement, when you need to work on an older locksroot
+    because the channel has been settled with an outdated balance proof.
+    """
+    assert update_from_state.settle_transaction, "Channel must be settled"
+    for old_state, new_state in [
+        (state_to_update.our_state, update_from_state.our_state),
+        (state_to_update.partner_state, update_from_state.partner_state),
+    ]:
+        # After settlement, no unlocks can be processed. So all locks that are
+        # still present in the balance proof are locked, unless their secret is
+        # registered on-chain.
+        old_state.secrethashes_to_lockedlocks.update(
+            {
+                secret: unlock.lock
+                for secret, unlock in old_state.secrethashes_to_unlockedlocks.items()
+            }
+        )
+        old_state.secrethashes_to_unlockedlocks = {}
+
+        # In the time between the states, some locks might have been unlocked
+        # on-chain. Update their state to "on-chain unlocked".
+        for secret, updated_unlock in new_state.secrethashes_to_onchain_unlockedlocks.items():
+            try:
+                del old_state.secrethashes_to_lockedlocks[secret]
+            except KeyError:
+                continue
+            old_state.secrethashes_to_onchain_unlockedlocks[secret] = updated_unlock
+
+        # If we don't have a task for the secret, then that lock can't be
+        # relevant to us, anymore. Otherwise, we would not have deleted the
+        # payment task.
+        # One case where this is necessary: We are a mediator and didn't unlock
+        # the receiver's BP, but the secret has been registered on-chain. We
+        # will receive an Unlock from our sender and delete our MediatorTask,
+        # since we got our tokens. After deleting the task, we won't listen for
+        # on-chain unlocks, so we wrongly consider the tokens in the outgoing
+        # channel to be ours and send an on-chain unlock although we won't
+        # unlock any tokens to our benefit.
+        old_state.secrethashes_to_lockedlocks = {
+            secret: lock
+            for secret, lock in old_state.secrethashes_to_lockedlocks.items()
+            if secret in chain_state.payment_mapping.secrethashes_to_task
+        }
 
 
 class EventHandler(ABC):
@@ -616,13 +668,11 @@ class RaidenEventHandler(EventHandler):
                 canonical_identifier=canonical_identifier,
                 state_change_identifier=state_change_identifier,
             )
+            _update_lock_info(restored_channel_state, channel_state, chain_state)
 
             gain = get_batch_unlock_gain(restored_channel_state)
 
-            skip_unlock = (
-                restored_channel_state.partner_state.address == participant
-                and gain.from_partner_locks == 0
-            )
+            skip_unlock = gain.from_partner_locks == 0
             if not skip_unlock:
                 unlock(
                     payment_channel=payment_channel,
@@ -657,13 +707,11 @@ class RaidenEventHandler(EventHandler):
                 canonical_identifier=canonical_identifier,
                 state_change_identifier=state_change_identifier,
             )
+            _update_lock_info(restored_channel_state, channel_state, chain_state)
 
             gain = get_batch_unlock_gain(restored_channel_state)
 
-            skip_unlock = (
-                restored_channel_state.our_state.address == participant
-                and gain.from_our_locks == 0
-            )
+            skip_unlock = gain.from_our_locks == 0
             if not skip_unlock:
                 try:
                     unlock(
