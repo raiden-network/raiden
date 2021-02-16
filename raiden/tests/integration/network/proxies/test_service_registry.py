7e04d9354526d160a4961aab1fc2730f7399c21c
Konrad Feldmeier <konrad@brainbot.com>
2021-02-16 11:22:41 +0100

    Add test for service_registry events

--- raiden/tests/integration/network/proxies/test_service_registry.py
+++ raiden/tests/integration/network/proxies/test_service_registry.py
@@ -3,13 +3,19 @@ from unittest.mock import Mock, patch
 import pytest
 from eth_utils import to_canonical_address
 
+from raiden.blockchain.decode import update_service_addresses_from_event
+from raiden.blockchain.events import decode_raiden_event_to_internal
+from raiden.blockchain.filters import RaidenContractFilter
 from raiden.constants import BLOCK_ID_LATEST
 from raiden.exceptions import BrokenPreconditionError
 from raiden.network.pathfinding import get_random_pfs, get_valid_pfs_url
+from raiden.settings import RAIDEN_CONTRACT_VERSION
 from raiden.tests.utils.factories import HOP1
 from raiden.tests.utils.smartcontracts import deploy_service_registry_and_set_urls
+from raiden.transfer.events import UpdateServicesAddresses
 from raiden.utils.keys import privatekey_to_address
 from raiden.utils.typing import FeeAmount, TokenNetworkRegistryAddress
+from raiden_contracts.contract_manager import ContractManager, contracts_precompiled_path
 
 token_network_registry_address_test_default = TokenNetworkRegistryAddress(
     to_canonical_address("0xB9633dd9a9a71F22C933bF121d7a22008f66B908")
@@ -76,3 +82,40 @@ def test_service_registry_random_pfs(
             get_random_pfs(c1_service_proxy, BLOCK_ID_LATEST, pathfinding_max_fee=FeeAmount(100))
             in urls
         )
+
+
+def test_service_registry_events(service_registry_address, private_keys, web3, contract_manager):
+    """
+    - Test that `RaidenContractFilter` successfully matches on `RegisteredService`.
+    - Test that blockchain-event, state_change and raiden-event decoding methods work.
+    """
+    c1_service_proxy, _ = deploy_service_registry_and_set_urls(
+        private_keys=private_keys,
+        web3=web3,
+        contract_manager=contract_manager,
+        service_registry_address=service_registry_address,
+    )
+    assert c1_service_proxy.ever_made_deposits_len(BLOCK_ID_LATEST) == 3
+
+    # register filter
+    event_filter = RaidenContractFilter(service_registry=c1_service_proxy)
+
+    manager = ContractManager(contracts_precompiled_path(RAIDEN_CONTRACT_VERSION))
+    flt = event_filter.to_web3_filters(
+        contract_manager=manager,
+        from_block=0,
+        to_block="latest",
+        node_address="0x6666666666666666666666666666666666666666",
+    )
+    web3 = c1_service_proxy.client.web3
+    flt[0].pop("_name")
+    events = web3.eth.getLogs(flt[0])
+    assert len(events) == 3
+
+    ABI = c1_service_proxy.proxy.abi
+    decoded_events = [decode_raiden_event_to_internal(ABI, 0, ev) for ev in events]
+    assert len(decoded_events) == 3
+    state_changes = [update_service_addresses_from_event(ev) for ev in decoded_events]
+    assert len(state_changes) == 3
+    raiden_events = [UpdateServicesAddresses.from_state_change(sc) for sc in state_changes]
+    assert len(raiden_events) == 3
