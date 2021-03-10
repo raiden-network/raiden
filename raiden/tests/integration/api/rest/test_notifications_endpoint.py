dcfb03fbc18faeb938dfab80b49133bfa22ce625
Fernando Cezar Bernardelli <fernando@brainbot.com>
2021-03-10 17:49:08 +0100

    configure flask client

--- raiden/tests/integration/api/rest/test_notifications_endpoint.py
+++ raiden/tests/integration/api/rest/test_notifications_endpoint.py
@@ -1,7 +1,11 @@
+import pytest
 from raiden.tests.utils.detect_failure import raise_on_failure
 
 
 @raise_on_failure
-def test_get_empty_notifications(client):
-    response = client.get("/notifications")
+@pytest.mark.parametrize("number_of_nodes", [1])
+@pytest.mark.parametrize("channels_per_node", [0])
+@pytest.mark.parametrize("enable_rest_api", [True])
+def test_get_empty_notifications(client, api_server_test_instance):
+    response = client.get("/api/v1/notifications")
     assert response.get_json() == []
