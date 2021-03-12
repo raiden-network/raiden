35bc61bcd0156056d95a4bc45c896af637b02bfd
Fernando Cezar Bernardelli <fernando@brainbot.com>
2021-03-12 16:47:59 +0100

    revert matrix-synapse upgrade

--- raiden/tests/integration/api/rest/test_notifications_endpoint.py
+++ raiden/tests/integration/api/rest/test_notifications_endpoint.py
@@ -60,7 +60,9 @@ def test_get_notifications_empty_queue(client, api_server_test_instance):
 @pytest.mark.parametrize("enable_rest_api", [True])
 def test_notifications_with_same_id_are_overwritten(client, api_server_test_instance):
     for i in range(3):
-        create_notification(api_server_test_instance, id="same_id", summary=f"summary_{i}")
+        create_notification(
+            api_server_test_instance, notification_id="same_id", summary=f"summary_{i}"
+        )
 
     response = client.get(notifications_endpoint)
     response = response.get_json()
@@ -68,9 +70,9 @@ def test_notifications_with_same_id_are_overwritten(client, api_server_test_inst
     assert response[0]["summary"] == "summary_2"
 
 
-def create_notification(api_server, id=None, summary=None, body=None, urgency=None):
+def create_notification(api_server, notification_id=None, summary=None, body=None, urgency=None):
     notification_opts = {
-        "id": id or fake.bs(),
+        "id": notification_id or fake.bs(),
         "summary": summary or fake.text(max_nb_chars=20),
         "body": body or fake.sentence(nb_words=7),
         "urgency": urgency or random.choice(["low", "normal", "critical"]),
