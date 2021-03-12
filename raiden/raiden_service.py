52232f1fd3acebc0b5585ca71deb576a780fd03d
Frederik Lührs <luehrs.fred@gmail.com>
2021-03-12 15:55:42 +0100

    modify raiden event_handler

--- raiden/raiden_service.py
+++ raiden/raiden_service.py
@@ -116,6 +116,7 @@ from raiden.utils.transfers import random_secret
 from raiden.utils.typing import (
     MYPY_ANNOTATION,
     Address,
+    AddressMetadata,
     BlockNumber,
     BlockTimeout,
     InitiatorAddress,
@@ -1362,11 +1363,11 @@ class RaidenService(Runnable):
         all_messages: List[MessagesQueue] = list()
         for queue_identifier, event_queue in events_queues.items():
 
-            queue_messages = list()
+            queue_messages: List[Tuple[Message, Optional[AddressMetadata]]] = list()
             for event in event_queue:
                 message = message_from_sendevent(event)
                 self.sign(message)
-                queue_messages.append(message)
+                queue_messages.append((message, None))
 
             all_messages.append(MessagesQueue(queue_identifier, queue_messages))
 
