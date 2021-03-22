6f50bf01fb8dbaf3606dae362df24e441435562f
Frederik Lührs <luehrs.fred@gmail.com>
2021-03-22 10:32:34 +0100

    Use Metadata in Locked transfer from initiator
    fix tests
    make address metadata optional
    consistent fixme
    
    changes requested
    
    fix lint
    
    fix lint

--- raiden/messages/metadata.py
+++ raiden/messages/metadata.py
@@ -1,20 +1,37 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 import rlp
 from eth_utils import keccak
 
 from raiden.messages.abstract import cached_property
 from raiden.utils.formatting import to_checksum_address
-from raiden.utils.typing import Address, List
+from raiden.utils.typing import Address, AddressMetadata, Any, Dict, List
+
+
+def primitive_dict_to_nested_lists(dic: Dict[Any, Any]) -> List[Any]:
+    serialized_dict = list()
+    for k, v in sorted(dic.items()):
+        if isinstance(v, dict):
+            v = primitive_dict_to_nested_lists(v)
+        serialized_dict.append([k, v])
+    return serialized_dict
 
 
 @dataclass(frozen=True)
 class RouteMetadata:
     route: List[Address]
+    address_metadata: Dict[Address, AddressMetadata] = field(default_factory=dict)
 
     @cached_property
     def hash(self) -> bytes:
-        return keccak(rlp.encode(self.route))
+        return keccak(self._rlp_serialize())
+
+    def _rlp_serialize(self) -> bytes:
+        return rlp.encode(
+            primitive_dict_to_nested_lists(
+                {"route": self.route, "address_metadata": self.address_metadata}
+            )
+        )
 
     def __repr__(self) -> str:
         return f"RouteMetadata: {' -> '.join([to_checksum_address(a) for a in self.route])}"
