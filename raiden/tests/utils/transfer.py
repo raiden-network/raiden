42c2dda462dea677511e32a84dbf33d85c853f76
Frederik Lührs <luehrs.fred@gmail.com>
2021-04-01 13:02:53 +0200

    dont require pfs for direct transfer

--- raiden/tests/utils/transfer.py
+++ raiden/tests/utils/transfer.py
@@ -315,6 +315,7 @@ def _transfer_expired(
         identifier=identifier,
         secret=secret,
         secrethash=secrethash,
+        route_states=[create_route_state_for_route([initiator_app, target_app], token_address)],
     )
 
     with Timeout(seconds=timeout):
@@ -360,6 +361,7 @@ def _transfer_secret_not_requested(
         identifier=identifier,
         secret=secret,
         secrethash=secrethash,
+        route_states=[create_route_state_for_route([initiator_app, target_app], token_address)],
     )
 
     with Timeout(seconds=timeout):
