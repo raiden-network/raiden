c17adf87512abe3dccbd4cb360f349b90e518a6e
Fernando Cezar Bernardelli <fernando@brainbot.com>
2021-05-20 10:26:14 +0200

    fix mypy issues

--- raiden/ui/cli.py
+++ raiden/ui/cli.py
@@ -594,10 +594,9 @@ def _run(ctx: Context, **kwargs: Any) -> None:
         )
 
         if kwargs["config_file"] is not None:
-            source = ctx.get_parameter_source("config_file")  # type: ignore
-            log.debug(
-                "Using config file", config_file=kwargs["config_file"], set_by=source.name.title()
-            )
+            source = ctx.get_parameter_source("config_file")
+            set_by = source.name.title() if source else None
+            log.debug("Using config file", config_file=kwargs["config_file"], set_by=set_by)
 
         enable_gevent_monitoring_signal()
 
