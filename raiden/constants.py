b397820a83b423896d18895e1eb88ae8dfe9ab95
Ivan Stanković <ivan@brainbot.com>
2021-06-01 14:25:41 +0200

    raiden: constants: update API doc URL
    Fixes #7111.

--- raiden/constants.py
+++ raiden/constants.py
@@ -26,7 +26,7 @@ from raiden.utils.typing import (
 
 LATEST = "https://api.github.com/repos/raiden-network/raiden/releases/latest"
 RELEASE_PAGE = "https://github.com/raiden-network/raiden/releases"
-DOC_URL = "https://docs.raiden.network/raiden-api-1"
+DOC_URL = "https://raiden-network.readthedocs.io/en/latest/rest_api.html"
 SECURITY_EXPRESSION = r"\[CRITICAL UPDATE.*?\]"
 
 RAIDEN_DB_VERSION = RaidenDBVersion(27)
