aa2b16204998e69b765ee0ba3a28ee8eba16b3fb
Paul Lange <palango@gmx.de>
2021-12-21 12:29:55 +0100

    Bump version to 3.0.0rc10

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,7 @@ master_doc = "index"
 project = "Raiden Network"
 author = "Raiden Project"
 
-version_string = "3.0.0rc9"
+version_string = "3.0.0rc10"
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
